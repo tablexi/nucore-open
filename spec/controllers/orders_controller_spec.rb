70e875443520b400712b241c36b2d84f0ee54e5a
Chris Stump <chris@tablexi.com>
2011-03-08 17:38:46 -0600

    Merge auth:nucore

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -195,67 +195,10 @@ describe OrdersController do
       @params[:order_detail_id]=@order_detail2.id
       maybe_grant_always_sign_in :staff
       do_request
->>>>>>> nucore
       response.response_code.should == 404
     end
   end
 
-<<<<<<< HEAD
-  context "update order_detail quantities" do
-    before(:each) do
-      @facility         = Factory.create(:facility)
-      @facility_account = @facility.facility_accounts.create(Factory.attributes_for(:facility_account))
-      @price_group      = @facility.price_groups.create(Factory.attributes_for(:price_group))
-      @user1            = Factory.create(:user)
-      @account1         = Factory.create(:nufs_account, :account_users_attributes => [Hash[:user => @user1, :created_by => @user1, :user_role => 'Owner']])
-      @order1           = @user1.orders.create(Factory.attributes_for(:order, :user => @user1, :created_by => @user1.id, :account => @account1))
-
-      @item      = @facility.items.create(Factory.attributes_for(:item, :facility_account_id => @facility_account.id))
-      @item_pp   = Factory.create(:item_price_policy, :item => @item, :price_group => @price_group)
-      @pg_member = Factory.create(:user_price_group_member, :user => @user1, :price_group => @price_group)
-
-      sign_in @admin
-      User.stubs(:find).returns(@user1)
-    end
-
-    it "should update the quantities of order_details" do
-      @user1.cart.add(@item,1)
-      @order_detail = @order1.reload.order_details[0]
-      @order_detail.quantity.should == 1
-      put :update, :id => @order1.id, "quantity#{@order_detail.id}" => "6"
-      @order_detail.reload.quantity.should == 6
-    end
-
-    it "should not allow updates of quantities for instruments" do
-      pending
-    end
-  end
-
-  context "cart meta data" do
-    before(:each) do
-      @facility         = Factory.create(:facility)
-      @facility_account = @facility.facility_accounts.create(Factory.attributes_for(:facility_account))
-      @price_group      = @facility.price_groups.create(Factory.attributes_for(:price_group))
-      @user             = Factory.create(:user)
-      @account          = Factory.create(:nufs_account, :account_users_attributes => [Hash[:user => @user, :created_by => @user, :user_role => 'Owner']])
-      @order            = @user.orders.create(Factory.attributes_for(:order, :user => @user, :created_by => @user.id, :account => @account))
-
-      @instrument       = @facility.instruments.create(Factory.attributes_for(:instrument, :facility_account_id => @facility_account.id))
-      @service          = @facility.services.create(Factory.attributes_for(:service, :facility_account_id => @facility_account.id))
-      @service_pp       = Factory.create(:service_price_policy, :service => @service, :price_group => @price_group)
-      @pg_member        = Factory.create(:user_price_group_member, :user => @user, :price_group => @price_group)
-      @item             = @facility.items.create(Factory.attributes_for(:item, :facility_account_id => @facility_account.id))
-
-      sign_in @admin
-      User.stubs(:find).returns(@user)
-    end
-
-    it "should show links for making a reservation for instruments" do
-      @order.add(@instrument)
-      @order_detail = @order.order_details.first
-      get :show, :id => @order.id
-      response.should have_tag 'a[href=?]', new_order_order_detail_reservation_path(@order, @order_detail)
-=======
 
   context "update order_detail quantities" do
     before(:each) do
@@ -299,8 +242,6 @@ describe OrdersController do
       it_should_allow :staff, "to show links for making a reservation for instruments" do
         response.should have_tag 'a[href=?]', new_order_order_detail_reservation_path(@order, @order_detail)
       end
-
->>>>>>> nucore
     end
 
     it "should show links for uploading files for services where required by service"
@@ -312,50 +253,6 @@ describe OrdersController do
       @service.service_surveys.first.active!
       @order.add(@service)
       @order_detail = @order.order_details.first
-<<<<<<< HEAD
-      get :show, :id => @order.id
-      response.should have_tag 'a[href=?]', "/orders/#{@order.id}/details/#{@order_detail.id}/surveys/#{@survey.access_code}"
-    end
-
-    it "should show links for edit survey metadata if survey has been completed" do
-      # add survey, make it active, create response set, mark it completed and add it to order detail
-      @survey = Survey.create(:title => "Survey 1", :access_code => '1234')
-      @service.surveys.push(@survey)
-      @service.service_surveys.first.active!
-      @response_set = @survey.response_sets.create(:access_code => 'set1')
-      @response_set.complete!
-      @response_set.save
-      @order.add(@service)
-      @order_detail = @order.order_details.first
-      @order_detail.response_set!(@response_set)
-      get :show, :id => @order.id
-    end
-  end
-
-  context "clear" do
-    before(:each) do
-      @facility         = Factory.create(:facility)
-      @facility_account = @facility.facility_accounts.create(Factory.attributes_for(:facility_account))
-      @price_group      = @facility.price_groups.create(Factory.attributes_for(:price_group))
-      @user             = Factory.create(:user)
-      @account          = Factory.create(:nufs_account, :account_users_attributes => [Hash[:user => @user, :created_by => @user, :user_role => 'Owner']])
-      @order            = @user.orders.create(Factory.attributes_for(:order, :user => @user, :created_by => @user.id, :account => @account))
-
-      @item             = @facility.items.create(Factory.attributes_for(:item, :facility_account_id => @facility_account.id))
-      @item_pp          = Factory.create(:item_price_policy, :item => @item, :price_group => @price_group)
-      @pg_member        = Factory.create(:user_price_group_member, :user => @user, :price_group => @price_group)
-
-      sign_in @admin
-      User.stubs(:find).returns(@user)
-
-      @order.add(@item, 10)
-    end
-
-    it "should clear the cart and redirect back to cart" do
-      put :clear, :id => @order.id
-      @order.order_details.size == 0
-      response.should redirect_to order_path(@order)
-=======
       maybe_grant_always_sign_in :staff
       do_request
       response.should have_tag 'a[href=?]', "/orders/#{@order.id}/details/#{@order_detail.id}/surveys/#{@survey.access_code}"
@@ -374,41 +271,10 @@ describe OrdersController do
     it_should_allow :staff, "to clear the cart and redirect back to cart" do
       @order.order_details.size == 0
       assert_redirected_to order_path(@order)
->>>>>>> nucore
     end
 
   end
 
-<<<<<<< HEAD
-  context "checkout" do
-    before(:each) do
-      @facility         = Factory.create(:facility)
-      @facility_account = @facility.facility_accounts.create(Factory.attributes_for(:facility_account))
-      @price_group      = @facility.price_groups.create(Factory.attributes_for(:price_group))
-      @user             = Factory.create(:user)
-      @account          = Factory.create(:nufs_account, :account_users_attributes => [Hash[:user => @user, :created_by => @user, :user_role => 'Owner']])
-      @order            = @user.orders.create(Factory.attributes_for(:order, :user => @user, :created_by => @user.id, :account => @account))
-      @item             = @facility.items.create(Factory.attributes_for(:item, :facility_account_id => @facility_account.id))
-      @item_pp          = Factory.create(:item_price_policy, :item => @item, :price_group => @price_group)
-      @pg_member        = Factory.create(:user_price_group_member, :user => @user, :price_group => @price_group)
-      @order.add(@item, 10)
-
-      sign_in @admin
-      User.stubs(:find).returns(@user)
-    end
-
-    it "should not allow viewing of cart that is purchased" do
-      define_open_account(@item.account, @account.account_number)
-      @order.validate_order!
-      @order.purchase!
-      get :show, :id => @order.id
-      response.should redirect_to "/orders/#{@order.id}/receipt"
-
-      get :choose_account, :id => @order.id
-      response.should redirect_to "/orders/#{@order.id}/receipt"
-
-      put :purchase, :id => @order.id
-=======
 
   context "checkout" do
     before(:each) do
@@ -436,7 +302,6 @@ describe OrdersController do
       @method=:put
       @action=:purchase
       do_request
->>>>>>> nucore
       response.should redirect_to "/orders/#{@order.id}/receipt"
 
       # TODO: add, etc.
