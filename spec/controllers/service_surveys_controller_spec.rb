04c0705f2c11d4489a147ffda9d4a3c5b9168cd3
Chris Stump <chris@tablexi.com>
2011-03-17 11:29:30 -0500

    Task #29585: full auth test coverage

--- spec/controllers/service_surveys_controller_spec.rb
+++ spec/controllers/service_surveys_controller_spec.rb
@@ -12,64 +12,48 @@ describe ServiceSurveysController do
       {:controller => 'service_surveys', :action => 'deactivate', :facility_id => 'alpha', :service_id => '1', :survey_code => 'xyz'}
   end
 
-  before(:all) do
-    # remove objects; seems like running survey parser breaks the testing transaction model
-#    ServiceSurvey.delete_all
-#    Survey.delete_all
-#    Service.delete_all
-#    OrderStatus.delete_all
-#    FacilityAccount.delete_all
-#    Facility.delete_all
-    create_users
-  end
+
+  before(:all) { create_users }
+
 
   before(:each) do
     @authable         = Factory.create(:facility)
     @facility_account = @authable.facility_accounts.create(Factory.attributes_for(:facility_account))
     @order_status     = Factory.create(:order_status)
     @service          = @authable.services.create(Factory.attributes_for(:service, :initial_order_status_id => @order_status.id, :facility_account_id => @facility_account.id))
+    @file1            = "#{RAILS_ROOT}/spec/files/alpha_survey.rb"
+    @survey1          = @service.import_survey(@file1)
+    @file2            = "#{RAILS_ROOT}/spec/files/beta_survey.rb"
+    @survey2          = @service.import_survey(@file2)
+    @request.env['HTTP_REFERER'] = "http://nucore.com/facilities/#{@authable.url_name}/services/#{@service.url_name}"
+    @params={ :facility_id => @authable.url_name, :service_id => @service.url_name, :survey_code => @survey1.access_code }
   end
 
-  context "create" do
-    # uploading surveys has been moved to the file upload controller
 
-    # it "should not allow facility director to create survey" do
-    #   @auth.in_group!('director', ['Facility Director', @facility.pers_affiliate_id])
-    #   @controller.stubs(:current_user).returns(@director)
-    #   post :create, :facility_id => @facility.url_name, :service_id => @service.url_name, :survey => {:upload => nil}
-    #   response.should render_template('403.html.erb')
-    # end
-    # 
-    # it "should allow admin to create survey" do
-    #   @controller.stubs(:current_user).returns(@admin)
-    #   @request.env['HTTP_REFERER'] = "http://nucore.com/facilities/#{@facility.url_name}/services/#{@service.id}"
-    #   @file = File.new("#{Rails.root}/spec/files/alpha_survey.rb")
-    #   post :create, :facility_id => @facility.url_name, :service_id => @service.url_name,
-    #                 :survey => {:upload => @file}
-    #   assigns[:service].should == @service
-    #   # should create survey
-    #   Survey.count.should == 1
-    #   @survey = Survey.first
-    #   @survey.title.should == 'Alpha service survey'
-    #   # should add to service survey collection
-    #   @service.surveys.should == [@survey]
-    # end
+  context 'deactivate' do
+
+    before(:each) do
+      @method=:put
+      @action=:deactivate
+    end
+
+    it_should_allow_managers_only :redirect do
+      should redirect_to @request.env['HTTP_REFERER']
+    end
+
+    it 'should test more than auth'
+
   end
+
 
   context "activate" do
 
     before(:each) do
-      @file1            = "#{RAILS_ROOT}/spec/files/alpha_survey.rb"
-      @survey1          = @service.import_survey(@file1)
-      @file2            = "#{RAILS_ROOT}/spec/files/beta_survey.rb"
-      @survey2          = @service.import_survey(@file2)
+      @method=:put
+      @action=:activate
     end
 
-    it "should allow admin to activate" do
-      sign_in @admin
-      @request.env['HTTP_REFERER'] = "http://nucore.com/facilities/#{@authable.url_name}/services/#{@service.url_name}"
-      # activate first survey
-      put :activate, :facility_id => @authable.url_name, :service_id => @service.url_name, :survey_code => @survey1.access_code
+    it_should_allow_managers_only :redirect do
       assigns[:service].should == @service
       assigns[:survey].should == @survey1
       @service.surveys.should == [@survey1, @survey2]
@@ -77,7 +61,8 @@ describe ServiceSurveysController do
       @service.service_surveys.active.size.should == 1
       @service.active_survey.should == @survey1
       # activate second survey
-      put :activate, :facility_id => @authable.url_name, :service_id => @service.url_name, :survey_code => @survey2.access_code
+      @params[:survey_code]=@survey2.access_code
+      do_request
       assigns[:service].should == @service
       assigns[:survey].should == @survey2
       @service.surveys.should == [@survey1, @survey2]
@@ -85,6 +70,7 @@ describe ServiceSurveysController do
       # should active second survey and disable first
       @service.service_surveys.active.size.should == 1
       @service.reload.active_survey.should == @survey2
+      should redirect_to @request.env['HTTP_REFERER']
     end
 
   end
--- spec/controllers/services_controller_spec.rb
+++ spec/controllers/services_controller_spec.rb
@@ -14,6 +14,7 @@ describe ServicesController do
     @authable         = Factory.create(:facility)
     @facility_account = @authable.facility_accounts.create(Factory.attributes_for(:facility_account))
     @service          = @authable.services.create(Factory.attributes_for(:service, :facility_account_id => @facility_account.id))
+    @params={ :facility_id => @authable.url_name }
   end
 
 
@@ -22,15 +23,118 @@ describe ServicesController do
     before :each do
       @method=:get
       @action=:index
-      @params={ :facility_id => @authable.url_name }
     end
 
-    it_should_deny :guest
-
-    it_should_allow_all facility_operators do
+    it_should_allow_operators_only do
       assigns[:services].should == [@service]
       response.should be_success
       response.should render_template('services/index.html.haml')
+    end
+
+  end
+
+
+  context "show" do
+
+    before :each do
+      @method=:get
+      @action=:show
+      @params.merge!(:id => @service.url_name)
+    end
+
+    it "should allow public access" do
+      do_request
+      assigns[:service].should == @service
+      response.should be_success
+      response.should render_template('services/show.html.haml')
+    end
+  
+    it_should_allow_all facility_users do
+      assigns[:service].should == @service
+      response.should be_success
+      response.should render_template('services/show.html.haml')
+    end
+
+  end
+
+
+  context 'new' do
+
+    before :each do
+      @method=:get
+      @action=:new
+    end
+
+    it_should_allow_operators_only do
+      should assign_to(:service).with_kind_of Service
+      assigns(:service).facility.should == @authable
+    end
+
+  end
+
+
+  context 'edit' do
+
+    before :each do
+      @method=:get
+      @action=:edit
+      @params.merge!(:id => @service.url_name)
+    end
+
+    it_should_allow_operators_only do
+      should render_template 'edit.html.haml'
+    end
+
+  end
+
+
+  context 'create' do
+
+    before :each do
+      @method=:post
+      @action=:create
+      @params.merge!(:service => Factory.attributes_for(:service, :facility_account_id => @facility_account.id))
+    end
+
+    it_should_allow_operators_only :redirect do
+      should assign_to(:service).with_kind_of Service
+      assigns(:service).facility.should == @authable
+      should set_the_flash
+      assert_redirected_to [:manage, @authable, assigns(:service)]
+    end
+
+  end
+
+
+  context 'update' do
+
+    before :each do
+      @method=:put
+      @action=:update
+      @params.merge!(:id => @service.url_name, :service => Factory.attributes_for(:service, :facility_account_id => @facility_account.id))
+    end
+
+    it_should_allow_operators_only :redirect do
+      should assign_to(:service).with_kind_of Service
+      should set_the_flash
+      assert_redirected_to manage_facility_service_url(@authable, assigns(:service))
+    end
+
+  end
+
+
+  context 'destroy' do
+
+    before :each do
+      @method=:delete
+      @action=:destroy
+      @params.merge!(:id => @service.url_name)
+    end
+
+    it_should_allow_operators_only :redirect do
+      assigns(:service).should == @service
+      should_be_destroyed @service
+      assert_redirected_to new_facility_service_url
     end
 
   end
@@ -44,35 +148,9 @@ describe ServicesController do
       @params={ :id => @service.url_name, :facility_id => @authable.url_name }
     end
 
-    it_should_deny :guest
-
-    it_should_allow_all facility_operators do
+    it_should_allow_operators_only do
       response.should be_success
       response.should render_template('services/manage.html.haml')
-    end
-
-  end
-
-
-  context "show" do
-
-    before :each do
-      @method=:get
-      @action=:show
-      @params={ :id => @service.url_name, :facility_id => @authable.url_name }
-    end
-
-    it "should allow public access" do
-      do_request
-      assigns[:service].should == @service
-      response.should be_success
-      response.should render_template('services/show.html.haml')
-    end
-  
-    it_should_allow_all ([ :guest ]+ facility_operators) do
-      assigns[:service].should == @service
-      response.should be_success
-      response.should render_template('services/show.html.haml')
     end
 
   end
