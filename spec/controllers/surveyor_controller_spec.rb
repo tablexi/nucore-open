555caccdd6a3d30e4f8e1cc2b7233f1318bace97
Chris Stump <chris@tablexi.com>
2011-03-17 14:18:58 -0500

    Revert "Task #29585: complete auth test coverage"
    This reverts commit 863e79037b42fa5b5286cd19af08e225e1d5b1cf.

--- spec/controllers/surveyor_controller_spec.rb
+++ spec/controllers/surveyor_controller_spec.rb
@@ -20,117 +20,6 @@ describe SurveyorController do
     @service           = @authable.services.create(Factory.attributes_for(:service, :initial_order_status_id => @order_status.id, :facility_account_id => @facility_account.id))
     @file1            = "#{RAILS_ROOT}/spec/files/alpha_survey.rb"
     @survey1          = @service.import_survey(@file1)
-    @params={ :facility_id => @authable.url_name }
-  end
-
-
-  context 'new' do
-
-    before :each do
-      @method=:get
-      @action=:new
-    end
-
-    it_should_require_login
-
-  end
-
-
-  context 'needs order detail' do
-
-    before :each do
-      @price_group       = @authable.price_groups.create(Factory.attributes_for(:price_group))
-      # find guest user
-      @account           = Factory.create(:nufs_account, :account_users_attributes => [Hash[:user => @guest, :created_by => @guest, :user_role => 'Owner']])
-      # create order + order detail
-      @order             = @guest.orders.create(Factory.attributes_for(:order, :created_by => @guest.id, :account => @account, :ordered_at => Time.zone.today))
-      @order.add(@service, 1)
-      @order_detail      = @order.order_details.first
-      @params={ :order_id => @order.id, :od_id => @order_detail.id, :survey_code => @survey1.access_code }
-    end
-
-
-    context "create" do
-
-      before(:each) do
-        @method=:get
-        @action=:create
-      end
-
-      it_should_allow :guest do
-        assigns[:order].should == @order
-        assigns[:detail].should == @order_detail
-        assigns[:survey].should == @survey1
-        @response_set = assigns[:response_set]
-        response.should redirect_to "/orders/#{@order.id}/details/#{@order_detail.id}/surveys/#{@survey1.access_code}/#{@response_set.access_code}/edit"
-      end
-
-    end
-
-
-    context 'needs response set' do
-
-      before :each do
-        @response_set=Factory.create(:response_set, :survey => @survey1, :user => @guest)
-        @params.merge!(:response_set_code => @response_set.access_code)
-      end
-
-
-      context 'show' do
-
-        before :each do
-          @method=:get
-          @action=:show
-        end
-
-        it_should_require_login
-
-        it 'should test more than auth'
-
-      end
-
-
-      context 'show_admin' do
-
-        before :each do
-          @method=:get
-          @action=:show_admin
-          @params.merge!(:facility_id => @authable.url_name, :order_detail_id => @params[:od_id])
-          @params.delete :od_id
-        end
-
-        it_should_allow_operators_only
-
-      end
-
-
-      context 'edit' do
-
-        before :each do
-          @method=:get
-          @action=:edit
-        end
-
-        it_should_require_login
-
-        it 'should test more than auth'
-
-      end
-
-
-      context 'update' do
-
-        before :each do
-          @method=:put
-          @action=:update
-        end
-
-        it_should_require_login
-
-      end
-
-    end
-
   end
 
 
@@ -139,7 +28,7 @@ describe SurveyorController do
     before(:each) do
       @method=:get
       @action=:preview
-      @params.merge!(:service_id => @service.url_name, :survey_code => @survey1.access_code)
+      @params={ :facility_id => @authable.url_name, :service_id => @service.url_name, :survey_code => @survey1.access_code }
     end
 
     it_should_allow_operators_only do
@@ -150,11 +39,39 @@ describe SurveyorController do
       service_survey.preview_code.should == assigns[:response_set].access_code
       response_set_count = ResponseSet.count
       # subsequent requests should return the same response set object
-      do_request
+      get :preview, :facility_id => @authable.url_name, :service_id => @service.url_name, :survey_code => @survey1.access_code
       assigns[:service].should == @service
       assigns[:survey].should == @survey1
       service_survey.preview_code.should == assigns[:response_set].access_code
       response_set_count.should == ResponseSet.count
+    end
+
+  end
+
+  context "create" do
+
+    before(:each) do
+      @price_group       = @authable.price_groups.create(Factory.attributes_for(:price_group))
+      # find guest user
+      @user              = @guest
+      @account           = Factory.create(:nufs_account, :account_users_attributes => [Hash[:user => @user, :created_by => @user, :user_role => 'Owner']])
+      # create order + order detail
+      @order             = @user.orders.create(Factory.attributes_for(:order, :created_by => @user.id, :account => @account))
+      @order.add(@service, 1)
+      @order_detail      = @order.order_details.first
+
+      @method=:get
+      @action=:create
+      @params={ :order_id => @order.id, :od_id => @order_detail.id, :survey_code => @survey1.access_code }
+    end
+
+
+    it_should_allow :guest do
+      assigns[:order].should == @order
+      assigns[:detail].should == @order_detail
+      assigns[:survey].should == @survey1
+      @response_set = assigns[:response_set]
+      response.should redirect_to "/orders/#{@order.id}/details/#{@order_detail.id}/surveys/#{@survey1.access_code}/#{@response_set.access_code}/edit"
     end
 
   end
