b82c14048cc8321450f97fb81149d405f9a8380c
Chris Stump <chris@tablexi.com>
2011-04-14 17:11:04 -0500

    Task #30441:  add DEFAULT_RESERVATION_WINDOW

--- app/models/price_group_product.rb
+++ app/models/price_group_product.rb
@@ -1,4 +1,6 @@
 class PriceGroupProduct < ActiveRecord::Base
+  DEFAULT_RESERVATION_WINDOW=14
+
   belongs_to :price_group
   belongs_to :product
   validates_presence_of :price_group_id, :product_id
