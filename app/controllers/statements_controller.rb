39c27399c6030a910fb01741e32f39aebcc0f630
Chris Stump <chris@tablexi.com>
2011-04-20 15:33:46 -0500

    Task #30441: don't show balances or payments on transaction list; color code amounts as estimated, final, or unassigned

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -24,24 +24,15 @@ class StatementsController < ApplicationController
 
     if params[:id].to_s.downcase == 'recent'
       @account_txns   = @account.account_transactions.facility_recent(@facility)
-      @prev_statement = @statement
-      @new_payments   = @account.facility_recent_payment_balance(@facility) # includes credits
-      @new_purchases  = @account.facility_recent_purchase_balance(@facility)
     else
-      @prev_statement = @account.statements.find(:first, :conditions => ['statements.created_at < ?', @statement.created_at], :order => 'statements.created_at DESC')
-      @new_payments   = @account.statement_payment_balance(@statement) # includes credits
-      @new_purchases  = @account.statement_purchase_balance(@statement)
       @account_txns   = @statement.account_transactions.finalized
     end
-    @balance_prev = @prev_statement ? @prev_statement.account_balance_due(@account) : 0
-    @balance_due  = @balance_prev + @new_purchases  + @new_payments # payments are negative in the DB already
 
     prawnto :prawn => {
                   :left_margin   => 50,
                   :right_margin  => 50,
                   :top_margin    => 50,
                   :bottom_margin => 75 }
-
   end
 
 
