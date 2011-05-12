3fd5fe346fc5c05471881c5b869d3db7f07b4466
Chris Stump <chris@tablexi.com>
2011-05-12 12:00:19 -0500

    Task #30441: require reservation_window if product is an Instrument

--- app/models/price_group_product.rb
+++ app/models/price_group_product.rb
@@ -4,4 +4,5 @@ class PriceGroupProduct < ActiveRecord::Base
   belongs_to :price_group
   belongs_to :product
   validates_presence_of :price_group_id, :product_id
+  validates_presence_of :reservation_window, :if => Proc.new {|pgp| pgp.product.is_a? Instrument }
 end
--- spec/models/price_group_product_spec.rb
+++ spec/models/price_group_product_spec.rb
@@ -5,7 +5,7 @@ describe PriceGroupProduct do
   before :each do
     @facility=Factory.create(:facility)
     @facility_account=@facility.facility_accounts.create(Factory.attributes_for(:facility_account))
-    @instrument=@facility.items.create(Factory.attributes_for(:instrument, :facility_account_id => @facility_account.id))
+    @instrument=@facility.instruments.create(Factory.attributes_for(:instrument, :facility_account_id => @facility_account.id))
     @price_group=Factory.create(:price_group, :facility => @facility)
   end
 
@@ -16,6 +16,15 @@ describe PriceGroupProduct do
 
   it 'should require price group' do
     PriceGroupProduct.new(:product => @instrument).should validate_presence_of :price_group_id
+  end
+
+  it 'should require reservation window' do
+    PriceGroupProduct.new(:product => @instrument, :price_group => @price_group).should validate_presence_of :reservation_window
+  end
+
+  it 'should not require reservation window' do
+    item=@facility.items.create(Factory.attributes_for(:item, :facility_account_id => @facility_account.id))
+    PriceGroupProduct.new(:product => item, :price_group => @price_group).should_not validate_presence_of :reservation_window
   end
 
 end
