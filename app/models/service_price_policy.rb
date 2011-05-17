159343a313a51f3c7711f73c72c0e7540f62dcfa
Chris Stump <chris@tablexi.com>
2011-05-17 15:59:48 -0500

    Task #30441: de-Oracle-ize the SQL; fix methods

--- app/models/service_price_policy.rb
+++ app/models/service_price_policy.rb
@@ -4,25 +4,32 @@ class ServicePricePolicy < PricePolicy
   validates_numericality_of :unit_cost, :unless => :restrict_purchase
   validate :subsidy_less_than_rate?, :unless => lambda { |pp| pp.unit_cost.nil? || pp.unit_subsidy.nil? }
 
-  named_scope :current,  lambda { |service|             { :conditions => ['TRUNC(start_date) = ? AND service_id = ?', self.current_date(service), service.id] } }
-  named_scope :next,     lambda { |service|             { :conditions => ['TRUNC(start_date) = ? AND service_id = ?', self.next_date(service), service.id] } }
-  named_scope :for_date, lambda { |service, start_date| { :conditions => ['TRUNC(start_date) = ? AND service_id = ?', start_date.to_date, service.id] } }
+  named_scope :current,  lambda { |service|             { :conditions => ['start_date = ? AND service_id = ?', self.current_date(service, true), service.id] } }
+  named_scope :next,     lambda { |service|             { :conditions => ['start_date = ? AND service_id = ?', self.next_date(service, true), service.id] } }
+  named_scope :for_date, lambda { |service, start_date| { :conditions => ['start_date = ? AND service_id = ?', start_date, service.id] } }
 
   before_save { |o| o.unit_subsidy = 0 if o.unit_subsidy.nil? && !o.unit_cost.nil? }
 
-  def self.current_date(service)
-    ipp = service.service_price_policies.find(:first, :conditions => ['TRUNC(start_date) <= ? AND TRUNC(expire_date) > ?', Time.zone.now, Time.zone.now], :order => 'start_date DESC')
-    ipp ? ipp.start_date.to_date : nil
+  def self.current_date(item, with_time=false)
+    ipp = item.service_price_policies.find(:first, :conditions => ['start_date <= ? AND expire_date > ?', Time.zone.now, Time.zone.now], :order => 'start_date DESC')
+    ipp.nil? ? nil : with_time ? ipp.start_date.to_datetime : ipp.start_date.to_date
   end
 
-  def self.next_date(service)
-    ipp = service.service_price_policies.find(:first, :conditions => ['TRUNC(start_date) > ?', Time.zone.now], :order => 'start_date')
-    ipp ? ipp.start_date.to_date : nil
+  def self.next_date(item, with_time=false)
+    ipp = nil
+    item.service_price_policies.sort{|p1,p2| p1.start_date <=> p2.start_date}.each{|pp| ipp=pp and break if pp.start_date > Time.zone.now}
+    ipp.nil? ? nil : with_time ? ipp.start_date.to_datetime : ipp.start_date.to_date
   end
 
-  def self.next_dates(service)
-    ipps = service.service_price_policies.find(:all, :conditions => ['TRUNC(start_date) > ?', Time.zone.now], :order => 'start_date', :select => 'DISTINCT(start_date) AS start_date')
-    start_dates = ipps.collect {|ipp| ipp.start_date.to_date}
+  def self.next_dates(item)
+    ipps = []
+
+    item.service_price_policies.each do |pp|
+      sdate=pp.start_date
+      ipps << sdate.to_date if sdate > Time.zone.now && !ipps.include?(sdate)
+    end
+
+    ipps
   end
 
   def subsidy_less_than_rate?
