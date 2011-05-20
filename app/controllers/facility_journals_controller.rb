30777d780d23201b2f2c9125775432b04d37d892
John Gore <john@tablexi.com>
2011-05-19 22:24:52 -0500

    Fixed syntax error in notice message.

--- app/controllers/facility_journals_controller.rb
+++ app/controllers/facility_journals_controller.rb
@@ -138,7 +138,7 @@ class FacilityJournalsController < ApplicationController
       end
       od.change_status!(rec_status)
     end
-    flash[:notice] = The select orders have been reconciled successfully
+    flash[:notice] = 'The select orders have been reconciled successfully'
     redirect_to facility_journal_url(current_facility, @journal) and return
   end
 end
