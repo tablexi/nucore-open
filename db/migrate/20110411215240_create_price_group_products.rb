7e95b86151163da23afffebfcfdb423aa31b1270
Chris Stump <chris@tablexi.com>
2011-05-20 13:45:37 -0500

    Task #31913: Update price policy group migration

--- db/migrate/20110411215240_create_price_group_products.rb
+++ db/migrate/20110411215240_create_price_group_products.rb
@@ -10,6 +10,29 @@ class CreatePriceGroupProducts < ActiveRecord::Migration
 
     add_index :price_group_products, :price_group_id
     add_index :price_group_products, :product_id
+
+    PriceGroupProduct.reset_column_information
+
+    price_policies=PricePolicy.find(:all, :conditions => [ 'start_date <= ? AND restrict_purchase != 1', Time.zone.now ])
+
+    price_policies.each do |pp|
+      pgp=PriceGroupProduct.new(:price_group => pp.price_group)
+
+      case pp
+        when InstrumentPricePolicy
+          pgp.product=pp.instrument
+          pgp.reservation_window=pp.reservation_window
+        when ServicePricePolicy
+          pgp.product=pp.service
+        when ItemPricePolicy
+          pgp.product=pp.item
+      end
+
+      pgp.save!
+    end
+
+    price_policies=PricePolicy.find(:all, :conditions => 'restrict_purchase = 1')
+    price_policies.each{|pp| pp.destroy }
   end
 
   
