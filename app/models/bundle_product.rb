81b924eb31c1b79d3eb205a98a81e50d2cf823fb
Chris Stump <chris@tablexi.com>
2011-05-25 12:44:39 -0500

    Task #32237 : bundle product listing fails on sort!

--- app/models/bundle_product.rb
+++ app/models/bundle_product.rb
@@ -10,4 +10,8 @@ class BundleProduct < ActiveRecord::Base
   def instrument_quantity
     errors.add("quantity", " must be 1 for instruments") if (product && product.is_a?(Instrument) && quantity.to_i != 1)
   end
+
+  def <=>(other)
+    self.product.name <=> other.product.name
+  end
 end
