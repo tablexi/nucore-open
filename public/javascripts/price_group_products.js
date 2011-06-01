cb7d6f6cd0d523b551d011f278c89e2c41899e4b
Chris Stump <chris@tablexi.com>
2011-06-01 18:23:59 -0500

    Task #32429 : Purchase restrictions enable/disable javascript issue

--- public/javascripts/price_group_products.js
+++ public/javascripts/price_group_products.js
@@ -1,15 +1,15 @@
 $(function() {
-    $('select').change(function() {
+    $('input:checkbox').change(function() {
         var name=$(this).attr('name');
         name=name.slice(0, name.indexOf('['));
         var res_win=$('#' + name + '_reservation_window');
 
         if(res_win.length > 0) {
-            if($(this).val().match(/no/i) == null) {
+            if($(this).is(':checked')) {
                 res_win.css('color', 'black');
                 res_win.removeAttr('disabled');
             }
-            else {
+            else {                
                 res_win.css('color', 'white');
                 res_win.attr('disabled', 'disabled');
             }
