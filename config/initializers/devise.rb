c423baba895a014d183415eb26809250bf0ca325
Chris Stump <chris@tablexi.com>
2011-06-20 17:55:30 -0500

    Task #33003 : Upgrade code base to Rails 3; specify orm

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,5 @@
+require 'devise/orm/active_record'
+
 # Use this hook to configure devise mailer, warden hooks and so forth. The first
 # four configuration values can also be set straight in your models.
 Devise.setup do |config|
