20f5494a7f2934e7f06df21aadbe3ab153ca6415
Ross Heflin <ross@tablexi.com>
2011-06-23 15:44:22 -0500

    with_details param to json call/views  and tooltip logic to js

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -26,8 +26,9 @@ class ReservationsController < ApplicationController
     end
 
     respond_to do |format|
-      format.js { render :json => @reservations.map{|r| r.as_calendar_object(:start_date => @start_date)}.flatten + 
-                                  @unavailable.map{|r| r.as_calendar_object(:start_date => @start_date)}.flatten }
+      as_calendar_object_options = {:start_date => @start_date, :with_details => params[:with_details]}
+      format.js { render :json => @reservations.map{|r| r.as_calendar_object(as_calendar_object_options)}.flatten + 
+                                  @unavailable.map{ |r| r.as_calendar_object(as_calendar_object_options)}.flatten }
     end
   end
 
