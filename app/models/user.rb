0b696bec538f2d3ff400cd71040411238a2a6a4f
Chris Stump <chris@tablexi.com>
2011-06-30 16:04:36 -0500

    make cancer center query in #price_groups NU specific

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,13 +62,6 @@ class User < ActiveRecord::Base
     groups = price_group_members.collect{ |pgm| pgm.price_group }
     # check internal/external membership
     groups << (self.username.match(/@/) ? PriceGroup.external.first : PriceGroup.northwestern.first)
-    # check cancer center membership
-    begin
-      # TODO: translate view t_v_cancer_center_members from bcsec to local
-      result = User.find_by_sql(["SELECT * from t_v_cancer_center_members where username = ?", self.username])
-      groups << PriceGroup.cancer_center.first if result.length > 0
-    rescue
-    end
     groups.flatten.uniq
   end
   
