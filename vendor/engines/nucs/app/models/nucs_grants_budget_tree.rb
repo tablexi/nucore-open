363bb9aa373385f51e68c6748636f09ca6596e50
Chris Stump <chris@tablexi.com>
2011-07-01 15:19:01 -0500

    Task #33003 : Upgrade code base to Rails 3; use :within not :maximum in validations

--- vendor/engines/nucs/app/models/nucs_grants_budget_tree.rb
+++ vendor/engines/nucs/app/models/nucs_grants_budget_tree.rb
@@ -2,12 +2,12 @@ class NucsGrantsBudgetTree < ActiveRecord::Base
   include NucsSourcedFromFile
 
   validates_format_of(:account, :with => /^\d{5,10}$/)
-  validates_length_of(:account_desc, :maximum => 30)
+  validates_length_of(:account_desc, :within => 1..30)
   validates_format_of(:roll_up_node, :with => /^\d{5,20}$/)
-  validates_length_of(:roll_up_node_desc, :maximum => 30)
+  validates_length_of(:roll_up_node_desc, :within => 1..30)
   validates_format_of(:parent_node, :with => /^\d{5,20}$/)
-  validates_length_of(:parent_node_desc, :maximum => 30)
-  validates_length_of(:tree, :maximum => 18)
+  validates_length_of(:parent_node_desc, :within => 1..30)
+  validates_length_of(:tree, :within => 1..18)
   validates_presence_of(:account_effective_at)
   validates_presence_of(:tree_effective_at)
 
