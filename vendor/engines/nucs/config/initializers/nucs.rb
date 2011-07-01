83c6fe7afd85e5c53dab67cd9e2a86bfbcab2d1b
Chris Stump <chris@tablexi.com>
2011-07-01 15:35:50 -0500

    Task #33003 : Upgrade code base to Rails 3; ignore db and gem configs; link to host app's

--- vendor/engines/nucs/config/initializers/nucs.rb
+++ vendor/engines/nucs/config/initializers/nucs.rb
@@ -3,4 +3,6 @@ unless Rails.env.production?
   require 'fileutils'
   base=File.dirname(__FILE__)
   FileUtils.ln_sf(File.join(base, '..', '..', '..', '..', '..', 'config', 'database.yml'), File.join(base, '..', 'database.yml'))
+  FileUtils.ln_sf(File.join(base, '..', '..', '..', '..', '..', 'Gemfile'), File.join(base, '..', '..', 'Gemfile'))
+  FileUtils.ln_sf(File.join(base, '..', '..', '..', '..', '..', 'Gemfile.lock'), File.join(base, '..', '..', 'Gemfile.lock'))
 end
