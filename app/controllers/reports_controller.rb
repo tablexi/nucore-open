05865ddeaf3a59a7ef9c5fb9018e4ba4b77cb314
Chris Stump <chris@tablexi.com>
2011-07-13 17:52:41 -0500

    Task #34423 : Reporting; extract #handle_downloadable_report_request for code reuse

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -16,36 +16,34 @@ class ReportsController < ApplicationController
   end
 
   def instrument_utilization
-    if request.post?
-      @date_start   = parse_usa_date(params[:date_start])
-      @date_end     = parse_usa_date(params[:date_end])
-      @reservations = Reservation.where(%q/reserve_start_at >= ? AND reserve_start_at <= ? AND canceled_at IS NULL AND (order_details.state IS NULL OR order_details.state = 'complete')/, @date_start, @date_end)
-                                 .joins('LEFT JOIN order_details ON reservations.order_detail_id = order_details.id')
-                                 .includes(:order, :order_detail, :account, :instrument)
-                                 .order('reserve_start_at ASC')
-                                 .all
-      respond_to do |format|
-        format.csv { render_csv("instrument_utilization_#{@date_start.strftime("%Y%m%d")}-#{@date_end.strftime("%Y%m%d")}") }
-      end
-    else
-      now         = Date.today
-      @date_start = Date.new(now.year, now.month, 1) - 1.month
-      @date_end   = @date_start + 42.days
-      @date_end   = Date.new(@date_end.year, @date_end.month) - 1.day
+    handle_downloadable_report_request 'instrument_utilization' do
+      Reservation.where(%q/reserve_start_at >= ? AND reserve_start_at <= ? AND canceled_at IS NULL AND (order_details.state IS NULL OR order_details.state = 'complete')/, @date_start, @date_end)
+                 .joins('LEFT JOIN order_details ON reservations.order_detail_id = order_details.id')
+                 .includes(:order, :order_detail, :account, :instrument)
+                 .order('reserve_start_at ASC')
     end
   end
 
   def product_order_summary
+    handle_downloadable_report_request 'product_order_summary' do
+      OrderDetail.where(%q/order_details.state = 'complete' AND orders.ordered_at >= ? AND orders.ordered_at <= ?/, @date_start, @date_end)
+                 .joins('LEFT JOIN orders ON order_details.order_id = orders.id')
+                 .includes(:order, :account, :price_policy, :product)
+                 .order('orders.ordered_at ASC')
+    end
+  end
+
+
+  private
+
+  def handle_downloadable_report_request(report_prefix)
     if request.post?
       @date_start    = parse_usa_date(params[:date_start])
       @date_end      = parse_usa_date(params[:date_end])
-      @order_details = OrderDetail.where(%q/order_details.state = 'complete' AND orders.ordered_at >= ? AND orders.ordered_at <= ?/, @date_start, @date_end)
-                                  .joins('LEFT JOIN orders ON order_details.order_id = orders.id')
-                                  .includes(:order, :account, :price_policy, :product)
-                                  .order('orders.ordered_at ASC')
-                                  .all
+      @reportables = yield.all
+
       respond_to do |format|
-        format.csv { render_csv("product_order_summary_#{@date_start.strftime("%Y%m%d")}-#{@date_end.strftime("%Y%m%d")}") }
+        format.csv { render_csv("#{report_prefix}_#{@date_start.strftime("%Y%m%d")}-#{@date_end.strftime("%Y%m%d")}") }
       end
     else
       now         = Date.today
@@ -55,7 +53,6 @@ class ReportsController < ApplicationController
     end
   end
 
-  private
 
   def render_csv(filename = nil)
     filename ||= params[:action]
