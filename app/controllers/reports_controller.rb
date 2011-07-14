8f3d44cddc9534b975f58c1e4f74e548f4f41512
Chris Stump <chris@tablexi.com>
2011-07-13 19:24:14 -0500

    Task #34423 : Reporting; setup default reports page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,20 +3,23 @@ class ReportsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :check_acting_as
   before_filter :init_current_facility
+  before_filter :setup_dates
 
   load_and_authorize_resource :class => ReportsController
+
 
   def initialize
     @active_tab = 'admin_reports'
     super
   end
 
+
   def index
-    flash.now[:notice] = "This page is still in development"
   end
 
+
   def instrument_utilization
-    handle_downloadable_report_request 'instrument_utilization' do
+    handle_report_download 'instrument_utilization' do
       Reservation.where(%q/reserve_start_at >= ? AND reserve_start_at <= ? AND canceled_at IS NULL AND (order_details.state IS NULL OR order_details.state = 'complete')/, @date_start, @date_end)
                  .joins('LEFT JOIN order_details ON reservations.order_detail_id = order_details.id')
                  .includes(:order, :order_detail, :account, :instrument)
@@ -24,8 +27,9 @@ class ReportsController < ApplicationController
     end
   end
 
+
   def product_order_summary
-    handle_downloadable_report_request 'product_order_summary' do
+    handle_report_download 'product_order_summary' do
       OrderDetail.where(%q/order_details.state = 'complete' AND orders.ordered_at >= ? AND orders.ordered_at <= ?/, @date_start, @date_end)
                  .joins('LEFT JOIN orders ON order_details.order_id = orders.id')
                  .includes(:order, :account, :price_policy, :product)
@@ -36,20 +40,26 @@ class ReportsController < ApplicationController
 
   private
 
-  def handle_downloadable_report_request(report_prefix)
+
+  def setup_dates
     if request.post?
       @date_start    = parse_usa_date(params[:date_start])
       @date_end      = parse_usa_date(params[:date_end])
-      @reportables = yield.all
-
-      respond_to do |format|
-        format.csv { render_csv("#{report_prefix}_#{@date_start.strftime("%Y%m%d")}-#{@date_end.strftime("%Y%m%d")}") }
-      end
     else
       now         = Date.today
       @date_start = Date.new(now.year, now.month, 1) - 1.month
       @date_end   = @date_start + 42.days
       @date_end   = Date.new(@date_end.year, @date_end.month) - 1.day
+    end
+  end
+
+
+  def handle_report_download(report_prefix)
+    return unless request.post?
+    @reportables = yield.all
+
+    respond_to do |format|
+      format.csv { render_csv("#{report_prefix}_#{@date_start.strftime("%Y%m%d")}-#{@date_end.strftime("%Y%m%d")}") }
     end
   end
 
