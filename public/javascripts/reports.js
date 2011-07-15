4f7e5f32cbd3da97cac8bb07bb5065e151375f8d
Chris Stump <chris@tablexi.com>
2011-07-15 17:12:35 -0500

    Task #34423 : Reporting; move inline JS to JS function in reports.js; fix & ajax-ize report refresh; ensure OrderDetails query is only made when report render GET

--- public/javascripts/reports.js
+++ public/javascripts/reports.js
@@ -1,3 +1,31 @@
+function getParamsString()
+{
+    return '?date_start=' + $('#date_start').val() + '&date_end=' + $('#date_end').val() + '&status_filter=' + $('#status_filter').val();
+}
+
+function createGeneralReportsTable(selectedIndex)
+{
+    $('#tabs').tabs({
+        selected: selectedIndex,
+        ajaxOptions: {
+          error: function(xhr, status, error) {
+            $('#error-msg').html('Sorry, but the tab could not load. Please try again soon.').show();
+          },
+
+          beforeSend: function(xhr) {
+            xhr.open(this.type, this.url + getParamsString(), this.async);
+          }
+        }
+    });
+
+    // capture a click on the refresh button and defer work to jQuery UI's tabs
+    $('#refresh-form').bind('ajax:beforeSend', function(event, xhr) {
+       var selected=$('#tabs').tabs('option', 'selected');
+       $('#tabs').tabs('load', selected);
+       return false;
+    })
+}
+
 $(document).ready(function() {
     $('.datepicker').each(function() {
       $(this).datepicker();
