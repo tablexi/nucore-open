2137f6ba6d1b2ce0bf02488b19149e93f22e4d84
Chris Stump <chris@tablexi.com>
2011-07-29 17:43:12 -0500

    Task #34815 : RoR3 Error Adding Product To Cart

--- db/migrate/20110729173029_change_vestal_versions.rb
+++ db/migrate/20110729173029_change_vestal_versions.rb
@@ -5,14 +5,14 @@ class ChangeVestalVersions < ActiveRecord::Migration
     add_column :versions, :commit_label, :string
 
     rename_column :versions, :data_changes, :modifications
-    execute %Q[ ALTER TABLE versions RENAME COLUMN "NUMBER" TO "version_number" ]
+    execute %Q[ ALTER TABLE versions RENAME COLUMN "NUMBER" TO "VERSION_NUMBER" ]
 
     add_index :versions, :commit_label
   end
 
   def self.down
     remove_index :versions, :commit_label
-    execute %Q[ ALTER TABLE versions RENAME COLUMN "version_number" TO "NUMBER" ]
+    execute %Q[ ALTER TABLE versions RENAME COLUMN "VERSION_NUMBER" TO "NUMBER" ]
     rename_column :versions, :modifications, :data_changes
     remove_columns :versions, :reason_for_update, :reverted_from, :commit_label
   end
