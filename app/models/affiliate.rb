4e695ca118662d82ee4fb78425f48fa487cfc287
Chris Stump <chris@tablexi.com>
2011-08-11 18:12:11 -0500

    Task #35319 : Fwd: Account owner affiliation; protect OTHER from destroy

--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -2,4 +2,10 @@ class Affiliate < ActiveRecord::Base
   validates_uniqueness_of :name
 
   OTHER=where(:name => 'Other').first
+
+  before_destroy :destroyable?
+
+  def destroyable?
+    self != OTHER
+  end
 end
--- spec/models/affiliate_spec.rb
+++ spec/models/affiliate_spec.rb
@@ -8,4 +8,15 @@ describe Affiliate do
     Affiliate::OTHER.should == Affiliate.where(:name => 'Other').first
   end
 
+  it 'should not allow OTHER to be destroyed' do
+    Affiliate::OTHER.destroy
+    Affiliate::OTHER.should_not be_destroyed
+  end
+
+  it 'should allow non-OTHER affiliates to be destroyed' do
+    affiliate=Affiliate.create!(:name => 'aff1')
+    affiliate.destroy
+    affiliate.should be_destroyed
+  end
+
 end
