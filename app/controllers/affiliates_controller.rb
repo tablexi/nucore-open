7d11dd2bee4aecc84b64a5c2cb5a58e2e8429c0a
Chris Stump <chris@tablexi.com>
2011-08-12 17:17:06 -0500

    Task #35319 : Fwd: Account owner affiliation; Implement CRUD and test it

--- app/controllers/affiliates_controller.rb
+++ app/controllers/affiliates_controller.rb
@@ -1,28 +1,79 @@
 class AffiliatesController < GlobalSettingsController
 
   def new
+    @affiliate=Affiliate.new
   end
 
 
   def create
-    Affiliate.create!(params[:affiliate])
+    attrs=params[:affiliate]
+
+    if attrs.blank?
+      flash[:error]='Affiliate attributes not found!'
+      return redirect_to new_affiliate_path
+    end
+
+    @affiliate=Affiliate.create(attrs)
+    return render :action => :new unless @affiliate.errors.empty?
+
+    flash[:notice]="Affiliate #{@affiliate.name} created"
     redirect_to affiliates_path
   end
 
 
   def edit
-    @affiliate=Affiliate.find(params[:id].to_i)
+    id=params[:id]
+
+    begin
+      @affiliate=Affiliate.find(id.to_i)
+    rescue ActiveRecord::RecordNotFound
+      flash[:error]="Affiliate with id #{id} not found!"
+      return redirect_to affiliates_path
+    end
   end
 
 
   def update
-    Affiliate.find(params[:id].to_i).update_attributes(params[:affiliate])
+    id=params[:id]
+
+    begin
+      @affiliate=Affiliate.find(id.to_i)
+    rescue ActiveRecord::RecordNotFound
+      flash[:error]="Affiliate with id #{id} not found!"
+    else
+      attrs=params[:affiliate]
+
+      if attrs.blank?
+        flash.now[:error]='Affiliate attributes not found!'
+        return render :action => :edit
+      end
+
+      return render :action => :edit unless @affiliate.update_attributes(attrs)
+
+      flash[:notice]="Affiliate #{@affiliate.name} updated"
+    end
+
     redirect_to affiliates_path
   end
 
 
   def destroy
-    Affiliate.destroy(params[:id].to_i)
+    id=params[:id]
+
+    begin
+      affiliate=Affiliate.find(id.to_i)
+    rescue ActiveRecord::RecordNotFound
+      flash[:error]="Affiliate with id #{id} not found!"
+    else
+      affiliate.destroy
+
+      if affiliate.destroyed?
+        flash[:notice]="Affiliate #{affiliate.name} removed"
+      else
+        flash[:error]="Affiliate #{affiliate.name} could not be removed"
+      end
+    end
+
     redirect_to affiliates_path
   end
 
