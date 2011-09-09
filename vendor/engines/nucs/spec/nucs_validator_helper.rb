5ac9f5fa165cc814e99af62d640113720bea0071
Chris Stump <chris@tablexi.com>
2011-09-09 14:07:47 -0500

    update to handle fiscal year overlaps

--- vendor/engines/nucs/spec/nucs_validator_helper.rb
+++ vendor/engines/nucs/spec/nucs_validator_helper.rb
@@ -15,7 +15,7 @@ module NucsValidatorHelper
     if attrs.has_key?(:expires_at) or attrs.has_key?(:starts_at)
       Factory.create(:nucs_gl066_with_dates, attrs)
     else
-      attrs.merge!(:budget_period => Time.zone.today.year) unless attrs.has_key?(:budget_period)
+      attrs.merge!(:budget_period => (Time.zone.today+1.year).year) unless attrs.has_key?(:budget_period)
       gl=Factory.create(:nucs_gl066_without_dates, attrs)
     end
 
