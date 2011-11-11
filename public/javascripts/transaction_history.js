272b6280cf607176c3d47b288a8097c71beb6b5c
Jason Hanggi <jason@tablexi.com>
2011-11-11 14:27:18 -0600

    set min/max dates in datepicker when updating the fields

--- public/javascripts/transaction_history.js
+++ public/javascripts/transaction_history.js
@@ -1,4 +1,23 @@
 $(function() {
 	$("select[multiple]").chosen();
+	
 	$(".datepicker").datepicker();
+	// call trigger("change") to make sure that it updates on page load
+	$(".datepicker[name=start_date]").change(DatePickerRange.updateEndMaxDate).trigger("change");
+	$(".datepicker[name=end_date]").change(DatePickerRange.updateStartMinDate).trigger("change");
+	
+	
+	
 });
+
+var DatePickerRange = {
+	updateEndMaxDate: function() {
+		var start = Date.parse($(this).val());
+		$end = $(".datepicker[name=end_date]").datepicker("option", {minDate: start });
+	},
+	updateStartMinDate: function() {
+		var end = Date.parse($(this).val());
+		$start = $(".datepicker[name=start_date]").datepicker("option", {maxDate: end});
+	}
+		
+}
