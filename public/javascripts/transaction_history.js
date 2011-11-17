597ce19f81af3ba503b2f6a1dfdbaaf9f10fdfdb
Jason Hanggi <jason@tablexi.com>
2011-11-17 14:47:37 -0600

    set maxdate of datepickers to today

--- public/javascripts/transaction_history.js
+++ public/javascripts/transaction_history.js
@@ -1,7 +1,7 @@
 $(function() {
 	$("select[multiple]").chosen();
 	
-	$(".datepicker").datepicker();
+	$(".datepicker").datepicker({maxDate: new Date()});
 	// call trigger("change") to make sure that it updates on page load
 	$(".datepicker[name=start_date]").change(DatePickerRange.updateEndMaxDate).trigger("change");
 	$(".datepicker[name=end_date]").change(DatePickerRange.updateStartMinDate).trigger("change");
