7d6f1fc463bc1d20db357a3fef96797e33e72c35
Jason Hanggi <jason@tablexi.com>
2011-11-18 10:18:06 -0600

    TransactionHistoryHelper specs were broken after new refactoring

--- app/helpers/transaction_history_helper.rb
+++ app/helpers/transaction_history_helper.rb
@@ -1,17 +1,11 @@
 module TransactionHistoryHelper
-  def transaction_search_submit_path(account)
-    if (account)
-      account_transaction_history_path(account)
-    else
-      transaction_history_path
-    end
-  end
-  
   def single_account?
+    return false unless @accounts
     @accounts.size == 1
   end
   
   def single_facility?
+    return false unless @facilities
     @facilities.size == 1
   end
 end
