ea1e3a7cfa961a22a9ccc3c8517503347f2f7dd9
Jason Hanggi <jason@tablexi.com>
2011-11-28 17:32:14 -0600

    begin localization create helper methods for easier translation of model names front end pages localized

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,15 +1,15 @@
 class OrderMailer < ActionMailer::Base
-  default :sender => "#{NUCore.app_name} Orders <noreply>"
+  default :sender => "#{I18n.t :app_name} Orders <noreply>"
 
 
   def order_receipt(order)
     @order=order
-    mail(:to => order.user.email, :subject => "#{NUCore.app_name} Order ##{order.id} Receipt")
+    mail(:to => order.user.email, :subject => "#{I18n.t :app_name} Order ##{order.id} Receipt")
   end
 
 
   def facility_order_notification(order)
     @order=order
-    mail(:to => order.facility.order_notification_email, :subject => "#{NUCore.app_name} Order ##{order.id} Notification")
+    mail(:to => order.facility.order_notification_email, :subject => "#{I18n.t app_name} Order ##{order.id} Notification")
   end
 end
