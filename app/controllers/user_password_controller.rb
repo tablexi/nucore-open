b2e907f8706536c2205b3a49277f649c599cb34b
Jason Hanggi <jason@tablexi.com>
2011-12-20 17:52:22 -0600

    extract passwords actions to their own controller.

---
+++ app/controllers/user_password_controller.rb
@@ -0,0 +1,64 @@
+class UserPasswordController < ApplicationController
+  before_filter :authenticate_user!, :only => :edit_current
+  before_filter :no_user_allowed, :only => [:reset, :edit, :update]
+  layout "application"
+  # GET /users/password/edit_current
+  # POST /users/password/edit_current
+  def edit_current
+    @user = current_user
+
+    unless @user.external?
+      render :no_password, :layout => "application" and return
+    end
+    
+    if request.post? and @user.update_password(params[:user])
+      flash[:notice] = "Your password has been updated" 
+    end    
+  end
+
+  
+  def reset
+    if request.post?
+      @user = User.find_by_email(params[:user][:email])
+      if @user
+        if @user.external?
+          @user.send_reset_password_instructions
+          flash[:notice] = "Instructions on how to reset your password have been sent to #{@user.email}"
+          redirect_to new_user_session_path
+        else
+          flash.now[:error] = "We cannot reset the password for that account. Please change it via the NetID website."
+        end
+      else
+        flash.now[:error] = "We cannot find #{params[:user][:email]} in our records."
+      end
+    end
+  end
+  
+  def edit
+    unless params[:reset_password_token] and @user = User.find_by_reset_password_token(params[:reset_password_token]) and @user.external?
+      flash[:error] = "The token is either invalid or has expired."
+      redirect_to :action => :reset  
+    end
+  end
+  
+  def update
+    
+  end
+  
+  private
+  def no_user_allowed
+    if current_user
+      @hide_form = true
+      flash.now[:error] = "You should not be accessing this page while already logged in."
+      return false
+    end
+  end
+end
+
+# if params[:reset_password_token]
+        # @user = User.find_by_reset_password_token(params[:reset_password_token])
+        # if @user and @user.external?
+          # render :action => :password, :layout => "application" and return
+        # end
+        # flash.now[:error] = "That link you clicked is no longer valid."
+      # end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,8 @@ Nucore::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.default_url_options = { :host => "localhost", :port => 3000 }
 
   # Print deprecation notices to the Rails logger
