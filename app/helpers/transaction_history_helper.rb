d9c3d001d64bd3944154f692fa119a98da6c793f
Jason Hanggi <jason@tablexi.com>
2011-12-30 13:36:07 -0600

    remove single_facility? helpers. better handling of when to show and hide search fields.

--- app/helpers/transaction_history_helper.rb
+++ app/helpers/transaction_history_helper.rb
@@ -1,14 +1,4 @@
 module TransactionHistoryHelper
-  def single_account?
-    return false unless @accounts
-    @accounts.size == 1
-  end
-  
-  def single_facility?
-    return false unless @facilities
-    @facilities.size == 1
-  end
-  
   def row_class(order_detail)
     needs_reconcile_warning?(order_detail) ? 'reconcile-warning' : ''
     # if @warning_method
