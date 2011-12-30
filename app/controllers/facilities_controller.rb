4719bd056eed6dd9b9d8014399218a0e35d4297f
Jason Hanggi <jason@tablexi.com>
2011-12-30 16:28:27 -0600

    fix navigation problems

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -1,6 +1,6 @@
 class FacilitiesController < ApplicationController
   customer_tab  :index, :list, :show
-  admin_tab     :edit, :manage, :schedule, :update, :agenda
+  admin_tab     :edit, :manage, :schedule, :update, :agenda, :transactions
   before_filter :authenticate_user!, :except => [:index, :show]  # public pages do not require authentication
   before_filter :check_acting_as, :except => [:index, :show]
   before_filter :init_current_facility, :only => [:edit, :manage, :schedule, :show, :update, :agenda, :transactions]
