f4c84a98948ba999792416b992d1f2304d0a460e
Jason Hanggi <jason@tablexi.com>
2012-01-04 16:18:55 -0600

    tests for transaction search

--- spec/transaction_search_spec_helper.rb
+++ spec/transaction_search_spec_helper.rb
@@ -2,23 +2,49 @@ def it_should_support_searching
   context "searching" do
     before :each do
       sign_in @authable.owner.user
+      @sql_datetime_format = '%Y-%m-%d %H:%M:%S'
+      @date_string = '2001-01-01'
+      @datetime = Time.zone.parse(@date_string)
     end
-    it "should take start date" do
-      @params.merge!({:start_date => '2001-01-01'})
-      do_request
-      response.should be_success
-      assigns[:order_details].where_values.should be_include("fulfilled_at > '2001-01-01 06:00:00'")
-    end
-    it "should take end date" do
-      @params.merge!({:end_date => '2012-01-01'})
-      do_request
-      response.should be_success
-      puts assigns[:order_details].where_values
-      assigns[:order_details].where_values.should be_include("fulfilled_at < '2011-01-02 05:59:59'")
+    it "should support the inner method" do
+      controller.should respond_to :"#{@action}_with_search"
     end
     
-    it "should take facilities"
-    it "should take accounts"
-    it "should take products"
+    it "should take start date" do
+      @params.merge!({:start_date => @date_string})
+      do_request
+      assigns[:order_details].where_values.should be_include("fulfilled_at > '#{@datetime.beginning_of_day.utc.strftime(@sql_datetime_format)}'")
+    end
+    
+    it "should take end date" do
+      @params.merge!({:end_date => @date_string})
+      do_request
+      assigns[:order_details].where_values.should be_include("fulfilled_at < '#{@datetime.end_of_day.utc.strftime(@sql_datetime_format)}'")
+    end
+    
+    it "should take facilities" do
+      @params.merge!({:facilities => [2, 3]})
+      do_request
+      assigns[:order_details].where_values.should be_include("orders.facility_id in (2,3)")
+    end
+    
+    it "should take accounts" do
+      @params.merge!({:accounts => [1,6]})
+      do_request
+      assigns[:order_details].where_values.should be_include("order_details.account_id in (1,6)")
+    end
+    
+    it "should take products" do
+      @params.merge!({:products => [2,4]})
+      do_request
+      assigns[:order_details].where_values.should be_include("order_details.product_id in (2,4)")
+    end
+    
+    it "should handle account owners" do
+      @params.merge!({:owners => [3,4]})
+      do_request
+      puts assigns[:order_details].where_values
+      assigns[:order_details].where_values.should be_include("account_users.user_id in (3,4)")
+    end
   end
 end
