bf8f65b0946808a9f633c092ffdeee69e8bf3ca8
Chris Stump <chris@tablexi.com>
2012-02-20 15:45:03 -0600

    rename

--- vendor/engines/validator/config/initializers/engine.rb
+++
@@ -1,8 +0,0 @@
-# use the host app's db config during development
-unless Rails.env.production?
-  require 'fileutils'
-  base=File.dirname(__FILE__)
-  FileUtils.ln_sf(File.join(base, '..', '..', '..', '..', '..', 'config', 'database.yml'), File.join(base, '..', 'database.yml'))
-  FileUtils.ln_sf(File.join(base, '..', '..', '..', '..', '..', 'Gemfile'), File.join(base, '..', '..', 'Gemfile'))
-  FileUtils.ln_sf(File.join(base, '..', '..', '..', '..', '..', 'Gemfile.lock'), File.join(base, '..', '..', 'Gemfile.lock'))
-end
