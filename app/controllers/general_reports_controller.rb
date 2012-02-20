f2797f1687614847b0734489ddc9754023fc1361
Chris Stump <chris@tablexi.com>
2012-02-20 14:19:15 -0600

    [Table XI #43839] General report- Owner tab should sort by last name instead of first

--- app/controllers/general_reports_controller.rb
+++ app/controllers/general_reports_controller.rb
@@ -14,7 +14,11 @@ class GeneralReportsController < ReportsController
   def account_owner
     render_report(2, 'Name') do |od|
       owner=od.account.owner.user
-      "#{owner.full_name} (#{owner.username})"
+      name=''
+      name += (owner.last_name || '')
+      name += ", " unless name.blank?
+      name += (owner.first_name || '')
+      "#{name} (#{owner.username})"
     end
   end
 
