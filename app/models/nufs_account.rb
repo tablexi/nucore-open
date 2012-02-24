10f87457cec21922a72f1b5b2aacc1f44b560ca9
Chris Stump <chris@tablexi.com>
2012-02-24 16:21:15 -0600

    Task #43091 : Abstract account validation interface and replace nucs with RRC validation engine; make validation system base lib classes; merge the validations module into it's only class NufsAccount; remove all chartstring-specific logic and naming

--- app/models/nufs_account.rb
+++ app/models/nufs_account.rb
@@ -1,20 +1,16 @@
 class NufsAccount < Account
-  include Validations
-  
   validates_format_of     :account_number, :with => ValidatorFactory.pattern, :message => I18n.t('activerecord.errors.messages.bad_payment_source_format', :pattern_format => ValidatorFactory.pattern_format)
   validates_uniqueness_of :account_number, :message => "already exists"
 
-  validate :check_chartstring
+  validate { validate_account_number }
 
-  def check_chartstring
-    validate_chartstring
-  end
 
   def set_expires_at!
     self.expires_at = ValidatorFactory.instance(account_number).latest_expiration
   end
-  
-  def account_open? (account_num)
+
+
+  def account_open?(account_num)
     begin
       ValidatorFactory.instance(account_number, account_num).account_is_open!
     rescue ValidatorError
@@ -22,5 +18,48 @@ class NufsAccount < Account
     end
 
     return true
+  end
+
+
+  def after_find
+    load_components
+  end
+
+
+  private
+
+  #
+  # Retrieves +#components+ from +ValidatorFactory#instance+ and sets
+  # the keys of the return as methods on this class (if necessary) and
+  # then sets the value on self.
+  # [_return_]
+  #   The Validator from which the components were retrieved
+  def load_components
+    validator=ValidatorFactory.instance(account_number, NUCore::COMMON_ACCOUNT)
+
+    validator.components.each do |k,v|
+      self.class.class_eval "attr_accessor :#{k}" unless respond_to? k
+      send("#{k}=", v)
+    end
+
+    validator
+  end
+
+
+  def validate_account_number
+    begin
+      validator = load_components
+    rescue ValidatorError => e
+      self.errors.add(:account_number, e.message)
+      return
+    end
+
+    begin
+      validator.account_is_open!
+    rescue ValidatorError => e
+      msg=e.message
+      msg="not found, is inactive, or is invalid" if msg.blank?
+      self.errors.add(:account_number, msg)
+    end
   end
 end
