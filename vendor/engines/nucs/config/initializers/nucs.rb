adc798d10136dacddda6c1d89d9bb77a8fc287fe
Chris Stump <chris@tablexi.com>
2012-03-20 14:43:06 -0500

    make factories overridable by engines, etc.; move factory env setup away from factory defs

--- vendor/engines/nucs/config/initializers/nucs.rb
+++ vendor/engines/nucs/config/initializers/nucs.rb
@@ -1,8 +1,22 @@
-# use the host app's db config during development
 unless Rails.env.production?
   require 'fileutils'
   base=File.dirname(__FILE__)
-  FileUtils.ln_sf(File.join(base, '..', '..', '..', '..', '..', 'config', 'database.yml'), File.join(base, '..', 'database.yml'))
-  FileUtils.ln_sf(File.join(base, '..', '..', '..', '..', '..', 'Gemfile'), File.join(base, '..', '..', 'Gemfile'))
-  FileUtils.ln_sf(File.join(base, '..', '..', '..', '..', '..', 'Gemfile.lock'), File.join(base, '..', '..', 'Gemfile.lock'))
+
+  #
+  # Use the host app's config during development
+  FileUtils.ln_sf File.expand_path('../../../../../config/database.yml', base), File.expand_path('../database.yml', base)
+  FileUtils.ln_sf File.expand_path('../../../../../Gemfile', base), File.expand_path('../../Gemfile', base)
+  FileUtils.ln_sf File.expand_path('../../../../../Gemfile.lock', base), File.expand_path('../../Gemfile.lock', base)
+
+  #
+  # These specs should be integrated into the main app's spec run
+  %w(models controllers).each do |spec_dir|
+    integrate_path=File.expand_path("../../../../../spec/#{spec_dir}/nucs", base)
+    # must integrate via a real directory (i.e. can't symlink dir) or rspec won't find specs
+    FileUtils.mkdir integrate_path unless File.exists? integrate_path
+
+    Dir[File.expand_path("../../spec/integrations/#{spec_dir}/*_spec.rb", base)].each do |spec|
+      FileUtils.ln_sf spec, integrate_path
+    end
+  end
 end
