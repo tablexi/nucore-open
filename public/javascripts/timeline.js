2727e5c955e2dd96748ed206aad1bbd8018d290e
Jason Hanggi <jason@tablexi.com>
2012-05-23 15:37:41 -0500

    enable drag and drop

--- public/javascripts/timeline.js
+++ public/javascripts/timeline.js
@@ -1,30 +1,30 @@
 $(function() {
     	  //Set Draggable Unit, grid and axis
-    		// $( ".unit" ).draggable({
-    		//   axis: "x",
-    		//   containment: "parent",
-    		//   grid: [ 2, 0],
-    		//   opacity: 0.7,
-    		//   revert: 'invalid',
-    		//     stop: function(){
-      //           $(this).draggable('option','revert','invalid');  
-      //       }
-    		// });
-    	 //  //The unit container will fit the units. 
-      // 	$('.unit_container').droppable({
-      //       tolerance: 'fit'
-      //   });
-      //   //Do not allow the overlap, yell at me if I try to drop a reservation on a reservation
-      //   $('.unit').droppable({
-      //     greedy: true,
-      //     tolerance: 'touch',
-      //     hoverClass: "invalid",
-      //     drop: function(event,ui){
-      //        $(this).off('invalid');
-      //         ui.draggable.draggable('option','revert',true);
-      //         alert("These Times Over Lap - You Can't Do that.");
-      //     }
-      //   })
+    		$( ".unit" ).draggable({
+    		  axis: "x",
+    		  containment: "parent",
+    		  grid: [ 2, 0],
+    		  opacity: 0.7,
+    		  revert: 'invalid',
+    		    stop: function(){
+                $(this).draggable('option','revert','invalid');  
+            }
+    		});
+    	  //The unit container will fit the units. 
+      	$('.unit_container').droppable({
+            tolerance: 'fit'
+        });
+        //Do not allow the overlap, yell at me if I try to drop a reservation on a reservation
+        $('.unit').droppable({
+          greedy: true,
+          tolerance: 'touch',
+          hoverClass: "invalid",
+          drop: function(event,ui){
+             $(this).off('invalid');
+              ui.draggable.draggable('option','revert',true);
+              alert("These Times Over Lap - You Can't Do that.");
+          }
+        })
         //Tool Tip
         tooltipContent = function($el, $tip) {
           var id = /block_reservation_(\d+)/.exec($el.attr("id"))[1];
@@ -35,8 +35,8 @@ $(function() {
         });
         //Get the Current Hour, create a class and add it the time div
         time = function() {
-          var currentTime = new Date()
-          var hours = currentTime.getHours()            
+          var currentTime = new Date();
+          var hours = currentTime.getHours();
           $('.current_time').addClass("hour_" + hours);
         };  
         time();
