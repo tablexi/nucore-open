75d1c27a88dd15610d37c849001d1d00de5ec833
Jason Hanggi <jason@tablexi.com>
2012-06-15 13:10:13 -0500

    fix broken test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -182,13 +182,15 @@ class ApplicationController < ActionController::Base
 
   def remove_ugly_params_and_redirect
     if (params[:commit] && request.get?)
-      # remove ugly parameters
-      params.delete(:commit)
-      params.delete(:utf8)
-      params.delete(:authenticity_token)
+      remove_ugly_params
       # redirect to self
       redirect_to params
       return false
+    end
+  end
+  def remove_ugly_params
+    [:commit, :utf8, :authenticity_token].each do |p|
+      params.delete(p)
     end
   end
 end
