de89fd36211ee4f960b5026165bc94147e135646
Chris Stump <chris@tablexi.com>
2012-06-22 13:17:46 -0500

    remove dup method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -170,30 +170,6 @@ Spork.each_run do
   #   The user who is ordering the item
   # [_facility_]
   #   The facility with which the order is placed
-  # [_item_]
-  #   The product being ordered
-  # [_account_]
-  #   The account under which the order is placed
-  def place_product_order(ordered_by, facility, product, account=nil)
-    @price_group=Factory.create(:price_group, :facility => facility)
-    o_attrs={ :created_by => ordered_by.id, :facility => facility, :ordered_at => Time.zone.now }
-    o_attrs.merge!(:account_id => account.id) if account
-    @order=ordered_by.orders.create(Factory.attributes_for(:order, o_attrs))
-    Factory.create(:user_price_group_member, :user => ordered_by, :price_group => @price_group)
-    @item_pp=product.send(:"#{product.class.name.downcase}_price_policies").create(Factory.attributes_for(:"#{product.class.name.downcase}_price_policy", :price_group_id => @price_group.id))
-    @item_pp.reload.restrict_purchase=false
-    od_attrs={ :product_id => product.id }
-    od_attrs.merge!(:account_id => account.id) if account
-    @order_detail = @order.order_details.create(Factory.attributes_for(:order_detail).update(od_attrs))
-  end
-
-
-  #
-  # Simulates placing an order for an item and having it marked complete
-  # [_ordered_by_]
-  #   The user who is ordering the item
-  # [_facility_]
-  #   The facility with which the order is placed
   # [_account_]
   #   The account under which the order is placed
   # [_reviewed_]
