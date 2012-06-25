4b3c54b30a3922a65fbc9ba777b4e7a09954a9b4
Chris Stump <chris@tablexi.com>
2012-06-25 17:08:22 -0500

    Task #46327 : settings; disable PO and CC possibilities; make the engine override main app's views

--- vendor/engines/c2po/lib/c2po.rb
+++ vendor/engines/c2po/lib/c2po.rb
@@ -5,6 +5,12 @@ module C2po
     config.to_prepare do
       Facility.send :include, C2po::FacilityExtension
       FacilityAccountsController.send :include, C2po::FacilityAccountsControllerExtension
+
+      # make this engine's views override the main app's views
+      paths=ActionController::Base.view_paths.dup
+      index=paths.index{|p| p.to_s.include? 'c2po'}
+      paths.unshift paths.delete_at(index)
+      ActionController::Base.view_paths=paths
     end
   end
 end
