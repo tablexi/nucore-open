add0e5de98bbe78d403944b5e6d4c5d52aadf733
Chris Stump <chris@tablexi.com>
2012-06-25 19:37:07 -0500

    Task #46327 : settings; disable PO and CC possibilities; move private methods for cc & po to c2po

--- vendor/engines/c2po/lib/c2po/facility_accounts_controller_extension.rb
+++ vendor/engines/c2po/lib/c2po/facility_accounts_controller_extension.rb
@@ -49,6 +49,61 @@ module C2po
       def update_purchase_orders
         update_account(PurchaseOrderAccount, purchase_orders_facility_accounts_path)
       end
+
+
+      private
+
+      def show_account(model_class)
+        @subnav     = 'billing_nav'
+        @active_tab = 'admin_billing'
+        @accounts   = model_class.need_reconciling(current_facility)
+
+        unless @accounts.empty?
+          selected_id=params[:selected_account]
+
+          if selected_id.blank?
+            @selected=@accounts.first
+          else
+            @accounts.each{|a| @selected=a and break if a.id == selected_id.to_i }
+          end
+
+          @unreconciled_details=OrderDetail.account_unreconciled(current_facility, @selected)
+          @unreconciled_details=@unreconciled_details.paginate(:page => params[:page])
+        end
+      end
+
+
+      def update_account(model_class, redirect_path)
+        @error_fields = {}
+        update_details = OrderDetail.find(params[:order_detail].keys)
+
+        OrderDetail.transaction do
+          count = 0
+          update_details.each do |od|
+            od_params = params[:order_detail][od.id.to_s]
+            od.reconciled_note=od_params[:notes]
+
+            begin
+              if od_params[:reconciled] == '1'
+                od.change_status!(OrderStatus.reconciled.first)
+                count += 1
+              else
+                od.save!
+              end
+            rescue
+              @error_fields = {od.id => od.errors.collect { |field,error| field}}
+              errors = od.errors.full_messages
+              errors = [$!.message] if errors.empty?
+              flash.now[:error] = (["There was an error processing the #{model_class.name.underscore.humanize.downcase} payments"] + errors).join("<br />")
+              raise ActiveRecord::Rollback
+            end
+          end
+
+          flash[:notice] = "#{count} payment#{count == 1 ? '' : 's'} successfully reconciled" if count > 0
+        end
+
+        redirect_to redirect_path
+      end
     end
 
   end
