ad89ea5dd2fadd9c0c6652384115c7d2cf5f7063
Jason Hanggi <jason@tablexi.com>
2012-07-09 16:44:33 -0500

    don't create any price rules in migration. more informative output from migration.

--- db/migrate/20120622185758_move_can_purchase_into_price_policy.rb
+++ db/migrate/20120622185758_move_can_purchase_into_price_policy.rb
@@ -18,35 +18,15 @@ class MoveCanPurchaseIntoPricePolicy < ActiveRecord::Migration
       else
         next if pgp.product.type == "Bundle" # skip bundles since they don't have their own price policies
         next if pgp.product.is_archived? # Ignore inactive products
-
-        puts "Didn't find any policies for #{pgp.product.facility} ##{pgp.product.id} | #{pgp.product} | #{pgp.price_group}"
-        model_class = "#{pgp.product.type}PricePolicy".constantize
-        # create an expired policy
-        pp = model_class.new(:product => pgp.product,
-                             :price_group => pgp.price_group,
-                             :can_purchase => true,
-                             :start_date => 1.year.ago,
-                             :expire_date => 1.year.ago)
-        begin
-          pp.save!(:validate => false)
-        rescue Exception => e
-          puts e.message
-          puts e.backtrace
-        end
+        
+        puts "Missing policies for | #{pgp.product.facility} | ##{pgp.product.id} | #{pgp.product} | #{pgp.price_group} | #{pgp.product.price_policies.size > 0 ? 'Has policies' : 'No policies'}"
+        
       end
     end
   end
 
   def self.down
-    remove_column :price_policies, :can_purchase
-    PricePolicy.reset_column_information
-    InstrumentPricePolicy.reset_column_information
-    ServicePricePolicy.reset_column_information
-    ItemPricePolicy.reset_column_information
-
-    PricePolicy.where(:unit_cost => nil, :usage_rate => nil, :can_purchase => true).each do |pp|
-      pp.destroy
-    end
+    remove_column :price_policies, :can_purchase    
   end
 
 end
