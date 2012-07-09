a5736a72030550f029ad50041cd50acf3698ba74
Jason Hanggi <jason@tablexi.com>
2012-07-09 12:51:02 -0500

    make new policies expired fixes to migration

--- db/migrate/20120622185758_move_can_purchase_into_price_policy.rb
+++ db/migrate/20120622185758_move_can_purchase_into_price_policy.rb
@@ -1,7 +1,12 @@
 class MoveCanPurchaseIntoPricePolicy < ActiveRecord::Migration
   def self.up
     add_column :price_policies, :can_purchase, :boolean, :after => :price_group_id, :default => false, :null => false
+    
     PricePolicy.reset_column_information
+    InstrumentPricePolicy.reset_column_information
+    ServicePricePolicy.reset_column_information
+    ItemPricePolicy.reset_column_information
+
     PriceGroupProduct.all.each do |pgp|
       price_policies = PricePolicy.find_all_by_price_group_id_and_product_id(pgp.price_group_id, pgp.product_id)
       if price_policies.any?
@@ -11,39 +16,37 @@ class MoveCanPurchaseIntoPricePolicy < ActiveRecord::Migration
           pp.update_attribute(:can_purchase,  1)
         end
       else
-        puts "Didn't find any policies for #{pgp.product} | #{pgp.price_group}"
-        # skip bundles
-        next if pgp.product.type == "Bundle"
+        next if pgp.product.type == "Bundle" # skip bundles since they don't have their own price policies
+        next if pgp.product.is_archived? # Ignore inactive products
+
+        puts "Didn't find any policies for #{pgp.product.facility} ##{pgp.product.id} | #{pgp.product} | #{pgp.price_group}"
         model_class = "#{pgp.product.type}PricePolicy".constantize
+        # create an expired policy
         pp = model_class.new(:product => pgp.product,
                              :price_group => pgp.price_group,
                              :can_purchase => true,
-                             :start_date => Time.zone.now,
-                             #TODO use SettingsHelper from fiscal_year branch
-                             :expire_date => fiscal_year_end(Time.zone.now))
-        pp.save!(:validate => false)
+                             :start_date => 1.year.ago,
+                             :expire_date => 1.year.ago)
+        begin
+          pp.save!(:validate => false)
+        rescue Exception => e
+          puts e.message
+          puts e.backtrace
+        end
       end
     end
   end
 
   def self.down
     remove_column :price_policies, :can_purchase
-    PricePolicy.where(:unit_cost => nil, :usage_rate => nil).each do |pp|
+    PricePolicy.reset_column_information
+    InstrumentPricePolicy.reset_column_information
+    ServicePricePolicy.reset_column_information
+    ItemPricePolicy.reset_column_information
+
+    PricePolicy.where(:unit_cost => nil, :usage_rate => nil, :can_purchase => true).each do |pp|
       pp.destroy
     end
   end
 
-  def self.fiscal_year_end(date=nil)
-    date ||= Time.zone.now
-    (fiscal_year_beginning(date) + 1.year- 1.day).end_of_day
-  end
-
-  def self.fiscal_year_beginning(date=nil)
-    date ||= Time.zone.now
-    fiscal_year_starts = fiscal_year(date.year) 
-    date.to_time >= fiscal_year_starts ? fiscal_year_starts : fiscal_year_starts - 1.year
-  end
-  def self.fiscal_year(year)
-    Time.zone.parse("#{year}-09-01").beginning_of_day
-  end
 end
