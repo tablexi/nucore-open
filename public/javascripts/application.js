fd8fee4be46acfbe85ade79335304214c81bdac6
Jason Hanggi <jason@tablexi.com>
2012-07-12 17:48:56 -0500

    When in order detail edit view, tabs counts were not going to the correct URL

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -99,12 +99,14 @@ $(document).ready(function() {
       }
     });
     if (tabs.length > 0) {
-      var base = '';
-      if (window.location.pathname.endsWith('reservations')) {
-        base = 'reservations/';
-      } else if (window.location.pathname.endsWith('orders')) {
-        base = 'orders/';
+      var base = FACILITY_PATH;
+      var active_tab = $('#main_navigation .active').attr('id')
+      if (active_tab.indexOf('reservations') > -1) {
+        base += '/reservations/';
+      } else if (active_tab.indexOf('orders') > -1) {
+        base += '/orders/';
       }
+      console.debug("BASE", base);
       $.ajax({
         url: base + 'tab_counts',
         dataType: 'json',
