4b918ec65221d4906d0d53bea27a6cbb0b99683f
Chris Stump <chris@tablexi.com>
2012-08-29 16:32:46 -0500

    Task #48377 : Add ability to attach new orders to old existing orders; reorder migrations and Oracle-ize

--- db/migrate/20120809185703_add_order_details_created_by.rb
+++ db/migrate/20120809185703_add_order_details_created_by.rb
@@ -1,12 +1,15 @@
 class AddOrderDetailsCreatedBy < ActiveRecord::Migration
   def self.up
-    add_column :order_details, :created_by, :integer, :null => false
+    add_column :order_details, :created_by, :integer
 
     OrderDetail.reset_column_information
 
     Order.all.each do |order|
       order.order_details.each{|detail| detail.update_attribute :created_by, order.created_by }
     end
+
+    # oracle won't let us put a not null constraint on a new column for an existing, non-empty table
+    change_column :order_details, :created_by, :integer, :null => false
   end
 
   def self.down
