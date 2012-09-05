0f11832cee3565c21e56750f67b582db00f1ca4a
Chris Stump <chris@tablexi.com>
2012-09-05 15:49:12 -0500

    fix tests so fiscal year doesn't interfere

--- vendor/engines/c2po/spec/factories.rb
+++ vendor/engines/c2po/spec/factories.rb
@@ -2,8 +2,8 @@ Factory.define :credit_card_account, :class => CreditCardAccount do |o|
   o.sequence(:account_number) { |n| "5276-4400-6542-1319" }
   o.description 'credit card account description'
   o.name_on_card 'Person'
-  o.expiration_month((Time.zone.now + 1.month).month)
-  o.expiration_year((Time.zone.now + 1.month).year)
+  o.expiration_month Time.zone.now.month
+  o.expiration_year((Time.zone.now + 1.year).year)
   o.expires_at Time.zone.now + 1.month
   o.created_by 0
   o.sequence(:affiliate) { |n| Affiliate.find_or_create_by_name("cc_affiliate#{n}") }
