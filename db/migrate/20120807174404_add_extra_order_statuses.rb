38d031f92129cb6c9cbbcff8a9f60e6943bcf77c
Chris Stump <chris@tablexi.com>
2012-10-18 15:12:18 -0500

    observer name changed; update related migration

--- db/migrate/20120807174404_add_extra_order_statuses.rb
+++ db/migrate/20120807174404_add_extra_order_statuses.rb
@@ -4,14 +4,14 @@ class AddExtraOrderStatuses < ActiveRecord::Migration
   # substatus of New.
   def self.up
     new_status = OrderStatus.find_by_name('New')
-    OrderDetailStatusChangeNotification.status_change_hooks.keys.each do |status|
+    OrderDetailObserver.status_change_hooks.keys.each do |status|
       OrderStatus.find_or_create_by_name_and_facility_id(:name => status.to_s.titleize, :facility_id => nil, :parent => new_status)
     end
   end
 
   def self.down
     new_status = OrderStatus.find_by_name('New')
-    OrderDetailStatusChangeNotification.status_change_hooks.keys.each do |status|
+    OrderDetailObserver.status_change_hooks.keys.each do |status|
       OrderStatus.destroy_all(:name => status.to_s.titleize, :facility_id => nil, :parent_id => new_status.id)
     end
   end
