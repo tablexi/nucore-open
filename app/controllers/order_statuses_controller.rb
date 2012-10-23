48acffa677a32e024b9e42a5faf1d0f0bb59c585
Jason Hanggi <jason@tablexi.com>
2012-10-23 18:44:32 -0500

    fix spec

--- app/controllers/order_statuses_controller.rb
+++ app/controllers/order_statuses_controller.rb
@@ -49,7 +49,6 @@ class OrderStatusesController < ApplicationController
 
     if @order_status.update_attributes(params[:order_status])
       flash[:notice] = 'The Order Status was successfully updated.'
-      puts "path"
       redirect_to facility_order_statuses_path
     else
       render :action => "edit"
