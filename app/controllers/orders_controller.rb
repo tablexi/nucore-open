319c49f5a835a74c04f631e203c6631e0a36637e
Jason Hanggi <jason@tablexi.com>
2012-10-23 18:29:46 -0500

    make sure <br> doesn't appear in error message

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -123,7 +123,7 @@ class OrdersController < ApplicationController
       end
 
       if @order.errors.any?
-        flash[:error] = "There were errors adding to your cart:<br>"+@order.errors.full_messages.join('<br>').html_safe
+        flash[:error] = "There were errors adding to your cart:<br>#{@order.errors.full_messages.join('<br>')}".html_safe
         raise ActiveRecord::Rollback
       end
     end
