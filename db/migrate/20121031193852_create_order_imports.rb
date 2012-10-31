e3e8d9060f29ec21b5e13d8df69215413b5b76c5
Chris Stump <chris@tablexi.com>
2012-10-31 18:29:01 -0500

    Task #38891 : Bulk importing of orders; index foreign keys

--- db/migrate/20121031193852_create_order_imports.rb
+++ db/migrate/20121031193852_create_order_imports.rb
@@ -6,9 +6,17 @@ class CreateOrderImports < ActiveRecord::Migration
       t.column :created_by, :integer, :null => false
       t.timestamps
     end
+
+    add_index :order_imports, :upload_file_id
+    add_index :order_imports, :error_file_id
+    add_index :order_imports, :created_by
   end
 
   def self.down
+    remove_index :order_imports, :upload_file_id
+    remove_index :order_imports, :error_file_id
+    remove_index :order_imports, :created_by
+
     drop_table :order_imports
   end
 end
