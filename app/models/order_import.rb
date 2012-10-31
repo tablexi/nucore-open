87b38f2bd24eb42be8210955a6386004c39ab670
Chris Stump <chris@tablexi.com>
2012-10-31 18:29:22 -0500

    Task #38891 : Bulk importing of orders; set created_by association

--- app/models/order_import.rb
+++ app/models/order_import.rb
@@ -1,6 +1,7 @@
 class OrderImport < ActiveRecord::Base
   belongs_to :upload_file, :class_name => :stored_file, :dependent => :destroy
   belongs_to :error_file, :class_name => :stored_file, :dependent => :destroy
+  belongs_to :creator, :class_name => :user, :foreign_key => :created_by
 
   validates_presence_of :upload_file_id, :created_by
 end
