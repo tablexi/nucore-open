f5e466c08fb021e7c2c56254952fabf86c56ee29
Chris Stump <chris@tablexi.com>
2012-10-31 18:30:18 -0500

    Task #38891 : Bulk importing of orders; test

--- spec/models/order_import_spec.rb
+++ spec/models/order_import_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe OrderImport do
 
+  it { should belong_to :creator }
+  it { should belong_to :upload_file }
+  it { should belong_to :error_file }
   it { should validate_presence_of :upload_file_id }
   it { should validate_presence_of :created_by }
 
