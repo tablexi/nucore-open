9d92e5760bcb57b0103b1d395dce012a8fc9c252
Chris Stump <chris@tablexi.com>
2012-11-01 14:29:58 -0500

    Task #38891 : Bulk importing of orders; don't specify col type in #down

--- db/migrate/20121031230321_alter_orders_add_order_import.rb
+++ db/migrate/20121031230321_alter_orders_add_order_import.rb
@@ -5,7 +5,7 @@ class AlterOrdersAddOrderImport < ActiveRecord::Migration
   end
 
   def self.down
-    remove_column :orders, :order_import_id, :integer
+    remove_column :orders, :order_import_id
     remove_index :orders, :order_import_id
   end
 end
