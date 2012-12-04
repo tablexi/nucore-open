da0a271ceb6469a1bdcff0794a1e9cde3467738a
Ross Heflin <ross@tablexi.com>
2012-12-03 21:01:56 -0600

    Task #38891: Add smoke tests to ensure orders aren't created for certain failure cases

--- spec/controllers/order_imports_controller_spec.rb
+++ spec/controllers/order_imports_controller_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 require 'controller_spec_helper'
 
+  # helpers
+  def upload_file(file_name)
+    ActionDispatch::TestProcess.fixture_file_upload("#{Rails.root}/spec/files/order_imports/#{file_name}", 'text/csv')
+  end
+
+  def get_real_order_count(import_id)
+    ActiveRecord::Base.connection.select_value("SELECT COUNT(*) FROM orders where order_import_id = #{import_id}")
+  end
 
 describe OrderImportsController do
   render_views
@@ -13,6 +21,7 @@ describe OrderImportsController do
   end
 
 
+  # contexts
   context 'starting an import' do
 
     before :each do
@@ -29,21 +38,18 @@ describe OrderImportsController do
 
 
   context 'doing an import' do
-
-    before :each do
-      @action=:create
-      @method=:post
-      @params.merge!({
-        :order_import => {
-          :upload_file => upload_file('blank.csv'),
-          :fail_on_error => false,
-          :send_receipts => false
-        }
-      })
-    end
-
-
     context 'with a blank file' do
+      before :each do
+        @action=:create
+        @method=:post
+        @params.merge!({
+          :order_import => {
+            :upload_file => upload_file('blank.csv'),
+            :fail_on_error => false,
+            :send_receipts => false
+          }
+        })
+      end
 
       it_should_allow_operators_only do
         flash[:error].should be_blank
@@ -60,13 +66,121 @@ describe OrderImportsController do
         maybe_grant_always_sign_in :director
         lambda { do_request }.should change(StoredFile, :count).from(0).to(1)
       end
-
     end
 
+    context "with an erroneous file" do
+      before :each do
+        # necessary to purchase them
+        @facility_account = @authable.facility_accounts.create!(Factory.attributes_for(:facility_account))
+        
+        grant_role(@director, @authable)
+        @item             = @authable.items.create!(Factory.attributes_for(:item,
+          :facility_account_id => @facility_account.id,
+          :name => "Example Item"
+        ))
+        @service          = @authable.services.create!(Factory.attributes_for(:service,
+          :facility_account_id => @facility_account.id,
+          :name => "Example Service"
+        ))
 
-    def upload_file(file_name)
-      ActionDispatch::TestProcess.fixture_file_upload("#{Rails.root}/spec/files/order_imports/#{file_name}", 'text/csv')
+        # price stuff
+        @price_group      = @authable.price_groups.create!(Factory.attributes_for(:price_group))
+        @pg_member        = Factory.create(:user_price_group_member, :user => @guest, :price_group => @price_group)
+        @item_pp=@item.item_price_policies.create!(Factory.attributes_for(:item_price_policy,
+          :price_group_id => @price_group.id
+        ))
+        @service_pp=@service.service_price_policies.create!(Factory.attributes_for(:service_price_policy,
+          :price_group_id => @price_group.id
+        ))
+
+        @guest2 = Factory.create :user, :username => 'guest2'
+        @pg_member        = Factory.create(:user_price_group_member, :user => @guest2, :price_group => @price_group)
+        @account          = Factory.create(:nufs_account,
+          :description => "dummy account",
+          :account_number => '111-2222222-33333333-01',
+          :account_users_attributes => [
+            Hash[:user => @guest, :created_by => @guest, :user_role => 'Owner'],
+            Hash[:user => @guest2, :created_by => @guest, :user_role => 'Purchaser']
+          ]
+        )
+      end
+
+      context "with erroroneous import file" do
+        context "save nothing mode" do
+          it 'should not create orders if first od fails' do
+            maybe_grant_always_sign_in :director
+            @action=:create
+            @method=:post
+            @params.merge!({
+              :order_import => {
+                :upload_file => upload_file('first_od_error.csv'),
+                :fail_on_error => true,
+                :send_receipts => true
+              }
+            })
+
+            do_request
+            import_id = assigns(:order_import).id
+
+            get_real_order_count(import_id).should == 0
+          end
+
+          it 'should not create orders if second od fails' do
+            maybe_grant_always_sign_in :director
+            @action=:create
+            @method=:post
+            @params.merge!({
+              :order_import => {
+                :upload_file => upload_file('second_od_error.csv'),
+                :fail_on_error => true,
+                :send_receipts => true
+              }
+            })
+
+            do_request
+            import_id = assigns(:order_import).id
+
+            get_real_order_count(import_id).should == 0
+          end
+        end
+
+        context "save complete orders (default) mode " do
+          it 'should not create orders if first od fails' do
+            maybe_grant_always_sign_in :director
+            @action=:create
+            @method=:post
+            @params.merge!({
+              :order_import => {
+                :upload_file => upload_file('first_od_error.csv'),
+                :fail_on_error => false,
+                :send_receipts => true
+              }
+            })
+
+            do_request
+            import_id = assigns(:order_import).id
+
+            get_real_order_count(import_id).should == 0
+          end
+
+          it 'should not create orders if second od fails' do
+            maybe_grant_always_sign_in :director
+            @action=:create
+            @method=:post
+            @params.merge!({
+              :order_import => {
+                :upload_file => upload_file('second_od_error.csv'),
+                :fail_on_error => false,
+                :send_receipts => true
+              }
+            })
+
+            do_request
+            import_id = assigns(:order_import).id
+            get_real_order_count(import_id).should == 0
+          end
+        end
+      end
     end
   end
-
 end
