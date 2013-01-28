8cf1a87d962e04cc8d27ffa13e911bc3fd83774b
Jason Hanggi <jason@tablexi.com>
2013-01-28 14:01:19 -0600

    handle dates with more than 4 digits in year

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -2,7 +2,7 @@ module DateHelper
 
   def parse_usa_date(date, extra_date_info=nil)
     begin
-      date_string=(date =~ /\d{1,2}\/\d{1,2}\/\d{4}/ ? Date.strptime(date, '%m/%d/%Y') : date).to_s
+      date_string=(date =~ /\d{1,2}\/\d{1,2}\/\d{4}/ ? Date.strptime($&, '%m/%d/%Y') : date).to_s
       date_string += " #{extra_date_info}" if extra_date_info
 
       Time.zone.parse(date_string)
