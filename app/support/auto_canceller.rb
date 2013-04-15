3ced8e9c0b111cb8d7bef532d20ac73ddd772b72
Jason Hanggi <jason@tablexi.com>
2013-04-15 15:21:50 -0500

    oracle fixes

--- app/support/auto_canceller.rb
+++ app/support/auto_canceller.rb
@@ -15,9 +15,7 @@ class AutoCanceller
   def cancelable_reservations
     @cancelable_reservations ||= Reservation.
       joins(:product, :order_detail).
-      where(build_sql, :now => Time.zone.now,
-        :new_state => OrderStatus.new_os.first.name,
-        :inprocess_state => OrderStatus.inprocess.first.name).
+      where(build_sql, :now => Time.zone.now).
       readonly(false)
   end
 
@@ -34,9 +32,9 @@ private
   def build_sql
     if NUCore::Database.oracle?
       time_condition = <<-CONDITION
-        (EXTRACT(MINUTE FROM (SYS_EXTRACT_UTC(:now) - reserve_start_at)) +
-         EXTRACT(HOUR FROM (SYS_EXTRACT_UTC(:now) - reserve_start_at))*60 +
-         EXTRACT(DAY FROM (SYS_EXTRACT_UTC(:now) - reserve_start_at))*24*60) >= auto_cancel_mins
+        (EXTRACT(MINUTE FROM (:now - reserve_start_at)) +
+         EXTRACT(HOUR FROM (:now - reserve_start_at))*60 +
+         EXTRACT(DAY FROM (:now - reserve_start_at))*24*60) >= auto_cancel_mins
       CONDITION
     else
       time_condition = " TIMESTAMPDIFF(MINUTE, reserve_start_at, :now) >= auto_cancel_mins"
@@ -53,7 +51,7 @@ private
       AND
         auto_cancel_mins > 0
       AND
-        (state = :new_state OR state = :inprocess_state)
+        (state = 'new' OR state = 'inprocess')
       AND
         #{time_condition}
     SQL
