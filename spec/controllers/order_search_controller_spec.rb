6a4b3bdfd3e0a9395b037fe0fee6be282dd4d8ef
Jason Hanggi <jason@tablexi.com>
2013-04-24 19:17:47 -0500

    account admins and owners should be able to see their order details
    part of task #52597

--- spec/controllers/order_search_controller_spec.rb
+++ spec/controllers/order_search_controller_spec.rb
@@ -1,12 +1,28 @@
 require 'spec_helper'
 require 'controller_spec_helper'
 
+def it_should_find_the_order(desc = '')
+  it 'should find the order ' + desc do
+    get :index, :search => order.id.to_s
+    assigns(:order_details).should == [order_detail]
+  end
+end
+
+def it_should_not_find_the_order(desc = '')
+  it 'should not find the order ' + desc do
+    get :index, :search => order.id.to_s
+    assigns(:order_details).should be_empty
+  end
+end
+
 describe OrderSearchController do
   before(:all) { create_users }
   let!(:product) { FactoryGirl.create(:setup_item) }
   let!(:order) { FactoryGirl.create(:purchased_order, :product => product) }
   let!(:order_detail) { order.order_details.first }
   let!(:facility) { order.facility }
+
+
 
   describe 'index' do
     describe 'permissions' do
@@ -23,16 +39,20 @@ describe OrderSearchController do
         sign_in @guest
       end
 
-      it 'should return the order if it is assigned to the user' do
-        order.update_attributes(:user => @guest)
-        get :index, :search => order.id.to_s
-        assigns(:order_details).should == [order_detail]
+      context 'when it is purchased for the user' do
+        before :each do
+          order.update_attributes(:user => @guest)
+        end
+
+        it_should_find_the_order
       end
 
-      it 'should not return the order if it is assigned to another user' do
-        order.update_attributes(:user => @admin)
-        get :index, :search => order.id.to_s
-        assigns(:order_details).should be_empty
+      context 'when it is purchased for another user' do
+        before :each do
+          order.update_attributes(:user => @admin)
+        end
+
+        it_should_not_find_the_order
       end
     end
 
@@ -42,10 +62,7 @@ describe OrderSearchController do
         sign_in @staff
       end
 
-      it 'should return the order even if it is under a different user' do
-        get :index, :search => order.id.to_s
-        assigns(:order_details).should == [order_detail]
-      end
+      it_should_find_the_order 'even if it is under a different user'
     end
 
     context 'when signed in as facility admin for a different facility' do
@@ -55,9 +72,43 @@ describe OrderSearchController do
         sign_in @staff
       end
 
-      it 'should not return the order' do
-        get :index, :search => order.id.to_s
-        assigns(:order_details).should be_empty
+      it_should_not_find_the_order
+    end
+
+    context 'when signed in as a billing manager' do
+      before :each do
+        sign_in @billing_admin
+      end
+
+      it_should_find_the_order
+    end
+
+    describe 'account roles' do
+      context 'when signed in as a business admin' do
+        before :each do
+          sign_in @staff
+          AccountUser.grant(@staff, AccountUser::ACCOUNT_ADMINISTRATOR, order_detail.account, @admin)
+        end
+
+        it_should_find_the_order
+      end
+
+      context 'when signed in as a purchaser' do
+        before :each do
+          sign_in @staff
+          AccountUser.grant(@staff, AccountUser::ACCOUNT_PURCHASER, order_detail.account, @admin)
+        end
+
+        it_should_not_find_the_order
+      end
+
+      context 'when signed in as an account owner' do
+        before :each do
+          sign_in @staff
+          order_detail.account.add_or_update_member(@staff,  AccountUser::ACCOUNT_OWNER, @admin)
+        end
+
+        it_should_find_the_order
       end
     end
 
@@ -73,10 +124,7 @@ describe OrderSearchController do
         assigns(:order_details).should be_empty
       end
 
-      it 'should return an order with the id' do
-        get :index, :search => order.id.to_s
-        assigns(:order_details).should =~ [order_detail]
-      end
+      it_should_find_the_order
 
       it 'should return the order detail with the id' do
         get :index, :search => order_detail.id.to_s
