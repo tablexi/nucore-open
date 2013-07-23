445481cd3fadfa83f3c8a21e840042fc291c71ee
Jason Hanggi <jason@tablexi.com>
2013-07-23 11:19:04 -0500

    clean up date helper from code climate feedback

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -23,14 +23,6 @@ module DateHelper
     date.strftime("%A, %B %e, %Y")
   end
 
-  def human_date_extra_info(date)
-    result = ""
-    result << "Today, " if date.beginning_of_day == Time.zone.now.beginning_of_day
-    result << "Tomorrow, " if date.beginning_of_day == (Time.zone.now.beginning_of_day + 1.day)
-    result << "Yesterday, " if date.beginning_of_day == (Time.zone.now.beginning_of_day - 1.day)
-    result << human_date_with_day(date)
-  end
-
   def human_datetime(dt, args = {})
     return nil if dt.nil?
     begin
@@ -53,13 +45,10 @@ module DateHelper
     end
   end
 
-  #TODO Replace calls to this with select_time(default_time, :ignore_date => true, :prefix => field, :ampm => true)
-  # once we've migrated to Rails 3.1.
-  # 3.0 doesn't support the :ampm option
   def time_select_tag(field, default_time = Time.zone.now)
     output = ""
-    output << select_tag("#{field}[hour]", options_for_select((1..12).map {|x| [x,x]}, default_time.strftime('%I').to_i))
-    output << select_tag("#{field}[minute]", options_for_select((0..59).map{|d| [sprintf('%02d', d),d]}, default_time.min))
+    output << select_tag("#{field}[hour]", options_for_select(hour_options, default_time.strftime('%I').to_i))
+    output << select_tag("#{field}[minute]", options_for_select(minute_options, default_time.min))
     output << select_tag("#{field}[ampm]", options_for_select(['AM', 'PM'], default_time.strftime('%p')))
     output.html_safe
   end
@@ -67,14 +56,14 @@ module DateHelper
   # This is to DRY up many of the legacy places where hour/min/meridian is used in forms
   def time_select(f, field)
     output =  f.select(:"#{field}_hour", (1..12).to_a)
-    output << f.select(:"#{field}_min", (0..59).step(5).map{|d| [sprintf('%02d', d),d]})
+    output << f.select(:"#{field}_min", minute_options)
     output << f.select(:"#{field}_meridian", ['AM', 'PM'])
     content_tag :div, output.html_safe, :class => 'time-select'
   end
 
   def time_select24(f, field)
     output =  f.select(:"#{field}_hour", (0..23).to_a)
-    output << f.select(:"#{field}_min", (0..59).step(5).map{|d| [sprintf('%02d', d),d]})
+    output << f.select(:"#{field}_min",minute_options)
     content_tag :div, output, :class => 'time-select'
   end
 
@@ -82,4 +71,13 @@ module DateHelper
     "#{values['hour']}:#{values['minute']} #{values['ampm']}"
   end
 
+  private
+
+  def minute_options(step = 5)
+    (0..59).step(step).map { |d| ['%02d' % d, d] }
+  end
+
+  def hour_options
+    (1..12).map {|x| [x,x]}
+  end
 end
