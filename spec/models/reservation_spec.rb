tablexi/nucore-open

commit c1db2aa02606dbed2a9adbf46b053758b68164f9

refactored Reservation#conflicting_reservation into scopes

app/models/reservation.rb
@@ -58,9 +58,33 @@ class Reservation < ActiveRecord::Base
   # Scopes
   #####
   def self.active
-    where("reservations.canceled_at IS NULL AND (orders.state = 'purchased' OR orders.state IS NULL)").
+    not_cancelled.
+    where("(orders.state = 'purchased' OR orders.state IS NULL)").
+    joins_order
+  end
+
+  def self.joins_order
     joins('LEFT JOIN order_details ON order_details.id = reservations.order_detail_id').
     joins('LEFT JOIN orders ON orders.id = order_details.order_id')
+  end
+
+  def self.not_cancelled
+    where(:canceled_at => nil)
+  end
+
+  def self.not_started
+    where(:actual_start_at => nil)
+  end
+
+  def self.not_this_reservation(reservation)
+    # old version
+    # where('reservations.id <> ?', id || 0)
+
+    if reservation.id
+      where('reservations.id <> ?', reservation.id)
+    else
+      scoped
+    end
   end
 
   def self.today
@@ -86,6 +110,19 @@ class Reservation < ActiveRecord::Base
     reservations=find(:all, :conditions => "reservations.canceled_at IS NULL AND (orders.state = 'purchased' OR orders.state IS NULL)", :order => 'reserve_end_at asc', :joins => ['LEFT JOIN order_details ON order_details.id = reservations.order_detail_id', 'LEFT JOIN orders ON orders.id = order_details.order_id'])
     reservations.delete_if{|r| r.reserve_end_at < t}
     reservations
+  end
+
+  def self.overlapping(start_at, end_at)
+    # remove millisecond precision from time
+    tstart_at = Time.zone.parse(start_at.to_s)
+    tend_at   = Time.zone.parse(end_at.to_s)
+    
+    where("((reserve_start_at <= :start AND reserve_end_at >= :end) OR
+          (reserve_start_at >= :start AND reserve_end_at <= :end) OR
+          (reserve_start_at <= :start AND reserve_end_at > :start) OR
+          (reserve_start_at < :end AND reserve_end_at >= :end) OR
+          (reserve_start_at = :start AND reserve_end_at = :end))",
+          :start => tstart_at, :end => tend_at)
   end
 
   # Instance Methods

app/support/reservations/validations.rb
@@ -50,25 +50,17 @@ module Reservations::Validations
   # purchased, admin, or in-cart reservation. Should not check reservations that
   # are unpurchased in other user's carts.
   def conflicting_reservation
-    # remove millisecond precision from time
-    tstart_at = Time.zone.parse(reserve_start_at.to_s)
-    tend_at   = Time.zone.parse(reserve_end_at.to_s)
-    order_id  = order_detail.nil? ? 0 : order_detail.order_id
+    # order_id  = order_detail.nil? ? 0 : order_detail.order_id
+    order_id = order_detail.try(:order_id) || 0
 
     Reservation.
-    joins('LEFT JOIN order_details ON order_details.id = reservations.order_detail_id',
-          'LEFT JOIN orders ON orders.id = order_details.order_id').
-    where("reservations.product_id = ? AND
-          reservations.id <> ? AND
-          reservations.canceled_at IS NULL AND
-          reservations.actual_end_at IS NULL AND
-          (orders.state = 'purchased' OR orders.state IS NULL OR orders.id = ?) AND
-          ((reserve_start_at <= ? AND reserve_end_at >= ?) OR
-          (reserve_start_at >= ? AND reserve_end_at <= ?) OR
-          (reserve_start_at <= ? AND reserve_end_at > ?) OR
-          (reserve_start_at < ? AND reserve_end_at >= ?) OR
-          (reserve_start_at = ? AND reserve_end_at = ?))",
-          product.id, id||0, order_id, tstart_at, tend_at, tstart_at, tend_at, tstart_at, tstart_at, tend_at, tend_at, tstart_at, tend_at).first
+      joins_order.
+      where(:product_id => product.id).
+      not_this_reservation(self).
+      not_cancelled.
+      not_started.
+      where("(orders.state = 'purchased' OR orders.state IS NULL OR orders.id = ?)", order_id).
+      overlapping(reserve_start_at, reserve_end_at).first
   end
 
   def satisfies_minimum_length?

commit 2c0f2a76d9a3ba06a7aebd77cbfb0c3b1501e9b5

reservations could overlap a started, but not ended reservation
Caused by c1db2aa02606dbed2a9adbf46b053758b68164f9

app/models/reservation.rb
@@ -77,6 +77,10 @@ class Reservation < ActiveRecord::Base
     where(:actual_start_at => nil)
   end
 
+  def self.not_ended
+    where(:actual_end_at => nil)
+  end
+
   def self.not_this_reservation(reservation)
     # old version
     # where('reservations.id <> ?', id || 0)

app/support/reservations/validations.rb
@@ -58,7 +58,7 @@ module Reservations::Validations
         where(:product_id => product.schedule.product_ids).
         not_this_reservation(self).
         not_cancelled.
-        not_started.
+        not_ended.
         where("(orders.state = 'purchased' OR orders.state IS NULL OR orders.id = ?)", order_id).
         overlapping(reserve_start_at, reserve_end_at)
 
@@ -90,7 +90,6 @@ module Reservations::Validations
   end
 
   def instrument_is_available_to_reserve? (start_at = self.reserve_start_at, end_at = self.reserve_end_at)
-
     # check for order_detail and order because some old specs don't set an order detail
     # if we're saving as an administrator, we want access to all schedule rules
     if (order_detail and order_detail.order and !@reserved_by_admin)

spec/models/reservation_spec.rb
@@ -338,6 +338,32 @@ describe Reservation do
       conflicting_admin_reservation.should be_valid
     end
 
+    context 'and the reservation has started' do
+      before :each do
+        # Actual start must be in the past
+        reservation.update_attributes! :reserve_start_at => 30.minutes.ago,
+                                       :reserve_end_at   => 30.minutes.from_now,
+                                       :actual_start_at  => 30.minutes.ago
+
+        conflicting_reservation.assign_attributes :reserve_start_at => 30.minutes.ago,
+                                                  :reserve_end_at   => 30.minutes.from_now
+      end
+
+      it 'should be a conflict' do
+        conflicting_reservation.should_not be_valid
+      end
+
+      context 'and ended' do
+        before :each do
+          reservation.update_attributes!(:actual_end_at => 20.minutes.ago)
+        end
+
+        it 'should not conflict' do
+          conflicting_reservation.should be_valid
+        end
+      end
+    end
+
     context 'overlapping scheduling rules' do
       context 'completely within a blacked out time' do
         before :each do