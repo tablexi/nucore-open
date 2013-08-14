2d97d22dd2fb965b325d508f1d122cf012a70baf
Jason Hanggi <jason@tablexi.com>
2013-08-14 13:33:09 -0500

    updated_at where clause was not being added properly

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -17,7 +17,7 @@ class Cart
     count = 0
     total = Cart.abandoned_carts.count
     # limit to 100 for better ability to watch progress
-    while (cart = abandoned_carts(100).where('updated_at < ?', before)).any?
+    while (cart = abandoned_carts(100).where('orders.updated_at < ?', before)).any?
       destroyed = cart.destroy_all
       Rails.logger.info "Removed #{count += destroyed.count} of #{total} carts"
     end
