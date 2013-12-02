1d5c7f1963c7b648f4bcd2b84b51e5d8c2c95512
Chris Stump <chris@tablexi.com>
2013-12-02 17:20:41 -0600

    widen price_policies.usage_* columns; add products.reserve_interval
    Pricing model changes (Task #73569)

---
+++ db/migrate/20131202225104_alter_products_add_reserve_interval.rb
@@ -0,0 +1,5 @@
+class AlterProductsAddReserveInterval < ActiveRecord::Migration
+  def change
+    add_column :products, :reserve_interval, :integer
+  end
+end
---
+++ db/migrate/20131202225331_alter_price_policies_change_precision.rb
@@ -0,0 +1,11 @@
+class AlterPricePoliciesChangePrecision < ActiveRecord::Migration
+  def up
+    change_column :price_policies, :usage_rate, :decimal, precision: 12, scale: 4
+    change_column :price_policies, :usage_subsidy, :decimal, precision: 12, scale: 4
+  end
+
+  def down
+    change_column :price_policies, :usage_rate, :decimal, precision: 10, scale: 2
+    change_column :price_policies, :usage_subsidy, :decimal, precision: 10, scale: 2
+  end
+end
