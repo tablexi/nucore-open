317b5dfbb30ebc2c9274e002faed420071ee0380
Chris Stump <chris@tablexi.com>
2013-12-02 13:07:25 -0600

    remove duplicate block
    Pricing model changes (Task #73569)

--- config/application.rb
+++ config/application.rb
@@ -11,14 +11,6 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
-
-
 module Nucore
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
