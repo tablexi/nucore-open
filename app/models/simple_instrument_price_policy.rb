769640894758ba5f0684a4df193b5477b79bc3a8
Chris Stump <chris@tablexi.com>
2013-12-04 14:48:40 -0600

    finish porting over auxiliary methods from InstrumentPricePolicy
    Pricing model changes (Task #73569)

--- app/models/simple_instrument_price_policy.rb
+++ app/models/simple_instrument_price_policy.rb
@@ -1,17 +1,38 @@
 class SimpleInstrumentPricePolicy < PricePolicy
 
-  validates :usage_rate, numericality: { greater_than_or_equal_to: 0 }
-  validates :minimum_cost, :usage_subsidy, :cancellation_cost, numericality: { allow_nil: true, greater_than_or_equal_to: 0 }
+  validates :usage_rate, :minimum_cost, :usage_subsidy, :cancellation_cost, numericality: { allow_nil: true, greater_than_or_equal_to: 0 }
   validates :reservation_rate, :reservation_subsidy, :overage_rate, :overage_subsidy, inclusion: [ nil ]
   validates :usage_rate, presence: true, unless: :restrict_purchase?
   validate :subsidy_less_than_rate?, unless: :restrict_purchase?
 
+  before_save {|pp| pp.usage_subsidy ||= 0 if pp.usage_rate }
 
-  def estimate_cost_and_subsidy(*args)
+  after_create do |pp|
+    # Make sure we have a default reservation window for this price group and product
+    pgp = PriceGroupProduct.find_by_price_group_id_and_product_id(pp.price_group.id, pp.product.id)
+
+    PriceGroupProduct.create(
+      price_group: pp.price_group,
+      product: pp.product,
+      reservation_window: PriceGroupProduct::DEFAULT_RESERVATION_WINDOW
+    ) unless pgp
   end
 
 
-  def calculate_cost_and_subsidy(*args)
+  def self.next_dates(product)
+    ipps = product.price_policies
+                  .where(dateize('start_date', ' > ?'), Time.zone.now)
+                  .order(:start_date)
+                  .select('DISTINCT(start_date) AS start_date')
+                  .all
+
+    ipps.collect{|ipp| ipp.start_date.to_date}.uniq
+  end
+
+
+  def reservation_window
+    pgp = PriceGroupProduct.find_by_price_group_id_and_product_id(price_group.id, product.id)
+    pgp.try(:reservation_window) || 0
   end
 
 
@@ -20,4 +41,34 @@ class SimpleInstrumentPricePolicy < PricePolicy
       errors.add("usage_subsidy", "cannot be greater than the Usage cost")
     end
   end
+
+
+  def has_subsidy?
+    usage_subsidy && usage_subsidy > 0
+  end
+
+
+  def free?
+    usage_rate.to_f == 0
+  end
+
+
+  def estimate_cost_and_subsidy_from_order_detail(order_detail)
+    reservation = order_detail.reservation
+    estimate_cost_and_subsidy reservation.reserve_start_at, reservation.reserve_end_at if reservation
+  end
+
+
+  def calculate_cost_and_subsidy_from_order_detail(order_detail)
+    calculate_cost_and_subsidy order_detail.reservation
+  end
+
+
+  def estimate_cost_and_subsidy(start_at, end_at)
+  end
+
+
+  def calculate_cost_and_subsidy(reservation)
+  end
+
 end
