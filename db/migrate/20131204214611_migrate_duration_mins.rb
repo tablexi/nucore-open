7b2bad2b38ad45f9de07136cb2ee5fe9c568aeb6
Chris Stump <chris@tablexi.com>
2013-12-04 18:05:28 -0600

    migrate duration_mins field from schedule_rules to products (instruments)
    Pricing model changes (Task #73569)

---
+++ db/migrate/20131204214611_migrate_duration_mins.rb
@@ -0,0 +1,27 @@
+class MigrateDurationMins < ActiveRecord::Migration
+  def up
+    instrument_duration_mins.each do |instrument_id, duration_mins|
+      Instrument.find(instrument_id).update_attribute :reserve_interval, duration_mins
+    end
+  end
+
+  def down
+    # because we can't restore the original values of products.reserve_interval
+    raise ActiveRecord::IrreversibleMigration
+  end
+
+  def instrument_duration_mins
+    idm = {}
+    rs = ScheduleRule.select('instrument_id,duration_mins').group :instrument_id, :duration_mins
+
+    rs.each do |r|
+      if idm.has_key?(r.instrument_id) && idm[r.instrument_id] != r.duration_mins
+        raise "ScheduleRule with instrument id #{r.instrument_id} has conflicting duration_mins (#{r.duration_mins}, #{idm[r.instrument_id]})!\n"
+      end
+
+      idm[r.instrument_id] = r.duration_mins
+    end
+
+    idm
+  end
+end
