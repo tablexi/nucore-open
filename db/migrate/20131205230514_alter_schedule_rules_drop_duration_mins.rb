b12ed73452e9a4e28b7a7bfc246cb102fc2a96b5
Chris Stump <chris@tablexi.com>
2013-12-05 17:09:52 -0600

    remove schedule_rules.duration_mins
    Pricing model changes (Task #73569)

---
+++ db/migrate/20131205230514_alter_schedule_rules_drop_duration_mins.rb
@@ -0,0 +1,9 @@
+class AlterScheduleRulesDropDurationMins < ActiveRecord::Migration
+  def up
+    remove_column :schedule_rules, :duration_mins
+  end
+
+  def down
+    add_column :schedule_rules, :duration_mins, :integer, null: false
+  end
+end
