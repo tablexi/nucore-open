72476db165ca432485740640ace9ae155ee63a0c
Chris Stump <chris@tablexi.com>
2013-12-06 15:32:10 -0600

    instrument setup should set reserve_interval
    Pricing model changes (Task #73569)

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -76,6 +76,8 @@ FactoryGirl.define do
   end
 
   factory :setup_instrument, :class => Instrument, :parent => :setup_product do
+    reserve_interval 5
+
     schedule { FactoryGirl.create(:schedule, :facility => facility) }
     after(:create) do |product|
       product.instrument_price_policies.create(FactoryGirl.attributes_for(:instrument_price_policy, :price_group => product.facility.price_groups.last, :usage_rate => 1))
