d9eb1eca84275985ee029d046477e24fc4c625f2
Chris Stump <chris@tablexi.com>
2013-12-19 15:43:00 -0600

    update for new PP

--- app/controllers/instrument_price_policies_controller.rb
+++ app/controllers/instrument_price_policies_controller.rb
@@ -1,11 +1,2 @@
 class InstrumentPricePoliciesController < PricePoliciesController
-
-  # GET /price_policies/new
-  def new
-    super
-    @price_policies.each do |pp|
-      pp.usage_mins = 15
-    end
-  end
-
 end
