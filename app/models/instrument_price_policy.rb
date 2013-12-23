235f84e302e4a069268da1c2e3ff6a19081ae9ae
Chris Stump <chris@tablexi.com>
2013-12-23 12:04:51 -0600

    enable new estimation calculation; always save usage mins as 60; ensure other *_mins attributes are nil
    Pricing model changes (Task #73569)

--- app/models/instrument_price_policy.rb
+++ app/models/instrument_price_policy.rb
@@ -1,5 +1,4 @@
 class InstrumentPricePolicy < PricePolicy
-  include InstrumentPricePolicyCalculations
 
   CHARGE_FOR = {
     usage: 1,
@@ -8,12 +7,20 @@ class InstrumentPricePolicy < PricePolicy
   }
 
   validates :usage_rate, :minimum_cost, :usage_subsidy, :cancellation_cost, numericality: { allow_nil: true, greater_than_or_equal_to: 0 }
-  validates :reservation_rate, :reservation_subsidy, :overage_rate, :overage_subsidy, inclusion: [ nil ]
   validates :usage_rate, presence: true, unless: :restrict_purchase?
   validates :charge_for, inclusion: CHARGE_FOR.values
+  validates :reservation_rate,
+            :reservation_subsidy,
+            :overage_rate,
+            :overage_subsidy,
+            :reservation_mins,
+            :overage_mins,
+            inclusion: [ nil ]
+
   validate :subsidy_less_than_rate?, unless: :restrict_purchase?
 
-  before_save :set_subsidy
+
+  before_save :set_subsidy, :set_usage_mins
 
   after_create do |pp|
     # Make sure we have a default reservation window for this price group and product
@@ -50,46 +57,76 @@ class InstrumentPricePolicy < PricePolicy
   end
 
 
-  #def estimate_cost_and_subsidy(start_at, end_at)
-  #  return nil if restrict_purchase? || end_at <= start_at
-  #
-  #  costs = {}
-  #
-  #  ## the instrument is free to use
-  #  if free?
-  #    costs[:cost]    = minimum_cost || 0
-  #    costs[:subsidy] = 0
-  #    return costs
-  #  end
-  #
-  #  duration = (end_at - start_at)/60
-  #  discount = 0
-  #
-  #  product.schedule_rules.each do |sr|
-  #    discount += sr.percent_overlap(start_at, end_at) * sr.discount_percent.to_f
-  #  end
-  #
-  #  discount = 1 - discount/100
-  #
-  #  costs[:cost] = ((duration/reservation_mins).ceil * reservation_rate.to_f + (duration/usage_mins).ceil * usage_rate.to_f) * discount
-  #  costs[:subsidy] = ((duration/reservation_mins).ceil * reservation_subsidy.to_f + (duration/usage_mins).ceil * usage_subsidy.to_f) * discount
-  #
-  #  if (costs[:cost] - costs[:subsidy]) < minimum_cost.to_f
-  #    costs[:cost]    = minimum_cost
-  #    costs[:subsidy] = 0
-  #  end
-  #
-  #  costs
-  #end
-  #
-  #
-  #def calculate_cost_and_subsidy(reservation)
-  #end
+  def estimate_cost_and_subsidy_from_order_detail(order_detail)
+    reservation = order_detail.reservation
+    estimate_cost_and_subsidy reservation.reserve_start_at, reservation.reserve_end_at if reservation
+  end
+
+
+  def estimate_cost_and_subsidy(start_at, end_at)
+    return nil if restrict_purchase? || end_at <= start_at
+
+    costs = {}
+
+    if free?
+      costs[:cost] = minimum_cost || 0
+      costs[:subsidy] = 0
+      return costs
+    end
+
+    duration = (end_at - start_at) / 60
+    discount = calculate_discount start_at, end_at
+
+    costs[:cost] = calculate_cost duration, discount
+    costs[:subsidy] = calculate_subsidy duration, discount
+
+    if (costs[:cost] - costs[:subsidy]) < minimum_cost.to_f
+      costs[:cost] = minimum_cost
+      costs[:subsidy] = 0
+    end
+
+    costs
+  end
+
+
+  def calculate_cost(duration, discount)
+    ((duration / usage_mins).ceil * usage_rate.to_f) * discount
+  end
+
+
+  def calculate_subsidy(duration, discount)
+    ((duration / usage_mins).ceil * usage_subsidy.to_f) * discount
+  end
+
+
+  def calculate_discount(start_at, end_at)
+    discount = 0
+
+    product.schedule_rules.each do |sr|
+      discount += sr.discount_for(start_at, end_at)
+    end
+
+    1 - discount / 100
+  end
+
+
+  def calculate_cost_and_subsidy_from_order_detail(order_detail)
+    calculate_cost_and_subsidy order_detail.reservation
+  end
+
+
+  def calculate_cost_and_subsidy(reservation)
+  end
 
 
   private
 
   def set_subsidy
     self.usage_subsidy ||= 0 if usage_rate
+  end
+
+
+  def set_usage_mins
+    self.usage_mins = 60
   end
 end
--- spec/models/instrument_price_policy_spec.rb
+++ spec/models/instrument_price_policy_spec.rb
@@ -9,7 +9,7 @@ describe InstrumentPricePolicy do
     it { should allow_value(nil).for attr.to_sym }
   end
 
-  %w(reservation_rate reservation_subsidy overage_rate overage_subsidy).each do |attr|
+  %w(reservation_rate reservation_subsidy overage_rate overage_subsidy reservation_mins overage_mins).each do |attr|
     it { should_not allow_value(5.0).for attr.to_sym }
     it { should allow_value(nil).for attr.to_sym }
   end
@@ -72,6 +72,12 @@ describe InstrumentPricePolicy do
       policy.usage_subsidy = nil
       expect(policy.save).to be_true
       expect(policy.reload.usage_subsidy).to be_nil
+    end
+
+    it 'always sets usage_mins to 60' do
+      policy.usage_mins = 0
+      expect(policy.save).to be_true
+      expect(policy.usage_mins).to eq 60
     end
   end
 
@@ -177,5 +183,87 @@ describe InstrumentPricePolicy do
     fake_order_detail = double 'OrderDetail', reservation: double('Reservation')
     expect(policy).to receive(:calculate_cost_and_subsidy).with fake_order_detail.reservation
     policy.calculate_cost_and_subsidy_from_order_detail fake_order_detail
+  end
+
+
+  describe 'estimating cost and subsidy' do
+    let(:start_at) { Time.zone.now + 1.hour }
+    let(:end_at) { start_at + 1.hour }
+    let(:duration) { (end_at - start_at) / 60 }
+
+    it 'returns nil if purchase is restricted' do
+      policy.stub(:restrict_purchase?).and_return true
+      expect(policy.estimate_cost_and_subsidy(start_at, end_at)).to be_nil
+    end
+
+    it 'returns nil if purchase if end_at is before start_at' do
+      expect(policy.estimate_cost_and_subsidy(end_at, start_at)).to be_nil
+    end
+
+    it 'returns nil if purchase if end_at equals start_at' do
+      expect(policy.estimate_cost_and_subsidy(start_at, start_at)).to be_nil
+    end
+
+    it 'gives a zero cost and subsidy if instrument is free to use and there is no minimum cost' do
+      policy.stub(:free?).and_return true
+      policy.stub(:minimum_cost).and_return nil
+      costs = policy.estimate_cost_and_subsidy start_at, end_at
+      expect(costs[:cost]).to eq 0
+      expect(costs[:subsidy]).to eq 0
+    end
+
+    it 'gives minimum cost and zero subsidy if instrument is free to use and there is a minimum cost' do
+      policy.stub(:free?).and_return true
+      policy.stub(:minimum_cost).and_return 10
+      costs = policy.estimate_cost_and_subsidy start_at, end_at
+      expect(costs[:cost]).to eq 10
+      expect(costs[:subsidy]).to eq 0
+    end
+
+    it 'calculates cost based on the given duration and discount' do
+      policy.usage_rate = 5
+      expect(policy.usage_mins).to eq 60
+      expect(policy.calculate_cost 120, 0.15).to eq 1.5
+    end
+
+    it 'calculates subsidy based on the given duration and discount' do
+      policy.usage_subsidy = 5
+      expect(policy.usage_mins).to eq 60
+      expect(policy.calculate_subsidy 120, 0.15).to eq 1.5
+    end
+
+    it 'calculates a discount based on the given time and configured schedule rules' do
+      policy.product.schedule_rules.first.update_attributes! start_hour: 0, end_hour: 24, on_sat: false, on_sun: false
+      policy.product.schedule_rules << create(:weekend_schedule_rule, instrument: policy.product, discount_percent: 0.25, start_hour: 0, end_hour: 24)
+      start_at = Time.zone.parse '2013-12-13 09:00'
+      end_at = Time.zone.parse '2013-12-14 12:00'
+      expect(policy.calculate_discount(start_at, end_at).round 3).to eq 0.999
+    end
+
+    it 'gives the calculated cost and subsidy' do
+      discount = 0.999
+      expect(policy).to receive(:calculate_discount).with(start_at, end_at).and_return discount
+      cost = 5.00
+      expect(policy).to receive(:calculate_cost).with(duration, discount).and_return cost
+      subsidy = 1.00
+      expect(policy).to receive(:calculate_subsidy).with(duration, discount).and_return subsidy
+      results = policy.estimate_cost_and_subsidy start_at, end_at
+      expect(results[:cost]).to eq cost
+      expect(results[:subsidy]).to eq subsidy
+    end
+
+    it 'gives the minimum cost and zero subsidy if below minimum cost' do
+      discount = 0
+      expect(policy).to receive(:calculate_discount).with(start_at, end_at).and_return discount
+      cost = 5.00
+      expect(policy).to receive(:calculate_cost).with(duration, discount).and_return cost
+      subsidy = 6.00
+      expect(policy).to receive(:calculate_subsidy).with(duration, discount).and_return subsidy
+      min_cost = 3.00
+      policy.stub(:minimum_cost).and_return min_cost
+      results = policy.estimate_cost_and_subsidy start_at, end_at
+      expect(results[:cost]).to eq min_cost
+      expect(results[:subsidy]).to eq 0
+    end
   end
 end
