c19204f22cc4ba1b22fddc12dc975e117dced851
Chris Stump <chris@tablexi.com>
2014-01-02 18:39:50 -0600

    introduce params modifier usage so we can customize params before request
    Pricing model changes (Task #73569)

--- spec/price_policies_controller_shared_examples.rb
+++ spec/price_policies_controller_shared_examples.rb
@@ -1,6 +1,7 @@
-shared_examples_for PricePoliciesController do |product_type|
+shared_examples_for PricePoliciesController do |product_type, params_modifier = nil|
   before(:each) do
     @product_type = product_type
+    @params_modifier = params_modifier
     @authable         = FactoryGirl.create(:facility)
     @facility_account = @authable.facility_accounts.create(FactoryGirl.attributes_for(:facility_account))
 
@@ -201,6 +202,8 @@ shared_examples_for PricePoliciesController do |product_type|
           :start_date => @start_date.to_s,
           :expire_date => @expire_date.to_s
         })
+
+        @params_modifier.before_create @params if @params_modifier.try :respond_to?, :before_create
       end
 
       it_should_allow_managers_only(:redirect) {}
@@ -266,6 +269,8 @@ shared_examples_for PricePoliciesController do |product_type|
         @params.merge!(:id => @price_policy.start_date.to_s,
           :start_date => @price_policy.start_date.to_s,
           :expire_date => @price_policy.expire_date.to_s)
+
+        @params_modifier.before_update @params if @params_modifier.try :respond_to?, :before_update
       end
 
       it_should_allow_managers_only(:redirect) {}
