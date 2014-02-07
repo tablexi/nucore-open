4fd3ac24ab8ec78f9bde090190d8ddd945147775
Chris Stump <chris@tablexi.com>
2014-02-07 14:42:32 -0600

    show rate per minute on ipp show
    issue #79127

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,18 +26,20 @@ module ApplicationHelper
   end
 
   def human_rate_calculation(rate, subsidy)
+    display = ""
+
     # handle nil input
     rate    = -1 if rate.nil?
     subsidy = 0 if subsidy.nil?
 
     # render appropriate string
     if subsidy > 0
-      "#{number_to_currency rate}<br />-#{number_to_currency subsidy}<br /> =<b>#{number_to_currency rate-subsidy}</b>".html_safe
+      display = subsidized_rate_display rate, subsidy
     elsif rate > -1
-      number_to_currency rate
-    else
-      ""
+      display = non_subsidized_rate_display rate
     end
+
+    display.html_safe
   end
 
   def sortable (column, title = nil)
@@ -77,4 +79,23 @@ module ApplicationHelper
   end
 
 
+  private
+
+  def non_subsidized_rate_display(rate)
+    <<-FORMAT
+      #{number_to_currency rate}
+      <p class="per-minute-show">#{number_to_currency rate / 60, precision: 4} / minute</p>
+    FORMAT
+  end
+
+  def subsidized_rate_display(rate, subsidy)
+    <<-FORMAT
+      #{number_to_currency rate}
+      <br/>
+      -#{number_to_currency subsidy}
+      <br/>
+      =<b>#{number_to_currency rate-subsidy}</b>
+      <p class="per-minute-show">#{number_to_currency (rate-subsidy) / 60, precision: 4} / minute</p>
+    FORMAT
+  end
 end
