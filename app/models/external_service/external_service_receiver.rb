208d5c6697c623eab610c060598dad120b29b158
Chris Stump <chris@tablexi.com>
2014-03-25 11:58:31 -0500

    create dynamic accessors for the response data
    http://pm.tablexi.com/issues/80421

--- app/models/external_service/external_service_receiver.rb
+++ app/models/external_service/external_service_receiver.rb
@@ -7,4 +7,24 @@ class ExternalServiceReceiver < ActiveRecord::Base
   belongs_to :receiver, :polymorphic => true
 
   validates_presence_of :external_service_id, :receiver_id, :response_data
+
+
+  def respond_to?(symbol, include_private=false)
+    super || parsed_response_data.has_key?(symbol)
+  end
+
+
+  private
+
+  def method_missing(symbol, *args)
+    parsed = parsed_response_data
+    return parsed[symbol] if parsed.has_key? symbol
+    super
+  end
+
+
+  def parsed_response_data
+    JSON.parse(response_data).symbolize_keys
+  end
+
 end
