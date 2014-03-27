f49beb6110e35da7a8597915768fcc12c71b8903
Chris Stump <chris@tablexi.com>
2014-03-27 13:37:02 -0500

    cleanup autoload path additions
    http://pm.tablexi.com/issues/80421

--- config/application.rb
+++ config/application.rb
@@ -18,11 +18,9 @@ module Nucore
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    [
-      [ "#{config.root}/config" ],
-      Dir["#{config.root}/lib/**/"],
-      Dir["#{config.root}/app/models/**/"]
-    ].each {|paths| config.autoload_paths += paths }
+    config.autoload_paths += %W(#{config.root}/config)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.autoload_paths += Dir["#{config.root}/app/models/**/"]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
