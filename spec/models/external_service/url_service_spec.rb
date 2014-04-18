78cdceb2b92ba715578cb338cedff45fd67d98a5
Chris Stump <chris@tablexi.com>
2014-04-17 19:50:57 -0500

    fixes CI; handles optional request coming into UrlService#rnew_url

--- spec/models/external_service/url_service_spec.rb
+++ spec/models/external_service/url_service_spec.rb
@@ -7,25 +7,34 @@ describe UrlService do
   let(:service) { create :setup_service }
   let(:order) { create :setup_order, product: service }
   let(:order_detail) { create :order_detail, order: order, product: service }
+
+  let(:host_params) do {
+      host: 'localhost.test',
+      port: 8080,
+      protocol: 'https'
+  } end
+
   let :url_components do {
-      :facility_id => order_detail.product.facility.url_name,
-      :service_id => order_detail.product.url_name,
-      :external_service_id => url_service.id,
-      :receiver_id => order_detail.id
+    :facility_id => order_detail.product.facility.url_name,
+    :service_id => order_detail.product.url_name,
+    :external_service_id => url_service.id,
+    :receiver_id => order_detail.id
   } end
 
 
   it 'uses the default host, port, and protocol if there is no request' do
     url_service.default_url_options[:host] = 'localhost'
     complete_survey_url = url_service.complete_survey_url url_components
-    url = "#{url_service.location}?#{{ success_url: complete_survey_url}.to_query}"
+    url = "#{url_service.location}?#{{ success_url: complete_survey_url, referer: nil}.to_query}"
     expect(url_service.new_url(order_detail)).to eq url
   end
 
   it 'uses the host, port, and protocol of the request' do
-    host_params = { host: 'localhost.test', port: 8080, protocol: 'https' }
     complete_survey_url = url_service.complete_survey_url url_components.merge(host_params)
-    url = "#{url_service.location}?#{{ success_url: complete_survey_url}.to_query}"
+    host_params[:host_with_port] = "#{host_params[:host]}:#{host_params[:port]}"
+    host_params[:fullpath] = '/path/to/somewhere'
+    referer_url = "#{host_params[:protocol]}#{host_params[:host_with_port]}#{host_params[:fullpath]}"
+    url = "#{url_service.location}?#{{ success_url: complete_survey_url, referer: referer_url}.to_query}"
     expect(url_service.new_url(order_detail, double(host_params))).to eq url
   end
 
