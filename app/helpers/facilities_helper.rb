a2fc6e000b9cbcf07c2e743b73516dd9f8f4f3c2
Chris Stump <chris@tablexi.com>
2014-04-24 15:00:31 -0500

    Link to daily view from products list
    http://pm.tablexi.com/issues/74607

--- app/helpers/facilities_helper.rb
+++ app/helpers/facilities_helper.rb
@@ -6,4 +6,10 @@ module FacilitiesHelper
   	  facility_orders_path(facility)
   	end
   end
+
+  def product_list_title(products, extra)
+    title = products.first.class.model_name.human.pluralize
+    title += " (#{extra})" if extra
+    title.html_safe
+  end
 end
