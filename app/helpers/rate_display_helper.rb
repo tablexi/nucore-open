cb85602e2abba3b44d0e828d258b1d93b256e49f
Dan Rench <dan.rench@tablexi.com>
2014-05-29 13:17:30 -0500

    Refactor rate display helpers

--- app/helpers/rate_display_helper.rb
+++ app/helpers/rate_display_helper.rb
@@ -4,61 +4,61 @@ module RateDisplayHelper
     cost = 0 if cost.nil?
     subsidy = 0 if subsidy.nil?
 
-    display = if cost > 0 && subsidy > 0
+    human_cost_calculation_display(cost, subsidy).html_safe
+  end
+
+  def human_cost_calculation_display(cost, subsidy)
+    if cost > 0 && subsidy > 0
       subsidized_cost_display cost, subsidy
     else
       number_to_currency cost
     end
-
-    display.html_safe
   end
 
   def subsidized_cost_display(cost, subsidy)
     subsidized_cost = cost * subsidy
-    <<-FORMAT
-      #{number_to_currency cost}
-      <br/>
-      -#{number_to_currency subsidized_cost}
-      <br/>
-      =<b>#{number_to_currency cost-subsidized_cost}</b>
-    FORMAT
+    "#{cost_display cost, subsidized_cost}=#{bold_number_as_currency cost - subsidized_cost}"
   end
 
   def human_rate_calculation(rate, subsidy)
-    display = ''
-
     # handle nil input
     rate    = -1 if rate.nil?
     subsidy = 0 if subsidy.nil?
 
-    # render appropriate string
-    if subsidy > 0
-      display = subsidized_rate_display rate, subsidy
-    elsif rate > -1
-      display = non_subsidized_rate_display rate
+    human_rate_calculation_display(rate, subsidy).html_safe
+  end
+
+  def human_rate_calculation_display(rate, subsidy)
+    case
+    when subsidy > 0
+      subsidized_rate_display rate, subsidy
+    when rate > -1
+      "#{number_to_currency rate}#{rate_per_minute_display rate}"
+    else
+      ''
     end
-
-    display.html_safe
   end
-
-
-  def non_subsidized_rate_display(rate)
-   <<-FORMAT
-     #{number_to_currency rate}
-     <p class="per-minute-show">#{number_to_currency rate / 60, precision: 4} / minute</p>
-   FORMAT
-  end
-
 
   def subsidized_rate_display(rate, subsidy)
-   <<-FORMAT
-     #{number_to_currency rate}
-     <br/>
-     -#{number_to_currency subsidy}
-     <br/>
-     =<b>#{number_to_currency rate-subsidy}</b>
-     <p class="per-minute-show">#{number_to_currency (rate-subsidy) / 60, precision: 4} / minute</p>
-   FORMAT
+    "#{cost_display rate, subsidy} =#{bold_number_as_currency rate - subsidy}" \
+    " #{rate_per_minute_display rate - subsidy}"
+  end
+
+  def cost_display(cost, subsidy)
+    <<-FORMAT
+      #{number_to_currency cost}
+      <br/>
+      -#{number_to_currency subsidy}
+      <br/>
+    FORMAT
+  end
+
+  def rate_per_minute_display(rate)
+    %(<p class="per-minute-show">#{number_to_currency rate / 60, precision: 4} / minute</p>)
+  end
+
+  def bold_number_as_currency(number)
+     "<b>#{number_to_currency number}</b>"
   end
 
 end
