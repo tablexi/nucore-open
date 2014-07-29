4b5ded189469dd370ca96b18c184f97d1d80cbc4
Dan Rench <dan.rench@tablexi.com>
2014-07-29 12:05:49 -0500

    Add form model for bulk reassignment

---
+++ app/models/chart_string_reassignment_form.rb
@@ -0,0 +1,23 @@
+class ChartStringReassignmentForm
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming
+
+  attr_reader :account_id, :available_accounts, :order_details
+
+  def initialize(order_details)
+    @order_details = order_details
+    @available_accounts = available_accounts
+  end
+
+  def available_accounts?
+    @available_accounts.any?
+  end
+
+  def available_accounts
+    @order_details.map(&:user).map(&:accounts).flatten.uniq.sort_by(&:description)
+  end
+
+  def persisted?
+    false
+  end
+end
