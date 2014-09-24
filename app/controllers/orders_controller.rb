08031c5ede9069b48cb8e8e8af46639622cb4d24
Matthew Lineen <matthew@lineen.com>
2014-09-24 18:30:12 -0500

    [#80431] Allow removal of order note
    while ordering on behalf of a user while editing a cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -351,7 +351,11 @@ class OrdersController < ApplicationController
 
     order_detail_updates = {}
     params.each do |key, value|
-      if /^(quantity|note)(\d+)$/ =~ key and value.present?
+      if /\A(quantity)(\d+)\z/ =~ key && value.present?
+        order_detail_updates[$2.to_i] ||= Hash.new
+        order_detail_updates[$2.to_i][$1.to_sym] = value
+      end
+      if /\A(note)(\d+)\z/ =~ key
         order_detail_updates[$2.to_i] ||= Hash.new
         order_detail_updates[$2.to_i][$1.to_sym] = value
       end
