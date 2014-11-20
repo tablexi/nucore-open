77b60c3a77cdb58ebedf6f56d92f3b8b64782464
Peter <peter@tablexi.com>
2014-11-20 14:20:45 -0600

    Add indexes to all foreign keys

---
+++ db/migrate/20141104233110_add_indexes_to_order_details.rb
@@ -0,0 +1,11 @@
+class AddIndexesToOrderDetails < ActiveRecord::Migration
+  def change
+    add_index :order_details, :assigned_user_id
+    add_index :order_details, :order_status_id
+    add_index :order_details, :response_set_id
+    add_index :order_details, :group_id
+    add_index :order_details, :statement_id
+    add_index :order_details, :journal_id
+    add_index :order_details, :state
+  end
+end
