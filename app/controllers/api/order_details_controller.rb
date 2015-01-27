bb45fe6bd76e6d85975807aa4eed0e5dd968389f
Dan Rench <dan.rench@tablexi.com>
2015-01-27 15:41:23 -0600

    [#88161] Anticipate missing data in API responses
    This should more gracefully handle cases where an order_detail is has
    no associated account, or where the order_detail does not exist.

--- app/controllers/api/order_details_controller.rb
+++ app/controllers/api/order_details_controller.rb
@@ -4,21 +4,36 @@ class Api::OrderDetailsController < ApplicationController
   http_basic_authenticate_with name: Settings.api.basic_auth_name, password: Settings.api.basic_auth_password
 
   def show
-    od = OrderDetail.find(params[:id])
-    account = od.account
-    owner = account.owner.user
-    ordered_for = od.order.user
-
-    render json: {
-      account: {
-        id: account.id,
-        owner: user_to_hash(owner),
-      },
-      ordered_for: user_to_hash(ordered_for),
-    }
+    render json: response_to_hash
   end
 
   private
+
+  def account
+    @account = order_detail.try(:account) unless defined?(@account)
+    @account
+  end
+
+  def account_to_hash
+    if account.present?
+      { id: account.id, owner: user_to_hash(account.owner.user) }
+    end
+  end
+
+  def ordered_for_to_hash
+    if order_detail.present?
+      user_to_hash(order_detail.order.user)
+    end
+  end
+
+  def response_to_hash
+    { account: account_to_hash, ordered_for: ordered_for_to_hash }
+  end
+
+  def order_detail
+    @order_detail = OrderDetail.find_by_id(params[:id]) unless defined?(@order_detail)
+    @order_detail
+  end
 
   def user_to_hash(user)
     {
