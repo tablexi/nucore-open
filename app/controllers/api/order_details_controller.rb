d457523ea1bfa196edb6df1e6caa3af14829abfb
Dan Rench <dan.rench@tablexi.com>
2015-01-29 15:10:40 -0600

    [#88161] Extract OrderDetail API response building

--- app/controllers/api/order_details_controller.rb
+++ app/controllers/api/order_details_controller.rb
@@ -5,48 +5,16 @@ class Api::OrderDetailsController < ApplicationController
   http_basic_authenticate_with name: Settings.api.basic_auth_name, password: Settings.api.basic_auth_password
 
   def show
-    render json: response_to_hash
+    render json: Api::OrderDetail.new(order_detail).to_h
   end
 
   private
-
-  def account
-    @account = order_detail.try(:account) unless defined?(@account)
-    @account
-  end
-
-  def account_to_hash
-    if account.present?
-      { id: account.id, owner: user_to_hash(account.owner.user) }
-    end
-  end
-
-  def ordered_for_to_hash
-    if order_detail.present?
-      user_to_hash(order_detail.order.user)
-    end
-  end
-
-  def response_to_hash
-    { account: account_to_hash, ordered_for: ordered_for_to_hash }
-  end
 
   def order_detail
     @order_detail ||= OrderDetail.find(params[:id])
   end
 
   def order_detail_not_found
-    respond_to do |format|
-      format.json { render text: { error: "not found" }.to_json, status: 404 }
-    end
-  end
-
-  def user_to_hash(user)
-    {
-      id: user.id,
-      name: user.name,
-      username: user.username,
-      email: user.email,
-    }
+    render json: { error: "not found" }, status: 404
   end
 end
