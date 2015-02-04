53cb29807e3998104d7ad67a385aa50347a9778b
Dan Rench <dan.rench@tablexi.com>
2015-02-04 15:25:19 -0600

    [#92893] Add admin_editable flag to PriceGroups

---
+++ db/migrate/20150204174650_add_admin_editable_to_price_group.rb
@@ -0,0 +1,13 @@
+class AddAdminEditableToPriceGroup < ActiveRecord::Migration
+  def up
+    add_column :price_groups, :admin_editable, :boolean, after: :is_internal, null: false, default: true
+
+    (PriceGroup.globals - PriceGroup.cancer_center).each do |price_group|
+      price_group.update_attribute(:admin_editable, false)
+    end
+  end
+
+  def down
+    remove_column :price_groups, :admin_editable
+  end
+end
