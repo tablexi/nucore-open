e378066df16f48968e9f9fa31980edf87eb44359
John Dzak <john.dzak@tablexi.com>
2015-03-02 15:40:00 -0600

    Rename flag to clarify it's use

--- app/helpers/problem_order_details_helper.rb
+++ app/helpers/problem_order_details_helper.rb
@@ -1,15 +1,15 @@
 module ProblemOrderDetailsHelper
-  def problem_order_date_header(reservation_list_page)
-    if reservation_list_page
+  def problem_order_date_header(show_reservation_start_at)
+    if show_reservation_start_at
       t('.th.actual_start')
     else
       Order.human_attribute_name(:ordered_at)
     end
   end
 
-  def problem_order_date(order_detail, reservation_list_page)
+  def problem_order_date(order_detail, show_reservation_start_at)
     date =
-      if reservation_list_page
+      if show_reservation_start_at
         order_detail.reservation.actual_start_at
       else
         order_detail.order.ordered_at
