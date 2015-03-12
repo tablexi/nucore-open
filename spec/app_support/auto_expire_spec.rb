ad3c4c761e56b6784dd9ec1a55e0d966b1c084d2
John Dzak <john.dzak@tablexi.com>
2015-03-11 20:41:30 -0500

    Check not-ended reservations aren't expired

--- spec/app_support/auto_expire_spec.rb
+++ spec/app_support/auto_expire_spec.rb
@@ -59,5 +59,22 @@ describe AutoExpire, :timecop_freeze do
         expect(order_detail.order_status_id).to be_blank
       end
     end
+
+    context 'a reservation which has not passed the end time' do
+      let!(:reservation) do
+        start_at = 30.minutes.ago
+        end_at = 1.minute.from_now
+
+        create(:purchased_reservation,
+            product: create(:setup_instrument, min_reserve_mins: 1),
+            reserve_start_at: start_at,
+            reserve_end_at: end_at)
+      end
+
+      it 'does not do anything' do
+        expect { action.perform }.not_to change { reservation.reload.actual_end_at }
+        expect(order_detail.state).to eq('new')
+      end
+    end
   end
 end
