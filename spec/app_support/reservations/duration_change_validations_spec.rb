7aa1180039559bef717f27db820a18bf0a6295ef
John Dzak <john.dzak@tablexi.com>
2015-04-21 15:15:59 -0500

    Add specs

--- spec/app_support/reservations/duration_change_validations_spec.rb
+++ spec/app_support/reservations/duration_change_validations_spec.rb
@@ -7,68 +7,55 @@ describe Reservations::DurationChangeValidations do
 
   describe "#start_time_not_changed" do
     context "with an upcoming reservation" do
-      before do
-        reservation.assign_attributes(
-          reserve_start_at: 1.minute.from_now,
-          reserve_end_at: 61.minutes.from_now)
+      let(:reservation) { create :setup_reservation, reserve_start_at: 5.minute.from_now, reserve_end_at: 70.minutes.from_now }
+
+      it 'allows changing start time' do
+        reservation.reserve_start_at = 10.minutes.from_now
+        validator.valid?
+        expect(validator.errors).to be_empty
       end
 
-      pending 'change start time'
-      pending 'shorten end time'
-      pending 'extend end time'
+      it 'allows shortening end time' do
+        reservation.reserve_end_at = 65.minutes.from_now
+        validator.valid?
+        expect(validator.errors).to be_empty
+      end
     end
 
     context "with an ongoing reseration" do
-      before do
-        reservation.assign_attributes(
-          reserve_start_at: 30.minutes.ago,
-          reserve_end_at: 30.minutes.from_now)
-        reservation.save(validate: false)
+      let(:reservation) { create :setup_reservation, reserve_start_at: 30.minutes.ago, reserve_end_at: 40.minutes.from_now }
+
+      it 'denies changing start time' do
+        reservation.reserve_start_at = 40.minutes.ago
+        validator.valid?
+        expect(validator.errors.full_messages)
+          .to include('Reserve start at cannot change once the reservation has started')
       end
 
-      context "change start time" do
-        before do
-          reservation.assign_attributes(reserve_start_at: 31.minutes.ago)
-          validator.valid?
-        end
-
-        it 'has an error' do
-          expect(validator.errors.full_messages)
-            .to include('Reserve start at cannot change once the reservation has started')
-        end
-      end
-
-      context 'extend end time' do
-        before do
-          reservation.assign_attributes(reserve_end_at: 31.minutes.from_now)
-        end
-
-        it { expect(validator).to be_valid }
-      end
-
-      context 'shorten end time' do
-        before do
-          reservation.assign_attributes(reserve_end_at: 29.minutes.from_now)
-          validator.valid?
-        end
-
-        it 'has an error' do
-          expect(validator.errors.full_messages)
-            .to include('Reserve end at cannot shorten once the reservation has started')
-        end
+      it 'denies shortening end time' do
+        reservation.reserve_end_at = 35.minutes.from_now
+        validator.valid?
+        expect(validator.errors.full_messages)
+          .to include('Reserve end at cannot be shortened once the reservation has started')
       end
     end
 
     context "with a past reservation" do
-      before do
-        reservation.assign_attributes(
-          reserve_start_at: 61.minutes.ago,
-          reserve_end_at: 1.minute.ago)
+      let(:reservation) { create :setup_reservation, reserve_start_at: 15.minutes.ago, reserve_end_at: 5.minutes.ago, product: create(:setup_instrument, min_reserve_mins: 5) }
+
+      it 'denies changing start time' do
+        reservation.reserve_start_at = 10.minutes.ago
+        validator.valid?
+        expect(validator.errors.full_messages)
+          .to include('Reserve start at cannot change once the reservation has started')
       end
 
-      pending 'change start time'
-      pending 'shorten end time'
-      pending 'extend end time'
+      it 'denies shortening end time' do
+        reservation.reserve_end_at = 10.minutes.ago
+        validator.valid?
+        expect(validator.errors.full_messages)
+          .to include('Reserve end at cannot be shortened once the reservation has started')
+      end
     end
   end
 
