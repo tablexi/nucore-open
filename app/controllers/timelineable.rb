9ec8cdf81ff5b16d57ea8a2d65d2f927d0dfd85d
Dan Rench <dan.rench@tablexi.com>
2015-07-27 14:02:00 -0500

    [#76059] Make @display_date a date instead of a time

--- app/controllers/timelineable.rb
+++ app/controllers/timelineable.rb
@@ -6,9 +6,13 @@ module Timelineable
   end
 
   def timeline
-    @display_date = parse_usa_date(params[:date]) if params[:date]
-    @display_date ||= Time.zone.now
-
+    @display_date = display_date_as_time.to_date
     @schedules = current_facility.schedules.active.order(:name)
+  end
+
+  private
+
+  def display_date_as_time
+    (parse_usa_date(params[:date]) if params[:date]) || Time.zone.now
   end
 end
