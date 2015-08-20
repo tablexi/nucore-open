4daa37d34799bc76365d0540f8ede9d91925324f
Jason Hanggi <jason@tablexi.com>
2015-08-20 13:14:25 -0500

    Use pluck
    order_details.update_all, no includes
      0.010000   0.000000   0.010000 (  0.078244)
      0.020000   0.000000   0.020000 (  0.077606)
      0.020000   0.000000   0.020000 (  0.099758)
      0.010000   0.000000   0.010000 (  0.107454)
      0.020000   0.000000   0.020000 (  0.077497)
      0.020000   0.000000   0.020000 (  0.079702)
      0.010000   0.000000   0.010000 (  0.081405)
      0.020000   0.000000   0.020000 (  0.072132)
      0.020000   0.000000   0.020000 (  0.076986)
      0.010000   0.010000   0.020000 (  0.074458)
    order_details.update_all, includes
      0.020000   0.000000   0.020000 (  0.074332)
      0.020000   0.000000   0.020000 (  0.072860)
      0.020000   0.000000   0.020000 (  0.080270)
      0.020000   0.000000   0.020000 (  0.080924)
      0.010000   0.000000   0.010000 (  0.072170)
      0.020000   0.000000   0.020000 (  0.072339)
      0.020000   0.000000   0.020000 (  0.078814)
      0.020000   0.000000   0.020000 (  0.077811)
      0.020000   0.000000   0.020000 (  0.073692)
      0.020000   0.010000   0.030000 (  0.077983)

--- app/support/notification_sender.rb
+++ app/support/notification_sender.rb
@@ -12,7 +12,7 @@ class NotificationSender
     @errors = []
 
     OrderDetail.transaction do
-      order_details_not_found = @order_detail_ids.map(&:to_i) - order_details.map(&:id)
+      order_details_not_found = @order_detail_ids.map(&:to_i) - order_details.pluck(:id)
 
       order_details_not_found.each do |order_detail_id|
         @errors << I18n.t('controllers.facility_notifications.send_notifications.order_error', :order_detail_id => order_detail_id)
