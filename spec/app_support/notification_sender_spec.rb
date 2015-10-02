e9e3f054b57389adfdd329cea731ca1fcf5b2daa
Jason Hanggi <jason@tablexi.com>
2015-10-01 19:14:15 -0500

    [#121396] Sending Notifications for more than 1000 OD causes Oracle error
    Oracle does not allow more than 1000 items in a `WHERE IN` clause. This
    breaks it up into 1000 item AR::Relations and loops over those as
    necessary.
    
    Closes #370

---
+++ spec/app_support/notification_sender_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+RSpec.describe NotificationSender, :aggregate_failures do
+  let(:user) { create(:user) }
+
+  let(:facility) { create(:facility) }
+
+  let(:facility_account) do
+    facility.facility_accounts.create(attributes_for(:facility_account))
+  end
+
+  let(:item) do
+    facility
+    .items
+    .create(attributes_for(:item, facility_account_id: facility_account.id))
+  end
+
+  let(:account) { create(:setup_account, owner: user, facility_id: facility.id) }
+  let(:ids) { order_details.map(&:id) }
+  let(:action) { described_class.new(facility, ids) }
+
+  describe "with a reasonable sized group" do
+    let!(:order_details) { 5.times.map { place_product_order(user, facility, item, account) } }
+
+    before { OrderDetail.update_all(state: "complete", price_policy_id: item.price_policies.first.id) }
+
+    it "notifies the appropriate accounts and sets the order details to reviewed" do
+      expect(action.perform).to be_truthy
+      expect(action.account_ids_notified).to eq([account.id])
+      expect(order_details).to be_all { |od| od.reload.reviewed_at? }
+    end
+
+    describe "with an id that does not exist" do
+      let(:ids) { [-1, order_details.first.id] }
+
+      it "has an error for the id" do
+        expect(action.perform).to be_falsey
+        expect(action.errors.first).to include("-1")
+        expect(order_details.first.reload).not_to be_reviewed
+      end
+    end
+  end
+end
