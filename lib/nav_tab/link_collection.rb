b71a7e30bca280dd32a08c3afc6f258bd3276617
Dan Rench <dan.rench@tablexi.com>
2015-11-12 13:49:46 -0600

    Suppress some admin tabs when in a cross-facility context

--- lib/nav_tab/link_collection.rb
+++ lib/nav_tab/link_collection.rb
@@ -36,25 +36,25 @@ class NavTab::LinkCollection
   end
 
   def admin_billing
-    if ability.can?(:manage_billing, facility)
+    if !facility.all_facility? && ability.can?(:manage_billing, facility)
       NavTab::Link.new(tab: :admin_billing, url: billing_tab_landing_path)
     end
   end
 
   def admin_orders
-    if ability.can?(:administer, Order)
+    if !facility.all_facility? && ability.can?(:administer, Order)
       NavTab::Link.new(tab: :admin_orders, url: facility_orders_path(facility))
     end
   end
 
   def admin_products
-    if ability.can?(:administer, Product)
+    if !facility.all_facility? && ability.can?(:administer, Product)
       NavTab::Link.new(tab: :admin_products, url: facility_products_path(facility))
     end
   end
 
   def admin_reports
-    if ability.can?(:manage, ReportsController)
+    if !facility.all_facility? && ability.can?(:manage, ReportsController)
       NavTab::Link.new(
         tab: :admin_reports,
         subnav: [general_reports, instrument_utilization_reports],
@@ -63,7 +63,7 @@ class NavTab::LinkCollection
   end
 
   def admin_reservations
-    if ability.can?(:administer, Reservation)
+    if !facility.all_facility? && ability.can?(:administer, Reservation)
       NavTab::Link.new(
         tab: :admin_reservations,
         url: timeline_facility_reservations_path(facility),
