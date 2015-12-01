ab6f3a9ecd5cb242ebf391c7f0c9021a9ed2ebdf
Benjamin Linton <ben.linton@tablexi.com>
2015-12-01 16:19:07 -0600

    Revert nufs_account factory

--- vendor/engines/split_accounts/spec/factories/splits.rb
+++ vendor/engines/split_accounts/spec/factories/splits.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :split do
     association :parent_split_account, factory: :split_account
-    subaccount { build(:nufs_account, :with_owner) }
+    subaccount { build(:setup_account) }
     percent 100
     extra_penny true
   end
