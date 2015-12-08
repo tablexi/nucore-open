594a171c9ed0f3f4f8085717575cc3da3f0309b2
Dan Rench <dan.rench@tablexi.com>
2015-12-08 10:57:09 -0600

    Refactor to eliminate single-use shared examples

--- spec/presenters/user_presenter_spec.rb
+++ spec/presenters/user_presenter_spec.rb
@@ -5,39 +5,27 @@ RSpec.describe UserPresenter do
   let(:global_role_list) { subject.global_role_list }
   let(:global_role_select_options) { subject.global_role_select_options }
 
-  shared_examples_for "it has no global roles" do
-    it { expect(global_role_list).to eq("") }
-    it { expect(global_role_select_options).not_to include('selected="selected"') }
-  end
-
-  shared_examples_for "it has one global role" do
-    it { expect(global_role_list).to eq("Administrator") }
-    it { expect(global_role_select_options).to include('selected="selected">Administrator') }
-    it { expect(global_role_select_options).not_to include('selected="selected">Billing Administrator') }
-  end
-
-  shared_examples_for "it has multiple global roles" do
-    it { expect(global_role_list).to eq("Administrator, Billing Administrator") }
-    it { expect(global_role_select_options).to include('selected="selected">Administrator') }
-    it { expect(global_role_select_options).to include('selected="selected">Billing Administrator') }
-  end
-
   context "when the user has no global roles" do
     let(:user) { create(:user) }
 
-    it_behaves_like "it has no global roles"
+    it { expect(global_role_list).to eq("") }
+    it { expect(global_role_select_options).not_to include('selected="selected"') }
   end
 
   context "when the user has one global role" do
     let(:user) { create(:user, :administrator) }
 
-    it_behaves_like "it has one global role"
+    it { expect(global_role_list).to eq("Administrator") }
+    it { expect(global_role_select_options).to include('selected="selected">Administrator') }
+    it { expect(global_role_select_options).not_to include('selected="selected">Billing Administrator') }
   end
 
   context "when the user has multiple global roles" do
     let(:user) { create(:user, :administrator, :billing_administrator) }
 
-    it_behaves_like "it has multiple global roles"
+    it { expect(global_role_list).to eq("Administrator, Billing Administrator") }
+    it { expect(global_role_select_options).to include('selected="selected">Administrator') }
+    it { expect(global_role_select_options).to include('selected="selected">Billing Administrator') }
   end
 
   describe "#name_last_comma_first" do
