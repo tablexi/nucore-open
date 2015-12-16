33dcc860ae215762634c031f28b2624f19fbb8fd
Dan Rench <dan.rench@tablexi.com>
2015-12-16 10:19:35 -0600

    Clean up specs

--- spec/factories/account_users.rb
+++ spec/factories/account_users.rb
@@ -1,6 +1,19 @@
 FactoryGirl.define do
   factory :account_user do
-    user_role 'Owner'
+    user_role 'Owner' # TODO: explicitly use the :owner trait
     created_by 0
+
+    trait :inactive do
+      deleted_at { 1.day.ago }
+      deleted_by 0
+    end
+
+    trait :owner do
+      user_role "Owner"
+    end
+
+    trait :purchaser do
+      user_role "Purchaser"
+    end
   end
 end
