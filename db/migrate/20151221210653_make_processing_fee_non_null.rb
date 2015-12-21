997265d89c3b27c6a7c366dbde3e2366755253af
Jason Hanggi <jason@tablexi.com>
2015-12-21 16:49:08 -0500

    [#103993] Set default Payment#processing_fee to $0
    And make it not-null.
    
    I didn’t want to give :amount a default because it’s something that
    should always be supplied by the user, whereas processing_fee could
    likely not be a feature that’s used.

---
+++ db/migrate/20151221210653_make_processing_fee_non_null.rb
@@ -0,0 +1,9 @@
+class MakeProcessingFeeNonNull < ActiveRecord::Migration
+  def up
+    change_column :payments, :processing_fee, :decimal, precision: 10, scale: 2, null: false, default: 0
+  end
+
+  def down
+    change_column :payments, :processing_fee, :decimal, precision: 10, scale: 2, null: true, default: nil
+  end
+end
