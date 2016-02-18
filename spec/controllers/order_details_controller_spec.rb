1671bfb1576d66aff554275cdea417280dde003d
Dan Rench <dan.rench@tablexi.com>
2016-02-18 12:52:42 -0600

    Add specs for GET #show

--- spec/controllers/order_details_controller_spec.rb
+++ spec/controllers/order_details_controller_spec.rb
@@ -1,12 +1,13 @@
 require "rails_helper"
 
 RSpec.describe OrderDetailsController do
+  let(:order) { order_detail.order }
+  let(:order_detail) { reservation.order_detail }
+  let(:reservation) { create(:purchased_reservation) }
+
   describe "#dispute" do
-    let(:user) { order_detail.user }
-    let(:reservation) { create(:purchased_reservation) }
-    let(:order_detail) { reservation.order_detail }
-    let(:order) { order_detail.order }
     let(:params) { { order_id: order.id, order_detail_id: order_detail.id } }
+    let(:user) { order_detail.user }
     before { sign_in user }
 
     context "when the order is not disputable" do
@@ -36,6 +37,25 @@ RSpec.describe OrderDetailsController do
           expect(order_detail.reload.dispute_by).to eq(user)
         end
       end
+    end
+  end
+
+  describe "#show" do
+    before(:each) do
+      sign_in user
+      get :show, order_id: order.id, order_detail_id: order_detail.id
+    end
+
+    context "when logged in as the user who owns the order" do
+      let(:user) { order_detail.user }
+
+      it { expect(response.code).to eq("200") }
+    end
+
+    context "when logged in as a user that does not own the order" do
+      let(:user) { create(:user) }
+
+      it { expect(response.code).to eq("404") }
     end
   end
 end
