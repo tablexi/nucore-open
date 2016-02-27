9dbcb767f71a312761b1a68453fca44927a1f0c2
Jason Hanggi <jason@tablexi.com>
2016-02-26 18:32:05 -0600

    [#120455] Nicer display of extra penny boolean

--- app/helpers/translation_helper.rb
+++ app/helpers/translation_helper.rb
@@ -2,16 +2,24 @@ module TranslationHelper
   def t_manage_models(clazz)
     I18n.t('pages.manage', :model => clazz.model_name.human.pluralize)
   end
+
   def t_create_model(clazz)
     I18n.t("pages.create", :model => clazz.model_name.human)
   end
+
   def t_create_models(clazz)
     I18n.t("pages.create", :model => clazz.model_name.human.pluralize)
   end
+
   def t_my(clazz)
     I18n.t('pages.my_tab', :model => clazz.model_name.human.pluralize)
   end
+
   def t_model_error(clazz, error, *options)
     I18n.t("activerecord.errors.models.#{clazz.model_name.underscore}.#{error}", *options)
+  end
+
+  def t_boolean(value)
+    I18n.t(value.to_s, scope: "boolean")
   end
 end
