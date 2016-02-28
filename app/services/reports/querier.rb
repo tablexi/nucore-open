bb726107ce9aecdefa1dbf6f022303a3f33b8ed6
Ben Linton <ben.linton@tablexi.com>
2016-02-28 16:01:13 -0600

    Extract report data query logic into a service object

---
+++ app/services/reports/querier.rb
@@ -0,0 +1,27 @@
+module Reports
+
+  class Querier
+
+    attr_reader :order_status_id, :current_facility, :date_range_field,
+      :date_range_start, :date_range_end
+
+    def initialize(options = {})
+      @order_status_id = options[:order_status_id]
+      @current_facility = options[:current_facility]
+      @date_range_field = options[:date_range_field]
+      @date_range_start = options[:date_range_start]
+      @date_range_end = options[:date_range_end]
+    end
+
+    def perform
+      return [] if order_status_id.blank?
+
+      OrderDetail.where(order_status_id: order_status_id)
+        .for_facility(current_facility)
+        .action_in_date_range(date_range_field, date_range_start, date_range_end)
+        .includes(:order, :account, :price_policy, :product, :order_status)
+    end
+
+  end
+
+end
