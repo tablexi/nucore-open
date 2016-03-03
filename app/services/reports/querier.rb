713c051d439eb5126039e30dca8ea3c19f619e1b
Ben Linton <ben.linton@tablexi.com>
2016-03-03 07:16:58 -0600

    Support split accounts and export raw for reports

--- app/services/reports/querier.rb
+++ app/services/reports/querier.rb
@@ -3,7 +3,7 @@ module Reports
   class Querier
 
     attr_reader :order_status_id, :current_facility, :date_range_field,
-      :date_range_start, :date_range_end
+      :date_range_start, :date_range_end, :extra_includes
 
     def initialize(options = {})
       @order_status_id = options[:order_status_id]
@@ -11,6 +11,7 @@ module Reports
       @date_range_field = options[:date_range_field]
       @date_range_start = options[:date_range_start]
       @date_range_end = options[:date_range_end]
+      @extra_includes = options[:includes]
     end
 
     def perform
@@ -19,10 +20,18 @@ module Reports
 
     def order_details
       return [] if order_status_id.blank?
+
+      includes = default_includes
+      includes.concat(extra_includes) if extra_includes.present?
+
       OrderDetail.where(order_status_id: order_status_id)
         .for_facility(current_facility)
         .action_in_date_range(date_range_field, date_range_start, date_range_end)
-        .includes(:order, :account, :price_policy, :product, :order_status)
+        .includes(*includes)
+    end
+
+    def default_includes
+      [:account, :order, :order_status, :price_policy, :product]
     end
 
   end
