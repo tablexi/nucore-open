6fcc61448a72bf8743f0bad143531e7a8ab381ec
Jason Hanggi <jason@tablexi.com>
2016-03-04 13:26:30 -0600

    [#120455] Fix split expires_at setting
    The test was previously disabled since the setting was turned off so
    the test didn’t catch the regression caused by #464.
    
    Pivotal #114611905

--- vendor/engines/split_accounts/spec/services/split_account_builder_spec.rb
+++ vendor/engines/split_accounts/spec/services/split_account_builder_spec.rb
@@ -20,35 +20,56 @@ RSpec.describe SplitAccounts::SplitAccountBuilder, :enable_split_accounts do
       }
     end
 
-    let(:params) do
-      ActionController::Parameters.new({
-        split_accounts_split_account: {
-          splits_attributes: {
-            "0" => {
-              subaccount_id: subaccount_2.id,
-              percent: 50,
-              extra_penny: true
+    describe "happy path" do
+      let(:params) do
+        ActionController::Parameters.new(
+          split_accounts_split_account: {
+            splits_attributes: {
+              "0" => {
+                subaccount_id: subaccount_2.id,
+                percent: 50,
+                extra_penny: true,
+              },
+              "1" => {
+                subaccount_id: subaccount_1.id,
+                percent: 50,
+                extra_penny: false,
+              },
             },
-            "1" => {
-              subaccount_id: subaccount_1.id,
-              percent: 50,
-              extra_penny: false
-            },
-          }
-        }
-      })
+          },
+        )
+      end
+
+      let(:subaccount_1) { create(:setup_account, expires_at: (Time.zone.now + 1.month).change(usec: 0)) }
+      let(:subaccount_2) { create(:setup_account, expires_at: (Time.zone.now + 2.months).change(usec: 0)) }
+
+      it "sets splits" do
+        expect(builder.build.splits.size).to be(2)
+      end
+
+      it "sets expired_at to earliest expiring subaccount" do
+        expect(builder.build.expires_at).to eq(subaccount_1.expires_at)
+      end
     end
 
-    let(:subaccount_1) { create(:setup_account, expires_at: (Time.zone.now + 1.month).change(usec: 0)) }
-    let(:subaccount_2) { create(:setup_account, expires_at: (Time.zone.now + 1.month).change(usec: 0)) }
+    describe "with a blank subaccount" do
+      let(:params) do
+        ActionController::Parameters.new(
+          split_accounts_split_account: {
+            splits_attributes: {
+              "0" => {
+                subaccount_id: "",
+                percent: 100,
+                extra_penny: true,
+              },
+            },
+          },
+        )
+      end
 
-    it "sets splits" do
-      expect(builder.build.splits.size).to be(2)
-    end
-
-    it "sets expired_at to earliest expiring subaccount" do
-      expect(builder.build.expires_at).to eq(subaccount_1.expires_at)
+      it "does not error" do
+        expect(builder.build.splits).to be_one
+      end
     end
   end
-
 end
