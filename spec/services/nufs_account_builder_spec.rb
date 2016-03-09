0b797ff6ac70e4ebfc586343a6b81e4b268ad1b2
Jason Hanggi <jason@tablexi.com>
2016-03-09 15:23:05 -0600

    Do not add validation errors for FacilityAccount#new
    https://www.pivotaltracker.com/story/show/115368465

--- spec/services/nufs_account_builder_spec.rb
+++ spec/services/nufs_account_builder_spec.rb
@@ -20,30 +20,42 @@ RSpec.describe NufsAccountBuilder, type: :service do
       }
     end
 
-    let(:params) do
-      ActionController::Parameters.new(
-        nufs_account: {
-          account_number: "1234",
-          description: "foobar",
-        }
-      )
+    describe "without params (like FacilityAccounts#new)" do
+      let(:params) { {} }
+
+      it "has no errors, even load_components raises an error" do
+        allow_any_instance_of(NufsAccount).to receive(:load_components).and_raise(ValidatorError, "validation error")
+
+        expect(builder.build.errors).to be_empty
+      end
     end
 
-    it "sets expired_at" do
-      expect(builder.build.expires_at).to be_present
-    end
+    describe "with params" do
+      let(:params) do
+        ActionController::Parameters.new(
+          nufs_account: {
+            account_number: "1234",
+            description: "foobar",
+          }
+        )
+      end
 
-    it "sets the account_number" do
-      expect(builder.build.account_number).to eq("1234")
-    end
+      it "sets expired_at" do
+        expect(builder.build.expires_at).to be_present
+      end
 
-    it "sets the description" do
-      expect(builder.build.description).to eq("foobar")
-    end
+      it "sets the account_number" do
+        expect(builder.build.account_number).to eq("1234")
+      end
 
-    it "still sets the expiration date on a validator error" do
-      allow_any_instance_of(ValidatorDefault).to receive(:latest_expiration).and_raise(ValidatorError)
-      expect(builder.build.expires_at).to be_present
+      it "sets the description" do
+        expect(builder.build.description).to eq("foobar")
+      end
+
+      it "still sets the expiration date on a validator error" do
+        allow_any_instance_of(ValidatorDefault).to receive(:latest_expiration).and_raise(ValidatorError)
+        expect(builder.build.expires_at).to be_present
+      end
     end
   end
 end
