225030e84af4456e6a7f2a9dbf81f670a2e531e3
Dan Rench <dan.rench@tablexi.com>
2016-03-11 15:42:54 -0600

    [#124958] Update payment source link logic
    When listing payment sources, if the user can manage the account, it generates
    a link to the facility_account_path, as opposed to the account's orders index.
    
    The account orders index view is now a tab within the facility_account_path view.
    
    Closes #479

---
+++ spec/helpers/accounts_helper_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe AccountsHelper do
+  describe "#payment_source_link_or_text" do
+    subject { payment_source_link_or_text(account) }
+    let(:account) { build_stubbed(:nufs_account) }
+    let(:current_ability) { create(:ability, facility: current_facility) }
+    let(:current_facility) { build_stubbed(:facility) }
+
+    before(:each) do
+      allow(current_ability)
+        .to receive(:can?)
+        .with(:manage, account)
+        .and_return(allowed?)
+    end
+
+    context "when allowed to manage the account" do
+      let(:allowed?) { true }
+      let(:expected_path) do
+        "/facilities/#{current_facility.url_name}/accounts/#{account.id}"
+      end
+
+      it { is_expected.to include(expected_path).and include(account.to_s) }
+    end
+
+    context "when not allowed to manage the account" do
+      let(:allowed?) { false }
+
+      it { is_expected.to eq(account.to_s) }
+    end
+  end
+end
