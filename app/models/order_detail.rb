051d236b5a8cae38edcaac4b539ea84603e46697
Matthew Lineen <matthew@lineen.com>
2016-03-22 14:57:32 -0500

    rubocop Style/SpaceAfterColon

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -233,7 +233,7 @@ class OrderDetail < ActiveRecord::Base
 
   def in_review?
     # check in the database if self.id is in the scope
-    self.class.all_in_review.find_by_id(id) ? true :false
+    self.class.all_in_review.find_by_id(id) ? true : false
     # this would work without hitting the database again, but duplicates the functionality of the scope
     # state == 'complete' and !reviewed_at.nil? and reviewed_at > Time.zone.now and (dispute_at.nil? or !dispute_resolved_at.nil?)
   end
