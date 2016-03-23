d6b28a90b3e3be1837ab8b0bbee4c4c7ae000564
Matthew Lineen <matthew@lineen.com>
2016-03-23 11:55:07 -0500

    rubocop Style/IndentArray

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -250,38 +250,38 @@ class OrderDetail < ActiveRecord::Base
                            {
                              joins: [:product, :account],
                              conditions: [
-      "products.facility_id = :facility_id
-        AND order_details.state = :state
-        AND problem = :problem
-        AND reviewed_at <= :reviewed_at
-        AND order_details.statement_id IS NULL
-        AND order_details.price_policy_id IS NOT NULL
-        AND accounts.type IN (:account_types)
-        AND (dispute_at IS NULL OR dispute_resolved_at IS NOT NULL)",
+                               "products.facility_id = :facility_id
+                                 AND order_details.state = :state
+                                 AND problem = :problem
+                                 AND reviewed_at <= :reviewed_at
+                                 AND order_details.statement_id IS NULL
+                                 AND order_details.price_policy_id IS NOT NULL
+                                 AND accounts.type IN (:account_types)
+                                 AND (dispute_at IS NULL OR dispute_resolved_at IS NOT NULL)",
       facility_id: facility.id,
       state: "complete",
       problem: false,
       reviewed_at: Time.zone.now,
       account_types: Account.config.statement_account_types,
-    ],
+                             ],
                            }}
 
   scope :need_journal, lambda {
                          {
                            joins: [:product, :account],
                            conditions: [
-      "order_details.state = :state
-        AND problem = :problem
-        AND reviewed_at <= :reviewed_at
-        AND accounts.type IN (:account_types)
-        AND journal_id IS NULL
-        AND order_details.price_policy_id IS NOT NULL
-        AND (dispute_at IS NULL OR dispute_resolved_at IS NOT NULL)",
+                             "order_details.state = :state
+                               AND problem = :problem
+                               AND reviewed_at <= :reviewed_at
+                               AND accounts.type IN (:account_types)
+                               AND journal_id IS NULL
+                               AND order_details.price_policy_id IS NOT NULL
+                               AND (dispute_at IS NULL OR dispute_resolved_at IS NOT NULL)",
       state: "complete",
       problem: false,
       reviewed_at: Time.zone.now,
       account_types: Account.config.journal_account_types,
-    ],
+                           ],
                          } }
 
   scope :statemented, lambda {|facility|
