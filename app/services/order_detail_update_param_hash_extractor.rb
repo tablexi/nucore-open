3588c40b1009b28893f713815d6a7fae38974ed6
Dan Rench <dan.rench@tablexi.com>
2016-03-30 13:10:47 -0500

    Add notes about param transformation

--- app/services/order_detail_update_param_hash_extractor.rb
+++ app/services/order_detail_update_param_hash_extractor.rb
@@ -8,6 +8,11 @@ class OrderDetailUpdateParamHashExtractor
 
   # This extracts and transforms a hash (like controller params) into the form
   # of a hash that Order#update_details expects.
+  #
+  # For example, if given controller params like this:
+  #   { "quantity123" => 2, "note123" => "Noted" }
+  # …the #to_h method would return:
+  #   { 123 => { quantity: 2, note: "Noted" } }
   def to_h
     params.each_with_object({}) do |(key, value), memo|
       match = key.match(/\A(note|quantity)(\d+)\z/) || next
