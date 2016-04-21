tablexi/nucore-open

commit e26cf27311d5470ad29cc7b60dbc487fcf8ea04e

[#124331] Support deactivated users (#534)
* Move bulk email helper logic into a service object.
* Remove BulkEmailHelper completely
* Restrict to active/inactive users
* Don't allow switch_to for deactivated users
* Refactor search views

app/controllers/search_controller.rb
@@ -11,6 +11,7 @@ class SearchController < ApplicationController
     @price_group = PriceGroup.find(params[:price_group_id]) if params[:price_group_id].present?
     @account = Account.find(params[:account_id]) if params[:account_id].present?
     @product = Product.find(params[:product_id]) if params[:product_id].present?
+    @search_type = valid_search_types.find { |t| t == params[:search_type] }
     @users, @count = UserFinder.search(params[:search_term], @limit)
 
     render layout: false
@@ -24,6 +25,12 @@ class SearchController < ApplicationController
   end
 
   private
+
+  def valid_search_types
+    %w(account_account_user create_new_user facility_account_account_user
+       global_user_role manage_user map_user user_accounts user_new_account
+       user_price_group_member user_product_approval)
+  end
 
   def load_facility
     @facility =

app/views/search/_user_product_approval_link.html.haml
@@ -1,1 +1,2 @@
-= link_to "#{user.last_name}, #{user.first_name}", self.send("new_facility_#{@product.class.name.downcase}_user_path", @facility, @product, :user => user.id), :confirm => "Are you sure you wish to authorize this user to order this #{@product.class.name.downcase}?"
+# TODO: I18n confirmation
+= link_to user.last_first_name, self.send("new_facility_#{@product.class.name.downcase}_user_path", @facility, @product, user: user.id), :confirm => "Are you sure you wish to authorize this user to order this #{@product.class.name.downcase}?"

lib/ability.rb
@@ -27,6 +27,8 @@ class Ability
           cannot :manage, User unless resource.is_a?(Facility) && resource.single_facility?
         end
       end
+
+      cannot(:switch_to, User) { |target_user| !target_user.active? }
       return
     end
 
@@ -116,8 +118,11 @@ class Ability
           fileupload.file_type == "sample_result"
         end
 
-        can [:administer, :switch_to], User
-        can :manage, User if controller.is_a?(UsersController)
+        can [:administer], User
+        if controller.is_a?(UsersController)
+          can :manage, User
+          cannot(:switch_to, User) { |target_user| !target_user.active? }
+        end
 
         can [:list, :show], Facility
         can :act_as, Facility

app/controllers/bulk_email_controller.rb
@@ -1,6 +1,5 @@
 class BulkEmailController < ApplicationController
 
-  include BulkEmailHelper
   include CSVHelper
 
   admin_tab :all
@@ -16,7 +15,11 @@ class BulkEmailController < ApplicationController
   before_filter :init_search_options
 
   def search
-    @users = do_search(@search_fields) if params[:search_type]
+    if params[:search_type]
+      searcher = BulkEmailSearcher.new(@search_fields)
+      @users = searcher.do_search
+      @order_details = searcher.order_details
+    end
 
     respond_to do |format|
       format.html { @users = @users.paginate(page: params[:page]) if @users }
@@ -30,10 +33,16 @@ class BulkEmailController < ApplicationController
   private
 
   def init_search_options
-    @search_fields = params.merge(facility_id: current_facility.id)
     @products = current_facility.products.active_plus_hidden.order("products.name").includes(:facility)
-    @search_types = BulkEmailHelper.search_types_and_titles
+    @search_fields = params.merge(facility_id: current_facility.id)
+    @search_types = search_types
     @search_types.delete(:authorized_users) unless @products.exists?(requires_approval: true)
+  end
+
+  def search_types
+    BulkEmailSearcher::SEARCH_TYPES.each_with_object({}) do |search_type, hash|
+      hash[search_type] = I18n.t("bulk_email.search_type.#{search_type}")
+    end
   end
 
 end

app/controllers/users_controller.rb
@@ -16,9 +16,8 @@ class UsersController < ApplicationController
   before_filter :init_current_facility, except: [:password, :password_reset]
   before_filter :authenticate_user!, except: [:password_reset]
   before_filter :check_acting_as
-  before_filter :load_user_from_user_id_param, only: [:access_list, :access_list_approvals, :accounts, :orders, :reservations, :switch_to]
 
-  load_and_authorize_resource except: [:password, :password_reset]
+  load_and_authorize_resource except: [:password, :password_reset], id_param: :user_id
 
   layout "two_column"
 
@@ -179,10 +178,6 @@ class UsersController < ApplicationController
     else
       []
     end
-  end
-
-  def load_user_from_user_id_param
-    @user = User.find(params[:user_id])
   end
 
   def username_lookup(username)

app/models/user.rb
@@ -157,8 +157,20 @@ class User < ActiveRecord::Base
     end
   end
 
+  def last_first_name
+    "#{last_name}, #{first_name}" + deactivated_string
+  end
+
   def to_s
-    full_name
+    full_name + deactivated_string
+  end
+
+  def deactivated_string
+    if active?
+      ""
+    else
+      " (#{self.class.human_attribute_name(:deactivated)})"
+    end
   end
 
   def recently_used_facilities(limit = 5)
@@ -167,6 +179,22 @@ class User < ActiveRecord::Base
       hash[key] = Facility.where(id: facility_ids).sorted
     end
     @recently_used_facilities[limit]
+  end
+
+  def deactivate
+    update_attribute(:deactivated_at, deactivated_at || Time.current)
+  end
+
+  def activate
+    update_attribute(:deactivated_at, nil)
+  end
+
+  def active?
+    deactivated_at.blank?
+  end
+
+  def self.active
+    where(deactivated_at: nil)
   end
 
 end

app/presenters/user_presenter.rb
@@ -16,8 +16,13 @@ class UserPresenter < SimpleDelegator
     options_for_select(UserRole.global_roles, selected: user_roles.map(&:role))
   end
 
-  def name_last_comma_first
-    "#{last_name}, #{first_name}"
+  # pretend to be a User
+  def kind_of?(clazz)
+    if clazz == User
+      true
+    else
+      super
+    end
   end
 
   private

app/services/bulk_email_searcher.rb
@@ -1,0 +1,75 @@
+class BulkEmailSearcher
+
+  include DateHelper
+
+  attr_reader :order_details, :search_fields
+
+  DEFAULT_SORT = [:last_name, :first_name].freeze
+  SEARCH_TYPES = [:customers, :account_owners, :customers_and_account_owners, :authorized_users].freeze
+
+  def initialize(search_fields)
+    @search_fields = search_fields
+  end
+
+  def do_search
+    return unless SEARCH_TYPES.include? search_fields[:search_type].to_sym
+    public_send(:"search_#{search_fields[:search_type]}")
+  end
+
+  def search_customers
+    order_details = find_order_details.joins(order: :user)
+    users.find_by_sql(order_details.select("distinct(users.id), users.*")
+                                          .reorder("users.last_name, users.first_name")
+                                          .merge(users)
+                                          .to_sql)
+  end
+
+  def search_account_owners
+    order_details = find_order_details_for_roles([AccountUser::ACCOUNT_OWNER])
+    User.find_by_sql(order_details.joins(account: { account_users: :user })
+                                   .select("distinct(users.id), users.*")
+                                   .reorder("users.last_name, users.first_name")
+                                   .merge(users)
+                                   .to_sql)
+  end
+
+  def search_customers_and_account_owners
+    customers = search_customers
+    account_owners = search_account_owners
+    (customers + account_owners).uniq.sort { |x, y| x.last_name <=> y.last_name }
+  end
+
+  def search_authorized_users
+    result = users.joins(:product_users)
+    # if we don't have any products, listed get them all for the current facility
+    product_ids = search_fields[:products].presence || Facility.find(search_fields[:facility_id]).products.map(&:id)
+    result.where(product_users: { product_id: product_ids }).reorder(*self.class::DEFAULT_SORT)
+  end
+
+  private
+
+  def users
+    User.active
+  end
+
+  def find_order_details
+    order_details = OrderDetail.for_products(search_fields[:products])
+                               .joins(:order)
+                               .where(orders: { facility_id: search_fields[:facility_id] })
+
+    @order_details = order_details.ordered_or_reserved_in_range(start_date, end_date)
+  end
+
+  def find_order_details_for_roles(roles)
+    find_order_details.joins(account: :account_users).where(account_users: { user_role: roles })
+  end
+
+  def start_date
+    parse_usa_date(search_fields[:start_date].to_s.tr("-", "/")) if search_fields[:start_date]
+  end
+
+  def end_date
+    parse_usa_date(search_fields[:end_date].to_s.tr("-", "/")) if search_fields[:end_date]
+  end
+
+end

commit 394f40020fc1b8d1be45061c47386e4614649715

[#124331] Fix broken search result
The user approval search was broken because of a syntax error. This was
broken by the refactoring in #534 for supporting deactivated users.

While writing tests, I also discovered that this causes a problem where
facility admins cannot order for after a search. I never noticed it
because I was always testing as a global admin.

app/controllers/search_controller.rb
@@ -5,7 +5,6 @@ class SearchController < ApplicationController
 
   ## return users of portal 'nucore'
   def user_search_results
-    raise NUCore::PermissionDenied if session_user.blank?
     @limit = 25
     load_facility
     @price_group = PriceGroup.find(params[:price_group_id]) if params[:price_group_id].present?

app/views/search/_user_product_approval_link.html.haml
@@ -1,2 +1,2 @@
-# TODO: I18n confirmation
+-# TODO: I18n confirmation
 = link_to user.last_first_name, self.send("new_facility_#{@product.class.name.downcase}_user_path", @facility, @product, user: user.id), :confirm => "Are you sure you wish to authorize this user to order this #{@product.class.name.downcase}?"

lib/ability.rb
@@ -119,7 +119,7 @@ class Ability
         end
 
         can [:administer], User
-        if controller.is_a?(UsersController)
+        if controller.is_a?(UsersController) || controller.is_a?(SearchController)
           can :manage, User
           cannot(:switch_to, User) { |target_user| !target_user.active? }
         end

spec/controllers/search_controller_spec.rb
@@ -1,0 +1,71 @@
+require "rails_helper"
+
+RSpec.describe SearchController do
+  render_views
+
+  let(:facility) { FactoryGirl.create(:setup_facility) }
+  let!(:user) { FactoryGirl.create(:user, first_name: "Firstname", last_name: "Lastname") }
+
+  shared_examples_for "searching" do |action|
+    let(:session_user) { FactoryGirl.create(:user, :facility_administrator, facility: facility) }
+    let(:params) { {} }
+    before do
+      sign_in session_user
+      post :user_search_results, params.merge(search_type: action, search_term: user.username)
+    end
+
+    it "#{action} finds the user" do
+      expect(response.body).to include(user.email)
+      expect(response.body).to include(user.first_name)
+      expect(response.body).to include(user.last_name)
+    end
+
+    it "#{action} renders the link" do
+      expect(response).to render_template("search/_#{action}_link")
+    end
+  end
+
+  describe "user_search_results" do
+    it "does not allow a non-logged in user" do
+      get :user_search_results
+      expect(response.code).to redirect_to(new_user_session_path)
+    end
+
+    it_behaves_like "searching", "account_account_user" do
+      let(:account) { FactoryGirl.create(:setup_account) }
+      let(:params) { { account_id: account.id } }
+    end
+
+    it_behaves_like "searching", "facility_account_account_user" do
+      let(:account) { FactoryGirl.create(:setup_account) }
+      let(:params) { { account_id: account.id } }
+    end
+
+    # This is safe for a non-admin to see because the links themselves are protected
+    it_behaves_like "searching", "global_user_role"
+
+    it_behaves_like "searching", "manage_user" do
+      let(:params) { { facility_id: facility.id } }
+
+      it "has order for", :focus do
+        expect(response.body).to include("Order For")
+      end
+    end
+
+    it_behaves_like "searching", "map_user"
+
+    it_behaves_like "searching", "user_accounts"
+
+    it_behaves_like "searching", "user_new_account"
+
+    it_behaves_like "searching", "user_price_group_member" do
+      let(:price_group) { FactoryGirl.create(:price_group, facility: facility) }
+      let(:params) { { price_group_id: price_group.id } }
+    end
+
+    it_behaves_like "searching", "user_product_approval" do
+      let(:product) { FactoryGirl.create(:setup_item, facility: facility) }
+      let(:params) { { product_id: product.id } }
+    end
+  end
+end