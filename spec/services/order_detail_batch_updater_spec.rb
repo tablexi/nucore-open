4543104a6d80a71a0461d89a38537b7b9727f881
Dan Rench <dan.rench@tablexi.com>
2016-05-12 13:46:51 -0500

    Refactor and add more batch updater specs

--- spec/services/order_detail_batch_updater_spec.rb
+++ spec/services/order_detail_batch_updater_spec.rb
@@ -20,75 +20,189 @@ RSpec.describe OrderDetailBatchUpdater do
   let(:order_detail) { order.order_details.first }
   let(:order_status_id) { "" }
   let(:params) do
-    {
-      assigned_user_id: assigned_user_id,
-      order_status_id: order_status_id,
-    }
+    { assigned_user_id: assigned_user_id, order_status_id: order_status_id }
   end
   let(:user) { FactoryGirl.create(:user) }
 
   describe "#update!" do
-    context "when associated with Orders" do
-      let(:record_type) { "orders" }
+    shared_examples_for "batch updating" do
+      context "when assigned_user_id is already set" do
+        before(:each) do
+          order_detail.update_attribute(:assigned_user_id, 2)
+        end
 
-      context "when assigned_user_id is set" do
-        let(:assigned_user_id) { "1" }
-
-        context "to the same value it was" do
-          before(:each) do
-            order_detail.update_attribute(:assigned_user_id, assigned_user_id)
+        context "and the assigned_user_id parameter is blank" do
+          it "does not change the assigned_user_id" do
+            expect { updater.update! }
+              .not_to change { order_detail.reload.assigned_user_id }
+              .from(2)
           end
+
+          it "returns a no-changes-required note" do
+            expect(updater.update!).to eq(notice: "No changes were required")
+          end
+        end
+
+        context "and the assigned_user_id parameter is a new value" do
+          let(:assigned_user_id) { "1" }
+
+          it "updates assigned_user_id to the new value" do
+            expect { updater.update! }
+              .to change { order_detail.reload.assigned_user_id }
+              .from(2)
+              .to(1)
+          end
+
+          it "returns a successful update note" do
+            expect(updater.update!)
+              .to eq(notice: "The #{record_type} were successfully updated")
+          end
+        end
+
+        context "and the assigned_user_id parameter is the same value" do
+          let(:assigned_user_id) { "2" }
 
           it "does not change the assigned_user_id" do
             expect { updater.update! }
               .not_to change { order_detail.reload.assigned_user_id }
-              .from(1)
+              .from(2)
           end
 
           it "returns a successful update note (despite nothing changing)" do
             expect(updater.update!)
-              .to eq(notice: "The orders were successfully updated")
+              .to eq(notice: "The #{record_type} were successfully updated")
+          end
+        end
+      end
+
+      context "when assigned_user_id is not set" do
+        context "and the assigned_user_id parameter is blank" do
+          it "does not change the assigned_user_id from nil" do
+            expect { updater.update! }
+              .not_to change { order_detail.reload.assigned_user_id }
+              .from(nil)
+          end
+
+          it "returns a no-changes-required note" do
+            expect(updater.update!).to eq(notice: "No changes were required")
           end
         end
 
-        context "to a new value" do
-          it "updates the assigned_user_id" do
+        context "and the assigned_user_id parameter is a new value" do
+          let(:assigned_user_id) { "1" }
+
+          it "updates assigned_user_id to the new value" do
             expect { updater.update! }
               .to change { order_detail.reload.assigned_user_id }
               .from(nil)
               .to(1)
           end
+
+          it "returns a successful update note" do
+            expect(updater.update!)
+              .to eq(notice: "The #{record_type} were successfully updated")
+          end
         end
 
-        it "returns a successful update note" do
-          expect(updater.update!)
-            .to eq(notice: "The orders were successfully updated")
+        context "and the assigned_user_id parameter is set to 'unassign'" do
+          it "does not change the assigned_user_id from nil" do
+            expect { updater.update! }
+              .not_to change { order_detail.reload.assigned_user_id }
+              .from(nil)
+          end
+
+          it "returns a no-changes-required note" do
+            expect(updater.update!).to eq(notice: "No changes were required")
+          end
         end
       end
 
-      context "when assigned_user_id is blank" do
-        before { order_detail.update_attribute(:assigned_user_id, 1) }
-
-        it "does not change the assigned_user_id" do
-          expect { updater.update! }
-            .not_to change { order_detail.reload.assigned_user_id }
-            .from(1)
+      context "when order_status_id is already set" do
+        before(:each) do
+          order_detail.update_attribute(:order_status_id, 2)
         end
 
+        context "and the order_status_id parameter is blank" do
+          it "does not change the order_status_id" do
+            expect { updater.update! }
+              .not_to change { order_detail.reload.order_status_id }
+              .from(2)
+          end
+
+          it "returns a no-changes-required note" do
+            expect(updater.update!).to eq(notice: "No changes were required")
+          end
+        end
+
+        context "and the order_status_id parameter is a new value" do
+          let(:order_status_id) { "1" }
+
+          it "updates order_status_id to the new value" do
+            expect { updater.update! }
+              .to change { order_detail.reload.order_status_id }
+              .from(2)
+              .to(1)
+          end
+
+          it "returns a successful update note" do
+            expect(updater.update!)
+              .to eq(notice: "The #{record_type} were successfully updated")
+          end
+        end
+
+        context "and the order_status_id parameter is the same value" do
+          let(:order_status_id) { "2" }
+
+          it "does not change the order_status_id" do
+            expect { updater.update! }
+              .not_to change { order_detail.reload.order_status_id }
+              .from(2)
+          end
+
+          it "returns a successful update note (despite nothing changing)" do
+            expect(updater.update!)
+              .to eq(notice: "The #{record_type} were successfully updated")
+          end
+        end
+      end
+
+      context "when order_status_id is not set" do
         it { expect(updater.update!).to eq(notice: "No changes were required") }
       end
+    end
 
-      context "when order_status_id is set" do
-        let(:order_status_id) { "1" }
-      end
+    context "when associated with Orders" do
+      let(:record_type) { "orders" }
 
-      context "when order_status_id is blank" do
-        it { expect(updater.update!).to eq(notice: "No changes were required") }
+      it_behaves_like "batch updating" do
+        context "when assigned_user_id is already set" do
+          before(:each) do
+            order_detail.update_attribute(:assigned_user_id, 2)
+          end
+
+          context "and the assigned_user_id parameter is set to 'unassign'" do
+            let(:assigned_user_id) { "unassign" }
+
+            it "changes the assigned_user_id to nil" do
+              expect { updater.update! }
+                .to change { order_detail.reload.assigned_user_id }
+                .from(2)
+                .to(nil)
+            end
+
+            it "returns a successful update note" do
+              expect(updater.update!)
+                .to eq(notice: "The #{record_type} were successfully updated")
+            end
+          end
+        end
       end
     end
 
     context "when associated with Reservations" do
       let(:record_type) { "reservations" }
+
+      it_behaves_like "batch updating"
     end
   end
 end
