56400388762793bf1337b8eef63ac6f2459ae7f2
Dan Rench <dan.rench@tablexi.com>
2016-05-13 16:13:48 -0500

    Split active/inactive Projects by tab on the index page

--- vendor/engines/projects/app/controllers/projects/projects_controller.rb
+++ vendor/engines/projects/app/controllers/projects/projects_controller.rb
@@ -8,7 +8,9 @@ module Projects
     load_and_authorize_resource through: :current_facility
 
     def index
-      @projects = @projects.paginate(page: params[:page])
+      filter_projects_for_index
+      @active_project_count = Project.active.count
+      @inactive_project_count = Project.inactive.count
     end
 
     def new
@@ -34,6 +36,11 @@ module Projects
       render action: :edit unless save_project
     end
 
+    def showing_archived?
+      params[:archived] == "true"
+    end
+    helper_method :showing_archived?
+
     private
 
     def project_params
@@ -46,6 +53,11 @@ module Projects
           I18n.t("controllers.projects.projects.#{action_name}.success", project_name: @project.name)
         redirect_to facility_projects_path(@project.facility)
       end
+    end
+
+    def filter_projects_for_index
+      @projects = showing_archived? ? @projects.inactive : @projects.active
+      @projects = @projects.paginate(page: params[:page])
     end
 
   end
--- vendor/engines/projects/app/models/projects/project.rb
+++ vendor/engines/projects/app/models/projects/project.rb
@@ -13,6 +13,7 @@ module Projects
               uniqueness: { case_sensitive: false, scope: :facility_id }
 
     scope :active, -> { where(active: true) }
+    scope :inactive, -> { where(active: false) }
 
     def to_s
       name
