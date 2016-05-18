tablexi/nucore-open

commit f9e3290ba5c190ebb7b874bca240329a1670b011

[#47665] Purchaser/Owner/BA can change change order detail account (#543)
* [#47665] Purchaser/owner/BA ability to change account on order

Also, refactor the order details controller to properly use cancan.

* [#47665] Prevent editing reviewed or canceled orders

* [#47665] Cleanup

* rubocop

app/controllers/order_details_controller.rb
@@ -7,11 +7,27 @@ class OrderDetailsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :check_acting_as, except: [:order_file, :upload_order_file, :remove_order_file]
   before_filter :init_order_detail
-  after_filter :set_active_tab
+  before_filter :set_active_tab
+  authorize_resource
 
-  def initialize
-    @active_tab = "orders"
-    super
+  # GET /orders/:order_id/order_details/:id
+  def show
+    @order_detail.send(:extend, PriceDisplayment)
+  end
+
+  # GET /orders/:order_id/order_details/:id
+  def edit
+  end
+
+  # Put /orders/:order_id/order_details/:id
+  def update
+    if order_editable? && @order_detail.update_attributes(order_detail_params)
+      flash[:notice] = I18n.t("order_details.update.success")
+      redirect_to [@order, @order_detail]
+    else
+      flash.now[:error] = I18n.t("order_details.update.failure")
+      render :edit
+    end
   end
 
   # PUT /orders/:order_id/order_details/:id/cancel
@@ -32,6 +48,7 @@ class OrderDetailsController < ApplicationController
     end
   end
 
+  # PUT /orders/:order_id/order_details/:id/dispute
   def dispute
     raise ActiveRecord::RecordNotFound unless @order_detail.can_dispute?
 
@@ -52,18 +69,6 @@ class OrderDetailsController < ApplicationController
         raise ActiveRecord::Rollback
       end
     end
-  end
-
-  # GET /orders/:order_id/order_details/:id
-  def show
-    set_active_tab
-    @order_detail.send(:extend, PriceDisplayment)
-  end
-
-  def init_order_detail
-    @order = Order.find(params[:order_id])
-    @order_detail = @order.order_details.find(params[:id] || params[:order_detail_id])
-    raise ActiveRecord::RecordNotFound unless @order.to_be_merged? || @order_detail.can_be_viewed_by?(acting_user)
   end
 
   # GET /orders/:order_id/order_details/:order_detail_id/order_file
@@ -105,14 +110,29 @@ class OrderDetailsController < ApplicationController
     redirect_to(order_path(@order))
   end
 
+  private
+
+  def order_editable?
+    @order_detail.customer_editable?
+  end
+  helper_method :order_editable?
+
+  def init_order_detail
+    @order = Order.find(params[:order_id])
+    @order_detail = @order.order_details.find(params[:id] || params[:order_detail_id])
+    raise ActiveRecord::RecordNotFound if @order.to_be_merged?
+  end
+
   def set_active_tab
     @active_tab = @order_detail.reservation.nil? ? "orders" : "reservations"
   end
 
-  private
-
   def ability_resource
     @order_detail
+  end
+
+  def order_detail_params
+    params.require(:order_detail).permit(:account_id)
   end
 
 end

spec/controllers/order_details_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe OrderDetailsController do
   let(:user) { order_detail.user }
 
   describe "#dispute" do
-    let(:params) { { order_id: order.id, order_detail_id: order_detail.id } }
+    let(:params) { { order_id: order.id, id: order_detail.id } }
     before { sign_in user }
 
     context "when the order is not disputable" do
@@ -43,17 +43,17 @@ RSpec.describe OrderDetailsController do
   describe "#show" do
     before(:each) do
       sign_in user
-      get :show, order_id: order.id, order_detail_id: order_detail.id
+      get :show, order_id: order.id, id: order_detail.id
     end
 
     context "when logged in as the user who owns the order" do
-      it { expect(response.code).to eq("200") }
+      it { expect(response).to access_the_page }
     end
 
     context "when logged in as a user that does not own the order" do
       let(:user) { create(:user) }
 
-      it { expect(response.code).to eq("404") }
+      it { expect(response.code).to eq("403") }
     end
   end
 
@@ -65,7 +65,7 @@ RSpec.describe OrderDetailsController do
         context "and the reservation is cancelable" do
           before(:each) do
             expect(reservation).to be_can_cancel
-            put :cancel, order_id: order.id, order_detail_id: order_detail.id
+            put :cancel, order_id: order.id, id: order_detail.id
           end
 
           it { expect(order_detail.reload).to be_canceled }
@@ -74,11 +74,156 @@ RSpec.describe OrderDetailsController do
         context "and the reservation is not cancelable" do
           before do
             reservation.update_attributes(actual_start_at: Time.current)
-            put :cancel, order_id: order.id, order_detail_id: order_detail.id
+            put :cancel, order_id: order.id, id: order_detail.id
           end
 
           it { expect(order_detail.reload).not_to be_canceled }
           it { expect(response.code).to eq("404") }
+        end
+      end
+    end
+  end
+
+  describe "edit/update" do
+    shared_examples_for "allows the proper users" do
+      describe "as the account owner" do
+        let(:signed_in_user) { order_detail.account.owner_user }
+        before { perform }
+
+        it "succeeds" do
+          expect(response).to access_the_page
+        end
+      end
+
+      describe "as a business admin" do
+        let(:signed_in_user) { FactoryGirl.create(:user) }
+        before do
+          FactoryGirl.create(:account_user, :business_administrator,
+                             user: signed_in_user, account: order_detail.account)
+          perform
+        end
+
+        it "succeeds" do
+          expect(response).to access_the_page
+        end
+      end
+
+      describe "as an account purchaser" do
+        let(:signed_in_user) { FactoryGirl.create(:user) }
+        before do
+          FactoryGirl.create(:account_user, :purchaser,
+                             user: signed_in_user, account: order_detail.account)
+          perform
+        end
+
+        it "does not load" do
+          expect(response.code).to eq("403")
+        end
+      end
+
+      describe "as the purchaser" do
+        let(:signed_in_user) { FactoryGirl.create(:user) }
+        before do
+          FactoryGirl.create(:account_user, :purchaser,
+                             user: signed_in_user, account: order_detail.account)
+          order.update_attributes(user: signed_in_user)
+          perform
+        end
+
+        it "succeeds" do
+          expect(response).to access_the_page
+        end
+      end
+
+      describe "as a random user" do
+        let(:signed_in_user) { FactoryGirl.create(:user) }
+        before { perform }
+
+        it "does not load" do
+          expect(response.code).to eq("403")
+        end
+      end
+    end
+
+    describe "#edit" do
+      def perform
+        sign_in signed_in_user
+        get :edit, order_id: order.id, id: order_detail.id
+      end
+
+      it_behaves_like "allows the proper users"
+    end
+
+    describe "#update" do
+      # need some params to satisfy strong params
+      let(:params) { { field: "dummy" } }
+      def perform
+        sign_in signed_in_user
+        put :update, order_id: order.id, id: order_detail.id, order_detail: params
+      end
+
+      it_behaves_like "allows the proper users"
+
+      describe "updating the account" do
+        let(:signed_in_user) { user }
+        let(:account2) { FactoryGirl.create(:setup_account, owner: user) }
+        let(:params) { { account_id: account2.id } }
+
+        shared_examples_for "changes the account" do
+          it "changes the account" do
+            expect { perform }.to change { order_detail.reload.account }.to(account2)
+          end
+        end
+
+        shared_examples_for "does not change the account" do
+          it "does not change the account" do
+            expect { perform }.not_to change { order_detail.reload.account }
+          end
+        end
+
+        describe "before completion" do
+          it_behaves_like "changes the account"
+        end
+
+        describe "after completion" do
+          let(:reservation) { FactoryGirl.create(:completed_reservation) }
+
+          describe "while in the review period" do
+            before { order_detail.update_attributes!(reviewed_at: 7.days.from_now) }
+            it_behaves_like "changes the account"
+          end
+
+          describe "when the review period is over" do
+            before { order_detail.update_attributes!(reviewed_at: 1.day.ago) }
+            it_behaves_like "does not change the account"
+          end
+
+          describe "when the review period is over, but it is being disputed" do
+            before { order_detail.update_attributes!(reviewed_at: 1.day.ago, dispute_at: 2.days.ago, dispute_reason: "reason") }
+            it_behaves_like "changes the account"
+          end
+
+          describe "when the dispute has been resolved" do
+            before { order_detail.update_attributes!(reviewed_at: 1.day.ago, dispute_at: 2.days.ago, dispute_resolved_at: Time.current, dispute_reason: "reason", dispute_resolved_reason: "notes") }
+            it_behaves_like "does not change the account"
+          end
+        end
+
+        describe "a canceled reservation" do
+          before do
+            order_detail.update_order_status!(user, OrderStatus.canceled_status)
+          end
+          it_behaves_like "does not change the account"
+        end
+      end
+
+      describe "does not update other fields" do
+        let(:signed_in_user) { user }
+        let(:params) { { actual_cost: 113, product_id: 999, user_id: 111, dispute_reason: "Dispute" } }
+
+        it "is successful, but does not change any fields" do
+          expect { perform }.not_to change { order_detail.reload.attributes }
+          expect(response).to be_redirect
         end
       end
     end

app/assets/stylesheets/app/typography.scss
@@ -40,6 +40,10 @@ label {
   margin-bottom: 0;
 }
 
+.normal-weight {
+  font-weight: normal;
+}
+
 // Custom headers
 .header-tight {
   line-height: 1.1;

app/controllers/orders_controller.rb
@@ -185,7 +185,7 @@ class OrdersController < ApplicationController
 
     redirect_to(cart_path) && return unless @product
 
-    @accounts = acting_user.accounts.for_facility(@product.facility).active
+    @accounts = AvailableAccountsFinder.new(acting_user, @product.facility).accounts
     @errors   = {}
     details   = @order.order_details
     @accounts.each do |account|

app/helpers/breadcrumb_helper.rb
@@ -1,0 +1,21 @@
+module BreadcrumbHelper
+
+  def order_reservation_breadcrumb
+    link_to my_breadcrumb_label, my_breadcrumb_path
+  end
+
+  private
+
+  def my_breadcrumb_label
+    if @active_tab == "reservations"
+      t_my(Reservation)
+    else
+      t_my(Order)
+    end
+  end
+
+  def my_breadcrumb_path
+    public_send("#{@active_tab}_path")
+  end
+
+end

app/models/order_detail.rb
@@ -559,6 +559,10 @@ class OrderDetail < ActiveRecord::Base
     in_review?
   end
 
+  def customer_editable?
+    !reviewed? && !canceled?
+  end
+
   def validate_for_purchase
     # can purchase product
     return "The product may not be purchased" unless product.available_for_purchase?

app/services/available_accounts_finder.rb
@@ -1,0 +1,16 @@
+class AvailableAccountsFinder
+
+  def initialize(user, facility, current: nil)
+    @user = user
+    @facility = facility
+    @current_account = current
+  end
+
+  def accounts
+    accounts = @user.accounts.for_facility(@facility).active
+    accounts += [@current_account] if @current_account
+    accounts
+  end
+  alias to_a accounts
+
+end

app/views/order_details/edit.html.haml
@@ -1,0 +1,17 @@
+= content_for :breadcrumb do
+  %ul.breadcrumb
+    %li= order_reservation_breadcrumb
+    %li &raquo;
+    %li= link_to t("order_details.show.head.h1", order_number: @order_detail), [@order, @order_detail]
+
+= content_for :h1 do
+  = t("order_details.show.head.h1", order_number: @order_detail)
+
+= simple_form_for([@order, @order_detail]) do |f|
+  %p.alert.alert-warning= t(".warning")
+
+  = f.input :account_id, collection: AvailableAccountsFinder.new(acting_user, @order_detail.facility, current: @order_detail.account), include_blank: false, label: Account.model_name.human
+
+  %ul.inline
+    %li= f.submit t(".save"), class: ["btn", "btn-primary"]
+    %li= link_to t(".cancel"), [@order, @order_detail]

commit 94f248838a4240d7c85d3e12e524fb953c7d4039

[#126252] Could not upload files when adding to existing order
This was broken by #543

app/controllers/order_details_controller.rb
@@ -128,7 +128,6 @@ class OrderDetailsController < ApplicationController
   def init_order_detail
     @order = Order.find(params[:order_id])
     @order_detail = @order.order_details.find(params[:id] || params[:order_detail_id])
-    raise ActiveRecord::RecordNotFound if @order.to_be_merged?
   end
 
   def set_active_tab

spec/controllers/order_details_controller_spec.rb
@@ -346,4 +346,33 @@ RSpec.describe OrderDetailsController do
       end
     end
   end
+
+  describe "#order_file" do
+    let(:product) { create(:setup_service, :with_order_form) }
+    let(:order_detail) { order.order_details.first }
+    let(:facility) { order.facility }
+
+    let(:params) { { order_id: order.id, id: order_detail.id } }
+    before { sign_in user }
+
+    describe "while in the cart" do
+      let(:order) { create(:setup_order, product: product) }
+
+      it "has access" do
+        get :order_file, params
+        expect(response).to be_success
+      end
+    end
+
+    describe "adding to an existing order" do
+      let(:order) { create(:purchased_order, product: product) }
+      let(:user) { create(:user, :staff, facility: facility) }
+      let(:merge_order) { create(:setup_order, product: product, account: order.account, user: order.user, merge_with_order_id: order.id) }
+
+      it "has access" do
+        get :order_file, order_id: merge_order.id, id: merge_order.order_details.first.id
+        expect(response).to be_success
+      end
+    end
+  end
 end

spec/factories/orders.rb
@@ -19,8 +19,9 @@ FactoryGirl.define do
     end
 
     after(:create) do |order, evaluator|
-      create(:account_price_group_member, account: order.account, price_group: evaluator.product.facility.price_groups.last)
-      FactoryGirl.create(:user_price_group_member, user: evaluator.user, price_group: evaluator.product.facility.price_groups.last)
+      # build().save will allow an already existing relation without raising an error
+      build(:account_price_group_member, account: order.account, price_group: evaluator.product.facility.price_groups.last).save
+      build(:user_price_group_member, user: evaluator.user, price_group: evaluator.product.facility.price_groups.last).save
       order.add(evaluator.product)
     end
 

spec/factories/products.rb
@@ -79,6 +79,12 @@ FactoryGirl.define do
           product.facility.facility_accounts.create(attributes_for(:facility_account))
       end
     end
+
+    trait :with_order_form do
+      after(:create) do |product|
+        create(:stored_file, :template, product: product)
+      end
+    end
   end
 
   factory :setup_instrument, class: Instrument, parent: :setup_product do

spec/factories/stored_files.rb
@@ -11,4 +11,11 @@ FactoryGirl.define do
     name { "clean_import.csv" }
     association :creator, factory: :user
   end
+
+  trait :template do
+    file { StringIO.new("c,s,v") }
+    file_type { "template" }
+    name { "template.csv" }
+    association :creator, factory: :user
+  end
 end