82b3da85624e30710a8c1ed447c030bb8c92c9a4
Jason Hanggi <jason@tablexi.com>
2016-05-20 20:56:49 -0500

    Fix accessories test failures
    This reverts "[REVISIT] Remove instrument_with_accessory factory" commit 1595724b40911fec35ff2f180b8c2e071a78c08c
    
    and makes a couple additional changes to fix some test failures.

--- spec/factories/accessories.rb
+++ spec/factories/accessories.rb
@@ -1,4 +1,14 @@
 FactoryGirl.define do
+  factory :instrument_with_accessory, parent: :setup_instrument do
+    transient do
+      accessory { create :setup_item, facility: facility }
+    end
+
+    after(:create) do |instrument, evaluator|
+      instrument.accessories << evaluator.accessory
+    end
+  end
+
   factory :accessory, parent: :setup_item do
     transient do
       parent { create :setup_instrument, facility: facility }
