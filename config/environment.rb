01e409bc03851b8266358bdae2b14ce239bd615a
Dan Rench <dan.rench@tablexi.com>
2016-05-24 10:50:10 -0500

    Restore the MySQL 5.7 patch
    This becomes unnecessary with Rails 4.1 and above.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path("../application", __FILE__)
 
 # Initialize the rails application
 Nucore::Application.initialize!
+
+# TODO:
+# MySQL 5.7+ does not allow NULL primary keys, but older versions of Rails
+# create primary key columns DEFAULT NULL.
+#
+# Remove this require and the patch file itself after upgrading to Rails 4.1
+# https://github.com/rails/rails/pull/13247#issuecomment-32425844
+if Rails.version < "4.1"
+  if defined?(ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter)
+    require File.expand_path("../../lib/patches/abstract_mysql_adapter", __FILE__)
+  end
+else
+  raise "Remove the reference to the abstract_mysql_adapter patch in #{__FILE__} for Rails 4.1+"
+end
