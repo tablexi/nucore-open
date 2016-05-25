ce41a8e62b17067d2070b9daf3d3e16177d5960a
Jason Hanggi <jason@tablexi.com>
2016-05-25 11:42:46 -0500

    Add spec for project searching

--- vendor/engines/projects/spec/controllers/global_search_controller_spec.rb
+++ vendor/engines/projects/spec/controllers/global_search_controller_spec.rb
@@ -7,4 +7,34 @@ RSpec.describe GlobalSearchController do
         .to include(Projects::GlobalSearch::ProjectSearcher)
     end
   end
+
+  describe "a facility staff" do
+    let(:facility) { project.facility }
+    let(:project) { FactoryGirl.create(:project) }
+    let(:user) { FactoryGirl.create(:user, :staff, facility: facility) }
+    let(:results) { assigns[:searchers].find { |s| s.template == "projects" }.results }
+    before { sign_in user }
+
+    context "while not in the facility" do
+      it "finds the project" do
+        get :index, search: project.name
+        expect(results).to include(project)
+      end
+    end
+
+    context "while in another facility" do
+      let(:facility2) { FactoryGirl.create(:facility) }
+      it "does not find the project" do
+        get :index, facility_id: facility2, search: project.name
+        expect(results).not_to include(project)
+      end
+    end
+
+    context "while in the facility" do
+      it "finds the project" do
+        get :index, facility: facility, search: project.name
+        expect(results).to include(project)
+      end
+    end
+  end
 end
