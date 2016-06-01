f13f11d77ff063cd9dc866ff3be5f6422e363c17
Dan Rench <dan.rench@tablexi.com>
2016-06-01 10:41:01 -0500

    Push to add to add engine migrations
    Adding worked in Rails 3.2 but not 4.0 and above.

--- vendor/engines/sanger_sequencing/lib/sanger_sequencing/engine.rb
+++ vendor/engines/sanger_sequencing/lib/sanger_sequencing/engine.rb
@@ -11,8 +11,8 @@ module SangerSequencing
     end
 
     initializer :append_migrations do |app|
-      unless app.root.to_s.match(root.to_s)
-        app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
+      config.paths["db/migrate"].expanded.each do |expanded_path|
+        app.config.paths["db/migrate"] << expanded_path
       end
     end
 
