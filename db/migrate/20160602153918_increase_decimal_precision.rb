1ec243f3738f0533355f3bd64ea833bc45713ef8
Dan Rench <dan.rench@tablexi.com>
2016-06-02 11:07:36 -0500

    Increase decimal precision on PricePolicy rates
    This brings the overage_rate and reservation_rate precision in line with
    usage_rate, which is to 4 decimal places.
    
    There are specs setting rates to values like "0.002777777777777783333333333"
    which would be truncated to zero otherwise.

---
+++ db/migrate/20160602153918_increase_decimal_precision.rb
@@ -0,0 +1,11 @@
+class IncreaseDecimalPrecision < ActiveRecord::Migration
+  def up
+    change_column :price_policies, :overage_rate, :decimal, precision: 12, scale: 4
+    change_column :price_policies, :reservation_rate, :decimal, precision: 12, scale: 4
+  end
+
+  def down
+    change_column :price_policies, :overage_rate, :decimal, precision: 10, scale: 2
+    change_column :price_policies, :reservation_rate, :decimal, precision: 10, scale: 2
+  end
+end
