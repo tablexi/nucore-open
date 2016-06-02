3355e84c6073326e6aa3ecc95bf271c508495bf2
Dan Rench <dan.rench@tablexi.com>
2016-06-02 17:51:09 -0500

    Remove code commented-out 4 years ago

--- app/helpers/facility_order_status_helper.rb
+++ app/helpers/facility_order_status_helper.rb
@@ -16,20 +16,9 @@ module FacilityOrderStatusHelper
       facility_ods.joins(:order_status)
                   .joins("LEFT JOIN #{User.table_name} ON order_details.assigned_user_id = #{User.table_name}.id")
                   .order("#{User.table_name}.last_name #{sort_direction}, #{User.table_name}.first_name #{sort_direction}, order_statuses.name, orders.ordered_at")
-    # facility_ods.find(:all,
-    #                   :joins => ['INNER JOIN order_statuses ON order_details.order_status_id = order_statuses.id ',
-    #                              'INNER JOIN orders ON orders.id = order_details.order_id ',
-    #                              "LEFT JOIN #{User.table_name} ON order_details.assigned_user_id = #{User.table_name}.id "],
-    #                   :conditions => ['(order_details.state = ? OR order_details.state = ?) AND orders.state = ?', 'new', 'inprocess', 'purchased'],
-    #                   :order => "#{User.table_name}.last_name #{sort_direction}, #{User.table_name}.first_name #{sort_direction}, order_statuses.name, orders.ordered_at")
     when "status"
       facility_ods.joins(:order_status)
                   .order("order_statuses.name #{sort_direction}, orders.ordered_at")
-    # facility_ods.find(:all,
-    #                   :joins => ['INNER JOIN orders ON orders.id = order_details.order_id ',
-    #                              'INNER JOIN order_statuses ON order_details.order_status_id = order_statuses.id '],
-    #                   :conditions => ['(order_details.state = ? OR order_details.state = ?) AND orders.state = ?', 'new', 'inprocess', 'purchased'],
-    #                   :order => "order_statuses.name #{sort_direction}, orders.ordered_at")
     else
       facility_ods.new_or_inprocess
     end
