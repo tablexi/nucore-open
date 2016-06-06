b8940fb0ba729864e1d743506062c6f77452fbe1
Jason Hanggi <jason@tablexi.com>
2016-06-06 15:59:21 -0500

    [#120813297] Enable Sanger tab per-facility
    I chose to not add a way to do this in the UI, since the vast majority
    of facilities will not need it. We can turn it on for facilities as
    needed.

---
+++ vendor/engines/sanger_sequencing/db/migrate/20160606205228_add_sanger_option_to_facility.rb
@@ -0,0 +1,7 @@
+class AddSangerOptionToFacility < ActiveRecord::Migration
+
+  def change
+    add_column :facilities, :sanger_sequencing_enabled, :boolean, null: false, default: false
+  end
+
+end
