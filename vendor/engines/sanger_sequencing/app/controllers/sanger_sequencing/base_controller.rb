3c4346b977fbdc15822be201a506d27aab08f21e
Jason Hanggi <jason@tablexi.com>
2016-06-23 11:42:27 -0500

    handle null current_facility

--- vendor/engines/sanger_sequencing/app/controllers/sanger_sequencing/base_controller.rb
+++ vendor/engines/sanger_sequencing/app/controllers/sanger_sequencing/base_controller.rb
@@ -7,7 +7,7 @@ module SangerSequencing
     private
 
     def assert_sanger_enabled_for_facility
-      raise ActionController::RoutingError, "Sanger not enabled for this facility" unless current_facility.sanger_sequencing_enabled?
+      raise ActionController::RoutingError, "Sanger not enabled for this facility" unless current_facility.try(:sanger_sequencing_enabled?)
     end
 
   end
