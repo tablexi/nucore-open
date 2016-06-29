55e5fe923f5f14581236fc95a9d9e5f3aefd62b1
Dan Rench <dan.rench@tablexi.com>
2016-06-29 16:34:06 -0500

    Split migration into up/down to allow rollback

--- db/migrate/20160614185348_add_type_to_reservations.rb
+++ db/migrate/20160614185348_add_type_to_reservations.rb
@@ -1,8 +1,14 @@
 class AddTypeToReservations < ActiveRecord::Migration
 
-  def change
+  def up
     add_column :reservations, :type, :string
     change_column :reservations, :reserve_end_at, :datetime, null: true
+  end
+
+  def down
+    Reservation.where.not(type: nil).delete_all
+    remove_column :reservations, :type
+    change_column :reservations, :reserve_end_at, :datetime, null: false
   end
 
 end
