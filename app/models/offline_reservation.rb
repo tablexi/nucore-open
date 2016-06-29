36ac5b892ee45ada08344547e0aaeaebbb11239c
Dan Rench <dan.rench@tablexi.com>
2016-06-29 11:43:42 -0500

    Only ongoing offline reservations should be editable

--- app/models/offline_reservation.rb
+++ app/models/offline_reservation.rb
@@ -10,19 +10,27 @@ class OfflineReservation < Reservation
   end
 
   def as_calendar_object(options = nil)
-    {
-      "admin" => true,
-      "start" => I18n.l(reserve_start_at, format: :calendar),
-      "end" => false,
-      "allDay" => false,
-      "offline" => true,
-      "title" => "Offline\n#{admin_note}",
-      "product" => product.name,
-      "editPath" => edit_path,
-    }
+    if reserve_end_at.present?
+      calendar_hash_defaults.merge(
+        "end" => I18n.l(reserve_end_at, format: :calendar),
+      )
+    else
+      calendar_hash_defaults.merge("editPath" => edit_path)
+    end
   end
 
   private
+
+  def calendar_hash_defaults
+    {
+      "admin" => true,
+      "allDay" => false,
+      "offline" => true,
+      "product" => product.name,
+      "start" => I18n.l(reserve_start_at, format: :calendar),
+      "title" => "Offline\n#{admin_note}",
+    }
+  end
 
   def edit_path
     Rails.application.routes.url_helpers.edit_facility_instrument_offline_reservation_path(facility, product, self)
