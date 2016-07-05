631cd7012ad12a56009b8a87a13bc467581e6cf3
Jason Hanggi <jason@tablexi.com>
2016-07-05 12:20:28 -0500

    [#120869939] Download Sanger Sequencing template file (#678)
    This modifies the batch creation so that reserved wells in unused rows don't get persisted: they appear as if they're blank in the well-plate export. But they still show up as "reserved" when creating the well plate.
    
    It also adds a ActionController::Renderer for CSV to the root app so we can do render csv: ObjectRespondingToToCSV.new, filename: "somefilename". There are a few places elsewhere in the app I'd like to swap this in in place of CSVHelper, but that seemed outside the scope of this PR.
    
    There are a few fields in the CSV that I'm not really sure if they're supposed to be dynamic or not. I figure we can address those when we meet with Chris next week.

---
+++ vendor/engines/sanger_sequencing/spec/presenters/sanger_sequencing/well_plate_presenter_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+RSpec.describe SangerSequencing::WellPlatePresenter do
+  let(:samples) { FactoryGirl.build_stubbed_list(:sanger_sequencing_sample, 10) }
+
+  describe "sample_rows" do
+    let(:mapping) do
+      {
+        "A01" => "reserved",
+        "A03" => samples[7].id,
+        "B01" => samples[0].id,
+        "B02" => "",
+        "C01" => samples[1].id,
+        "D01" => samples[2].id,
+        "E01" => samples[3].id,
+        "F01" => samples[4].id,
+        "G01" => samples[5].id,
+        "H01" => samples[6].id,
+      }
+    end
+
+    let(:well_plate) { SangerSequencing::WellPlate.new(mapping, samples: samples) }
+    let(:presenter) { described_class.new(well_plate) }
+    let(:sample_rows) { presenter.sample_rows }
+    let(:expected_results_group) { a_kind_of(String) }
+    let(:expected_instrument_protocol) { a_kind_of(String) }
+    let(:expected_analysis_protocol) { a_kind_of(String) }
+
+    it "renders" do
+      expect(sample_rows[0]).to match(["A01", "", "", expected_results_group, expected_instrument_protocol, expected_analysis_protocol])
+      expect(sample_rows[1]).to match(["B01", samples[0].id.to_s, samples[0].customer_sample_id, expected_results_group, expected_instrument_protocol, expected_analysis_protocol])
+      expect(sample_rows[2]).to match(["C01", samples[1].id.to_s, samples[1].customer_sample_id, expected_results_group, expected_instrument_protocol, expected_analysis_protocol])
+      expect(sample_rows[3]).to match(["D01", samples[2].id.to_s, samples[2].customer_sample_id, expected_results_group, expected_instrument_protocol, expected_analysis_protocol])
+      expect(sample_rows[4]).to match(["E01", samples[3].id.to_s, samples[3].customer_sample_id, expected_results_group, expected_instrument_protocol, expected_analysis_protocol])
+      expect(sample_rows[5]).to match(["F01", samples[4].id.to_s, samples[4].customer_sample_id, expected_results_group, expected_instrument_protocol, expected_analysis_protocol])
+      expect(sample_rows[6]).to match(["G01", samples[5].id.to_s, samples[5].customer_sample_id, expected_results_group, expected_instrument_protocol, expected_analysis_protocol])
+      expect(sample_rows[7]).to match(["H01", samples[6].id.to_s, samples[6].customer_sample_id, expected_results_group, expected_instrument_protocol, expected_analysis_protocol])
+      expect(sample_rows[8]).to match(["A03", samples[7].id.to_s, samples[7].customer_sample_id, expected_results_group, expected_instrument_protocol, expected_analysis_protocol])
+    end
+  end
+end
