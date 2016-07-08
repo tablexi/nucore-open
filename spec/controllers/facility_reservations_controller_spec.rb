9d330d23be61e3b10a4bd3c310fea5225d45c567
Dan Rench <dan.rench@tablexi.com>
2016-07-08 10:25:36 -0500

    Clean up specs + set split_times param

--- spec/controllers/facility_reservations_controller_spec.rb
+++ spec/controllers/facility_reservations_controller_spec.rb
@@ -102,53 +102,68 @@ RSpec.describe FacilityReservationsController do
   end
 
   context '#create' do
-    before :each do
+    let(:reservation_attributes) do
+      FactoryGirl.attributes_for(
+        :reservation,
+        reserve_start_at: reserve_start_at,
+        reserve_end_at: reserve_end_at,
+        split_times: true,
+      )
+    end
+    let(:reserve_start_at) { 1.hour.from_now.change(sec: 0) }
+    let(:reserve_end_at) { reserve_start_at + 1.hour }
+
+    before(:each) do
       @method = :post
       @action = :create
-      @time = 1.hour.from_now.change(sec: 0)
       @params = {
-        facility_id: @authable.url_name,
-        instrument_id: @product.url_name,
-        reservation: FactoryGirl.attributes_for(:reservation, reserve_start_at: @time, reserve_end_at: @time + 1.hour),
+        facility_id: facility.url_name,
+        instrument_id: product.url_name,
+        reservation: reservation_attributes,
       }
       parametrize_dates(@params[:reservation], :reserve)
     end
 
     it_should_allow_operators_only(:redirect) {}
 
-    context "while signed in" do
-      before :each do
-        maybe_grant_always_sign_in :director
-      end
-      context "a success" do
-        before(:each) { do_request }
-        it "should create the reservation" do
-          expect(assigns[:reservation]).not_to be_nil
+    context "when signed in as a facility director" do
+      before { maybe_grant_always_sign_in :director }
+
+      context "when the reservation is valid" do
+        before { do_request }
+
+        it "creates the reservation", :aggregate_failures do
+          expect(assigns[:reservation]).to be_present
           expect(assigns[:reservation]).not_to be_new_record
         end
-        it "should be an admin reservation" do
+
+        it "is an admin reservation" do
           expect(assigns[:reservation]).to be_admin
         end
-        it "should set the times" do
-          expect(assigns[:reservation].reserve_start_at).to eq(@time)
-          expect(assigns[:reservation].reserve_end_at).to eq(@time + 1.hour)
+
+        it "sets times", :aggregate_failures do
+          expect(assigns[:reservation].reserve_start_at).to eq(reserve_start_at)
+          expect(assigns[:reservation].reserve_end_at).to eq(reserve_end_at)
         end
-        it "should redirect to the facility's schedule page" do
-          expect(response).to redirect_to facility_instrument_schedule_path
+
+        it "redirects to the facility's schedule page" do
+          is_expected.to redirect_to facility_instrument_schedule_path
         end
       end
 
-      context "fails validations" do
-
-        it "should not allow an invalid reservation" do
+      context "when the reservation is invalid" do
+        before(:each) do
           # Used to fail by overlapping existing reservation, but now admin reservations are
           # allowed to per ticket 38975
           allow_any_instance_of(Reservation).to receive(:valid?).and_return(false)
           @params[:reservation] = FactoryGirl.attributes_for(:reservation)
           parametrize_dates(@params[:reservation], :reserve)
           do_request
+        end
+
+        it "does not save the reservation", :aggregate_failures do
           expect(assigns[:reservation]).to be_new_record
-          expect(response).to render_template :new
+          is_expected.to render_template :new
         end
       end
     end
