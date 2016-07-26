336968ad360d539e518e093fc1edd3cf9e5ab84c
Jason Hanggi <jason@tablexi.com>
2016-07-25 19:46:50 -0500

    Explicitly set RAILS_ENV in schedule.rb
    Without this, when rails loaded, it did not have an environment set, so
    it was trying to load up the `development` database.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
-rails = File.join Dir.getwd, "config", "environment.rb"
-require rails
+ENV['RAILS_ENV'] = @environment
+require File.expand_path(File.dirname(__FILE__) + "/environment")
 
 # IMPORTANT!!! You should never edit this file in your custom fork.
 # If you want to add custom jobs to your instance, add them to schedule_custom.rb
