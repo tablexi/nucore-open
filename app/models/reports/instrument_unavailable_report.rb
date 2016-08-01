e8f2df9fe2efd77cde2b1174bd943d5dff40561d
Dan Rench <dan.rench@tablexi.com>
2016-08-01 11:06:36 -0500

    Refactor report row processing

--- app/models/reports/instrument_unavailable_report.rb
+++ app/models/reports/instrument_unavailable_report.rb
@@ -20,10 +20,13 @@ module Reports
     end
 
     def rows
-      @rows ||= raw_values.map do |row|
-        row[:hours] = format("%.2f", row.delete(:seconds) / 3600.0).to_f
-        row
-      end.map(&:values)
+      @rows ||= grouped_query.map do |key, reservations|
+        key +
+          [
+            reservations.count,
+            format_as_hours(reservations_total_seconds(reservations)),
+          ]
+      end
     end
 
     def total_hours
@@ -36,13 +39,24 @@ module Reports
 
     private
 
-    def raw_values
-      query.each_with_object({}) do |row, report|
-        key = "#{row.product_id}/#{row.type}/#{row.category}"
-        report[key] ||= row_to_h(row)
-        report[key][:quantity] += 1
-        report[key][:seconds] += row_duration_in_seconds(row)
-      end.values
+    def format_as_hours(seconds)
+      format("%.2f", (seconds / 3600.0)).to_f
+    end
+
+    def grouped_query
+      @grouped_query ||= query.group_by do |res|
+        [
+          res.product.name,
+          res.type.to_s.sub(/Reservation\z/, ""),
+          reservation_category_label(res.category),
+        ]
+      end
+    end
+
+    def reservations_total_seconds(reservations)
+      reservations.inject(0) do |sum, r|
+        sum + row_duration_in_seconds(r)
+      end
     end
 
     # If a reservation begins and ends within the bounds of the report, then
@@ -70,16 +84,6 @@ module Reports
       reserve_end_at = end_time if reserve_end_at > end_time
       reserve_start_at = row.reserve_start_at > start_time ? row.reserve_start_at : start_time
       reserve_end_at - reserve_start_at
-    end
-
-    def row_to_h(row)
-      {
-        instrument_name: row.product.name,
-        type: row.type.to_s.sub(/Reservation\z/, ""),
-        category: reservation_category_label(row.category),
-        quantity: 0,
-        seconds: 0,
-      }
     end
 
     def query
