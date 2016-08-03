18efd2cfd5adffb551fcb69e89312f0d5ec4b645
Jason Hanggi <jason@tablexi.com>
2016-08-03 12:40:16 -0500

    Support for asynchronous file processing (#730)
    * Enable delayed_job as the ActiveJob adapter
    * Add "Upload" shared translation
    * Async file processing
    * Add view hook to sidenav global

---
+++ app/jobs/file_processing_job.rb
@@ -0,0 +1,14 @@
+# Used by models/concerns/async_file_processing in AsyncFileProcessing
+# This should only be used by a model including that class
+class FileProcessingJob < ApplicationJob
+
+  # process_file! is expected to raise an error if anything goes wrong
+  def perform(object)
+    object.process_file!
+    object.succeed!
+  rescue => e
+    message = e.message + e.backtrace.join("\n")
+    object.fail!(message)
+  end
+
+end
