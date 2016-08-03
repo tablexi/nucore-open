9db67faf8757ba907253f82b46f5afb30a1dd0ea
Jason Hanggi <jason@tablexi.com>
2016-08-03 11:56:52 -0500

    Remove sanger sample ON UPDATE foreign key (#732)
    Oracle doesn’t support ON UPDATE, and since it’s just the
    auto-increment primary key, it’ll never change, so we don’t need to
    worry about ON UPDATE.

---
+++ vendor/engines/sanger_sequencing/db/migrate/20160802202924_fix_sanger_sequencing_submission_cascades_for_oracle.rb
@@ -0,0 +1,10 @@
+class FixSangerSequencingSubmissionCascadesForOracle < ActiveRecord::Migration
+
+  def change
+    # Oracle does not support on_update so we need to remove and re-add it to keep
+    # consistent with the MySQL installations
+    remove_foreign_key :sanger_sequencing_samples, column: :submission_id
+    add_foreign_key :sanger_sequencing_samples, :sanger_sequencing_submissions, column: :submission_id, on_delete: :cascade
+  end
+
+end
