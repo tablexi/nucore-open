71545cea51d71af6423bc0406782f3730b3f5408
Matthew Lineen <matthew@lineen.com>
2016-08-08 14:54:24 -0500

    percent w

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server "nucore.stage.tablexi.com", user: "nucore", roles: %(web app db)
+server "nucore.stage.tablexi.com", user: "nucore", roles: %w(web app db)
 set :deploy_to, "/home/nucore/nucore.stage.tablexi.com"
 set :rails_env, "stage"
 set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
