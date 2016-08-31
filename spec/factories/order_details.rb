f04ecbc792351f39f28a5b6514ad55dd0bf08cce
Dan Rench <dan.rench@tablexi.com>
2016-08-31 15:09:40 -0500

    Add default_status trait to the OrderDetail factory

--- spec/factories/order_details.rb
+++ spec/factories/order_details.rb
@@ -2,5 +2,11 @@ FactoryGirl.define do
   factory :order_detail do
     quantity 1
     created_by 0
+
+    trait :default_status do
+      after(:create) do |order_detail|
+        order_detail.set_default_status!
+      end
+    end
   end
 end
