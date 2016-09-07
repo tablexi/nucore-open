99b8c3f6ab2908221d194db5ac8b6069d048ea25
Jason Hanggi <jason@tablexi.com>
2016-09-07 12:10:20 -0500

    [#132271] Order Imports showed processed_at in wrong timezone
    In Oracle, `processed_at` is a TIMESTAMP, not a DATETIME, and the
    adapter converts that to a Time, rather than a TimeWithZone. That means
    that it was being displayed in UTC, rather than the application
    timezone.
    
    In MySQL, the migration will have no effect.

---
+++ db/migrate/20160907170059_change_order_import_processed_at_type.rb
@@ -0,0 +1,12 @@
+# This has no effect in MySQL, but corrects a timezone issue in Oracle.
+class ChangeOrderImportProcessedAtType < ActiveRecord::Migration
+
+  def up
+    change_column :order_imports, :processed_at, :datetime
+  end
+
+  def down
+    change_column :order_imports, :processed_at, :timestamp
+  end
+
+end
