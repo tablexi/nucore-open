c30442d6a9c9276d18661dd65d947781112933b1
Dan Rench <dan.rench@tablexi.com>
2016-10-05 10:14:00 -0500

    Default recipient search to 'customers'

--- vendor/engines/bulk_email/app/helpers/bulk_email/application_helper.rb
+++ vendor/engines/bulk_email/app/helpers/bulk_email/application_helper.rb
@@ -6,6 +6,7 @@ module BulkEmail
 
     def admin_instrument_send_mail_path(instrument)
       facility_bulk_email_path(
+        bulk_email: { user_types: ["customers"] },
         products: [instrument.id],
         start_date: l(Date.today, format: :usa),
         end_date: l(7.days.from_now.to_date, format: :usa),
