2fc066c0401ded1d05589a42102f25e3654213d4
kt32291 <Katharine322@gmail.com>
2016-10-13 20:23:17 -0500

    Fixed last test

--- app/models/admin_reservation.rb
+++ app/models/admin_reservation.rb
@@ -2,8 +2,6 @@ class AdminReservation < Reservation
 
   include ActiveModel::ForbiddenAttributesProtection
 
-  validates :category, presence: true
-
   belongs_to :product
 
   # TODO: Move admin parts of Reservation here
