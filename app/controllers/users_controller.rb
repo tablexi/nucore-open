d94d189d2a33340e634c3a1b66045c407fa7ffb9
kt32291 <Katharine322@gmail.com>
2016-10-25 12:53:58 -0500

    Remove to_a on users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,6 @@ class UsersController < ApplicationController
       User
       .with_recent_orders(current_facility)
       .order(:last_name, :first_name)
-      .to_a
       .paginate(page: params[:page])
   end
 
