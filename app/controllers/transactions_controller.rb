d904de7a5de7495b33b867ef86d66fa6109dc3f8
Dan Rench <dan.rench@tablexi.com>
2016-11-29 16:03:41 -0600

    [#133349] Show transactions for accounts the current user administers

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -12,14 +12,25 @@ class TransactionsController < ApplicationController
   end
 
   def index_with_search
-    @recently_reviewed = current_user.order_details.recently_reviewed.paginate(page: params[:page])
-    @order_details = current_user.order_details.all_in_review
+    @recently_reviewed = administered_order_details.recently_reviewed.paginate(page: params[:page])
+    @order_details = administered_order_details.all_in_review
     @extra_date_column = :reviewed_at
     @order_detail_link = {
       text: text("shared.dispute"),
       display?: proc { |order_detail| order_detail.can_dispute? },
       proc: proc { |order_detail| order_order_detail_path(order_detail.order, order_detail) },
     }
+  end
+
+  private
+
+  def administered_accounts
+    Account.administered_by(current_user)
+  end
+
+  def administered_order_details
+    @administered_order_details ||=
+      OrderDetail.where(account_id: administered_accounts)
   end
 
 end
