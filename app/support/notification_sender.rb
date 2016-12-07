e2b917816e28f3714648b6605d7bf88cb13ffc54
Dan Rench <dan.rench@tablexi.com>
2016-12-07 11:28:23 -0600

    [#133351] Document #notifications_hash

--- app/support/notification_sender.rb
+++ app/support/notification_sender.rb
@@ -76,6 +76,9 @@ class NotificationSender
 
     private
 
+    # This builds a Hash of account_id Arrays, keyed by user_id.
+    # The user_ids are the administrators (owners and business administrators)
+    # of the given accounts.
     def notifications_hash(account_ids_to_notify)
       account_ids_to_notify.each_with_object({}) do |account_id, notifications|
         Account.find(account_id).administrators.each do |administrator|
