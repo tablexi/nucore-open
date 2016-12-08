a1b801f53fb455bbab275b2b9b1db7cb5e396772
kt32291 <Katharine322@gmail.com>
2016-12-08 14:39:08 -0600

    Fixes scope to return overlap of accounts
    This changes the scope so it only returns accounts that both the
    acting as user and the order detail user have access to

--- app/services/available_accounts_finder.rb
+++ app/services/available_accounts_finder.rb
@@ -1,18 +1,27 @@
 class AvailableAccountsFinder
 
-  def initialize(user, facility, current: nil)
+  def initialize(user, facility, current: nil, current_user: nil)
     @user = user
+    @current_user = current_user
     @facility = facility
     @current_account = current
   end
 
   def accounts
-    accounts = @user.accounts.for_facility(@facility).active
+    accounts = available_accounts
     if @current_account && !accounts.include?(@current_account)
       accounts += [@current_account]
     end
     accounts
   end
   alias to_a accounts
+
+  private
+
+  def available_accounts
+    available_accounts = @user.accounts.for_facility(@facility).active
+    available_accounts = available_accounts & @current_user.accounts.for_facility(@facility).active if @current_user
+    available_accounts
+  end
 
 end
