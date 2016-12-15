77a0a609f991bd44d916042ecb8d667267e008af
Dan Rench <dan.rench@tablexi.com>
2016-12-15 13:40:50 -0600

    [#133417] Remove redundant ability grants
    Account Managers should already have :manage (all) to Accounts

--- vendor/engines/split_accounts/app/models/split_accounts/ability_extension.rb
+++ vendor/engines/split_accounts/app/models/split_accounts/ability_extension.rb
@@ -8,12 +8,8 @@ module SplitAccounts
       @ability = ability
     end
 
-    def extend(user, resource)
+    def extend(user, _resource)
       ability.cannot :create, SplitAccounts::SplitAccount unless user.administrator?
-
-      if user.account_manager? && resource == Facility.cross_facility
-        ability.can [:suspend, :unsuspend], SplitAccounts::SplitAccount
-      end
     end
 
   end
