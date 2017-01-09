a812b6087360c02c2fd33771d19c45b2a481dc44
kt32291 <Katharine322@gmail.com>
2017-01-09 14:06:36 -0600

    [#133446] Adds reply to for facility email (#842)
    * Adds reply to address for facility emails
    
    This adds the reply_to option to all facility related emails so
    they can communicate with the facility in a reply email

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -36,7 +36,7 @@ class Notifier < ActionMailer::Base
 
   def order_notification(order, recipient)
     @order = order
-    send_nucore_mail recipient, text("views.notifier.order_notification.subject"), "order_receipt"
+    send_nucore_mail recipient, text("views.notifier.order_notification.subject"), template_name: "order_receipt", reply_to: reply_to_email(@order, @order.facility)
   end
 
   # Custom order forms send out a confirmation email when filled out by a
@@ -45,7 +45,7 @@ class Notifier < ActionMailer::Base
     @user = args[:user]
     @order = args[:order]
     @greeting = text("views.notifier.order_receipt.intro")
-    send_nucore_mail args[:user].email, text("views.notifier.order_receipt.subject")
+    send_nucore_mail args[:user].email, text("views.notifier.order_receipt.subject"), reply_to: reply_to_email(@order, @order.facility)
   end
 
   def review_orders(args)
@@ -63,7 +63,7 @@ class Notifier < ActionMailer::Base
     @account = args[:account]
     @statement = args[:statement]
     attach_statement_pdf
-    send_nucore_mail args[:user].email, text("views.notifier.statement.subject", facility: @facility)
+    send_nucore_mail args[:user].email, text("views.notifier.statement.subject", facility: @facility), reply_to: reply_to_email(@statement.order_details.first.order, @facility)
   end
 
   def order_detail_status_change(order_detail, old_status, new_status, to)
@@ -71,7 +71,7 @@ class Notifier < ActionMailer::Base
     @old_status = old_status
     @new_status = new_status
     template = "order_status_changed_to_#{new_status.downcase_name}"
-    send_nucore_mail to, t("views.notifier.#{template}.subject", order_detail: order_detail, user: order_detail.order.user, product: order_detail.product), template
+    send_nucore_mail to, t("views.notifier.#{template}.subject", order_detail: order_detail, user: order_detail.order.user, product: order_detail.product), template_name: template, reply_to: reply_to_email(order_detail.order, order_detail.facility)
   end
 
   private
@@ -87,8 +87,14 @@ class Notifier < ActionMailer::Base
     @statement_pdf ||= StatementPdfFactory.instance(@statement)
   end
 
-  def send_nucore_mail(to, subject, template_name = nil)
-    mail(subject: subject, to: to, template_name: template_name)
+  def send_nucore_mail(to, subject, template_name: nil, reply_to: nil)
+    options = { subject: subject, to: to, template_name: template_name }
+    options[:reply_to] = reply_to if reply_to
+    mail(options)
+  end
+
+  def reply_to_email(order, facility)
+    order.order_details.first.product.contact_email || facility.email
   end
 
 end
