4a7fd36077b96afe95827c8da8709b704d23b692
Dan Rench <dan.rench@tablexi.com>
2017-01-10 10:32:08 -0600

    Refactor AutoCanceler queries to use AR

--- app/support/auto_canceler.rb
+++ app/support/auto_canceler.rb
@@ -13,7 +13,10 @@ class AutoCanceler
   def cancelable_reservations
     @cancelable_reservations ||= Reservation
                                  .joins(:product, order_detail: :order)
-                                 .where(build_sql, now: Time.zone.now)
+                                 .where(actual_start_at: nil, actual_end_at: nil, canceled_at: nil)
+                                 .where("auto_cancel_mins IS NOT NULL AND auto_cancel_mins > 0")
+                                 .where(order_details: { state: %w(new inprocess) })
+                                 .where(time_condition, now: Time.current)
                                  .merge(Order.purchased)
                                  .readonly(false)
   end
@@ -29,28 +32,10 @@ class AutoCanceler
 
   def time_condition
     if NUCore::Database.oracle?
-      "(:now - reserve_start_At) >= NumToDSInterval(auto_cancel_mins, 'MINUTE')"
+      "(:now - reserve_start_at) >= NumToDSInterval(auto_cancel_mins, 'MINUTE')"
     else
       "TIMESTAMPDIFF(MINUTE, reserve_start_at, :now) >= auto_cancel_mins"
     end
-  end
-
-  def build_sql
-    <<-SQL
-        actual_start_at IS NULL
-      AND
-        actual_end_at IS NULL
-      AND
-        canceled_at IS NULL
-      AND
-        auto_cancel_mins IS NOT NULL
-      AND
-        auto_cancel_mins > 0
-      AND
-        (order_details.state = 'new' OR order_details.state = 'inprocess')
-      AND
-        #{time_condition}
-    SQL
   end
 
   def admin
