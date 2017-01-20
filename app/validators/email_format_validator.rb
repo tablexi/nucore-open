3fabcb5d6c160cdc7fd36ab152a0e4f0d4e982b5
kt32291 <Katharine322@gmail.com>
2017-01-20 10:45:26 -0600

    Allow apostrophes in emails
    This allows apostrophes in emails, which is not a typical pattern
    but is appearing for some peoples emails

--- app/validators/email_format_validator.rb
+++ app/validators/email_format_validator.rb
@@ -1,6 +1,6 @@
 class EmailFormatValidator < ActiveModel::EachValidator
 
-  EMAIL_PATTERN = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,6}\z/i
+  EMAIL_PATTERN = /\A[a-z0-9._%+'-]+@[a-z0-9.-]+\.[a-z]{2,6}\z/i
 
   def validate_each(record, attribute, value)
     return if value.nil?
