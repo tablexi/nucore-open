c0bc003dc514d7ddfb162fbce90daa7ec6bdffb3
Dan Rench <dan.rench@tablexi.com>
2017-02-09 09:40:57 -0600

    [#133442] Move #blank? check

--- app/mailers/order_assignment_mailer.rb
+++ app/mailers/order_assignment_mailer.rb
@@ -1,9 +1,9 @@
 class OrderAssignmentMailer < BaseMailer
 
   def notify_assigned_user(order_details)
-    return if order_details.blank?
-
     @order_details = Array(order_details)
+    return if @order_details.blank?
+
     @user = @order_details.first.assigned_user
 
     mail(to: @user.email, subject: text("notify_assigned_user.subject"))
