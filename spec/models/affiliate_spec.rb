efbcf4485b1650f7ba205c370c3596edff07ace6
Dan Rench <dan.rench@tablexi.com>
2017-02-10 15:47:29 -0600

    [#126402] Clean up model specs

--- spec/models/affiliate_spec.rb
+++ spec/models/affiliate_spec.rb
@@ -3,19 +3,18 @@ require "rails_helper"
 RSpec.describe Affiliate do
 
   it { is_expected.to validate_uniqueness_of(:name) }
-
   it { is_expected.to validate_presence_of(:name) }
 
-  it "should maintain other as a constant" do
-    expect(Affiliate.OTHER).to eq(Affiliate.where(name: "Other").first)
+  it "maintains 'Other' as a constant" do
+    expect(Affiliate.OTHER).to eq(Affiliate.find_by(name: "Other"))
   end
 
-  it "should not allow OTHER to be destroyed" do
+  it "does not allow OTHER to be destroyed" do
     Affiliate.OTHER.destroy
     expect(Affiliate.OTHER).not_to be_destroyed
   end
 
-  it "should allow non-OTHER affiliates to be destroyed" do
+  it "allows non-OTHER affiliates to be destroyed" do
     affiliate = Affiliate.create!(name: "aff1")
     affiliate.destroy
     expect(affiliate).to be_destroyed
