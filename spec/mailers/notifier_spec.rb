cb5bace1c60573b60a0f9d723dc7647d3b7863b4
meara <mcharnetzki@gmail.com>
2017-02-14 15:39:30 -0600

    appease rubocop, fix tests

--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -65,7 +65,7 @@ RSpec.describe Notifier do
   if EngineManager.engine_loaded?(:c2po)
     describe ".statement" do
       let(:account) { FactoryGirl.build_stubbed(:purchase_order_account) }
-      let(:statement) { FactoryGirl.build_stubbed(:statement, facility: facility, account: account)}
+      let(:statement) { FactoryGirl.build_stubbed(:statement, facility: facility, account: account) }
       let(:email_html) { email.html_part.to_s.gsub(/&nbsp;/, " ") } # Markdown changes some whitespace to &nbsp;
       let(:email_text) { email.text_part.to_s }
 
@@ -74,7 +74,7 @@ RSpec.describe Notifier do
           user: user,
           facility: facility,
           account: account,
-          statement: statement
+          statement: statement,
         ).deliver_now
       end
 
