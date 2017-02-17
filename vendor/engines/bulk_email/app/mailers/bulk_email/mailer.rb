975bf1c2201028371751dfbb5238847a26438d07
meara <meara@users.noreply.github.com>
2017-02-17 12:27:42 -0600

    [#134549] facility name displayed in From for bulk email (#883)
    * facility name displayed in From for bulk email
    
    * appease codeclimate
    
    * [#134549] DRY up bulk email sender default from

--- vendor/engines/bulk_email/app/mailers/bulk_email/mailer.rb
+++ vendor/engines/bulk_email/app/mailers/bulk_email/mailer.rb
@@ -2,12 +2,21 @@ module BulkEmail
 
   class Mailer < BaseMailer
 
-    def send_mail(recipient:, subject:, body:, reply_to: nil)
+    def send_mail(recipient:, subject:, body:, reply_to: nil, facility:)
       @recipient = recipient
       @body = body
-      options = { to: recipient.email, subject: subject }
+      @facility = facility
+      options = { from: sender, to: recipient.email, subject: subject }
       options[:reply_to] = reply_to if reply_to.present?
       mail(options)
+    end
+
+    def sender
+      if @facility.try(:single_facility?)
+        "#{@facility.name} <#{default_params[:from]}>"
+      else
+        default_params[:from]
+      end
     end
 
   end
