eb9ffd66e50838b418fee2d9a74c3fa80a82378d
Jason Hanggi <jason@tablexi.com>
2017-02-17 19:09:40 -0600

    Initial Secure Room engine setup

--- vendor/engines/secure_rooms/config/routes.rb
+++ vendor/engines/secure_rooms/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :secure_rooms, except: [:show] do
       facility_product_routing_concern
       resources :price_policies, controller: "secure_room_price_policies", except: :show
+      resources :users, controller: "product_users", except: [:show, :edit, :create]
     end
   end
 end
