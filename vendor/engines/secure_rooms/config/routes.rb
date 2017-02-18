dd2ffcf610397bb6d6e7aff1f80ba60b573627ee
Jason Hanggi <jason@tablexi.com>
2017-02-17 20:06:02 -0600

    Fixes for file uploads controller

--- vendor/engines/secure_rooms/config/routes.rb
+++ vendor/engines/secure_rooms/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       facility_product_routing_concern
       resources :price_policies, controller: "secure_room_price_policies", except: :show
       resources :users, controller: "product_users", except: [:show, :edit, :create]
+      get ":product/:product_id/files/upload", to: 'file_uploads#upload'
     end
   end
 end
