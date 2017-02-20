eb1a7bf8003991c6e57d8d0075a5d7b67fc5cfda
Jason Hanggi <jason@tablexi.com>
2017-02-20 13:07:05 -0600

    Refactor routes to use a shareable concern between main routes and secure rooms

--- vendor/engines/secure_rooms/config/routes.rb
+++ vendor/engines/secure_rooms/config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   resources :facilities, only: [], path: I18n.t("facilities_downcase") do
     resources :secure_rooms, except: [:show] do
-      get :manage, on: :member
+      facility_product_routing_concern
       resources :price_policies, controller: "secure_room_price_policies", except: :show
-      resources :users, controller: "product_users", except: [:show, :edit, :create]
-      get ":product/:product_id/files/upload", to: 'file_uploads#upload'
     end
   end
 end
