37484d969659574c3bcdeec6eb8647954e3a89e5
Jason Hanggi <jason@tablexi.com>
2017-03-03 18:29:30 -0600

    Rubocop

--- docker/oracle/create_databases.rb
+++ docker/oracle/create_databases.rb
@@ -4,16 +4,16 @@ require 'erb'
 puts "Loading database config"
 config_file_path = File.expand_path('../../../config/database.yml', __FILE__)
 erb = ERB.new(File.read(config_file_path)).result
-config = YAML.load( erb )
+config = YAML.safe_load(erb)
 
 sql_file = File.expand_path("../setup.sql", __FILE__)
 
-begin
+loop do
   puts "Creating database users"
-  connection_string = "system/oracle@#{config["development"]["database"]}"
+  connection_string = "system/oracle@#{config['development']['database']}"
   output = `sqlplus #{connection_string} < #{sql_file}`
   puts output
-# On a fresh create, it may take a few seconds for oracle server to come up,
-# so rety this block until we create the user
-end until output.include?("User created.")
-
+  break if output.include?("User created.")
+  # On a fresh create, it may take a few seconds for oracle server to come up,
+  # so rety this block until we create the user
+end
