92dba42ba77aefea2abc60a0ae8a91058d23fef9
Matt Wagner <waggles@tablexi.com>
2017-03-06 09:28:35 -0600

    Style

--- vendor/engines/secure_rooms/app/controllers/secure_rooms/card_numbers_controller.rb
+++ vendor/engines/secure_rooms/app/controllers/secure_rooms/card_numbers_controller.rb
@@ -19,8 +19,7 @@ module SecureRooms
       super
     end
 
-    def edit
-    end
+    def edit; end
 
     def update
       if @user.update_attributes(update_user_params)
--- vendor/engines/secure_rooms/app/models/secure_rooms/card_number_ability.rb
+++ vendor/engines/secure_rooms/app/models/secure_rooms/card_number_ability.rb
@@ -7,9 +7,7 @@ module SecureRooms
     def initialize(user, facility)
       return unless user && facility
 
-      if user.administrator? || user.operator_of?(facility)
-        can :edit, User
-      end
+      can :edit, User if user.administrator? || user.operator_of?(facility)
     end
 
   end
