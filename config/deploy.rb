a5dbb613efa1464a25c734cc57ed4482b5a5dcbf
Jason Hanggi <jason@tablexi.com>
2017-03-13 11:39:19 -0500

    Deployment configuration for open (#920)
    * updated capistrano setup for stage server
    * skip oracle
    * add eye
    * use binstubs
    * move to secrets.yml
    * add stage environment
    * tweak required deployment files
    * add route for heartbeat
    * percent w
    * Use SMTP
    * Updates for capistrano 3.7
    * Disable exception notifier
    * gitignore secrets
    * Create secrets file on CI
    * Move dependency
    * Fix eye template escaping and decrease unicorn workers

---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+# config valid only for current version of Capistrano
+lock "~> 3.7.0"
+
+set :application, "nucore"
+
+set :repo_url, "git@github.com:tablexi/nucore-open.git"
+
+set :bundle_without, "#{fetch(:bundle_without)} oracle"
+
+set :linked_files, fetch(:linked_files, []).concat(
+  %w(config/database.yml config/secrets.yml config/settings.local.yml config/eye.yml.erb),
+)
+set :linked_dirs, fetch(:linked_dirs, []).concat(
+  %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/files),
+)
+
+set :eye_config, "config/eye.yml.erb"
+set :eye_env, -> { { rails_env: fetch(:rails_env) } }
+
+# set :rollbar_token, ENV["ROLLBAR_ACCESS_TOKEN"]
+# set :rollbar_env, Proc.new { fetch :rails_env }
+# set :rollbar_role, Proc.new { :app }
---
+++ config/deploy/staging.rb
@@ -0,0 +1,4 @@
+server "nucore.stage.tablexi.com", user: "nucore", roles: %w(web app db)
+set :deploy_to, "/home/nucore/nucore.stage.tablexi.com"
+set :rails_env, "stage"
+set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
