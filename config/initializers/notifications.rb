bcae820154539d6dcad7178d2b3b0b79251af6e0
Jason Hanggi <jason@tablexi.com>
2017-03-13 16:29:16 -0500

    Only try to notify ExceptionNotifier if it's enabled

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -1,7 +1,9 @@
 ActiveSupport::Notifications.subscribe("background_error") do |_name, _start, _finish, _id, payload|
   exception = payload[:exception]
   options = payload[:information] ? { data: { message: payload[:information] } } : {}
-  ExceptionNotifier.notify_exception(exception, options)
+  if defined?(ExceptionNotifier)
+    ExceptionNotifier.notify_exception(exception, options)
+  end
   Rails.logger.error exception
   Rails.logger.error exception.backtrace.try(:join, "\n")
 end
