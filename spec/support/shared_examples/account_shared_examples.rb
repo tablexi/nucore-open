0df958576b2b23019832764c4fe6b4ca1376a275
Jason Hanggi <jason@tablexi.com>
2017-03-15 19:43:28 -0500

    Share schedule rule specs for SecureRooms

--- spec/support/shared_examples/account_shared_examples.rb
+++ spec/support/shared_examples/account_shared_examples.rb
@@ -2,7 +2,7 @@ RSpec.shared_examples_for "an Account" do
   context "#to_s" do
     context "when not suspended" do
       it "does not append '(SUSPENDED)'" do
-        expect(account.to_s).not_to match /\s+\(SUSPENDED\)\Z/
+        expect(account.to_s).not_to match(/\s+\(SUSPENDED\)\Z/)
       end
     end
 
@@ -10,7 +10,7 @@ RSpec.shared_examples_for "an Account" do
       before { account.update_attribute(:suspended_at, Time.zone.now) }
 
       it "appends '(SUSPENDED)'" do
-        expect(account.to_s).to match /\s+\(SUSPENDED\)\Z/
+        expect(account.to_s).to match(/\s+\(SUSPENDED\)\Z/)
       end
     end
   end
