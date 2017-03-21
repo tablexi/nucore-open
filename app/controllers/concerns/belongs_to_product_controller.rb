c633b22575f9a894c08b357a868f6ce9dcbd2526
Jason Hanggi <jason@tablexi.com>
2017-03-21 11:18:48 -0500

    [#140850111] Genericize Product Access Groups and apply to SecureRooms (#937)
    This pulls the product_access_group features (aka scheduling groups) out of instruments and into the ScheduleRule support module so the feature can be shared with SecureRooms. This PR also makes necessary changes so the controller can support multiple types of products by making a `BelongsToProductController` module that will load the product by service_id, item_id, etc.
    
    * Add schedule groups to SecureRooms
    * Refactor product access group controller spec
    * Move ProductAccessGroupController spec to be shared for SecureRoom

---
+++ app/controllers/concerns/belongs_to_product_controller.rb
@@ -0,0 +1,27 @@
+module BelongsToProductController
+
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :authenticate_user!
+    before_action :check_acting_as
+    before_action :init_current_facility
+    before_action :init_product
+  end
+
+  private
+
+  def init_product
+    @product = current_facility.products.find_by!(url_name: product_id)
+  end
+
+  def product_id
+    params[product_key]
+  end
+
+  def product_key
+    valid_ids = Product.types.map { |t| "#{t.model_name.param_key}_id" }
+    params.keys.find { |k| k.in?(valid_ids) }
+  end
+
+end
