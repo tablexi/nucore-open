d7cd89315bd566644577a9f9d11dda8f9b5e9458
Jason Hanggi <jason@tablexi.com>
2017-03-21 19:03:35 -0500

    Remove dead JS that was relying on draggable/droppable (#939)

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -1,30 +1,4 @@
 $(function() {
-  //Set Draggable Unit, grid and axis
-  $( ".reschedulable .unit:not(.blackout)" ).draggable({
-    axis: "x",
-    containment: "parent",
-    grid: [ 2, 0],
-    opacity: 0.7,
-    revert: 'invalid',
-      stop: function(){
-          $(this).draggable('option','revert','invalid');
-      }
-  });
-  //The unit container will fit the units.
-  $('.reschedulable .unit_container').droppable({
-      tolerance: 'fit'
-  });
-  //Do not allow the overlap, yell at me if I try to drop a reservation on a reservation
-  $('.reschedulable .unit').droppable({
-    greedy: true,
-    tolerance: 'touch',
-    hoverClass: "invalid",
-    drop: function(event,ui){
-       $(this).off('invalid');
-        ui.draggable.draggable('option','revert',true);
-        alert("These Times Over Lap - You Can't Do that.");
-    }
-  })
   //Tool Tip
   tooltipContent = function($el, $tip) {
     var match = $el.attr("id").match(/block_(\w+_)?reservation_(\d+)/);
