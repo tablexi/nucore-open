32617e51abd707df3d2f31b9ffbd433863431b66
Matt Wagner <waggles@tablexi.com>
2017-03-28 17:05:23 -0500

    [#140076489] Updates

--- vendor/engines/secure_rooms/app/models/secure_rooms/card_reader.rb
+++ vendor/engines/secure_rooms/app/models/secure_rooms/card_reader.rb
@@ -5,7 +5,7 @@ module SecureRooms
     belongs_to :secure_room, foreign_key: :product_id
 
     validates :product_id, :card_reader_number, :control_device_number, presence: true
-    validates :card_reader_number, uniqueness: [:control_device_number]
+    validates :card_reader_number, uniqueness: { scope: :control_device_number }
 
   end
 
