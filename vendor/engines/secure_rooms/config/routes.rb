2e8e31d058838f6f823fce30c3efcbdca1b382cb
Matt Wagner <waggles@tablexi.com>
2017-03-30 11:27:23 -0500

    [#140076489] Manage SecureRooms Card Readers (#936)
    * Ability to Add/Edit/Remove card readers from a SecureRoom.
    * Use same permissions as Product#edit, i.e. only senior staff and above

--- vendor/engines/secure_rooms/config/routes.rb
+++ vendor/engines/secure_rooms/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :secure_rooms, except: [:show] do
       facility_product_routing_concern
       resources :price_policies, controller: "secure_room_price_policies", except: :show
+      resources :card_readers, controller: "secure_rooms/card_readers", except: :show
       resources :schedule_rules, except: [:show]
       resources :product_access_groups
       put "update_restrictions", to: "product_users#update_restrictions"
