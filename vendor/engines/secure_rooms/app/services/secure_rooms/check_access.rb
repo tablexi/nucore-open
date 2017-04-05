f38036394bb1c6c70e90e300b8eba7a9cd262ca1
Matt Wagner <waggles@tablexi.com>
2017-04-05 17:41:40 -0500

    [#140849739] Deny access if not on access list

--- vendor/engines/secure_rooms/app/services/secure_rooms/check_access.rb
+++ vendor/engines/secure_rooms/app/services/secure_rooms/check_access.rb
@@ -5,6 +5,7 @@ module SecureRooms
     DEFAULT_RULES = [
       AccessRules::OperatorRule,
       AccessRules::ArchivedProductRule,
+      AccessRules::RequiresApprovalRule,
       AccessRules::AccountSelectionRule,
       AccessRules::DenyAllRule,
     ].freeze
