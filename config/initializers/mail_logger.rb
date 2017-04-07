916be61a62e250b77f62aeac3bb127d110ad190a
meara <mcharnetzki@gmail.com>
2017-04-07 15:00:44 -0500

    log email info + timestamp

--- config/initializers/mail_logger.rb
+++ config/initializers/mail_logger.rb
@@ -1,7 +1,9 @@
 class MailLogger < ActiveSupport::LogSubscriber
   def deliver(event)
     info do
-      event.payload
+      pairs = ["sent_at: #{event.time.to_s}"]
+      event.payload.select { |k, v| [:mailer, :subject, :to, :from].include?(k) }.each_pair { |k, v| pairs << "#{k}: #{v}" }
+      "\n>>>MailLogger>>> " + pairs.join(", ")
     end
   end
 end
