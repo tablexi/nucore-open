655dae72c8af2a19ef904d714993693461420fd2
Matt Wagner <waggles@tablexi.com>
2017-04-17 16:30:37 -0500

    [#140938049] Add feature spec

--- vendor/engines/secure_rooms/spec/factories/occupancy.rb
+++ vendor/engines/secure_rooms/spec/factories/occupancy.rb
@@ -2,5 +2,23 @@ FactoryGirl.define do
   factory :occupancy, class: SecureRooms::Occupancy do
     secure_room
     user
+
+    trait :active do
+      entry_event { create :event }
+      entry_at { 1.hour.ago }
+    end
+
+    trait :orphan do
+      entry_event { create :event }
+      entry_at { 2.hours.ago }
+      orphaned_at { 1.hour.ago }
+    end
+
+    trait :complete do
+      entry_event { create :event }
+      entry_at { 2.hours.ago }
+      exit_event { create :event }
+      exit_at { 1.hour.ago }
+    end
   end
 end
