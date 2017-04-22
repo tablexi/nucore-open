7364566508592263fbb89dea915782ae9e0e9bd9
Jason Hanggi <jason@tablexi.com>
2017-04-21 19:03:26 -0500

    Go back to using product-based fetching

--- app/models/time_data.rb
+++
@@ -1,9 +0,0 @@
-module TimeData
-
-  def self.for(order_detail)
-    "#{self}::#{order_detail.product.type}Data".constantize.new(order_detail).time_data
-  rescue NameError => e
-    # noop, return nil
-  end
-
-end
