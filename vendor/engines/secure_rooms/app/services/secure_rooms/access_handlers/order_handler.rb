a7fdc4f9b23b0f522dc84b2c3d238dad4622f00f
Matt Wagner <waggles@tablexi.com>
2017-04-24 14:06:51 -0500

    [#144058475] Use helper methods

--- vendor/engines/secure_rooms/app/services/secure_rooms/access_handlers/order_handler.rb
+++ vendor/engines/secure_rooms/app/services/secure_rooms/access_handlers/order_handler.rb
@@ -4,28 +4,47 @@ module SecureRooms
 
     class OrderHandler
 
+      attr_reader :occupancy, :order, :order_detail
+
       def self.process(occupancy)
+        new(occupancy).process
+      end
+
+      def initialize(occupancy)
+        @occupancy = occupancy
+      end
+
+      def process
         return unless occupancy.account_id?
 
-        order = Order.create!(
+        create_order
+        create_order_detail
+
+        order.validate_order!
+        order.purchase!
+
+        order
+      end
+
+      private
+
+      def create_order
+        @order = Order.create!(
           account: occupancy.account,
           user: occupancy.user,
           facility: occupancy.facility,
           created_by_user: occupancy.user,
         )
+      end
 
-        order.order_details.create!(
+      def create_order_detail
+        @order_detail = order.order_details.create!(
           account: occupancy.account,
           product: occupancy.secure_room,
           occupancy: occupancy,
           created_by_user: occupancy.user,
           quantity: 1,
         )
-
-        order.validate_order!
-        order.purchase!
-
-        order
       end
 
     end
