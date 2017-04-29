d3dbe91bb36bcdcbae21e4bac981f53a066903d6
Jason Hanggi <jason@tablexi.com>
2017-04-28 20:16:29 -0500

    [#144318485] Secure Room PricePolicy calculations

--- vendor/engines/secure_rooms/app/models/secure_room_price_policy.rb
+++ vendor/engines/secure_rooms/app/models/secure_room_price_policy.rb
@@ -2,4 +2,22 @@ class SecureRoomPricePolicy < PricePolicy
 
   include PricePolicies::Usage
 
+  def estimate_cost_and_subsidy_from_order_detail(order_detail)
+    calculate_cost_and_subsidy_from_order_detail(order_detail)
+  end
+
+  def calculate_cost_and_subsidy_from_order_detail(order_detail)
+    entry_at = order_detail.occupancy.entry_at
+    exit_at = order_detail.occupancy.exit_at
+    return unless entry_at && exit_at
+
+    calculator.calculate(entry_at, exit_at)
+  end
+
+  private
+
+  def calculator
+    PricePolicies::TimeBasedPriceCalculator.new(self)
+  end
+
 end
