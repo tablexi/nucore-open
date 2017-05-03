033d7edf79eb524f8e174e8df105b2fbb265606e
Matt Wagner <waggles@tablexi.com>
2017-05-03 14:47:31 -0500

    [#144059001] Add Admin Occupancies Tab

--- vendor/engines/secure_rooms/app/models/secure_rooms/order_detail_extension.rb
+++ vendor/engines/secure_rooms/app/models/secure_rooms/order_detail_extension.rb
@@ -6,6 +6,9 @@ module SecureRooms
 
     included do
       has_one :occupancy, dependent: :destroy, inverse_of: :order_detail, class_name: SecureRooms::Occupancy
+
+      scope :non_occupancies, -> { joins(:product).where("products.type <> 'SecureRoom'") }
+      scope :occupancies, -> { joins(:product).where("products.type = 'SecureRoom'") }
     end
 
   end
