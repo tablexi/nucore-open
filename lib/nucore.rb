2062d631f4e72eb6607a9ecea99df567fd3579ab
Jason Hanggi <jason@tablexi.com>
2017-05-11 13:47:55 -0500

    Prevent SQL injection warnings (though no vulnerability should exist)
    The field should always be coming from code, not user input.

--- lib/nucore.rb
+++ lib/nucore.rb
@@ -106,14 +106,15 @@ module NUCore
       def self.included(base)
         base.extend ClassMethods
       end
+
       module ClassMethods
 
         def order_by_asc_nulls_first(field)
-          NUCore::Database.oracle? ? order("#{field} asc nulls first") : order(field)
+          NUCore::Database.oracle? ? order("#{sanitize_sql(field)} asc nulls first") : order(field)
         end
 
         def order_by_desc_nulls_first(field)
-          NUCore::Database.oracle? ? order("#{field} desc nulls first") : order("-#{field}")
+          NUCore::Database.oracle? ? order("#{sanitize_sql(field)} desc nulls first") : order("-#{sanitize_sql(field)}")
         end
 
       end
