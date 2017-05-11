tablexi/nucore-open

commit 1716fb77a612e7af220c37b87f6ebb9cc345ac0d

[#144058895] Refactor problem descriptions for secure room support (#1007)
* [#144058895] Refactor problem descriptions

* [#144058895] Remove #problem?, reorganize code

* Refactor localization

* Updates

* Update badges

* [#144058895] Update and turn on PricePolicy spec

* [#144058895] Use safe_join

* [#144058895] Use factory

* [#144058895] Handle nil case

app/helpers/facility_orders_helper.rb
@@ -9,9 +9,7 @@ module FacilityOrdersHelper
     notices << "can_reconcile" if order_detail.can_reconcile_journaled?
     notices << "in_open_journal" if order_detail.in_open_journal?
 
-    warnings = []
-    warnings << "missing_actuals" if order_detail.complete? && order_detail.reservation.try(:requires_but_missing_actuals?)
-    warnings << "missing_price_policy" if order_detail.missing_price_policy? && !order_detail.reservation.try(:requires_but_missing_actuals?)
+    warnings = Array(order_detail.problem_description)
 
     { warnings: warnings, notices: notices }
   end
@@ -19,10 +17,10 @@ module FacilityOrdersHelper
   def order_detail_badges(order_detail)
     notices = order_detail_notices(order_detail)
 
-    output = build_badges(notices[:warnings], "label-important")
-    output += build_badges(notices[:notices], "label-info")
+    output = build_warnings(notices[:warnings])
+    output += build_notices(notices[:notices])
 
-    output.join(" ").html_safe
+    safe_join(output)
   end
 
   def banner_date_label(object, field, label = nil)
@@ -46,9 +44,15 @@ module FacilityOrdersHelper
 
   private
 
-  def build_badges(notices, label_class)
+  def build_notices(notices)
     notices.map do |notice|
-      content_tag(:span, t("order_details.notices.#{notice}.badge"), class: ["label", label_class])
+      content_tag(:span, t("order_details.notices.#{notice}.badge"), class: ["label", "label-info"])
+    end
+  end
+
+  def build_warnings(warnings)
+    warnings.map do |warning|
+      content_tag(:span, warning, class: ["label", "label-important"])
     end
   end
 

app/models/order_detail.rb
@@ -8,6 +8,7 @@ class OrderDetail < ActiveRecord::Base
   include OrderDetail::Accessorized
   include NUCore::Database::WhereIdsIn
   include NUCore::Database::ClobSafeDistinct
+  include TextHelpers::Translation
 
   versioned
 
@@ -30,7 +31,7 @@ class OrderDetail < ActiveRecord::Base
 
   before_save :set_problem_order
   def set_problem_order
-    self.problem = complete? && (price_policy.blank? || time_data.problem?)
+    self.problem = complete? && problem_description.present?
     update_fulfilled_at_on_resolve if time_data.present?
     true # problem might be false; we need the callback chain to continue
   end
@@ -55,7 +56,6 @@ class OrderDetail < ActiveRecord::Base
 
   delegate :edit_url, to: :external_service_receiver, allow_nil: true
   delegate :invoice_number, to: :statement, prefix: true
-  delegate :requires_but_missing_actuals?, to: :reservation, allow_nil: true
   # TODO: Refactor this from Reservation into OrderDetail
   delegate :canceled_at, to: :reservation, allow_nil: true
 
@@ -851,6 +851,17 @@ class OrderDetail < ActiveRecord::Base
     else
       TimeData::NullTimeData.new
     end
+  end
+
+  def translation_scope
+    "activerecord.models.order_detail"
+  end
+
+  def problem_description
+    time_data_problem = time_data.problem_description
+    price_policy_problem = text(:price_policy_missing) if price_policy.blank?
+
+    time_data_problem || price_policy_problem
   end
 
   private

app/models/reservation.rb
@@ -8,6 +8,7 @@ class Reservation < ActiveRecord::Base
   include Reservations::Rendering
   include Reservations::RelaySupport
   include Reservations::MovingUp
+  include TextHelpers::Translation
 
   # Associations
   #####
@@ -280,7 +281,14 @@ class Reservation < ActiveRecord::Base
   def requires_but_missing_actuals?
     !!(!canceled? && product.control_mechanism != Relay::CONTROL_MECHANISMS[:manual] && !has_actuals?) # TODO: refactor?
   end
-  alias problem? requires_but_missing_actuals?
+
+  def problem_description
+    text(:actual_usage_missing) if requires_but_missing_actuals?
+  end
+
+  def translation_scope
+    "activerecord.models.reservation"
+  end
 
   def locked?
     !(admin_editable? || can_edit_actuals?)

app/models/time_data/null_time_data.rb
@@ -6,8 +6,7 @@ module TimeData
       true
     end
 
-    def problem?
-      false
+    def problem_description
     end
 
     # Gives us both `blank?` and `present?`

app/models/time_data/required_time_data.rb
@@ -2,12 +2,18 @@ module TimeData
 
   class RequiredTimeData
 
+    include TextHelpers::Translation
+
     def order_completable?
       false
     end
 
-    def problem?
-      true
+    def problem_description
+      text(:actual_usage_missing)
+    end
+
+    def translation_scope
+      "activerecord.models.time_data.required_time_data"
     end
 
     # Gives us both `blank?` and `present?`

app/views/shared/_problem_order_details.html.haml
@@ -28,9 +28,9 @@
         %td.centered= order_detail.quantity
         %td= order_detail_description(order_detail)
         %td= order_detail.account
-        - if order_detail.requires_but_missing_actuals?
-          %td.centered= "#{t ".actual_usage_missing"} #{link_to "Enter", manage_order_detail_path(order_detail), class: "manage-order-detail"}".html_safe
-        - else
-          %td.centered= "#{t ".price_policy_missing"} #{link_to "Assign", manage_order_detail_path(order_detail), class: "manage-order-detail"}".html_safe
+        %td.centered
+          = order_detail.problem_description
+          = link_to text("shared.update"), manage_order_detail_path(order_detail), class: "manage-order-detail"
+
 = will_paginate(@order_details)
 = render partial: "shared/reconcile_footnote"

config/locales/en.models.yml
@@ -142,12 +142,14 @@ en:
       reservation:
         one: Reservation
         other: Reservations
+        actual_usage_missing: Actual Usage Missing
       order:
         one: Order
         other: Orders
       order_detail:
         one: Order Detail
         other: Order Details
+        price_policy_missing: Price Policy Missing
       order_status:
         one: Current Order Status
         other: Order Statuses
@@ -164,6 +166,9 @@ en:
       statement:
         one: Statement
         other: Statements
+      time_data:
+        required_time_data:
+          actual_usage_missing: Actual Usage Missing
       training_request:
         one: Training Request
         other: Training Requests

vendor/engines/secure_rooms/app/models/secure_rooms/occupancy.rb
@@ -2,6 +2,8 @@ module SecureRooms
 
   class Occupancy < ActiveRecord::Base
 
+    include TextHelpers::Translation
+
     belongs_to :secure_room, foreign_key: :product_id
     belongs_to :user
     belongs_to :account
@@ -61,6 +63,18 @@ module SecureRooms
 
     def actual_duration_mins
       range.duration_mins
+    end
+
+    def problem_description
+      if entry_at.blank?
+        text(:missing_entry)
+      elsif exit_at.blank?
+        text(:missing_exit)
+      end
+    end
+
+    def translation_scope
+      "activerecord.models.secure_rooms/occupancy"
     end
 
     private

commit 496569ca5d29f55443de5d7675aa5e1c3f095aee

[#144058895] Remove model-based translation of problem description (#1021)
Fixes an issue introduced in #1007 where the longer messages around problem orders in the order detail modal were breaking.

app/helpers/facility_orders_helper.rb
@@ -9,7 +9,7 @@ module FacilityOrdersHelper
     notices << "can_reconcile" if order_detail.can_reconcile_journaled?
     notices << "in_open_journal" if order_detail.in_open_journal?
 
-    warnings = Array(order_detail.problem_description)
+    warnings = Array(order_detail.problem_description_key)
 
     { warnings: warnings, notices: notices }
   end
@@ -17,8 +17,8 @@ module FacilityOrdersHelper
   def order_detail_badges(order_detail)
     notices = order_detail_notices(order_detail)
 
-    output = build_warnings(notices[:warnings])
-    output += build_notices(notices[:notices])
+    output = build_badges(notices[:notices], "label-info")
+    output += build_badges(notices[:warnings], "label-important")
 
     safe_join(output)
   end
@@ -44,15 +44,9 @@ module FacilityOrdersHelper
 
   private
 
-  def build_notices(notices)
+  def build_badges(notices, label_class)
     notices.map do |notice|
-      content_tag(:span, t("order_details.notices.#{notice}.badge"), class: ["label", "label-info"])
-    end
-  end
-
-  def build_warnings(warnings)
-    warnings.map do |warning|
-      content_tag(:span, warning, class: ["label", "label-important"])
+      content_tag(:span, t("order_details.notices.#{notice}.badge"), class: ["label", label_class])
     end
   end
 

app/models/order_detail.rb
@@ -31,7 +31,7 @@ class OrderDetail < ActiveRecord::Base
 
   before_save :set_problem_order
   def set_problem_order
-    self.problem = complete? && problem_description.present?
+    self.problem = complete? && problem_description_key.present?
     update_fulfilled_at_on_resolve if time_data.present?
     true # problem might be false; we need the callback chain to continue
   end
@@ -864,11 +864,11 @@ class OrderDetail < ActiveRecord::Base
     "activerecord.models.order_detail"
   end
 
-  def problem_description
-    time_data_problem = time_data.problem_description
-    price_policy_problem = text(:price_policy_missing) if price_policy.blank?
+  def problem_description_key
+    time_data_problem_key = time_data.problem_description_key
+    price_policy_problem_key = :missing_price_policy if price_policy.blank?
 
-    time_data_problem || price_policy_problem
+    time_data_problem_key || price_policy_problem_key
   end
 
   private

app/models/reservation.rb
@@ -8,7 +8,6 @@ class Reservation < ActiveRecord::Base
   include Reservations::Rendering
   include Reservations::RelaySupport
   include Reservations::MovingUp
-  include TextHelpers::Translation
 
   # Associations
   #####
@@ -282,12 +281,8 @@ class Reservation < ActiveRecord::Base
     !!(!canceled? && product.control_mechanism != Relay::CONTROL_MECHANISMS[:manual] && !has_actuals?) # TODO: refactor?
   end
 
-  def problem_description
-    text(:actual_usage_missing) if requires_but_missing_actuals?
-  end
-
-  def translation_scope
-    "activerecord.models.reservation"
+  def problem_description_key
+    :missing_actuals if requires_but_missing_actuals?
   end
 
   def locked?

app/models/time_data/null_time_data.rb
@@ -6,7 +6,7 @@ module TimeData
       true
     end
 
-    def problem_description
+    def problem_description_key
     end
 
     # Gives us both `blank?` and `present?`

app/models/time_data/required_time_data.rb
@@ -2,18 +2,12 @@ module TimeData
 
   class RequiredTimeData
 
-    include TextHelpers::Translation
-
     def order_completable?
       false
     end
 
-    def problem_description
-      text(:actual_usage_missing)
-    end
-
-    def translation_scope
-      "activerecord.models.time_data.required_time_data"
+    def problem_description_key
+      :missing_actuals
     end
 
     # Gives us both `blank?` and `present?`

app/views/shared/_problem_order_details.html.haml
@@ -29,7 +29,7 @@
         %td= order_detail_description(order_detail)
         %td= order_detail.account
         %td.centered
-          = order_detail.problem_description
+          = t("order_details.notices.#{order_detail.problem_description_key}.badge")
           = link_to text("shared.update"), manage_order_detail_path(order_detail), class: "manage-order-detail"
 
 = will_paginate(@order_details)

config/locales/en.models.yml
@@ -146,14 +146,12 @@ en:
       reservation:
         one: Reservation
         other: Reservations
-        actual_usage_missing: Actual Usage Missing
       order:
         one: Order
         other: Orders
       order_detail:
         one: Order Detail
         other: Order Details
-        price_policy_missing: Price Policy Missing
       order_status:
         one: Current Order Status
         other: Order Statuses
@@ -173,9 +171,6 @@ en:
       statement:
         one: Statement
         other: Statements
-      time_data:
-        required_time_data:
-          actual_usage_missing: Actual Usage Missing
       training_request:
         one: Training Request
         other: Training Requests

vendor/engines/secure_rooms/app/models/secure_rooms/occupancy.rb
@@ -2,8 +2,6 @@ module SecureRooms
 
   class Occupancy < ActiveRecord::Base
 
-    include TextHelpers::Translation
-
     belongs_to :secure_room, foreign_key: :product_id
     belongs_to :user
     belongs_to :account
@@ -65,16 +63,12 @@ module SecureRooms
       range.duration_mins
     end
 
-    def problem_description
+    def problem_description_key
       if entry_at.blank?
-        text(:missing_entry)
+        :missing_entry
       elsif exit_at.blank?
-        text(:missing_exit)
+        :missing_exit
       end
-    end
-
-    def translation_scope
-      "activerecord.models.secure_rooms/occupancy"
     end
 
     private

spec/models/reservation_spec.rb
@@ -1488,13 +1488,13 @@ RSpec.describe Reservation do
     end
   end
 
-  describe "#problem_description" do
-    subject(:problem_description) { reservation.problem_description }
+  describe "#problem_description_key" do
+    subject(:problem_description_key) { reservation.problem_description_key }
 
     context "requires_but_missing_actuals?" do
       before { expect(reservation).to receive(:requires_but_missing_actuals?).and_return(true) }
 
-      it { is_expected.to eq reservation.text(:actual_usage_missing) }
+      it { is_expected.to eq :missing_actuals }
     end
 
     context "usage exists" do

spec/models/time_data/null_time_data_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe TimeData::NullTimeData, type: :model do
   let(:null_time_data) { described_class.new }
 
-  describe "#problem_description" do
-    subject(:problem_description) { null_time_data.problem_description }
+  describe "#problem_description_key" do
+    subject(:problem_description_key) { null_time_data.problem_description_key }
 
     it { is_expected.to be_blank }
   end

spec/models/time_data/required_time_data_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 RSpec.describe TimeData::RequiredTimeData, type: :model do
   let(:required_time_data) { described_class.new }
 
-  describe "#problem_description" do
-    subject(:problem_description) { required_time_data.problem_description }
+  describe "#problem_description_key" do
+    subject(:problem_description_key) { required_time_data.problem_description_key }
 
-    it { is_expected.to eq required_time_data.text(:actual_usage_missing) }
+    it { is_expected.to eq :missing_actuals }
   end
 end

vendor/engines/secure_rooms/spec/models/secure_rooms/occupancy_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe SecureRooms::Occupancy do
 
   let(:occupancy) { build :occupancy }
 
-  describe "#problem_description" do
-    subject(:problem_description) { occupancy.problem_description }
+  describe "#problem_description_key" do
+    subject(:problem_description_key) { occupancy.problem_description_key }
 
     context "entry_at.blank?" do
       before { expect(occupancy).to receive(:entry_at).and_return(nil) }
 
-      it { is_expected.to eq occupancy.text(:missing_entry) }
+      it { is_expected.to eq :missing_entry }
     end
 
     context "exit_at.blank?" do
@@ -21,7 +21,7 @@ RSpec.describe SecureRooms::Occupancy do
         expect(occupancy).to receive(:exit_at).and_return(nil)
       end
 
-      it { is_expected.to eq occupancy.text(:missing_exit) }
+      it { is_expected.to eq :missing_exit }
     end
   end
 end