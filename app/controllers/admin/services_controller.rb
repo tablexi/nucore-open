778c936fc22d0ae49cd5c1b5faa0657f50333d46
Matt Wagner <waggles@tablexi.com>
2017-05-19 15:13:37 -0500

    [#144059389] Add modifiable list of workers

--- app/controllers/admin/services_controller.rb
+++ app/controllers/admin/services_controller.rb
@@ -2,7 +2,7 @@ module Admin
 
   class ServicesController < ApplicationController
 
-    skip_before_action :verify_authenticity_token, only: :cancel_reservations_for_offline_instruments
+    skip_before_action :verify_authenticity_token
 
     def cancel_reservations_for_offline_instruments
       InstrumentOfflineReservationCanceler.new.cancel!
@@ -10,13 +10,14 @@ module Admin
     end
 
     def process_five_minute_tasks
-      workers = [AutoExpireReservation, EndReservationOnly, AutoLogout]
-
-      workers.each do |worker|
+      self.class.five_minute_tasks.each do |worker|
         worker.new.perform
       end
-
       render nothing: true
+    end
+
+    def self.five_minute_tasks
+      @five_minute_tasks ||= [AutoExpireReservation, EndReservationOnly, AutoLogout]
     end
 
   end
