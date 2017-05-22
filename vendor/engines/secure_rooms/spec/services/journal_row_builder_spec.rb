393b937483f92271db422c7bd5316faf557d1df7
Matt Wagner <waggles@tablexi.com>
2017-05-22 12:13:41 -0500

    [#144059291] Add journal row spec for occupancies (#1046)

---
+++ vendor/engines/secure_rooms/spec/services/journal_row_builder_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe JournalRowBuilder, type: :service do
+  let(:builder) { described_class.new(journal, order.order_details) }
+
+  describe "#build" do
+    let(:journal) { build(:journal, facility: facility, created_by: 1) }
+
+    let(:facility) { create(:setup_facility) }
+    let(:secure_room) { create(:secure_room, facility: facility) }
+    let(:order) { create(:purchased_order, product: secure_room) }
+
+    let!(:occupancy) do
+      create(
+        :occupancy,
+        :complete,
+        secure_room: secure_room,
+        order_detail: order.order_details.first,
+        account: order.account,
+      )
+    end
+
+    before do
+      order.order_details.each(&:to_complete!)
+    end
+
+    describe "row description" do
+      subject { builder.build.journal_rows.first.description }
+      it { is_expected.to include("#{secure_room.name} x1") }
+    end
+  end
+end
