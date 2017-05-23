48246c3707579ed49ac00303a5717859c60b3fce
Jason Hanggi <jason@tablexi.com>
2017-05-23 12:10:42 -0500

    Add spec

---
+++ vendor/engines/secure_rooms/spec/services/order_row_importer_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe OrderRowImporter do
+  subject { OrderRowImporter.new(row, order_import) }
+
+  let(:secure_room) { create(:secure_room) }
+  let(:facility) { secure_room.facility }
+  let(:user) { create(:user) }
+  let(:order_import) { build(:order_import, creator: user, facility: facility) }
+  let(:account) { create(:nufs_account, :with_account_owner, owner: user) }
+
+  let(:row) do
+    ref = {
+      "Netid / Email" => user.username,
+      "Chart String" => account.account_number,
+      "Product Name" => secure_room.name,
+      "Quantity" => "1",
+      "Order Date" => "12/16/2016",
+      "Fulfillment Date" => "12/16/2016",
+      "Errors" => "",
+      "Note" => "",
+    }
+    CSV::Row.new(ref.keys, ref.values)
+  end
+
+  it "does not allow importing secure rooms" do
+    expect { subject.import }.not_to change(OrderDetail, :count)
+
+    # Errors is an array, so find one item matching the substring
+    expect(subject.errors).to include(include("Secure Room orders not allowed"))
+  end
+end
