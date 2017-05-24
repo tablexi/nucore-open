tablexi/nucore-open

commit 5e99a27fdf712ea089430f8c30322c1720bfd091

[#134672] Responsive - reserve an instrument (#945)

app/views/reservations/_reservation_fields.html.haml
@@ -1,21 +1,23 @@
-.datetime-block
-  - start_disabled = start_time_disabled?(f.object)
-  = f.input :reserve_start_date, input_html: { class: "datepicker", disabled: start_disabled }
-  .control-group
-    .controls
-      .string.optional.control-label &nbsp;
-      = time_select f, :reserve_start, { minute_step: @instrument.reserve_interval }, disabled: start_disabled
-
-  = f.input :duration_mins,
-    input_html: { value: f.object.duration_mins || default_duration, class: "timeinput", disabled: end_time_disabled?(f.object) }
-
-  - if f.object.actual_start_at?
-    .datetime-block
-      .started-at= "Started: #{l(f.object.actual_start_at, format: :usa)}"
-
-.datetime-block
-  = f.input :reserve_end_date, input_html: { class: "datepicker" }
-  .control-group
-    .controls
-      .string.optional.control-label &nbsp;
-      = time_select f, :reserve_end, { minute_step: @instrument.reserve_interval }
+- start_disabled = start_time_disabled?(f.object)
+.well
+  .container
+    .row
+      .span7
+        = label_tag :reservation_reserve_start_date, "Reserve Start", class: "string optional control-label"
+        .row
+          .span3
+            = text_field_tag "reservation[reserve_start_date]", f.object.reserve_start_date, class: "datepicker string optional span3"
+            - if f.object.actual_start_at?
+              .started-at= "Started: #{l(f.object.actual_start_at, format: :usa)}"
+          .span4
+            = time_select f, :reserve_start, { minute_step: @instrument.reserve_interval }, disabled: start_disabled
+      .span5
+        = f.input :duration_mins, input_html: { value: f.object.duration_mins || default_duration, class: "timeinput", disabled: end_time_disabled?(f.object) }
+    .row
+      .span7
+        = label_tag :reservation_reserve_end_date, "Reserve End", class: "string optional control-label"
+        .row
+          .span3
+            = text_field_tag "reservation[reserve_end_date]", f.object.reserve_end_date, class: "datepicker string optional span3"
+          .span4
+            = time_select f, :reserve_end, { minute_step: @instrument.reserve_interval }

app/views/reservations/new.html.haml
@@ -29,10 +29,7 @@
 = simple_form_for([@order, @order_detail, @reservation]) do |f|
   = f.error_messages
   = render "account_field", f: f unless @order_detail.bundled?
-
-  .row
-    .well.span12
-      = render "reservation_fields", f: f
+  = render "reservation_fields", f: f
 
   - if acting_as?
     .row

app/assets/stylesheets/app/reservation_edit.scss
@@ -4,12 +4,11 @@
     float: left;
     margin-right: 1em;
   }
+}
 
-  .started-at {
-    @extend .control-group;
-    margin-top: -16px;
-    font-style: italic;
-  }
+.started-at {
+  margin-top: -10px;
+  font-style: italic;
 }
 
 .time-select {

app/assets/stylesheets/calendar/fullcalendar.extensions.scss
@@ -45,5 +45,38 @@
 	padding:10px 0;
 }
 
+#calendar .fc-header-left, #calendar .fc-header-right,  {
+  width: 50%;
+  vertical-align: bottom;
+}
 
+#calendar .fc-header-center {
+  width: 0;
+}
 
+#calendar .fc-header-title {
+  width: 100%;
+  vertical-align: bottom;
+}
+
+#calendar .fc-header-title h2 {
+  font-size: 18px;
+  line-height: 1em;
+  margin-bottom: 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+#calendar .fc-header .fc-button {
+  margin-bottom: 0;
+  vertical-align: bottom;
+}
+
+#calendar .fc-header {
+  table-layout: fixed;
+}
+
+#calendar .fc-agenda .fc-agenda-axis {
+  width: 3em;
+}

app/views/reservations/_account_field.html.haml
@@ -1,23 +1,22 @@
-.row
-  .well.span12
-    .container
-      .row
-        .span6
-          = simple_fields_for @order do |order_fields|
-            - accounts = @order.user.accounts_for_product(@instrument)
+.well
+  .container
+    .row
+      .span6
+        = simple_fields_for @order do |order_fields|
+          - accounts = @order.user.accounts_for_product(@instrument)
 
-            - selected_account_id = @order.account_id || params[:order_account]
-            - if selected_account_id.blank? && accounts.count == 1
-              - selected_account_id = accounts.first.id
+          - selected_account_id = @order.account_id || params[:order_account]
+          - if selected_account_id.blank? && accounts.count == 1
+            - selected_account_id = accounts.first.id
 
-            = order_fields.input :account,
-              collection: accounts,
-              input_html: { name: "order_account" },
-              prompt: t(".prompt"),
-              selected: selected_account_id
+          = order_fields.input :account,
+            collection: accounts,
+            input_html: { name: "order_account" },
+            prompt: t(".prompt"),
+            selected: selected_account_id
 
-          - if acting_as?
-            = render_view_hook "after_account", f: f, order_detail: @order_detail
+        - if acting_as?
+          = render_view_hook "after_account", f: f, order_detail: @order_detail
 
-        - if show_note_input_to_user?(@order_detail)
-          .span6= f.input :note
+      - if show_note_input_to_user?(@order_detail)
+        .span6= f.input :note

app/views/reservations/edit.html.haml
@@ -23,9 +23,8 @@
 = simple_form_for([@order, @order_detail, @reservation]) do |f|
   = f.error_messages
 
-  .row
-    .well.span12
-      = render partial: 'reservation_fields', locals: { f: f }
+  = render "reservation_fields", f: f
+
   %ul.inline
     %li= f.submit 'Save', class: 'btn'
     - if @order.purchased?

commit 6b51a692d9641862db84c1e14976f2bd83033ffd

[#136397] Fix "Create & Start" on new reservations
Broken by responsive changes in
https://github.com/tablexi/nucore-open/pull/945 because a class was
removed, probably because it didn’t adhere to the newer best practice
of prefixing with `.js-`.

This also splits some of the js into separate files so each file is
only doing one thing.

app/assets/javascripts/app/logout_after_end_reservation.coffee
@@ -1,0 +1,8 @@
+$ ->
+  # The modal is present on the page after ending of a reservation. Auto-log
+  # the user out of their session after 60 seconds.
+  $logoutModal = $("#logout_modal")
+  if $logoutModal.length > 0
+    $logoutModal.modal("show")
+    logoutLink = $logoutModal.find(".logout").attr("href")
+    window.setTimeout((-> window.location.href = logoutLink), 60000)

app/assets/javascripts/app/reservation_time_checker.coffee
@@ -1,3 +1,5 @@
+# Validates that the selected reservation is valid for max/min durations as well
+# as interval (5, 10, 15, 30 minutes).
 class window.ReservationTimeChecker
   constructor: (@selector, @alertId = 'duration-alert')->
     if @validPage()
@@ -60,3 +62,10 @@ class window.ReservationTimeChecker
         @showError()
       else
         @hideError()
+
+$ ->
+  target = ".js--reservationValidations #reservation_duration_mins, .edit_reservation #reservation_duration_mins"
+
+  if $(target).length
+    new ReservationTimeChecker(target)
+

app/assets/javascripts/app/reservations.coffee
@@ -1,35 +1,25 @@
 $ ->
-  target = '#new_reservation #reservation_duration_mins, .edit_reservation #reservation_duration_mins'
+  if isBundle? && !isBundle && !ordering_on_behalf
+    $(".js--reservationUpdateCreateAndStart").find("select, input").change ->
 
-  if $(target).length
-    new ReservationTimeChecker(target)
+      return if ctrlMechanism == 'manual'
 
-    if isBundle? && !isBundle && !ordering_on_behalf
-      $('#new_reservation .datetime-block select, #new_reservation #reservation_reserve_start_date').change ->
+      now = new Date()
+      future = now.clone().addMinutes(5)
+      date = $('#reservation_reserve_start_date').val()
+      hour = $('#reservation_reserve_start_hour').val()
+      hour = "0#{hour}" if hour < 10
+      mins = $('#reservation_reserve_start_min').val()
+      mins = "0#{mins}" if mins < 10
+      meridian = $('#reservation_reserve_start_meridian').val()
 
-        return if ctrlMechanism == 'manual'
+      date_string = "#{date} #{hour}:#{mins}:00 #{meridian}"
 
-        now = new Date()
-        future = now.clone().addMinutes(5)
-        date = $('#reservation_reserve_start_date').val()
-        hour = $('#reservation_reserve_start_hour').val()
-        hour = "0#{hour}" if hour < 10
-        mins = $('#reservation_reserve_start_min').val()
-        mins = "0#{mins}" if mins < 10
-        meridian = $('#reservation_reserve_start_meridian').val()
+      picked = new Date(date_string)
 
-        date_string = "#{date} #{hour}:#{mins}:00 #{meridian}"
+      # change reservation creation button based on Reservation
+      text = if instrumentOnline && picked.between(now, future) then 'Create & Start' else 'Create'
+      $('#reservation_submit').attr('value', text)
 
-        picked = new Date(date_string)
+    .trigger('change')
 
-        # change reservation creation button based on Reservation
-        text = if instrumentOnline && picked.between(now, future) then 'Create & Start' else 'Create'
-        $('#reservation_submit').attr('value', text)
-
-      .trigger('change')
-
-  $logoutModal = $("#logout_modal")
-  if $logoutModal.length > 0
-    $logoutModal.modal("show")
-    logoutLink = $logoutModal.find(".logout").attr("href")
-    window.setTimeout((-> window.location.href = logoutLink), 60000)

app/views/reservations/_reservation_fields.html.haml
@@ -1,5 +1,5 @@
 - start_disabled = start_time_disabled?(f.object)
-.well
+.well.js--reservationValidations
   .container
     .row
       .span7

app/views/reservations/new.html.haml
@@ -29,7 +29,8 @@
 = simple_form_for([@order, @order_detail, @reservation]) do |f|
   = f.error_messages
   = render "account_field", f: f unless @order_detail.bundled?
-  = render "reservation_fields", f: f
+  .js--reservationUpdateCreateAndStart
+    = render "reservation_fields", f: f
 
   - if acting_as?
     .row