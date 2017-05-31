1c50d3a4faa667f84715e56483eda49ec41f5a3b
Matt Wagner <waggles@tablexi.com>
2017-05-31 14:41:50 -0500

    [#140895293] Remove TODO

--- vendor/engines/secure_rooms/app/controllers/secure_rooms_api/events_controller.rb
+++ vendor/engines/secure_rooms/app/controllers/secure_rooms_api/events_controller.rb
@@ -3,7 +3,6 @@ module SecureRoomsApi
   class EventsController < SecureRoomsApi::ApiController
 
     def create
-      # TODO: Discuss how to handle errors
       SecureRooms::AlarmEvent.create!(
         alarm_event_params.merge(
           message_time: parse_time(params[:message_time]),
