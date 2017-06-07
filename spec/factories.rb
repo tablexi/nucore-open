147bf616f645def155a2c2dcbcf3084eee21a94e
meara <mcharnetzki@gmail.com>
2017-06-07 16:02:39 -0500

    global without_validation trait

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,1 +1,7 @@
 require File.expand_path("factories_env", File.dirname(__FILE__))
+
+FactoryGirl.define do
+  trait :without_validation do
+    to_create { |instance| instance.save(validate: false) }
+  end
+end
