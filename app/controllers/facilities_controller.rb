4934d64b96f1f8f0514459635205ab78100b3582
meara <mcharnetzki@gmail.com>
2017-06-14 14:09:26 -0500

    move redirector out of controller, add specs

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -27,16 +27,6 @@ class FacilitiesController < ApplicationController
       acting_user.present? && acting_user.recently_used_facilities.sorted
   end
 
-  class DefaultFacilityHomepageRedirector
-    def self.redirect_path(facility)
-      if facility.instruments.active.any?
-        Rails.application.routes.url_helpers.timeline_facility_reservations_path(facility)
-      else
-        Rails.application.routes.url_helpers.facility_orders_path(facility)
-      end
-    end
-  end
-
   cattr_accessor(:facility_homepage_redirector) { DefaultFacilityHomepageRedirector }
 
   # GET /facilities/:facility_url/dashboard
