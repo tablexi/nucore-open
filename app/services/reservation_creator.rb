6bd9956c7e1716e8867b9aaf6bde68f474784786
Jason Hanggi <jason@tablexi.com>
2017-06-30 18:45:54 -0500

    Purchase validators now take order details rather than reservations

--- app/services/reservation_creator.rb
+++ app/services/reservation_creator.rb
@@ -42,8 +42,7 @@ class ReservationCreator
         @error = e.message
         raise ActiveRecord::Rollback
       rescue => e
-        @error = I18n.t("orders.purchase.error")
-        @error += " #{e.message}" if e.message
+        @error = I18n.t("orders.purchase.error", message: e.message).html_safe
         raise ActiveRecord::Rollback
       end
     end
--- app/validators/all_order_details_validator.rb
+++ app/validators/all_order_details_validator.rb
@@ -1,31 +1,32 @@
-class AllDetailsOnOrderValidator
+class AllOrderDetailsValidator
 
   cattr_accessor :order_detail_validator_class
+  attr_reader :order_details
 
-  # Build a validator that will take an Order in its constructor and validate each
-  # child order detail using the order_detail_validator_class.
+  # Build a validator that will take a collection of order details in its constructor
+  # and validate each one.
   #
   # Example:
   # validator = AllDetailsOnOrder.build(NotePresenceValidator)
-  # validator.new(order).valid?
+  # validator.new(order_details).valid?
   #
   # This will run NotePresenceValidator.new(order_detail).valid? for each
-  # element of @order.order_details
+  # element of @order_details
   def self.build(order_detail_validator_class)
-    Class.new(AllDetailsOnOrderValidator) do
+    Class.new(self) do
       self.order_detail_validator_class = order_detail_validator_class
     end
   end
 
   # This class should only be initialized through the `.build` method
-  def initialize(order)
+  def initialize(order_details)
     raise "Should not be initialized directly. Use the `.build` factory method instead" unless order_detail_validator_class
-    @order = order
+    @order_details = order_details
   end
 
   def valid?
     # Loop over everything so that all order details get `errors` applied to them
-    invalid_orders = @order.order_details.reject do |od|
+    invalid_orders = order_details.reject do |od|
       order_detail_validator_class.new(od).valid?
     end
 
