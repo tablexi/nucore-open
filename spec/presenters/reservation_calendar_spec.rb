9c81d5b6ba2ff6f7c9ee1064060b1e3b72faa751
Noel Rappin <noelrappin@gmail.com>
2017-07-05 16:13:11 -0500

    Cops

--- spec/presenters/reservation_calendar_spec.rb
+++ spec/presenters/reservation_calendar_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe ReservationCalendar do
 
   let(:reservation) do
     build(:reservation,
-      reserve_start_date: 1.day.from_now.to_date,
-      reserve_start_hour: 10,
-      reserve_start_min: 0,
-      reserve_start_meridian: "am",
-      duration_mins: 60,
-      split_times: true,
-      order_detail: build(:order_detail),
-    )
+          reserve_start_date: 1.day.from_now.to_date,
+          reserve_start_hour: 10,
+          reserve_start_min: 0,
+          reserve_start_meridian: "am",
+          duration_mins: 60,
+          split_times: true,
+          order_detail: build(:order_detail),
+         )
   end
   let(:calendar) { ReservationCalendar.new(reservation) }
   let(:ical) { calendar.as_ical }
