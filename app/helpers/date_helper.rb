tablexi/nucore-open

commit aa2434c0e32ac1b6a36d547eca1a92ec2d9e8758

[#136792] Consider 2-digit years as an invalid format (#1134)
* Stricter date format from user input

Force to always use 4 digit years. Before, “3/4/17” was being treated
as  the date “0003-4-17”.

Since the method returns nil for invalid dates, this could still cause
unexpected behavior. For example, you can backdate an order, but if the
input is blank or invalid, it defaults to Time.current. This could
result in setting the ordered_at to now rather than what the user
expected to see.

* [#136792] Validate date format in date picker widgets

This highlights the form element in red if the date format is invalid
(only happens if the user manually adjusts the date w/o using the
widget).

I considered doing something like disabling the form, but that started
to get weird with also wanting to disable the submit buttons when
thinking about multiple things possibly enabling/disabling.

I’m hoping just highlighting the problem will dissuade users from
hitting submit.

app/helpers/date_helper.rb
@@ -1,7 +1,9 @@
 module DateHelper
 
+  USA_DATE_FORMAT = %r(\A\d{1,2}/\d{1,2}/\d{4}\z)
+
   def usa_formatted_date?(date_string)
-    date_string.present? && date_string =~ %r(\A\d{1,2}/\d{1,2}/\d{4}\z)
+    date_string.present? && date_string =~ USA_DATE_FORMAT
   end
 
   def parse_usa_import_date(date_string)
@@ -13,12 +15,25 @@ module DateHelper
     end
   end
 
+  # Expected date format: "MM/DD/YYYY"
+  # Time string: "HH:MM AM/PM"
   def parse_usa_date(date, time_string = nil)
-    date_string = (date =~ /\d{1,2}\/\d{1,2}\/\d{4}/ ? Date.strptime($&, "%m/%d/%Y") : date).to_s
-    date_string += " #{time_string}" if time_string
+    date = date.to_s.strip
 
-    Time.zone.parse(date_string)
-  rescue
+    # TODO: Many tests pass either a Date, a Time, or an YYYY-MM-DD formatted
+    # string as a parameter. This conditional will handle those cases. We should
+    # probably go through and clean up the tests at some point.
+    date = format_usa_date(Date.parse(date)) if date =~ /\A\d{4}-\d{2}-\d{2}/
+
+    return unless usa_formatted_date?(date)
+    date = Date.strptime(date, "%m/%d/%Y")
+
+    if time_string
+      Time.zone.parse("#{date} #{time_string}")
+    else
+      date.to_time
+    end
+  rescue ArgumentError
     nil
   end
 

app/assets/javascripts/app/datepicker-data.coffee
@@ -3,12 +3,16 @@ class window.DatePickerData
     # This will set up a datepicker based on data attributes
     # Make sure you call `to_s` or `iso8601` on any dates you set in the views
     # to ensure the proper format.
-    $picker = $(".datepicker__data")
+    $pickers = $(".datepicker__data")
 
-    $picker.datepicker(
-      minDate: new Date($picker.data("min-date")) # will be unbounded if not provided
-      maxDate: new Date($picker.data("max-date")) # will be unbounded if not provided
-    )
+    $pickers.each (i, picker) ->
+      $picker = $(picker)
+      $picker.datepicker(
+        minDate: new Date($picker.data("min-date")) # will be unbounded if not provided
+        maxDate: new Date($picker.data("max-date")) # will be unbounded if not provided
+      )
+
+    new DatePickerValidate($pickers).activate()
 
 $ ->
   DatePickerData.activate()

app/assets/javascripts/app/datepicker-validate.coffee
@@ -1,0 +1,42 @@
+# Activated by DatePickerData
+class window.DatePickerValidate
+  constructor: (pickerSelector) ->
+    @$picker = $(pickerSelector)
+
+  activate: ->
+    unless @$picker.data("datepicker-validate-enabled")
+      @$picker.on("change", @dateChanged)
+              .data("datepicker-validate-enabled", true)
+
+  dateChanged: (e) =>
+    $input = $(e.target)
+
+    format = $input.datepicker("option", "dateFormat")
+    minDate = @_parseDate($input.datepicker("option", "minDate"), format)
+    maxDate = @_parseDate($input.datepicker("option", "maxDate"), format)
+
+    try
+      date = @_parseDate($input.val(), format)
+
+      if maxDate && date > maxDate
+        throw new Error("cannot be after #{@_formatDate(maxDate)}")
+      if minDate && date < minDate
+        throw new Error("cannot be before #{@_formatDate(minDate)}")
+
+      $input.closest(".control-group").removeClass("error")
+      $input.siblings(".help-inline").remove()
+    catch e
+      $input.closest(".control-group").addClass("error")
+      $input.after("<span class='help-inline'>#{e.message}</span>")
+
+  _parseDate: (dateOrString, format) ->
+    if typeof dateOrString is "string"
+      try
+        $.datepicker.parseDate(format, dateOrString)
+      catch e
+        throw new Error("invalid date format", e)
+    else
+      dateOrString
+
+  _formatDate: (date) ->
+    new TimeFormatter(date).dateString()

app/views/orders/_edit_date.html.haml
@@ -1,18 +1,27 @@
 - classes = ['collapsable']
 - classes << 'collapsed' if params['backdate_order'].blank?
-%fieldset#backdate-orders{ :class => classes }
+%fieldset#backdate-orders{ class: classes }
   %label.legend= t("orders.show.more_options")
   = hidden_field_tag :backdate_order, true
   - default_time = time_floor(Time.zone.now)
-  = simple_fields_for :backdate, :builder => ModelLessFormBuilder, :defaults => { :required => false } do |f|
-    = f.input :order_date, :input_html => { :value => params[:order_date] || format_usa_date(default_time), :class => 'datepicker' }
+  = simple_fields_for :backdate, builder: ModelLessFormBuilder, defaults: { required: false } do |f|
+    = f.input :order_date,
+      input_html: { value: params[:order_date] || format_usa_date(default_time),
+          class: "datepicker__data",
+          data: { max_date: Time.current.iso8601 } }
 
     = label_tag :order_time
     .time-select= time_select_tag :order_time, params[:order_datetime] || default_time
 
-    = f.input :order_status_id, :collection => @order_statuses, :label_method => :name_with_level, :selected => params[:order_status_id] || @order_statuses.first.try(:id), :label => OrderDetail.human_attribute_name(:order_status)
+    = f.input :order_status_id,
+      collection: @order_statuses,
+      label_method: :name_with_level,
+      selected:  params[:order_status_id] || @order_statuses.first.try(:id),
+      label: OrderDetail.human_attribute_name(:order_status)
 
-    = f.input :send_notification, :as => :boolean, :input_html => { :value => 1 }, :default => params[:send_notification], :label => false, :inline_label => true
-
-  :javascript
-    $(function() { $('.datepicker').datepicker({maxDate: '#{format_usa_date(Time.zone.now)}'}); });
+    = f.input :send_notification,
+      as: :boolean,
+      input_html: { value: 1 },
+      default: params[:send_notification],
+      label: false,
+      inline_label: true

spec/helpers/date_helper_spec.rb
@@ -41,20 +41,20 @@ RSpec.describe DateHelper do
 
   describe "#parse_usa_date" do
 
-    context "passed bad dates" do
-      ["05012012", "somegarbage", "11/31/2012", nil, "9/"].each do |bad_date_string|
-        it "should not raise error for: #{bad_date_string}" do
+    context "given bad dates" do
+      ["05012012", "somegarbage", "11/31/2012", nil, "9/", "5/1/20114", "1/4/16"].each do |bad_date_string|
+        it "does not raise error for: #{bad_date_string}" do
           expect { parse_usa_date(bad_date_string) }.not_to raise_error
         end
 
-        it "should return nil for #{bad_date_string}" do
+        it "returns nil for #{bad_date_string}" do
           expect(parse_usa_date(bad_date_string)).to be_nil
         end
       end
     end
 
-    context "passed valid dates" do
-      it "parse a usa formatted date properly" do
+    context "given valid dates" do
+      it "parses a date with month and day in USA style" do
         expect(parse_usa_date("05/10/2012")).to eq(Time.zone.parse("2012-05-10"))
       end
 
@@ -62,11 +62,53 @@ RSpec.describe DateHelper do
         expect(parse_usa_date("5/1/2012")).to eq(Time.zone.parse("2012-05-01"))
       end
 
-      it "should truncate a date with more than four digits in year" do
-        expect(parse_usa_date("5/1/20114")).to eq(Time.zone.parse("2011-05-01"))
+      it "can handle leading/trailing whitespace" do
+        expect(parse_usa_date("  5/1/2012  ")).to eq(Time.zone.parse("2012-05-01"))
+      end
+    end
+
+    context "with a time as well" do
+      it "parses a date with month and day in USA style" do
+        expect(parse_usa_date("05/10/2012", "3:17 PM")).to eq(Time.zone.parse("2012-05-10 15:17"))
       end
 
+      it "parses a date/time in 24 hour format" do
+        expect(parse_usa_date("05/10/2012", "15:17")).to eq(Time.zone.parse("2012-05-10 15:17"))
+      end
+
+      it "returns nil with invalid time" do
+        expect(parse_usa_date("5/10/2012", "34:23 FM")).to be_nil
+      end
+
+      it "returns nil for valid time, but nil input" do
+        expect(parse_usa_date(nil, "3:17 PM")).to be_nil
+      end
     end
+
+    # TODO: These should not be officially supported, but many tests rely on this
+    # behavior. In the main app, this method is only every called with user input,
+    # usually coming from a datepicker. Until we clean the tests up, we support
+    # these formats.
+    describe "semi-valid inputs" do
+      it "returns the datetime when given a Date" do
+        date = Date.new(2012, 5, 1)
+        expect(parse_usa_date(date)).to eq(Time.zone.parse("2012-05-01"))
+      end
+
+      it "returns the datetime when given a Date and time string" do
+        date = Date.new(2012, 5, 1)
+        expect(parse_usa_date(date, "3:17 PM")).to eq(Time.zone.parse("2012-05-01 15:17"))
+      end
+
+      it "returns the datetime for YYYY-MM-DD format" do
+        expect(parse_usa_date("2012-05-01")).to eq(Time.zone.parse("2012-05-01"))
+      end
+
+      it "returns the datetime for YYYY-MM-DD format with a time" do
+        expect(parse_usa_date("2012-05-01", "4:19 PM")).to eq(Time.zone.parse("2012-05-01 16:19"))
+      end
+    end
+
   end
 
   describe "#human_datetime" do

spec/javascripts/app/datepicker_validate_spec.coffee
@@ -1,0 +1,79 @@
+#= require helpers/jasmine-jquery
+
+describe "DatePickerValidate", ->
+  describe "with no constraints on the picker", ->
+    fixture.set '
+      <div class="control-group">
+        <input id="picker1" class="datepicker__data" value="06/22/2017" />
+      </div>
+    '
+
+    beforeEach ->
+      DatePickerData.activate()
+
+    it "has an error if the date is invalid", ->
+      $("#picker1").val("06/22/17").trigger("change")
+      expect($(".control-group")).toHaveClass("error")
+      expect($(".help-inline")).toHaveText("invalid date format")
+
+    it "is changed to something valid", ->
+      $("#picker1").val("07/23/2016").trigger("change")
+      expect($(".control-group")).not.toHaveClass("error")
+
+  describe "with min and maxDate constraints on the picker", ->
+    fixture.set '
+      <div class="control-group">
+        <input id="picker1" class="datepicker__data" value="06/22/2017"
+               data-min-date="2017-01-01" data-max-date="2017-12-31" />
+      </div>
+    '
+
+    beforeEach ->
+      DatePickerData.activate()
+
+    it "does not have an error if in the range", ->
+      $("#picker1").val("06/22/2017").trigger("change")
+      expect($(".control-group")).not.toHaveClass("error")
+
+    it "has an error if the date is before the min date", ->
+      $("#picker1").val("06/22/2016").trigger("change")
+      expect($(".control-group")).toHaveClass("error")
+      expect($(".help-inline")).toHaveText(/cannot be before/)
+
+    it "has an error if the date is after the max date", ->
+      $("#picker1").val("07/23/2018").trigger("change")
+      expect($(".control-group")).toHaveClass("error")
+      expect($(".help-inline")).toHaveText(/cannot be after/)
+
+  describe "with two pickers with different options", ->
+    fixture.set '
+      <div id="control1" class="control-group">
+        <input id="picker1" class="datepicker__data" value="06/22/2017"
+               data-min-date="2017-01-01" data-max-date="2017-12-31" />
+      </div>
+      <div id="control2" class="control-group">
+        <input id="picker2" class="datepicker__data" value="04/13/2016"
+               data-min-date="2016-01-01" data-max-date="2016-12-31" />
+      </div>
+    '
+
+    beforeEach ->
+      DatePickerData.activate()
+
+    describe "picker1", ->
+      it "does not have an error if in its own range", ->
+        $("#picker1").val("06/22/2017").trigger("change")
+        expect($("#control1")).not.toHaveClass("error")
+
+      it "has an error if it is within the others range, but not its own", ->
+        $("#picker1").val("06/22/2016").trigger("change")
+        expect($("#control1")).toHaveClass("error")
+
+    describe "picker2", ->
+      it "does not have an error if in its own range", ->
+        $("#picker2").val("06/22/2016").trigger("change")
+        expect($("#control2")).not.toHaveClass("error")
+
+      it "has an error if it is within the others range, but not its own", ->
+        $("#picker2").val("06/22/2017").trigger("change")
+        expect($("#control2")).toHaveClass("error")

spec/transaction_search_shared_examples.rb
@@ -1,6 +1,6 @@
 RSpec.shared_examples_for TransactionSearch do |date_range_field|
   context "when searching" do
-    let(:date_string) { "2001-01-01" }
+    let(:date_string) { "01/01/2011" }
     let(:datetime) { Time.zone.parse(date_string) }
 
     it "supports the inner method" do

commit a4668cee87b816b4287712875174309f058214f6

Fix Date to Time conversion so it uses the app's timezone
`to_time` uses the system's timezone, not the app's.

This was broken by changes made in https://github.com/tablexi/nucore-open/pull/1134

app/helpers/date_helper.rb
@@ -26,13 +26,11 @@ module DateHelper
     date = format_usa_date(Date.parse(date)) if date =~ /\A\d{4}-\d{2}-\d{2}/
 
     return unless usa_formatted_date?(date)
-    date = Date.strptime(date, "%m/%d/%Y")
 
-    if time_string
-      Time.zone.parse("#{date} #{time_string}")
-    else
-      date.to_time
-    end
+    date_string = Date.strptime(date, "%m/%d/%Y").to_s
+    date_string += " #{time_string}" if time_string
+
+    Time.zone.parse(date_string)
   rescue ArgumentError
     nil
   end