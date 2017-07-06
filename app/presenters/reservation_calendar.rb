feed704b772daaea7975c4ee9500481ff3482eb8
Noel Rappin <noelrappin@gmail.com>
2017-07-06 11:49:41 -0500

    Adding URL support

--- app/presenters/reservation_calendar.rb
+++ app/presenters/reservation_calendar.rb
@@ -1,11 +1,17 @@
 class ReservationCalendar < SimpleDelegator
 
+  attr_accessor :hostname
+
   def self.to_calendar(reservation)
     ReservationCalendar.new(reservation).as_ical
   end
 
-  def as_ical
+  def initialize(reservation, hostname)
+    super(reservation)
+    @hostname = hostname
+  end
 
+  def as_ical
     cal = Icalendar::Calendar.new
     cal.event do |e|
       e.dtstart = Icalendar::Values::DateTime.new(reserve_start_at)
@@ -13,6 +19,10 @@ class ReservationCalendar < SimpleDelegator
       e.summary = "#{facility.abbreviation}: #{product.name}"
       e.description = "#{facility.name} reservation for #{product.name}. Order number #{order.id}"
       e.location = facility.name
+      e.url = Icalendar::Values::Uri.new(
+        Rails.application.routes.url_helpers.order_order_detail_reservation_url(
+          order_id: order.id, order_detail_id: order_detail_id,
+          id: id, host: @hostname))
       e.ip_class = "PRIVATE"
     end
     cal
--- spec/presenters/reservation_calendar_spec.rb
+++ spec/presenters/reservation_calendar_spec.rb
@@ -4,18 +4,17 @@ RSpec.describe ReservationCalendar do
 
   let(:order) { build_stubbed(:order) }
   let(:reservation) do
-    build(:reservation,
-          reserve_start_date: 1.day.from_now.to_date,
-          reserve_start_hour: 10,
-          reserve_start_min: 0,
-          reserve_start_meridian: "am",
-          duration_mins: 60,
-          split_times: true,
-          order: order,
-          order_detail: build(:order_detail, order: order),
-         )
+    build_stubbed(:reservation,
+                  reserve_start_date: 1.day.from_now.to_date,
+                  reserve_start_hour: 10,
+                  reserve_start_min: 0,
+                  reserve_start_meridian: "am",
+                  duration_mins: 60,
+                  split_times: true,
+                  order: order,
+                  order_detail: build_stubbed(:order_detail, order: order))
   end
-  let(:calendar) { ReservationCalendar.new(reservation) }
+  let(:calendar) { ReservationCalendar.new(reservation, "localhost") }
   let(:facility) { build(:facility, name: "Facility", abbreviation: "FA") }
   let(:ical) { calendar.as_ical }
 
@@ -35,6 +34,7 @@ RSpec.describe ReservationCalendar do
       expect(event.summary).to eq("FA: Instrument")
       expect(event.description).to eq("Facility reservation for Instrument. Order number #{order.id}")
       expect(event.location).to eq("Facility")
+      expect(event.url.to_s).to eq("http://localhost/orders/1001/order_details/1002/reservations/1003")
       expect(event.ip_class).to eq("PRIVATE")
     end
 
