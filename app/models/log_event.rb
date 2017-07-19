fc8b6ad20f0eb6e813ce27c418ee906b68c4718b
Noel Rappin <noelrappin@gmail.com>
2017-07-19 13:22:10 -0500

    Responding to comments

--- app/models/log_event.rb
+++ app/models/log_event.rb
@@ -4,8 +4,7 @@ class LogEvent < ActiveRecord::Base
   belongs_to :loggable, polymorphic: true
 
   def self.log(loggable, event_type, user)
-    LogEvent.create(
-      loggable: loggable, event_type: event_type, user_id: user.id)
+    create(loggable: loggable, event_type: event_type, user_id: user.id)
   end
 
 end
