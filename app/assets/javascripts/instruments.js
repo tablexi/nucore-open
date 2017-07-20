b8ce172148930bf6bcd70ebadfa1f9bbb255def1
Jason Hanggi <jason@tablexi.com>
2017-07-20 17:35:54 -0500

    Silly JS, changing the constructor api continued to work...

--- app/assets/javascripts/instruments.js
+++ app/assets/javascripts/instruments.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  new FullCalendarConfig($("#calendar")).init({
+  new FullCalendarConfig($("#calendar"), {
     header: {
       left: '',
       center: 'title',
       right: 'prev,next today agendaDay,agendaWeek,month',
-    },
-  });
+    }
+  }).init();
 });
