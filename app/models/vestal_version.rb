9d4ee72bd5c6635da83a66e1bc0834d5eee9fbad
Noel Rappin <noelrappin@gmail.com>
2017-07-21 13:54:54 -0500

    Allow the old vestal_version data to be accessed as an activeRecord association.

---
+++ app/models/vestal_version.rb
@@ -0,0 +1,9 @@
+# This is a _temporary_ class to make up for the fact that the
+# vestal versions gem is no longer in the project. It's here to
+# allow access to the vestal data.
+# once that data is no longer needed, this class can be removed
+# along with the has_many clause in OrderDetail
+
+class VestalVersion < ActiveRecord::Base
+  belongs_to :versioned, polymorphic: true
+end
