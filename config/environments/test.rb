4967c01190c332530712ac8a420e8cc20b1b49d5
meara <mcharnetzki@gmail.com>
2017-07-28 17:10:48 -0500

    rm quiet assets

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -42,6 +42,9 @@ Nucore::Application.configure do
   # Raise exception on mass assignment protection for Active Record models
   # config.active_record.mass_assignment_sanitizer = :strict
 
+  # Do not log assets path
+  config.assets.quiet = true
+
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
   Delayed::Worker.delay_jobs = false
