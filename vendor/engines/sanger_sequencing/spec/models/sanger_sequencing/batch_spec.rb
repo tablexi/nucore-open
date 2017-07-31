4dc7e523160db0b68b37ccbc75e93aae106b7343
Matt Wagner <waggles@tablexi.com>
2017-07-31 17:07:29 -0500

    [#136038] Update default group scope for Batch

---
+++ vendor/engines/sanger_sequencing/spec/models/sanger_sequencing/batch_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe SangerSequencing::Batch do
+  describe ".for_product_group" do
+    subject { described_class.for_product_group(group) }
+
+    before { FactoryGirl.create(:product_group, :fragment) }
+    let!(:blank_batch) { FactoryGirl.create(:sanger_sequencing_batch, group: "") }
+    let!(:nil_batch) { FactoryGirl.create(:sanger_sequencing_batch, group: nil) }
+    let!(:grouped_batch) { FactoryGirl.create(:sanger_sequencing_batch, group: "fragment") }
+
+    context "with group given as nil" do
+      let(:group) { nil }
+      it { is_expected.to eq [blank_batch, nil_batch] }
+    end
+
+    context "with group given as empty string" do
+      let(:group) { "" }
+      it { is_expected.to eq [blank_batch, nil_batch] }
+    end
+
+    context "with a group provided" do
+      let(:group) { "fragment" }
+      it { is_expected.to eq [grouped_batch] }
+    end
+  end
+end
