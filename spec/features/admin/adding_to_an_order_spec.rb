723c45c983e52de5dd3605be3c12f9406243de7d
Jason Hanggi <jason@tablexi.com>
2017-09-07 14:43:23 -0500

    Specs for adding to existing orders

--- spec/features/admin/adding_to_an_order_spec.rb
+++ spec/features/admin/adding_to_an_order_spec.rb
@@ -1,20 +1,22 @@
 require "rails_helper"
 
 RSpec.describe "Adding to an existing order" do
-  let(:facility) { item.facility }
-  let(:item) { create(:setup_item, :with_facility_account) }
-  let(:order) { create(:purchased_order, product: item) }
+  let(:facility) { product.facility }
+  let(:order) { create(:purchased_order, product: product) }
   let(:user) { create(:user, :staff, facility: facility) }
 
   before do
     login_as user
-    visit facility_order_path(facility, order)
   end
 
   describe "adding an item" do
+    let(:product) { create(:setup_item, :with_facility_account) }
+
     before do
+      visit facility_order_path(facility, order)
+
       fill_in "product_add_quantity", with: "2"
-      select item.name, from: "product_add"
+      select product.name, from: "product_add"
       fill_in "Note", with: "My Note"
     end
 
@@ -30,23 +32,69 @@ RSpec.describe "Adding to an existing order" do
     end
 
     describe "adding it as Complete" do
+      let(:fulfilled_at_string) { I18n.l(1.day.ago.to_date, format: :usa) }
+
       before do
         select "Complete", from: "order_status_id"
-        fill_in "fulfilled_at", with: "10/14/2016"
+        fill_in "fulfilled_at", with: fulfilled_at_string
         click_button "Add To Order"
       end
 
       it "has a new order detail with the right status and fulfilled_at" do
         expect(order.reload.order_details.count).to be(2)
         expect(order.order_details.last).to be_complete
-        # TODO cannot be before previous fiscal year
-        expect(I18n.l(order.order_details.last.fulfilled_at, :usa)).to eq("10/14/2016")
+        expect(I18n.l(order.order_details.last.fulfilled_at.to_date, format: :usa)).to eq(fulfilled_at_string)
       end
     end
   end
 
+  describe "adding a backdated service with an order form" do
+    let(:product) { create(:setup_service, :with_facility_account, :with_order_form) }
+    let(:fulfilled_at_string) { I18n.l(1.day.ago.to_date, format: :usa) }
 
-  # TODO: Adding a service with an order form
-  # TODO: Adding an instrument
+    before do
+      visit facility_order_path(facility, order)
+
+      fill_in "product_add_quantity", with: "1"
+      select product.name, from: "product_add"
+      select "Complete", from: "order_status_id"
+      fill_in "fulfilled_at", with: fulfilled_at_string
+      click_button "Add To Order"
+    end
+
+    it "requires a file to be uploaded before adding to the order" do
+      expect(page).to have_content("The following order details need your attention.")
+
+      click_link "Upload Order Form"
+      attach_file "stored_file[file]", Rails.root.join("spec", "files", "template1.txt")
+      click_button "Upload"
+
+      expect(order.reload.order_details.count).to be(2)
+      expect(order.order_details.last).to be_complete
+      expect(I18n.l(order.order_details.last.fulfilled_at.to_date, format: :usa)).to eq(fulfilled_at_string)
+    end
+  end
+
+  describe "adding an instrument reservation" do
+    let(:product) { create(:setup_item, :with_facility_account) }
+    let!(:instrument) { create(:setup_instrument, facility: facility) }
+
+    before do
+      visit facility_order_path(facility, order)
+      fill_in "product_add_quantity", with: "1"
+      select instrument.name, from: "product_add"
+      click_button "Add to Order"
+    end
+
+    it "requires a reservation to be set up before adding to the order" do
+      expect(page).to have_content("The following order details need your attention.")
+
+      click_link "Make a Reservation"
+      click_button "Create"
+
+      expect(order.reload.order_details.count).to be(2)
+      expect(order.order_details.last.product).to eq(instrument)
+    end
+  end
 
 end
