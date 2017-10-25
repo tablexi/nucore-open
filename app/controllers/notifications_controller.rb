81a123ae5511be1cb7b7a68ac149ad28e657178c
meara <mcharnetzki@gmail.com>
2017-10-25 16:04:45 -0500

    clean up old hash syntax

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,24 +5,8 @@ class NotificationsController < ApplicationController
   before_action :check_acting_as
   before_action :check_notifications
 
-  # respond_to :js, :only => :update
-
   def index
   end
-
-  #
-  # Currently not routed to, but if you want to allow
-  # dismissal of notifications you should enable this action
-  # def update
-  #  begin
-  #    notice=Notification.find params[:id].to_i
-  #    notice.update_attribute :dismissed_at, Time.zone.now
-  #  rescue => e
-  #    Rails.logger.warn "#{e.message}\n#{e.backtrace.join("\n")}"
-  #  end
-  #
-  #  respond_with nil
-  # end
 
   private
 
