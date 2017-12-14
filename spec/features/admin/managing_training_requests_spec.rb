9a5d7810fdb6b3c684a34a850026814d61231e20
meara <mcharnetzki@gmail.com>
2017-12-14 15:57:57 -0600

    add spec;

---
+++ spec/features/admin/managing_training_requests_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe "Managing training a request" do
+  let(:director) { create(:user, :facility_director, facility: facility) }
+  let!(:training_request) { create(:training_request) }
+  let(:facility) { training_request.product.facility }
+  let(:instrument) { training_request.product }
+  let(:user) { training_request.user }
+
+  before { login_as director }
+
+  it "can remove a training request" do
+    visit facility_training_requests_path(facility)
+
+    expect(page).to have_content(user.email)
+
+    click_button "Remove"
+
+    expect(page).not_to have_content(user.email)
+    expect(page).to have_content("There are no outstanding training requests.")
+  end
+end
