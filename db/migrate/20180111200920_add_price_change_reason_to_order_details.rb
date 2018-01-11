a21f9f625551da6259b7a1cdd842f4f59b0fd205
meara <mcharnetzki@gmail.com>
2018-01-11 14:17:31 -0600

    migrate

---
+++ db/migrate/20180111200920_add_price_change_reason_to_order_details.rb
@@ -0,0 +1,7 @@
+class AddPriceChangeReasonToOrderDetails < ActiveRecord::Migration
+  def change
+    add_column :order_details, :price_change_reason, :string
+    add_column :order_details, :price_changed_by_user_id, :integer
+    add_index :order_details, :price_changed_by_user_id
+  end
+end
