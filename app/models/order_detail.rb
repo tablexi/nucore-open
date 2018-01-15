f58477144cede56035a48f338c0deffe28a7d776
meara <mcharnetzki@gmail.com>
2018-01-15 17:22:37 -0600

    i18n

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -115,7 +115,7 @@ class OrderDetail < ActiveRecord::Base
     return if cost_estimated?
 
     if price_change_reason.blank? && actual_costs_differ_from_calculated?
-      errors.add :price_change_reason, "is required"
+      errors.add :price_change_reason, :blank
     end
   end
 
