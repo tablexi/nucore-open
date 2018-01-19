bb86f1682bc428f058546e08f2a760f74675f642
Jason Hanggi <jason@tablexi.com>
2018-01-19 17:58:27 -0600

    Keep split account transformation lazy so it works better with find_each

--- vendor/engines/split_accounts/app/services/split_accounts/order_detail_list_transformer.rb
+++ vendor/engines/split_accounts/app/services/split_accounts/order_detail_list_transformer.rb
@@ -10,13 +10,36 @@ module SplitAccounts
 
     def perform(options = {})
       options ||= {} # in case it comes in as nil
-      order_details.each_with_object([]) do |order_detail, results|
-        # We will need to refactor the general_reports_controller_spec in
-        # order to remove the `try` methods below.
+
+      nested_map(order_details) do |order_detail|
         if order_detail.account.try(:splits).try(:present?)
-          results.concat SplitAccounts::OrderDetailSplitter.new(order_detail, split_time_data: options[:time_data]).split
+          SplitAccounts::OrderDetailSplitter.new(order_detail, split_time_data: options[:time_data]).split
         else
-          results << order_detail
+          order_detail
+        end
+      end
+    end
+
+    private
+
+    # Wraps a lazy enumerator with another enumerator so that if the block returns
+    # an Array, each value of that array gets yielded.
+    # Example:
+    # output = nested_map(1..Float::Infinity) do |i|
+    #   if i.even?
+    #     [i, i]
+    #   else
+    #     i
+    #   end
+    # end
+    #
+    # > output.take(5).to_a
+    # => [1, 2, 2, 3, 4]
+    def nested_map(enumerable)
+      Enumerator::Lazy.new(enumerable.to_enum) do |yielder, *values|
+        results = yield(*values)
+        Array(results).each do |val|
+          yielder << val
         end
       end
     end
