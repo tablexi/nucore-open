9db4217f3ccea071cdc7ed6ac020bd74be13dcec
Jason Hanggi <jason@tablexi.com>
2018-01-22 14:18:30 -0600

    Bump to synaccess 0.3 (#1347)
    That version removes the telnet interface because it was unreliable. We
    weren’t using it anywhere so it allowed some minor cleanup where it was
    configurable.

--- app/models/relay_synaccess_rev_a.rb
+++ app/models/relay_synaccess_rev_a.rb
@@ -11,8 +11,7 @@ class RelaySynaccessRevA < Relay
   end
 
   def relay_connection
-    clazz = "#{Settings.relays.connect_module}::RevA".constantize
-    @relay_connection ||= clazz.new(host, connection_options)
+    @relay_connection ||= NetBooter::Http::RevA.new(host, connection_options)
   end
 
 end
