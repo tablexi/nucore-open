69e023ca243de45da1ff6d06d2b90b0f065a7446
meara <meara@users.noreply.github.com>
2018-01-29 16:00:18 -0600

    [#139828] explicitly list bundleable product types (#1367)
    * explicitly list bundleable product types
    
    * rubocop

--- app/models/bundle.rb
+++ app/models/bundle.rb
@@ -3,8 +3,10 @@ class Bundle < Product
   has_many :products, through: :bundle_products
   has_many :bundle_products, foreign_key: :bundle_product_id
 
+  cattr_accessor :bundleable_product_types { ["Instrument", "Item", "Service", "TimedService"] }
+
   def products_for_group_select
-    products = facility.products.where.not(type: "Bundle").order(:type, :name)
+    products = facility.products.where(type: bundleable_product_types).order(:type, :name)
     options = Hash.new { |h, k| h[k] = [] }
     products.group_by { |product| product.class.name.pluralize }.each do |cname, ps|
       options[cname] = ps.map { |p| [p.to_s_with_status, p.id] }
