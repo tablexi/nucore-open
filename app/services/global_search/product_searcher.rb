0daf7536d39e69db0be2c40dc305be142a57d17d
meara <mcharnetzki@gmail.com>
2018-01-30 16:26:08 -0600

    speed up query

--- app/services/global_search/product_searcher.rb
+++ app/services/global_search/product_searcher.rb
@@ -14,10 +14,11 @@ module GlobalSearch
     end
 
     def restrict(products)
-      facilities = user ? user.operable_facilities : []
-      products.in_active_facility.not_archived.select { |product| product.visible? || facilities.include?(product.facility) }
+      facilities = user ? user.operable_facilities.pluck(:id) : []
+
+      products.in_active_facility.not_archived.where("is_hidden = ? OR products.facility_id in (?)", false, facilities)
       # TODO: Try this (or something like it) in Rails 5
-      # products.in_active_facility.not_archived.merge(Product.where(hidden: false).or(Product.where(facility: facilities)))
+      # products.in_active_facility.not_archived.merge(Product.where(is_hidden: false).or(Product.where(facility: facilities)))
     end
 
   end
