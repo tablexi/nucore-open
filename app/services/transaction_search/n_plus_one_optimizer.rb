4fea17958641aef707e761b894b8b7a8b00efbc8
Jason Hanggi <jason@tablexi.com>
2018-02-07 12:09:48 -0600

    Fix N+1s in Transaction Search

---
+++ app/services/transaction_search/n_plus_one_optimizer.rb
@@ -0,0 +1,13 @@
+module TransactionSearch
+
+  class NPlusOneOptimizer < BaseOptimizer
+
+    def optimize
+      order_details.includes(order: :user)
+        .includes(:reservation)
+        .preload(:bundle)
+    end
+
+  end
+
+end
