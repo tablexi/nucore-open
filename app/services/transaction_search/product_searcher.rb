03e5ade98877d5e8f452ee17d6f1e4fdbc8b7531
meara <mcharnetzki@gmail.com>
2018-03-06 17:57:53 -0600

    tweak

--- app/services/transaction_search/product_searcher.rb
+++ app/services/transaction_search/product_searcher.rb
@@ -3,7 +3,7 @@ module TransactionSearch
   class ProductSearcher < BaseSearcher
 
     def options
-      Product.where(id: order_details.select("distinct order_details.product_id")).reorder(:name)
+      Product.where(id: order_details.select("distinct order_details.product_id")).order(:name)
     end
 
     def search(params)
