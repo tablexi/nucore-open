4b12cfe791062fc8d95eb6e9a101c334bb180780
Gilad Shanan <giladshanan@gmail.com>
2018-03-09 15:33:53 -0600

    remove deprecated code

--- app/services/most_recently_used_searcher.rb
+++ app/services/most_recently_used_searcher.rb
@@ -8,9 +8,6 @@ class MostRecentlyUsedSearcher
   def recently_used_facilities(limit = 5)
     return [] unless user
 
-    # facility_ids = user.orders.purchased.order("MAX(ordered_at) DESC").limit(limit).group(:facility_id).pluck(:facility_id)
-    # Facility.where(id: facility_ids).active.sorted
-
     Facility.active.joins(products: { order_details: :order }).merge(Order.for_user(user).group("orders.facility_id").order('MAX(orders.ordered_at) DESC')).limit(limit)
   end
 
@@ -18,8 +15,6 @@ class MostRecentlyUsedSearcher
     return [] unless user
 
     Product.active.in_active_facility.joins(order_details: :order).merge(Order.for_user(user).group(:product_id).order('MAX(orders.ordered_at) DESC')).limit(limit)
-
-    # user.order_details.purchased.joins(:product).merge(Product.active.in_active_facility).group(:product_id).order("MAX(orders.ordered_at) DESC").limit(10).map(&:product)
   end
 
 end
