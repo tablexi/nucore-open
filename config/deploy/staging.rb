bbf2438dec65031ba569deb35c1f95c219c58a42
Federico Kauffman <fedekau@users.noreply.github.com>
2018-03-12 19:36:55 -0300

    Update capistrano server (#1439)

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server "nucore.stage.tablexi.com", user: "nucore", roles: %w(web app db)
+server "tablexi-shared02.txihosting.com", user: "nucore", roles: %w(web app db)
 set :deploy_to, "/home/nucore/nucore.stage.tablexi.com"
 set :rails_env, "stage"
 set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
