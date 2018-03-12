b8bf31d548f901078453191d1691077c8df59b4c
Gilad Shanan <giladshanan@gmail.com>
2018-03-12 10:53:55 -0500

    rubocop

--- app/services/most_recently_used_searcher.rb
+++ app/services/most_recently_used_searcher.rb
@@ -10,22 +10,22 @@ class MostRecentlyUsedSearcher
     return [] unless user
 
     Facility.active
-      .joins(products: { order_details: :order })
-      .merge(Order.for_user(user)
-        .group(:id)
-        .order('MAX(orders.ordered_at) DESC'))
-      .limit(limit)
+            .joins(products: { order_details: :order })
+            .merge(Order.for_user(user)
+              .group(:id)
+              .order('MAX(orders.ordered_at) DESC'))
+            .limit(limit)
   end
 
   def recently_used_products(limit = 10)
     return [] unless user
 
     Product.active.in_active_facility
-      .joins(order_details: :order)
-      .merge(Order.for_user(user)
-        .group(:id)
-        .order('MAX(orders.ordered_at) DESC'))
-      .limit(limit)
+           .joins(order_details: :order)
+           .merge(Order.for_user(user)
+             .group(:id)
+             .order('MAX(orders.ordered_at) DESC'))
+           .limit(limit)
   end
 
 end
