29dc7086af4cdc38460420abc43901bcd28af96e
meara <mcharnetzki@gmail.com>
2018-03-14 13:54:20 -0500

    rubocop

--- app/controllers/global_search_controller.rb
+++ app/controllers/global_search_controller.rb
@@ -12,14 +12,14 @@ class GlobalSearchController < ApplicationController
   end
 
   def index
-    if session_user.nil? || session_user != acting_user
-      # if user is not logged in, or if ordering on behalf, only product searching is available
-      @searchers = [GlobalSearch::ProductSearcher.new(acting_user, current_facility, params[:search])]
-    else
-      @searchers = self.class.searcher_classes.map do |searcher_class|
-        searcher_class.new(acting_user, current_facility, params[:search])
-      end
-    end
+    @searchers = if session_user.nil? || session_user != acting_user
+                   # if user is not logged in, or if ordering on behalf, only product searching is available
+                   [GlobalSearch::ProductSearcher.new(acting_user, current_facility, params[:search])]
+                 else
+                   self.class.searcher_classes.map do |searcher_class|
+                     searcher_class.new(acting_user, current_facility, params[:search])
+                   end
+                 end
   end
 
 end
