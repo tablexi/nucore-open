90b57178dae0c36069f89b4001bdac469a907bc5
meara <mcharnetzki@gmail.com>
2018-03-15 11:56:16 -0500

    flip conditional

--- app/controllers/global_search_controller.rb
+++ app/controllers/global_search_controller.rb
@@ -12,13 +12,12 @@ class GlobalSearchController < ApplicationController
   end
 
   def index
-    @searchers = if session_user.nil? || session_user != acting_user
-                   # if user is not logged in, or if ordering on behalf, only product searching is available
-                   [GlobalSearch::ProductSearcher.new(acting_user, current_facility, params[:search])]
-                 else
+    @searchers = if user_signed_in? && !acting_as?
                    self.class.searcher_classes.map do |searcher_class|
                      searcher_class.new(acting_user, current_facility, params[:search])
                    end
+                 else
+                   [GlobalSearch::ProductSearcher.new(acting_user, current_facility, params[:search])]
                  end
   end
 
