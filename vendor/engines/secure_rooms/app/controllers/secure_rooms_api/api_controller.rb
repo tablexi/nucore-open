584664599a20c86572183d328135ac002c9642e9
meara <mcharnetzki@gmail.com>
2018-03-28 17:35:45 -0500

    unbreak stage deploy (hopefully)

--- vendor/engines/secure_rooms/app/controllers/secure_rooms_api/api_controller.rb
+++ vendor/engines/secure_rooms/app/controllers/secure_rooms_api/api_controller.rb
@@ -3,7 +3,7 @@ module SecureRoomsApi
   class ApiController < ApplicationController
 
     http_basic_authenticate_with(
-      name: Settings.secure_rooms_api.basic_auth_name,
+      name: Secrets.secure_rooms_api.basic_auth_name,
       password: Settings.secure_rooms_api.basic_auth_password,
     )
 
