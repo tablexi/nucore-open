45b1c2b394c9c9c131dc99c2cf331d67b8a97282
meara <mcharnetzki@gmail.com>
2018-04-06 16:50:12 -0500

    rubocop

--- spec/features/admin/disputed_orders_search_spec.rb
+++ spec/features/admin/disputed_orders_search_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe "Disputed Orders Search" do
   let(:item) { create(:setup_item, facility: facility) }
   let(:accounts) { create_list(:setup_account, 2) }
   let(:orders) do
-    accounts.map { |account| create(:complete_order, product: item, account: account ) }
+    accounts.map { |account| create(:complete_order, product: item, account: account) }
   end
 
   let(:order_details) { orders.map(&:order_details).flatten }
 
   before do
     order_details.each do |detail|
-      detail.update(dispute_at: Time.now, dispute_reason: "because i said so")
+      detail.update(dispute_at: Time.current, dispute_reason: "because i said so")
     end
   end
 
