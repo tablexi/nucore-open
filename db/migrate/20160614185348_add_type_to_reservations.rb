0f37b37c9bad83246a851e3a0e99e1b3f2fab7e2
Jason Hanggi <jason@tablexi.com>
2018-04-09 17:49:40 -0500

    Fix migration from scratch caused by paranoia (#1479)
    Tech task: Migration from scratch became broken after we introduced Paranoia.

--- db/migrate/20160614185348_add_type_to_reservations.rb
+++ db/migrate/20160614185348_add_type_to_reservations.rb
@@ -1,9 +1,12 @@
 class AddTypeToReservations < ActiveRecord::Migration
 
+  class Reservation < ActiveRecord::Base
+  end
+
   def up
     add_column :reservations, :type, :string
     change_column :reservations, :reserve_end_at, :datetime, null: true
-    admin_reservations = Reservation.where(order_detail: nil)
+    admin_reservations = Reservation.where(order_detail_id: nil)
     admin_reservations.where(reserve_end_at: nil).update_all(type: "OfflineReservation")
     admin_reservations.where.not(reserve_end_at: nil).update_all(type: "AdminReservation")
   end
