1674ab0a90cdabeeb6df51a417515a25458be994
meara <mcharnetzki@gmail.com>
2018-04-09 17:27:54 -0500

    only use settings/stage.yml when in stage environment

--- config/initializers/02_add_settings_overrides.rb
+++ config/initializers/02_add_settings_overrides.rb
@@ -1,3 +1,3 @@
-Settings.add_source!("#{Rails.root}/config/settings/stage.yml")
+Settings.add_source!("#{Rails.root}/config/settings/stage.yml") if ENV["RAILS_ENV"] == "stage"
 Settings.add_source!("#{Rails.root}/config/settings/override.yml")
 Settings.reload!
