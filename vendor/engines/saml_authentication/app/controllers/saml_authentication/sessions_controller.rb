9bc3ca222724a17ee9c217205fce85dac418495a
Jason Hanggi <jason@tablexi.com>
2018-04-13 17:57:39 -0500

    logout path based on winning strategy

--- vendor/engines/saml_authentication/app/controllers/saml_authentication/sessions_controller.rb
+++ vendor/engines/saml_authentication/app/controllers/saml_authentication/sessions_controller.rb
@@ -4,7 +4,8 @@ module SamlAuthentication
 
     # Some SSO providers are strict and require that the name_identifier matches
     # what's logged in on their system for SLO.
-    before_action(only: :destroy) { @signed_out_username = current_user.public_send(Devise.saml_default_user_key) }
+    before_action :store_logging_out_username, only: :destroy
+    after_action :store_winning_strategy, only: :create
 
     protected
 
@@ -19,6 +20,18 @@ module SamlAuthentication
       config = saml_config(idp_entity_id).dup
       config.name_identifier_value = @signed_out_username
       request.create(config)
+    end
+
+    private
+
+    def store_logging_out_username
+      @signed_out_username = current_user.public_send(Devise.saml_default_user_key)
+    end
+
+    # This will be used to choose which Logout link is shown to the user.
+    # https://github.com/apokalipto/devise_saml_authenticatable/wiki/Supporting-multiple-authentication-strategies
+    def store_winning_strategy
+      warden.session(resource_name)[:strategy] = warden.winning_strategies[resource_name].class.name.demodulize.underscore.to_sym
     end
 
   end
