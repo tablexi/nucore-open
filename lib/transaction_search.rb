e536fc9b1ba323c842ef707e03f839c73d451a6f
meara <mcharnetzki@gmail.com>
2018-04-13 18:45:35 -0500

    add back slimmed down transaction search module

---
+++ lib/transaction_search.rb
@@ -0,0 +1,14 @@
+module TransactionSearch
+
+  # Register a TransactionSearch::BaseSearcher. Unless specified otherwise,
+  # it will be added to the list of default searchers.
+  def self.register(searcher, default: true)
+    Searcher.default_searchers << searcher if default
+    SearchForm.send(:attr_accessor, searcher.key)
+  end
+
+  def self.register_optimizer(optimizer)
+    Searcher.optimizers << optimizer
+  end
+
+end
