dacf32967c50daeeb5205d478abef3a521228be9
Jason Hanggi <jason@tablexi.com>
2018-04-17 12:53:14 -0500

    Include actual username in logout request

--- vendor/engines/saml_authentication/app/controllers/saml_authentication/sessions_controller.rb
+++ vendor/engines/saml_authentication/app/controllers/saml_authentication/sessions_controller.rb
@@ -2,6 +2,9 @@ module SamlAuthentication
 
   class SessionsController < Devise::SamlSessionsController
 
+    # Some SSO providers are strict and require that the name_identifier matches
+    # what's logged in on their system for SLO.
+    before_action :store_logging_out_username, only: :destroy
     after_action :store_winning_strategy, only: :create
 
     protected
@@ -9,6 +12,20 @@ module SamlAuthentication
     # Remove recall, so failures redirect back to sign_in page
     def auth_options
       { scope: resource_name }
+    end
+
+    def after_sign_out_path_for(_)
+      idp_entity_id = get_idp_entity_id(params)
+      request = OneLogin::RubySaml::Logoutrequest.new
+      config = saml_config(idp_entity_id).dup
+      config.name_identifier_value = @signed_out_username
+      request.create(config)
+    end
+
+    private
+
+    def store_logging_out_username
+      @signed_out_username = current_user.public_send(Devise.saml_default_user_key)
     end
 
     # This will be used to choose which Logout link is shown to the user.
