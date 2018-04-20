88bc7acbc3e2b16dc37ddefc99f4e1d4ff7db49f
Rob Stolarz <robert.stolarz@yandex.com>
2018-04-20 10:29:19 -0400

    ViewHook journal download formats (UConn side)

--- vendor/engines/nucore_kfs/lib/nucore_kfs/engine.rb
+++ vendor/engines/nucore_kfs/lib/nucore_kfs/engine.rb
@@ -1,4 +1,9 @@
 module NucoreKfs
   class Engine < ::Rails::Engine
+    config.to_prepare do
+      ViewHook.add_hook "facility_journals.downloads",
+                        "other_formats",
+                        "kfs_csv_partial"
+    end
   end
 end
