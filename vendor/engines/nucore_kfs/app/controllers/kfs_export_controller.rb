2cfea8d3654d7958c5a32fce30a37bea0b2bec69
Rob Stolarz <robert.stolarz@yandex.com>
2018-04-20 13:23:28 -0400

    Allow user to download file instead of viewing

--- vendor/engines/nucore_kfs/app/controllers/kfs_export_controller.rb
+++ vendor/engines/nucore_kfs/app/controllers/kfs_export_controller.rb
@@ -1,6 +1,8 @@
 class KfsExportController < ApplicationController
+  include CSVHelper
   def show
     @journal = Journal.find(params[:id])
     @journal_rows = @journal.journal_rows
+    set_csv_headers("#{params[:id]}.csv")
   end
 end
