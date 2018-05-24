76811ff06d0b7fb0804afacf76491fb4ea47f4dd
Colin McNeil <colinmcneil@gmail.com>
2018-05-24 15:06:22 -0400

    Replace thrown error with redirect and error message flash

--- vendor/engines/nucore_kfs/app/controllers/kfs_export_controller.rb
+++ vendor/engines/nucore_kfs/app/controllers/kfs_export_controller.rb
@@ -3,6 +3,17 @@ class KfsExportController < ApplicationController
   def show
     @journal = Journal.find(params[:id])
     @journal_rows = @journal.journal_rows
+    @journal.journal_rows.each do |journal_row|
+      od = journal_row.order_detail
+      next unless journal_row.order_detail
+      next unless od.account.account_number.start_with? "KFS" # do we need this
+      prod = journal_row.order_detail.product
+      if !(prod.facility_account and prod.facility_account.account_number.start_with? "KFS") then
+        flash[:error] = "ERROR: Couldn't determine where to return revenue for #{prod.facility.name}'s #{prod.name}"
+        redirect_to facility_journals_path(current_facility)
+        
+      end
+    end
     set_csv_headers("#{params[:id]}.csv")
   end
 end
