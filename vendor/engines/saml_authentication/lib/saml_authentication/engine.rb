0424e8d39e8563ed99fbc840e774971b8506bea1
Vladimir Andrijevikj <vandrijevik@users.noreply.github.com>
2018-06-08 16:10:56 +0200

    [#142184] Allow customizing error message for non-existent users signing in via SAML (#1589)
    Allow customizing error message for non-existent users signing in via SAML

--- vendor/engines/saml_authentication/lib/saml_authentication/engine.rb
+++ vendor/engines/saml_authentication/lib/saml_authentication/engine.rb
@@ -2,6 +2,7 @@ require "devise_saml_authenticatable"
 require "saml_authentication/routes"
 require "saml_authentication/model"
 require "saml_authentication/devise_configurator"
+require "saml_authentication/saml_authenticatable_with_custom_error"
 
 module SamlAuthentication
 
@@ -10,10 +11,12 @@ module SamlAuthentication
     config.to_prepare do
       next if Settings.saml.blank?
 
-      User.send(:devise, :saml_authenticatable)
-      ViewHook.add_hook "devise.sessions.new",
-                        "before_login_form",
-                        "saml_authentication/sessions/new"
+      User.send(:devise, :saml_authenticatable_with_custom_error)
+      if Settings.saml.login_enabled
+        ViewHook.add_hook "devise.sessions.new",
+                          "before_login_form",
+                          "saml_authentication/sessions/new"
+      end
 
       OneLogin::RubySaml::Logging.logger.level = Rails.env.production? ? Logger::INFO : Logger::DEBUG
     end
---
+++ vendor/engines/saml_authentication/lib/saml_authentication/saml_authenticatable_with_custom_error.rb
@@ -0,0 +1,40 @@
+module Devise
+
+  module Models
+
+    SamlAuthenticatableWithCustomError = SamlAuthenticatable
+
+  end
+
+  module Strategies
+
+    class SamlAuthenticatableWithCustomError < SamlAuthenticatable
+
+      private
+
+      # In the original SamlAuthenticatable, this method calls fail!(:invalid), which
+      # causes Devise to use the error message defined in devise.failure.invalid. That
+      # error message is used by other strategies, including the email and password strategy
+      # that we also use, so if we change the message we'll incorrectly affect the other
+      # strategies as well. Therefore, we override the method responsible for failing
+      # authentication to call fail!(:saml_invalid), which allows us to specify a separate
+      # failure message for authentication via SAML.
+      def failed_auth(msg)
+        DeviseSamlAuthenticatable::Logger.send(msg)
+        fail!(:saml_invalid)
+        Devise.saml_failed_callback.new.handle(@response, self) if Devise.saml_failed_callback # rubocop:disable Style/SafeNavigation
+      end
+
+    end
+
+  end
+
+end
+
+Warden::Strategies.add(:saml_authenticatable_with_custom_error, Devise::Strategies::SamlAuthenticatableWithCustomError)
+
+Devise.add_module(:saml_authenticatable_with_custom_error,
+                  route: :saml_authenticatable,
+                  strategy: true,
+                  controller: :saml_sessions,
+                  model: 'devise_saml_authenticatable/model')
