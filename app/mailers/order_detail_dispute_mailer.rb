0186789f612401e88b0280972a7a84a151eb481f
Jason Hanggi <jason@tablexi.com>
2018-07-18 17:03:37 -0500

    Dispute email

---
+++ app/mailers/order_detail_dispute_mailer.rb
@@ -0,0 +1,22 @@
+class OrderDetailDisputeMailer < BaseMailer
+
+  add_template_helper DateHelper
+
+  def dispute_resolved(order_detail:, user:)
+    @order_detail = order_detail
+    @user = user
+    mail(
+      to: @user.email,
+      subject: text("subject",
+        facility_abbreviation: @order_detail.facility.abbreviation,
+      )
+    )
+  end
+
+  protected
+
+  def translation_scope
+    "views.order_detail_dispute_mailer.dispute_resolved"
+  end
+
+end
---
+++ spec/mailers/previews/order_detail_dispute_mailer_preview.rb
@@ -0,0 +1,8 @@
+class OrderDetailDisputeMailerPreview < ActionMailer::Preview
+
+  def dispute_resolved
+    order_detail = NUCore::Database.random(OrderDetail.where.not(dispute_resolved_at: nil))
+    OrderDetailDisputeMailer.dispute_resolved(order_detail: order_detail, user: order_detail.user)
+  end
+
+end
