57266325ba8264119c1c30ccfc65b4b824143643
Jason Hanggi <jason@tablexi.com>
2018-07-24 12:28:20 -0500

    Fix C2poAccountValidator naming (#1622)
    This was causing occassional unknown constant errors in development due to class reloading and the `to_prepare` statement. `Facility.facility_account_validators << C2po::C2poAccountValidator`.

---
+++ vendor/engines/c2po/app/validators/c2po/c2po_account_validator.rb
@@ -0,0 +1,13 @@
+module C2po
+
+  class C2poAccountValidator < FacilityAccountValidator
+
+    def valid?
+      return false if account.is_a?(PurchaseOrderAccount) && !facility.accepts_po?
+      return false if account.is_a?(CreditCardAccount) && !facility.accepts_cc?
+      true
+    end
+
+  end
+
+end
