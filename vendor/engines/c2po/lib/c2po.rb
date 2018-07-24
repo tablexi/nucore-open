9fbcc45103d34399a1eff8ac0f58728464bf7dd5
Jason Hanggi <jason@tablexi.com>
2018-07-24 09:44:08 -0500

    Upgrade to Circle 2.0 (#1626)
    Switch to Circle 2.0 for CI.

--- vendor/engines/c2po/lib/c2po.rb
+++ vendor/engines/c2po/lib/c2po.rb
@@ -43,6 +43,12 @@ module C2po
       app.reloader.to_run(&C2PO_ACCOUNT_TYPES_APPENDER)
     end
 
+    initializer "model_core.factories", after: "factory_girl.set_factory_paths" do
+      if defined?(FactoryBot)
+        FactoryBot.definition_file_paths << File.expand_path("../../spec/factories", __FILE__)
+      end
+    end
+
   end
 
 end
