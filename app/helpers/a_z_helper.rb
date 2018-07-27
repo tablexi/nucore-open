1d168a737d206b1fffd7d98c9fcb2fee69a22590
Colin McNeil <colinmcneil@gmail.com>
2018-07-27 10:32:46 -0400

    Add A-Z list to facilities page (#1639)
    Add an A-Z navigation bar to the facilities home page.
    
    Feature flagged, defaulted to off.
    
    Merges #1639

---
+++ app/helpers/a_z_helper.rb
@@ -0,0 +1,13 @@
+module AZHelper
+
+  # Generates empty hash with keys for each letter
+  ALPHABET = ("A".."Z").to_a.map { |x| [x, []] }.to_h
+  def get_az_list(facilities)
+    ALPHABET.merge(facilities.group_by { |row| row.name[0] })
+  end
+
+  def az_classname_for_facility(index, _letter)
+    "js--azlist#{index}"
+  end
+
+end
