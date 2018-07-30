d26a6e63060d04df9b5278b242f36c99443d7495
Colin McNeil <colinmcneil@gmail.com>
2018-07-30 13:17:37 -0400

    Refactor single-quotes to double-quotes

--- app/assets/javascripts/app/azlist.js
+++ app/assets/javascripts/app/azlist.js
@@ -1,27 +1,27 @@
 
-var oldText = 'All Facilities'
+var oldText = "All Facilities"
 
 function removeExtraClasses(classlist) {
-    if (classlist.includes('all')) return 'all'
-    else if(classlist.includes('recent')) return 'recent'
-    return 'js--azlist' + classlist.replace(' ', '').split('js--azlist')[1]
+    if (classlist.includes("all")) return "all"
+    else if(classlist.includes("recent")) return "recent"
+    return "js--azlist" + classlist.replace(" ", "").split("js--azlist")[1]
 }
 
 $(document).ready(function () {
-    oldText = $('.allHeader').text()
+    oldText = $(".allHeader").text()
 })
-$(document).on('click', '.js--az_listing', function () {
-    $('.js--facility_listing').hide();
-    var category = removeExtraClasses( $(this).attr('class') )
+$(document).on("click", ".js--az_listing", function () {
+    $(".js--facility_listing").hide();
+    var category = removeExtraClasses( $(this).attr("class") )
     console.log(category)
-    if (category === 'recent') $('.recent').show()
-    else if (category === 'all') {
-        $('.allHeader').text(oldText)
-        $('.js--facility_listing').show()
+    if (category === "recent") $(".recent").show()
+    else if (category === "all") {
+        $(".allHeader").text(oldText)
+        $(".js--facility_listing").show()
     }
     else {
-        $('.' + category).show()
-        $('.allHeader').show()
-        $('.allHeader').text($(this).text())
+        $("." + category).show()
+        $(".allHeader").show()
+        $(".allHeader").text($(this).text())
     }
 });
--- app/helpers/a_z_helper.rb
+++ app/helpers/a_z_helper.rb
@@ -1,13 +1,13 @@
 module AZHelper
 
   # Generates empty hash with keys for each letter
-  ALPHABET = ('A'..'Z').to_a.map { |x| [x, []] }.to_h
+  ALPHABET = ("A".."Z").to_a.map { |x| [x, []] }.to_h
   def get_az_list(facilities)
     ALPHABET.merge(facilities.group_by { |row| row.name[0] })
   end
 
   def get_classname_for_facility(index, letter)
-    'js--azlist' + index.to_s
+    "js--azlist" + index.to_s
   end
 
 end
