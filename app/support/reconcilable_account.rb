80ab5fc41167d6d88ab55fa8d368ae9dd190e002
Vladimir Andrijevikj <vandrijevik@users.noreply.github.com>
2018-09-10 16:53:47 +0200

    Remove unused method ReconcilableAccount::ClassMethods#need_reconciling (#1686)
    TECH TASK: Remove unused method ReconcilableAccount::ClassMethods#need_reconciling

--- app/support/reconcilable_account.rb
+++ app/support/reconcilable_account.rb
@@ -2,15 +2,4 @@
 
 module ReconcilableAccount
 
-  extend ActiveSupport::Concern
-
-  module ClassMethods
-
-    def need_reconciling(facility)
-      accounts = joins(:order_details).merge(OrderDetail.complete.statemented(facility))
-      where(id: accounts.distinct.pluck(:id))
-    end
-
-  end
-
 end
