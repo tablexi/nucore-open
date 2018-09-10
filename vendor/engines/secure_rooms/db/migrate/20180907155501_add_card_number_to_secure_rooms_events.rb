763cdc7bff6c7895cde135eaf4c3a8db38192823
Vladimir Andrijevikj <vandrijevik@users.noreply.github.com>
2018-09-10 11:21:29 +0200

    Appease rubocop

--- vendor/engines/secure_rooms/db/migrate/20180907155501_add_card_number_to_secure_rooms_events.rb
+++ vendor/engines/secure_rooms/db/migrate/20180907155501_add_card_number_to_secure_rooms_events.rb
@@ -1,17 +1,19 @@
 class AddCardNumberToSecureRoomsEvents < ActiveRecord::Migration
+
   def up
     add_column :secure_rooms_events, :card_number, :string
-    update <<~END_OF_SQL
+    update <<~SQL
       UPDATE secure_rooms_events
       SET card_number =
         (SELECT card_number
          FROM users
          WHERE users.id = secure_rooms_events.user_id)
-    END_OF_SQL
+    SQL
     change_column :secure_rooms_events, :card_number, :string, null: false
   end
 
   def down
     remove_column :secure_rooms_events, :card_number
   end
+
 end
