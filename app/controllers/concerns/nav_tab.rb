tablexi/nucore-open

commit 9ef31df15c09490754521dac347c0ead716d4bfa

Use/Manage menu items (#1683)
# Release Notes

Implementation of our Use/Manage design separation. Currently, "Use" will bring the user back to the facility list home page unless they are managing a facility. If they are an admin and managing a facility, then they will be taken to the facility-specific homepage with the product list.

Manage mode will take the user to the manageable facility list, however it could also be a dropdown such as in our original design.

# Screenshot
![screen shot 2018-09-06 at 9 47 16 am](https://user-images.githubusercontent.com/5000430/45161460-e68f0980-b1b9-11e8-91c8-60cfca6bc0fd.png)

app/controllers/concerns/nav_tab.rb
@@ -12,6 +12,7 @@ module NavTab
     helper_method(:admin_tab?)
     helper_method(:global_navigation_links)
     helper_method(:navigation_links)
+    helper_method(:home_button)
   end
 
   module ClassMethods
@@ -37,12 +38,16 @@ module NavTab
     case
     when customer_tab? && acting_user.present?
       link_collection.customer.compact
+    when customer_tab? && acting_user.present? && current_facility.present? && current_facility != Facility.cross_facility
+      link_collection.manager
     when admin_tab? && current_facility.present? && current_facility != Facility.cross_facility
       link_collection.admin
     else
       link_collection.default
     end
   end
+
+  delegate :home_button, to: :link_collection
 
   def global_navigation_links
     acting_as? ? [] : global_link_collection.links

app/controllers/facilities_controller.rb
@@ -46,8 +46,8 @@ class FacilitiesController < ApplicationController
     raise ActiveRecord::RecordNotFound unless current_facility.try(:is_active?)
     @order_form = nil
     @order_form = Order.new if acting_user && current_facility.accepts_multi_add?
-    @active_tab = "home"
     set_column_class
+    @active_tab = "use"
     render layout: "application"
   end
 

app/presenters/nav_tab/link_collection.rb
@@ -27,15 +27,27 @@ class NavTab::LinkCollection
   end
 
   def admin
-    default + admin_only
+    if SettingsHelper.feature_on?(:use_manage)
+      [home_button] + [manage] + admin_only
+    else
+      [home_button] + admin_only
+    end
   end
 
   def customer
-    default + [orders, reservations, payment_sources, files]
+    default
   end
 
   def default
-    [home]
+    [orders, reservations, payment_sources, files]
+  end
+
+  def home_button
+    if SettingsHelper.feature_on?(:use_manage)
+      use
+    else
+      home
+    end
   end
 
   private
@@ -131,8 +143,13 @@ class NavTab::LinkCollection
     )
   end
 
-  def home
-    NavTab::Link.new(tab: :home, url: root_path)
+  def use
+    url = facility ? facility_path(facility) : root_path
+    NavTab::Link.new(tab: :use, url: url)
+  end
+
+  def manage
+    NavTab::Link.new(text: I18n.t("pages.manage", model: Facility.model_name.human(count: 2)), url: list_facilities_url)
   end
 
   def instrument_utilization_reports
@@ -152,6 +169,10 @@ class NavTab::LinkCollection
       text: t_my(Reservation),
       url: reservations_path,
     )
+  end
+
+  def home
+    NavTab::Link.new(tab: :home, url: root_path)
   end
 
 end

app/views/shared/nav/_nav_links.html.haml
@@ -2,6 +2,11 @@
   - if responsive?
     -# .hidden-with-nav is hidden > 979px
     %li.hidden-with-nav= link_to t('pages.cart') + " (#{current_cart.order_details.count})", :cart
+    %li{class: [home_button.tab_class(controller), "useButton"], id: home_button.tab_id }  
+      = home_button.to_html
+    - if SettingsHelper.feature_on?(:use_manage)
+      = render "shared/nav/manage_facilities"
+
   - navigation_links.each do |link|
     %li{class: link.tab_class(controller), id: link.tab_id }
       - if link.subnav.present?
@@ -14,6 +19,7 @@
               %li= subnav_link.to_html
       - else
         = link.to_html
+  
 
 -# .visible-with-nav is visible > 979px
 = form_tag global_search_path, class: "navbar-search pull-right visible-with-nav" do
@@ -21,6 +27,7 @@
 
 -# trailing "<" enables use of .nav-collapse .nav:empty in boostrap-responsive-overrides.scss
 %ul.nav.pull-right<
-  = render "shared/nav/manage_facilities"
+  - if !SettingsHelper.feature_on?(:use_manage)
+    = render "shared/nav/manage_facilities"
   - global_navigation_links.each do |link|
     %li{class: link.tab_class(controller)}= link.to_html

app/models/serializable_facility.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class SerializableFacility < SimpleDelegator
 
   include GlobalID::Identification

config/locales/en.yml
@@ -47,6 +47,8 @@ en:
         statements: Statements
 
   pages:
+    home: Home
+    use: Use
     admin_billing: Billing
     admin_facility: Admin
     admin_orders: Orders
@@ -62,7 +64,6 @@ en:
     global_billing: Billing
     global_settings: Global Settings
     global_user_roles: Global User Roles
-    home: Home
     instrument_utilization_reports: Instrument Utilization
     login: Login
     logout: Logout

config/settings.yml
@@ -122,6 +122,7 @@ feature:
   cross_facility_reports_on: false
   product_list_columns_on: false
   azlist_on: false
+  use_manage_on: false
 
 split_accounts:
   # Roles are allowed to create Split Accounts

commit cfca15445ba55e686f79712c800b1dce5e1d362f

Fix Use/Manage links for global settings
# Release Notes

* Fixed navbar in global settings
* Fixed active tab when acting as another user

# Additional Context

Addresses some minor issues introduced in #1683

Merges #1704

app/controllers/concerns/nav_tab.rb
@@ -36,14 +36,13 @@ module NavTab
 
   def navigation_links
     case
-    when customer_tab? && acting_user.present?
+    when customer_tab? && !acting_as?
       link_collection.customer.compact
     when customer_tab? && acting_user.present? && current_facility.present? && current_facility != Facility.cross_facility
       link_collection.manager
     when admin_tab? && current_facility.present? && current_facility != Facility.cross_facility
       link_collection.admin
-    else
-      link_collection.default
+    else []
     end
   end
 

app/controllers/facilities_controller.rb
@@ -34,7 +34,7 @@ class FacilitiesController < ApplicationController
   def index
     @facilities = Facility.active.alphabetized
     @recently_used_facilities = MostRecentlyUsedSearcher.new(acting_user).recently_used_facilities.alphabetized
-    @active_tab = "home"
+    @active_tab = SettingsHelper.feature_on?(:use_manage) ? "use" : "home"
     @recent_products = MostRecentlyUsedSearcher.new(acting_user).recently_used_products.includes(:facility).alphabetized
     @azlist = get_az_list(@facilities)
     render layout: "application"
@@ -47,7 +47,7 @@ class FacilitiesController < ApplicationController
     @order_form = nil
     @order_form = Order.new if acting_user && current_facility.accepts_multi_add?
     set_column_class
-    @active_tab = "use"
+    @active_tab = SettingsHelper.feature_on?(:use_manage) ? "use" : "home"
     render layout: "application"
   end
 

app/presenters/nav_tab/link_collection.rb
@@ -27,18 +27,10 @@ class NavTab::LinkCollection
   end
 
   def admin
-    if SettingsHelper.feature_on?(:use_manage)
-      [home_button] + [manage] + admin_only
-    else
-      [home_button] + admin_only
-    end
+    admin_only
   end
 
   def customer
-    default
-  end
-
-  def default
     [orders, reservations, payment_sources, files]
   end
 

app/views/shared/nav/_nav_links.html.haml
@@ -1,11 +1,11 @@
 %ul.nav
+  %li{class: [home_button.tab_class(controller), "useButton"], id: home_button.tab_id }
+    = home_button.to_html
+  - if SettingsHelper.feature_on?(:use_manage)
+    = render "shared/nav/manage_facilities"
   - if responsive?
     -# .hidden-with-nav is hidden > 979px
     %li.hidden-with-nav= link_to t('pages.cart') + " (#{current_cart.order_details.count})", :cart
-    %li{class: [home_button.tab_class(controller), "useButton"], id: home_button.tab_id }  
-      = home_button.to_html
-    - if SettingsHelper.feature_on?(:use_manage)
-      = render "shared/nav/manage_facilities"
 
   - navigation_links.each do |link|
     %li{class: link.tab_class(controller), id: link.tab_id }
@@ -19,7 +19,7 @@
               %li= subnav_link.to_html
       - else
         = link.to_html
-  
+
 
 -# .visible-with-nav is visible > 979px
 = form_tag global_search_path, class: "navbar-search pull-right visible-with-nav" do