d881a483e47fdfeb0dbe116f65835c431de2fe64
Jason Hanggi <jason@tablexi.com>
2018-09-20 12:19:31 -0500

    Fix busted spec

--- vendor/engines/projects/spec/features/reserving_an_instrument_on_a_project_spec.rb
+++ vendor/engines/projects/spec/features/reserving_an_instrument_on_a_project_spec.rb
@@ -30,8 +30,8 @@ RSpec.describe "Reserving an instrument on a project" do
       click_button "Create"
     end
 
-    it "returns to My Reservations" do
-      expect(page).to have_content "My Reservations"
+    it "takes you to the receipt and has the project set" do
+      expect(page).to have_content "Order Receipt"
       expect(OrderDetail.last.project).to eq(project)
     end
   end
