21dff8c57ad8aee05d999df76015947288a9afdb
Vladimir Andrijevikj <vandrijevik@users.noreply.github.com>
2018-10-02 12:52:43 +0200

    Improve spec for ReconcilliationsController

--- spec/controllers/reconcilliations_controller_spec.rb
+++ spec/controllers/reconcilliations_controller_spec.rb
@@ -4,16 +4,10 @@ require "rails_helper"
 require "controller_spec_helper"
 
 RSpec.describe ReconcilliationsController do
-  before(:all) { create_users }
-
   let(:user) { FactoryBot.create(:user) }
   let(:facility) { FactoryBot.create(:setup_facility) }
   let(:item) { FactoryBot.create(:item, facility: facility) }
-  let(:account) do
-    FactoryBot.create(:nufs_account, account_users_attributes: [FactoryBot.attributes_for(:account_user, user: user)]).tap do |account|
-      define_open_account item.account, account.account_number
-    end
-  end
+  let(:account) { FactoryBot.create(:setup_account, owner: user) }
   let(:order_detail) { place_and_complete_item_order(user, facility, account) }
 
   before :each do
@@ -22,8 +16,10 @@ RSpec.describe ReconcilliationsController do
 
   describe "DELETE to :destroy" do
     context "for an administrator" do
+      let(:admin) { FactoryBot.create(:user, :administrator) }
+
       before :each do
-        sign_in @admin
+        sign_in admin
         delete :destroy, params: { facility_id: facility.url_name, order_id: order_detail.order.id, order_detail_id: order_detail.id }
       end
 
@@ -37,8 +33,10 @@ RSpec.describe ReconcilliationsController do
     end
 
     context "for a user that is not an administrator" do
+      let(:director) { create(:user, :facility_director, facility: facility) }
+
       before :each do
-        sign_in @director
+        sign_in director
         delete :destroy, params: { facility_id: facility.url_name, order_id: order_detail.order.id, order_detail_id: order_detail.id }
       end
 
