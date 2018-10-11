7740b8a0d4f8d5ab92c813c0234ada778cbdb738
colinmcneil <colinmcneil@gmail.com>
2018-10-11 13:22:53 -0400

    Remove comments, fix rubocop offenses

--- app/presenters/account_user_presenter.rb
+++ app/presenters/account_user_presenter.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
 
 class AccountUserPresenter < SimpleDelegator
+
   def self.localized_role(role)
-    I18n.t(role.downcase.parameterize.underscore, scope: "accounts.role") # Move the en.yml location to be more generic than `member_table`
+    I18n.t(role.downcase.parameterize.underscore, scope: "accounts.role")
   end
 
   def self.selectable_user_roles(granting_user = nil, facility = nil)
-    # Nothing other than the collection uses `selectable_user_roles`, so it could even be pulled up into this class
-    AccountUser.selectable_user_roles(granting_user, facility).map {  |role| [localized_role(role), role] }
+    AccountUser.selectable_user_roles(granting_user, facility).map { |role| [localized_role(role), role] }
   end
 
   def localized_role
     self.class.localized_role(user_role)
   end
+
 end
