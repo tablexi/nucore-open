64e50f3ff6719396ae2b9656ffac6bbd998b3ea1
Vladimir Andrijevikj <vandrijevik@users.noreply.github.com>
2018-11-02 19:35:44 +0100

    [#143728] Allow searching users by card number (#1747)
    * Add spec for UserFinder
    
    * Refactor specifying of searchable columns in UserFinder so it is overridable
    
    * Match on card_number in UserFinder
    
    * Display card number in user results table when the secure_rooms engine is turned on

--- app/services/user_finder.rb
+++ app/services/user_finder.rb
@@ -4,6 +4,16 @@ class UserFinder
 
   include SearchHelper
 
+  class_attribute :searchable_columns
+
+  self.searchable_columns = [
+    "first_name",
+    "last_name",
+    "username",
+    "CONCAT(first_name, last_name)",
+    "email",
+  ]
+
   def self.search(search_term, limit)
     if search_term.present?
       new(search_term, limit).result
@@ -23,23 +33,11 @@ class UserFinder
 
   private
 
-  def condition_sql
-    <<-SQL
-      (
-          LOWER(first_name) LIKE :search_term
-        OR
-          LOWER(last_name) LIKE :search_term
-        OR
-          LOWER(username) LIKE :search_term
-        OR
-          LOWER(CONCAT(first_name, last_name)) LIKE :search_term
-        OR
-          LOWER(email) LIKE :search_term
-      )
-    SQL
-  end
-
   def relation
+    condition_sql = searchable_columns
+                    .map { |column| "LOWER(#{column}) LIKE :search_term" }
+                    .join(" OR ")
+
     @relation ||= User.where(condition_sql, search_term: @search_term)
   end
 
---
+++ vendor/engines/secure_rooms/spec/services/user_finder_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe UserFinder do
+  describe ".search" do
+    it "finds users by card_number" do
+      card_number = SecureRandom.hex(8)
+      user = FactoryBot.create(:user, card_number: card_number)
+      found_users = UserFinder.search(card_number, nil).first
+      expect(found_users).to include(user)
+    end
+  end
+end
