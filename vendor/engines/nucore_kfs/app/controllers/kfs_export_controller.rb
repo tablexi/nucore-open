1ca7362c28b848921ed5bb40b33da4a9d6fdb26f
Rob Stolarz <robert.stolarz@yandex.com>
2018-11-09 12:33:25 -0500

    [WIP] KFS .dat integration

--- vendor/engines/nucore_kfs/app/controllers/kfs_export_controller.rb
+++ vendor/engines/nucore_kfs/app/controllers/kfs_export_controller.rb
@@ -1,19 +1,20 @@
 class KfsExportController < ApplicationController
-  include CSVHelper
   def show
     @journal = Journal.find(params[:id])
-    @journal_rows = @journal.journal_rows
     @journal.journal_rows.each do |journal_row|
       od = journal_row.order_detail
       next unless journal_row.order_detail
-      next unless od.account.account_number.start_with? "KFS" # do we need this
       prod = journal_row.order_detail.product
-      if !(prod.facility_account and prod.facility_account.account_number.start_with? "KFS") then
-        flash[:error] = "ERROR: Couldn't determine where to return revenue for #{prod.facility.name}'s #{prod.name}"
+      aan_out = od.account.account_number
+      fan_out = prod.facility_account.account_number
+
+      flash[:error] = "for id #{od.id}: order account not a kfs account: #{aan_out}" unless aan_out.match(/^KFS-(?<obj_code>\d{4})-(?<acct_num>\d{0,7})$/)
+      flash[:error] = "for id #{od.id}: recharge account not a kfs account: #{fan_out}" unless fan_out.match(/^KFS-(?<obj_code>\d{4})-(?<acct_num>\d{0,7})$/)
+      if flash[:error]
         redirect_to facility_journals_path(current_facility)
-        
+        return
       end
     end
-    set_csv_headers("#{params[:id]}.csv")
+    render action: "show.#{params[:format]}.erb", layout: false, content_type: "application/kfs-collector"
   end
 end
