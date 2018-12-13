9161d730dced8c10fe77abfcfeafd0efb513c80a
colinmcneil <colinmcneil@gmail.com>
2018-12-13 13:32:20 -0500

    Include same header for capybara

--- db/schema.rb
+++ db/schema.rb
@@ -161,6 +161,10 @@ ActiveRecord::Schema.define(version: 20181119211456) do
     t.string   "order_notification_recipient"
     t.boolean  "sanger_sequencing_enabled",                  default: false, null: false
     t.text     "banner_notice",                limit: 65535
+    t.string   "thumbnail_file_name"
+    t.string   "thumbnail_content_type"
+    t.bigint   "thumbnail_file_size"
+    t.datetime "thumbnail_updated_at"
     t.index ["abbreviation"], name: "index_facilities_on_abbreviation", unique: true, using: :btree
     t.index ["is_active", "name"], name: "index_facilities_on_is_active_and_name", using: :btree
     t.index ["name"], name: "index_facilities_on_name", unique: true, using: :btree
@@ -228,8 +232,8 @@ ActiveRecord::Schema.define(version: 20181119211456) do
   end
 
   create_table "log_events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "loggable_id"
     t.string   "loggable_type"
+    t.integer  "loggable_id"
     t.string   "event_type"
     t.integer  "user_id"
     t.datetime "created_at",    null: false
@@ -780,10 +784,10 @@ ActiveRecord::Schema.define(version: 20181119211456) do
   end
 
   create_table "vestal_versions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "versioned_id"
     t.string   "versioned_type"
-    t.integer  "user_id"
+    t.integer  "versioned_id"
     t.string   "user_type"
+    t.integer  "user_id"
     t.string   "user_name"
     t.text     "modifications",     limit: 65535
     t.integer  "version_number"
@@ -802,8 +806,6 @@ ActiveRecord::Schema.define(version: 20181119211456) do
     t.index ["versioned_id", "versioned_type"], name: "index_vestal_versions_on_versioned_id_and_versioned_type", using: :btree
   end
 
-  add_foreign_key "account_users", "accounts", name: "fk_accounts"
-  add_foreign_key "account_users", "users"
   add_foreign_key "accounts", "facilities", name: "fk_account_facility_id"
   add_foreign_key "bulk_email_jobs", "facilities"
   add_foreign_key "bulk_email_jobs", "users"
