f96124e574778e29bd09b1ac44b5c044801245da
Vladimir Andrijevikj <vandrijevik@users.noreply.github.com>
2018-12-18 11:52:46 +0100

    [#144074] Some Occupancies remain "In Progress" (#1800)
    * Fix processing of occupancies where the exit is after the allowed schedule
    
    * Remove now-unnecessary debug logging for SecureRooms method calls

--- vendor/engines/secure_rooms/app/services/secure_rooms/access_handlers/order_handler.rb
+++ vendor/engines/secure_rooms/app/services/secure_rooms/access_handlers/order_handler.rb
@@ -17,43 +17,31 @@ module SecureRooms
       end
 
       def process
-        Rails.logger.info("[SecureRooms] Entered SecureRooms::AccessHandlers::OrderHandler#process (occupancy id #{occupancy.id})")
         return if user_exempt_from_purchase?
 
         find_or_create_order
-        Rails.logger.info("[SecureRooms] order id #{order.id}; order detail id #{@order_detail.id}")
-        Rails.logger.info("[SecureRooms] occupancy.order_completable? = #{occupancy.order_completable?}")
         complete_order if occupancy.order_completable?
 
-        Rails.logger.info("[SecureRooms] Exiting SecureRooms::AccessHandlers::OrderHandler#process")
         order
       end
 
       private
 
       def find_or_create_order
-        Rails.logger.info("[SecureRooms] Entered SecureRooms::AccessHandlers::OrderHandler#find_or_create_order")
         if occupancy.order_detail_id?
-          Rails.logger.info("[SecureRooms] Processing branch occupancy.order_detail_id?")
           @order_detail = occupancy.order_detail
           @order = order_detail.order
         else
-          Rails.logger.info("[SecureRooms] Processing branch else")
           create_order
         end
-        Rails.logger.info("[SecureRooms] Exiting SecureRooms::AccessHandlers::OrderHandler#find_or_create_order")
       end
 
       def complete_order
-        Rails.logger.info("[SecureRooms] Entered SecureRooms::AccessHandlers::OrderHandler#complete_order")
         if occupancy.orphaned_at?
-          Rails.logger.info("Processing branch occupancy.orphaned_at?")
           MoveToProblemQueue.move!(order_detail)
         else
-          Rails.logger.info("[SecureRooms] Processing branch else")
           order_detail.complete!
         end
-        Rails.logger.info("[SecureRooms] Exiting SecureRooms::AccessHandlers::OrderHandler#complete_order")
       end
 
       def create_order
@@ -71,17 +59,8 @@ module SecureRooms
         occupancy.update(account: accounts.first)
       end
 
-      # Whether or not an occupant must pay for their time is determined by
-      # CheckAccess at scan-in time. In the event we don't have access to
-      # scan-in information, we check the user's access with a room's in-reader
-      # to find what the verdict would have been. A verdict returning accounts
-      # signifies the user would have needed to select one to enter.
       def user_exempt_from_purchase?
-        Rails.logger.info("[SecureRooms] Entered SecureRooms::AccessHandlers::OrderHandler#user_exempt_from_purchase?")
-        in_reader = occupancy.secure_room.card_readers.ingress.first
-        result = SecureRooms::CheckAccess.new.authorize(occupancy.user, in_reader).accounts.blank?
-        Rails.logger.info("[SecureRooms] Exiting SecureRooms::AccessHandlers::OrderHandler#user_exempt_from_purchase? and returning #{result}")
-        result
+        occupancy.user.user_roles.operator?(occupancy.facility)
       end
 
       def create_order_and_detail
