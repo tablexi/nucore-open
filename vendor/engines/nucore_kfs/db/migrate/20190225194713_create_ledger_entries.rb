06901d8cdaa63e4214067d5d41c40b358a31df81
Joseph O'Shea <joshea0@gmail.com>
2019-03-13 10:36:42 -0400

    fix migration in nucore_kfs engine

--- vendor/engines/nucore_kfs/db/migrate/20190225194713_create_ledger_entries.rb
+++ vendor/engines/nucore_kfs/db/migrate/20190225194713_create_ledger_entries.rb
@@ -8,9 +8,6 @@ class CreateLedgerEntries < ActiveRecord::Migration
       t.references :journal_row, index: true, foreign_key: true
 
       t.timestamps null: false
-
-      add_index :ledger_entries, :kfs_status
-      add_index :ledger_entries, :document_numer
     end
   end
 end
