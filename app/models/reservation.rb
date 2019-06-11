df218b25d074328c57ea54871a4c0c90f4f7d70c
Vladimir Andrijevikj <vandrijevik@users.noreply.github.com>
2019-06-11 15:38:01 +0200

    Calculate billable_minutes for completed order details in Reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -37,7 +37,7 @@ class Reservation < ApplicationRecord
   # Delegations
   #####
   delegate :note, :note=, :ordered_on_behalf_of?, :complete?, :account, :order,
-           :complete!, to: :order_detail, allow_nil: true
+           :complete!, :price_policy, to: :order_detail, allow_nil: true
 
   delegate :account, :in_cart?, :user, to: :order, allow_nil: true
   delegate :facility, to: :product, allow_nil: true
@@ -345,6 +345,19 @@ class Reservation < ApplicationRecord
   end
 
   def set_billable_minutes
+    if order_detail&.complete?
+      case price_policy.charge_for
+      when InstrumentPricePolicy::CHARGE_FOR.fetch(:reservation)
+        self.billable_minutes = TimeRange.new(reserve_start_at, reserve_end_at).duration_mins
+      when InstrumentPricePolicy::CHARGE_FOR.fetch(:usage)
+        self.billable_minutes = TimeRange.new(actual_start_at, actual_end_at).duration_mins
+      when InstrumentPricePolicy::CHARGE_FOR.fetch(:overage)
+        end_time = [reserve_end_at, actual_end_at].max
+        self.billable_minutes = TimeRange.new(reserve_start_at, end_time).duration_mins
+      end
+    else
+      self.billable_minutes = nil
+    end
   end
 
 end
