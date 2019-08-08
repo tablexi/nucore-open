3bdbde04f2394d66958ffa13eefba3822ff29efc
Vladimir Andrijevikj <vandrijevik@users.noreply.github.com>
2019-08-08 13:29:13 +0200

    Reformat and alphabetize Capistrano deployment config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
-# config valid only for current version of Capistrano
 lock "~> 3.11.0"
 
 set :application, "nucore"
-
+set :eye_config, "config/eye.yml.erb"
+set :eye_env, -> { { rails_env: fetch(:rails_env) } }
 set :repo_url, "git@github.com:tablexi/nucore-open.git"
-
-set :bundle_without, "#{fetch(:bundle_without)} oracle"
+set :rollbar_env, Proc.new { fetch :rails_env }
+set :rollbar_role, Proc.new { :app }
+set :rollbar_token, ENV["ROLLBAR_ACCESS_TOKEN"]
 
 set :linked_files, fetch(:linked_files, []).concat(
   %w(config/database.yml config/secrets.yml config/eye.yml.erb),
@@ -15,10 +16,3 @@ set :linked_files, fetch(:linked_files, []).concat(
 set :linked_dirs, fetch(:linked_dirs, []).concat(
   %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/files),
 )
-
-set :eye_config, "config/eye.yml.erb"
-set :eye_env, -> { { rails_env: fetch(:rails_env) } }
-
-set :rollbar_token, ENV["ROLLBAR_ACCESS_TOKEN"]
-set :rollbar_env, Proc.new { fetch :rails_env }
-set :rollbar_role, Proc.new { :app }
