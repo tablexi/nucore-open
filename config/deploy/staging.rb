4e8e6047fc09b06fb83e476540322eecaf51fdbb
Joseph O'Shea <joshea0@gmail.com>
2019-10-16 11:06:04 -0400

    Change stage environment to point to the UConn staging environment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-server "tablexi-shared02.txihosting.com", user: "nucore", roles: %w(web app db)
-set :deploy_to, "/home/nucore/nucore.stage.tablexi.com"
+server "test.cider.core.uconn.edu", user: "deploy", roles: %w(web app db)
+set :deploy_to, "/home/deploy/workspace/deploy"
 set :rails_env, "stage"
 set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -4,8 +4,13 @@ require_relative "production.rb"
 
 Nucore::Application.configure do
 
-  config.action_mailer.delivery_method = :sendmail # :smtp
-  Rails.application.routes.default_url_options =
-    config.action_mailer.default_url_options = { host: "nucore.stage.tablexi.com", protocol: "https" }
+  config.action_mailer.delivery_method       = :smtp
+  Rails.application.routes.default_url_options = { host: "test.cider.core.uconn.edu", protocol: "https" }
+  config.action_mailer.default_url_options   = { host: "test.cider.core.uconn.edu", protocol: "https" }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.uconn.edu",
+    port: 25,
+    domain: "test.cider.core.uconn.edu",
+  }
 
 end
