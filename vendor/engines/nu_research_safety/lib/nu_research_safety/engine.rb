f9cd65d2c8a6426fc03bb76b2decfe5d058fb006
Jason Hanggi <jason@tablexi.com>
2019-12-16 14:29:55 +0000

    move secure room rule out of research safety

--- vendor/engines/nu_research_safety/lib/nu_research_safety/engine.rb
+++ vendor/engines/nu_research_safety/lib/nu_research_safety/engine.rb
@@ -6,16 +6,6 @@ module NuResearchSafety
 
     config.to_prepare do
       OrderPurchaseValidator.additional_validations << NuResearchSafety::OrderCertificateValidator
-
-      if EngineManager.engine_loaded?("SecureRooms")
-        # This is in the lib path so it is not eager loaded
-        require "nu_research_safety/secure_room_access_rule"
-        ArrayUtil.insert_before(
-          SecureRooms::CheckAccess.rules,
-          NuResearchSafety::SecureRoomAccessRule,
-          SecureRooms::AccessRules::AccountSelectionRule,
-        )
-      end
     end
 
     initializer :append_migrations do |app|
