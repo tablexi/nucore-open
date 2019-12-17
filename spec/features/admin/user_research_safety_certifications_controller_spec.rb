ec46992f62d094af1afb6eb9fc89274496b22680
Jason Hanggi <jason@tablexi.com>
2019-12-17 11:15:40 +0000

    Hide tabs if there are no certificates configured

--- spec/features/admin/user_research_safety_certifications_controller_spec.rb
+++ spec/features/admin/user_research_safety_certifications_controller_spec.rb
@@ -2,22 +2,32 @@ require "rails_helper"
 
 RSpec.describe "Viewing a user's safety certifications" do
   let(:user) { FactoryBot.create(:user) }
-  let!(:certificate_a) { FactoryBot.create(:research_safety_certificate) }
-  let!(:certificate_b) { FactoryBot.create(:research_safety_certificate) }
   let(:facility) { FactoryBot.create(:facility) }
   let(:admin) { FactoryBot.create(:user, :facility_director, facility: facility) }
 
-  before do
-    login_as admin
+  before { login_as admin }
 
-    expect(ResearchSafetyCertificationLookup).to receive(:certified?).with(user, certificate_a).and_return(true)
-    expect(ResearchSafetyCertificationLookup).to receive(:certified?).with(user, certificate_b).and_return(false)
+  describe "with certificates" do
+    let!(:certificate_a) { FactoryBot.create(:research_safety_certificate) }
+    let!(:certificate_b) { FactoryBot.create(:research_safety_certificate) }
+
+    before do
+      expect(ResearchSafetyCertificationLookup).to receive(:certified?).with(user, certificate_a).and_return(true)
+      expect(ResearchSafetyCertificationLookup).to receive(:certified?).with(user, certificate_b).and_return(false)
+    end
+
+    it "can see the user's certifications" do
+      visit facility_user_path(facility, user)
+      click_link "Certifications"
+      expect(page).to have_selector("li.muted", text: certificate_b.name)
+      expect(page).to have_selector("li:not(.muted)", text: certificate_a.name)
+    end
   end
 
-  it "can see the user's certifications" do
-    visit facility_user_path(facility, user)
-    click_link "Certifications"
-    expect(page).to have_selector("li.muted", text: certificate_b.name)
-    expect(page).to have_selector("li:not(.muted)", text: certificate_a.name)
+  describe "without certificates" do
+    it "does not have the certifications tab" do
+      visit facility_user_path(facility, user)
+      expect(page).not_to have_link "Certifications"
+    end
   end
 end
