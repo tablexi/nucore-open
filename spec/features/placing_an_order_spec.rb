bc0be1f189047dc5118fafd73b6a45bda1944c5d
Jason Hanggi <jason@tablexi.com>
2020-01-15 15:26:53 -0600

    Add spec for purchasing a bundle

--- spec/features/placing_an_order_spec.rb
+++ spec/features/placing_an_order_spec.rb
@@ -61,4 +61,59 @@ RSpec.describe "Placing an item order" do
       expect(page).to have_content "Order Receipt"
     end
   end
+
+  describe "adding a bundle" do
+    let(:bundle) { FactoryBot.create(:bundle, facility: facility) }
+
+    def add_to_cart
+      visit facility_bundle_path(facility.url_name, bundle.url_name)
+      click_link "Add to cart"
+      choose account.to_s
+      click_button "Continue"
+    end
+
+    describe "that has multiple items" do
+      let!(:product2) { FactoryBot.create(:setup_item, facility: facility) }
+      let!(:price_policy2) do
+        FactoryBot.create(:item_price_policy,
+                          price_group: PriceGroup.base, product: product2,
+                          unit_cost: 12)
+      end
+
+      before do
+        bundle.bundle_products.create!(product: product, quantity: 2)
+        bundle.bundle_products.create!(product: product2, quantity: 3)
+      end
+
+      it "adds both items to the cart and can purchase them" do
+        add_to_cart
+        expect(page).to have_content(product.name)
+        expect(page).to have_content(product2.name)
+        expect(page).to have_link("Remove").once # Only has one "remove" link
+
+        click_button "Purchase"
+        expect(page).to have_content "Order Receipt"
+      end
+    end
+
+    describe "that has a multiple of instruments" do
+      let!(:instrument) { FactoryBot.create(:setup_instrument, facility: facility) }
+      let!(:price_policy) do
+        FactoryBot.create(:instrument_price_policy,
+                          price_group: PriceGroup.base, product: instrument)
+      end
+
+      before do
+        bundle.bundle_products.create!(product: instrument, quantity: 2)
+      end
+
+      it "adds two rows to the cart" do
+        add_to_cart
+        expect(page).to have_content(instrument.name).twice
+        expect(page).to have_link("Remove").once # Only has one "remove" link
+        expect(page).not_to have_button("Purchase")
+        expect(page).to have_link("Make a Reservation").twice
+      end
+    end
+  end
 end
