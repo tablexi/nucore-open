625cd7a874f55d3b77c4579c4d8c78152dc910cf
Jason Hanggi <jason@tablexi.com>
2020-01-16 13:13:14 -0600

    Fix recharge account editing (#2165)
    # Release Notes
    
    Fix recharge account editing. Clicking "Save" would take the user to a 404 page.
    
    # Additional Context
    
    Editing a recharge account was broken and we didn't have a good spec around it. It's possible it was broken in #2136 by changing `form_for` to `simple_form_for`.
    
    The spec is a little funky in how it fills out the form, but it needs to be in order for NU and Dartmouth to pass since they use different sets of fields.

---
+++ spec/features/admin/recharge_accounts_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+
+RSpec.describe "Managing recharge accounts (FacilityFacilityAccountsController)" do
+
+  let(:facility) { FactoryBot.create(:facility) }
+  let!(:director) { FactoryBot.create(:user, :facility_director, facility: facility) }
+  let(:default_revenue_account) { Settings.accounts.revenue_account_default }
+  let(:dummy_account) { build(:facility_account) }
+
+  before do
+    define_open_account default_revenue_account, dummy_account.account_number
+    login_as director
+    visit manage_facility_path(facility)
+  end
+
+  it "can create a recharge account" do
+    click_link "Recharge Chart Strings"
+    click_link "Add Recharge Chart String"
+
+    # This is a somewhat convoluted way to fill out the form, but each school will
+    # have different fields for their chart strings. E.g. NU has Fund, Department,
+    # Project, etc. while the default just has a
+    # Individual schools should implement a similar spec in their engine that
+    # is more explicit in which fields it fills out.
+    dummy_account.account_number_fields.each do |field, _values|
+      fill_in I18n.t("facility_account.account_fields.label.account_number.#{field}"), with: dummy_account.public_send(field)
+    end
+
+    click_button "Create"
+
+    expect(page).to have_content("Recharge Chart String was successfully created")
+    expect(page).to have_link(dummy_account.to_s)
+  end
+
+  describe "editing an existing" do
+    let!(:facility_account) { FactoryBot.create(:facility_account, facility: facility) }
+
+    it "cannot edit most fields, but can mark it as inactive" do
+      click_link "Recharge Chart Strings"
+      click_link facility_account.to_s
+
+      dummy_account.account_number_fields.each do |field, _values|
+        expect(page).to have_field(I18n.t(field, scope: "facility_account.account_fields.label.account_number"), readonly: true)
+      end
+
+      uncheck "Is Active?"
+      click_button "Save"
+      expect(page).to have_content("(inactive)")
+    end
+  end
+end
