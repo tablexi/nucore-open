22d4b84c44c389dc83885df63d4f619c5daddfee
Jason Hanggi <jason@tablexi.com>
2020-01-20 13:22:29 -0600

    [open] Send email

--- app/controllers/facility_accounts_controller.rb
+++ app/controllers/facility_accounts_controller.rb
@@ -4,6 +4,7 @@ class FacilityAccountsController < ApplicationController
 
   include AccountSuspendActions
   include SearchHelper
+  include CsvEmailAction
 
   admin_tab     :all
   before_action :authenticate_user!
@@ -96,7 +97,9 @@ class FacilityAccountsController < ApplicationController
           render layout: false
         end
         format.csv do
-          render csv: Reports::AccountSearchCsv.new(@accounts), filename: "#{current_facility}_account_search_results.csv"
+          send_csv_email_and_respond do |email|
+            AccountSearchResultMailer.search_result(email, params[:search_term], SerializableFacility.new(current_facility)).deliver_later
+          end
         end
       end
     else
---
+++ app/mailers/account_search_result_mailer.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class AccountSearchResultMailer < BaseMailer
+
+  def search_result(to_email, search_term, facility)
+    accounts = AccountSearcher.new(search_term, scope: Account.for_facility(facility)).results
+    attachments["accounts.csv"] = Reports::AccountSearchCsv.new(accounts).to_csv
+    mail(to: to_email, subject: text("views.account_search_result_mailer.search_result.subject"))
+  end
+
+end
