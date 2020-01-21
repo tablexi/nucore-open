878ebe45f297a3e6b5d2ac62b3b727f09ff1de33
Jason Hanggi <jason@tablexi.com>
2020-01-21 16:14:09 -0600

    Only run the spec if it's in open

--- spec/features/admin/recharge_accounts_spec.rb
+++ spec/features/admin/recharge_accounts_spec.rb
@@ -5,36 +5,19 @@ RSpec.describe "Managing recharge accounts (FacilityFacilityAccountsController)"
   let(:facility) { FactoryBot.create(:facility) }
   let!(:director) { FactoryBot.create(:user, :facility_director, facility: facility) }
   let(:default_revenue_account) { Settings.accounts.revenue_account_default }
-  # Build and destroy so any school-specific callbacks run, but the actual FacilityAccount
-  # should no longer exist. `account_number_parts` must be initialized before the destruction
-  # or we trigger a "cannot modify frozen Hash" error
-  let(:dummy_account) { create(:facility_account).tap(&:account_number_parts).destroy }
+  # Use this so schools can override their own formats
+  let(:dummy_account) { build(:facility_account) }
 
-  # Setup
-  before do
-    allow_any_instance_of(FacilityFacilityAccountsController).to receive(:form_class).and_return ::FacilityAccountForm
-    define_open_account default_revenue_account, dummy_account.account_number
-  end
-
-  it "can create a recharge account" do
+  # This will run only in schools who have not overridden their validator with a
+  # custom one. If it's custom, then the school should write their own feature spec.
+  it "can create a recharge account", if: ValidatorFactory.validator_class == ::ValidatorDefault do
     login_as director
     visit manage_facility_path(facility)
 
     click_link "Recharge Chart Strings"
     click_link "Add Recharge Chart String"
 
-    # This is a somewhat convoluted way to fill out the form, but each school will
-    # have different fields for their chart strings. E.g. NU has Fund, Department,
-    # Project, etc. while the default just has a
-    # Individual schools should implement a similar spec in their engine that
-    # is more explicit in which fields it fills out.
-    dummy_account.account_number_fields.each do |field, _options|
-      if field == :account_number
-        fill_in "Account Number", with: dummy_account.account_number
-      else
-        fill_in I18n.t(field, scope: "facility_account.account_fields.label.account_number"), with: dummy_account.account_number_parts[field]
-      end
-    end
+    fill_in "Account Number", with: dummy_account.account_number
 
     click_button "Create"
 
