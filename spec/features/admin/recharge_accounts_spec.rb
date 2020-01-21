51429a173994ba65501c7133b2c089431d035fc0
Jason Hanggi <jason@tablexi.com>
2020-01-21 15:53:58 -0600

    Fix recharge spec

--- spec/features/admin/recharge_accounts_spec.rb
+++ spec/features/admin/recharge_accounts_spec.rb
@@ -5,15 +5,21 @@ RSpec.describe "Managing recharge accounts (FacilityFacilityAccountsController)"
   let(:facility) { FactoryBot.create(:facility) }
   let!(:director) { FactoryBot.create(:user, :facility_director, facility: facility) }
   let(:default_revenue_account) { Settings.accounts.revenue_account_default }
-  let(:dummy_account) { build(:facility_account) }
+  # Build and destroy so any school-specific callbacks run, but the actual FacilityAccount
+  # should no longer exist. `account_number_parts` must be initialized before the destruction
+  # or we trigger a "cannot modify frozen Hash" error
+  let(:dummy_account) { create(:facility_account).tap(&:account_number_parts).destroy }
 
+  # Setup
   before do
+    allow_any_instance_of(FacilityFacilityAccountsController).to receive(:form_class).and_return ::FacilityAccountForm
     define_open_account default_revenue_account, dummy_account.account_number
-    login_as director
-    visit manage_facility_path(facility)
   end
 
   it "can create a recharge account" do
+    login_as director
+    visit manage_facility_path(facility)
+
     click_link "Recharge Chart Strings"
     click_link "Add Recharge Chart String"
 
@@ -22,11 +28,11 @@ RSpec.describe "Managing recharge accounts (FacilityFacilityAccountsController)"
     # Project, etc. while the default just has a
     # Individual schools should implement a similar spec in their engine that
     # is more explicit in which fields it fills out.
-    dummy_account.account_number_fields.each do |field, _values|
+    dummy_account.account_number_fields.each do |field, _options|
       if field == :account_number
         fill_in "Account Number", with: dummy_account.account_number
       else
-        fill_in I18n.t("facility_account.account_fields.label.account_number.#{field}"), with: dummy_account.account_number_parts[field]
+        fill_in I18n.t(field, scope: "facility_account.account_fields.label.account_number"), with: dummy_account.account_number_parts[field]
       end
     end
 
@@ -40,6 +46,9 @@ RSpec.describe "Managing recharge accounts (FacilityFacilityAccountsController)"
     let!(:facility_account) { FactoryBot.create(:facility_account, facility: facility) }
 
     it "cannot edit most fields, but can mark it as inactive" do
+      login_as director
+      visit manage_facility_path(facility)
+
       click_link "Recharge Chart Strings"
       click_link facility_account.to_s
 
