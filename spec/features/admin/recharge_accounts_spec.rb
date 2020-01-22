37ac9d22bb811ddc1eefcbf5fc39e164bcb985cc
Jason Hanggi <jason@tablexi.com>
2020-01-22 11:05:33 -0600

    Fix downstream specs (#2181)
    # Release Notes
    
    Tech task: Fix specs that break downstream in the schools.
    
    # Additional Context
    
    I've run these changes in all three schools, but I'm only issuing the PR here for now.
    
    The second commit is my first attempt at fixing the recharge feature spec. It's particularly nasty, though. It needs to account for making sure Dartmouth doesn't make an API call, create the `api_speed_type` for UMass (which we only do on `create`, and handle a case where NU is setting the components of a chart string lazily so trying to change an attribute of a destroyed model triggers an error.
    
    I ended up deciding it's much cleaner to conditionally run the spec based on if we're in `open` or a school that hasn't built custom validation yet.

--- spec/features/admin/recharge_accounts_spec.rb
+++ spec/features/admin/recharge_accounts_spec.rb
@@ -5,30 +5,19 @@ RSpec.describe "Managing recharge accounts (FacilityFacilityAccountsController)"
   let(:facility) { FactoryBot.create(:facility) }
   let!(:director) { FactoryBot.create(:user, :facility_director, facility: facility) }
   let(:default_revenue_account) { Settings.accounts.revenue_account_default }
+  # Use this so schools can override their own formats
   let(:dummy_account) { build(:facility_account) }
 
-  before do
-    define_open_account default_revenue_account, dummy_account.account_number
+  # This will run only in schools who have not overridden their validator with a
+  # custom one. If it's custom, then the school should write their own feature spec.
+  it "can create a recharge account", if: ValidatorFactory.validator_class == ::ValidatorDefault do
     login_as director
     visit manage_facility_path(facility)
-  end
 
-  it "can create a recharge account" do
     click_link "Recharge Chart Strings"
     click_link "Add Recharge Chart String"
 
-    # This is a somewhat convoluted way to fill out the form, but each school will
-    # have different fields for their chart strings. E.g. NU has Fund, Department,
-    # Project, etc. while the default just has a
-    # Individual schools should implement a similar spec in their engine that
-    # is more explicit in which fields it fills out.
-    dummy_account.account_number_fields.each do |field, _values|
-      if field == :account_number
-        fill_in "Account Number", with: dummy_account.account_number
-      else
-        fill_in I18n.t("facility_account.account_fields.label.account_number.#{field}"), with: dummy_account.account_number_parts[field]
-      end
-    end
+    fill_in "Account Number", with: dummy_account.account_number
 
     click_button "Create"
 
@@ -40,6 +29,9 @@ RSpec.describe "Managing recharge accounts (FacilityFacilityAccountsController)"
     let!(:facility_account) { FactoryBot.create(:facility_account, facility: facility) }
 
     it "cannot edit most fields, but can mark it as inactive" do
+      login_as director
+      visit manage_facility_path(facility)
+
       click_link "Recharge Chart Strings"
       click_link facility_account.to_s
 
