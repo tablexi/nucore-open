1258cb5a11054b4a8bf8c2c55a54f8b97ccf19d9
Mak Arnautovic <me@mak.io>
2020-01-29 19:14:56 -0600

    Update feature specs

--- spec/features/admin/clones_account_memberships_spec.rb
+++ spec/features/admin/clones_account_memberships_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Cloning account membership" do
   let(:admin) { create(:user, :administrator) }
   let(:original_user) { create(:user) }
   let(:new_user) { create(:user) }
-  let(:owned_account) { create(:account, :with_account_owner, owner: original_user) }
+  let!(:owned_account) { create(:account, :with_account_owner, owner: original_user) }
   let(:business_admin_account) { create(:account, :with_account_owner) }
   let(:purchaser_account) { create(:account, :with_account_owner) }
   let(:purchaser_account_to_not_clone) { create(:account, :with_account_owner) }
@@ -13,7 +13,7 @@ RSpec.describe "Cloning account membership" do
   before do
     create(:account_user, :business_administrator, user: original_user, account: business_admin_account)
     create(:account_user, :purchaser, user: original_user, account: purchaser_account)
-    create(:account_user, :purchaser, user: original_user, account: purchaser_account_to_not_clone)
+    create(:account_user, :purchaser, user: new_user, account: purchaser_account_to_not_clone)
 
     login_as admin
   end
@@ -22,15 +22,22 @@ RSpec.describe "Cloning account membership" do
     visit facility_user_accounts_path(facility, new_user)
 
     click_link "Clone Payment Source Membership"
-    fill_in "User", with: original_user.name
+    fill_in "Search by name or email", with: original_user.name
+    click_button "Search"
 
-    click_link original_user.name
+    click_link "Clone Account Memberships"
 
-    check owned_account.to_s
-    check business_admin_account.to_s
-    check purchaser_account.to_s
+    find(:css, "#accountId#{owned_account.id}").set(true)
+    find(:css, "#accountId#{business_admin_account.id}").set(true)
+    find(:css, "#accountId#{purchaser_account.id}").set(true)
+
+    expect(page).to have_content(owned_account.to_s)
+    expect(page).to have_content(business_admin_account.to_s)
+    expect(page).to have_content(purchaser_account.to_s)
+    expect(page).not_to have_content(purchaser_account_to_not_clone.to_s)
 
     click_button "Clone"
 
+    expect(page).to have_content(I18n.t(:success, scope: "forms.clone_account_membership"))
   end
 end
