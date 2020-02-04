4798b362132f38890dea078c3b73afb792e634bd
Jason Hanggi <jason@tablexi.com>
2020-02-04 16:25:28 -0600

    don't store statuses

--- app/services/instrument_status_fetcher.rb
+++ app/services/instrument_status_fetcher.rb
@@ -20,31 +20,23 @@ class InstrumentStatusFetcher
 
   def instrument_status_for(instrument)
     # Always return true/on if the relay feature is disabled
-    current_on = SettingsHelper.relays_enabled_for_admin? ? status_of(instrument.relay) : true
-    previous_instrument_status = instrument.current_instrument_status
-    # if the status hasn't changed, don't create a new status
-    if previous_instrument_status && current_on == previous_instrument_status.on?
-      previous_instrument_status
-    else
-      # || false will ensure that the value of is_on is not nil (causes a DB error)
-      instrument.instrument_statuses.create!(on: current_on || NUCore::Database.boolean(false))
-    end
-  rescue => e
-    Rails.logger.error e.message
-    InstrumentStatus.new(instrument: instrument, error_message: e.message)
-  end
+    return InstrumentStatus.new(on: true, instrument: instrument) unless SettingsHelper.relays_enabled_for_admin?
 
-  def status_of(relay)
-    @cache ||= {}
     # Shared schedules might use the same relay on multiple instruments. In these
     # situations, we only want to fetch the status once.
-    key = [relay.ip, relay.ip_port, relay.outlet]
+    key = [instrument.relay.ip, instrument.relay.ip_port, instrument.relay.outlet]
 
-    if @cache.key?(key)
-      @cache[key]
-    else
-      @cache[key] = relay.get_status
+    return status_cache[key] if status_cache.key?(key)
+
+    begin
+      status_cache[key] = InstrumentStatus.new(on: instrument.relay.get_status, instrument: instrument)
+    rescue => e
+      status_cache[key] = InstrumentStatus.new(error_message: e.message, instrument: instrument)
     end
+  end
+
+  def status_cache
+    @status_cache ||= {}
   end
 
 end
