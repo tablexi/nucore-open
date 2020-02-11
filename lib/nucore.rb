5a4b4ff2b172e00515e68154ae51915144f1a74b
Jason Hanggi <jason@tablexi.com>
2020-02-11 16:55:49 -0600

    fix oracle issues

--- lib/nucore.rb
+++ lib/nucore.rb
@@ -156,10 +156,12 @@ module NUCore
       module OracleFullTextSearchHelper
 
         def full_text(fields, query)
+          formatted_query = query.split.join(",")
           # If we leave the label out `:label`, we end up with duplicate labels which Oracle doesn't like.
           # If we don't handle the `order` ourselves, then AR's `or` complains about incompatible `ORDER BY`s.
           array = Array(fields)
-          relation = array.map.with_index { |field, i| contains(field, query, label: i).unscope(:order) }.inject(&:or)
+          relation = array.map.with_index { |field, i| contains("#{arel_table.name}.#{field}", formatted_query, label: i).unscope(:order) }.inject(&:or)
+
           array.length.times.inject(relation) do |relation, label|
             relation.order(Arel.sql("SCORE(#{label}) DESC"))
           end
