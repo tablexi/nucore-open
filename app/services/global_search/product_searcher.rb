22dee21f74f58d9e6a5ea5aff614a2603421e19d
Jason Hanggi <jason@tablexi.com>
2020-02-11 15:01:51 -0600

    cleanup and specs

--- app/services/global_search/product_searcher.rb
+++ app/services/global_search/product_searcher.rb
@@ -11,28 +11,13 @@ module GlobalSearch
     private
 
     def search
-      if NUCore::Database.oracle?
-        Product.contains(:name, query).or(Product.contains(:description, query))
-      else
-        Product.includes(:facility).where("MATCH(products.name, products.description) AGAINST (?)", query)
-      end
-      # name_search.or(description_search)
+      Product.includes(:facility).full_text([:name, :description], query)
     end
 
     def restrict(products)
       facilities = user ? user.operable_facilities.pluck(:id) : []
 
       products.in_active_facility.not_archived.merge(Product.where(is_hidden: false).or(Product.where(facility: facilities)))
-    end
-
-    def name_search
-      # query_string = "%#{query}%"
-      # Product.includes(:facility).where("LOWER(products.name) LIKE ?", query_string.downcase)
-    end
-
-    def description_search
-      # query_string = "%#{query}%"
-      # Product.includes(:facility).where("LOWER(products.description) LIKE ?", query_string.downcase)
     end
 
   end
--- spec/services/global_search/product_searcher_spec.rb
+++ spec/services/global_search/product_searcher_spec.rb
@@ -6,13 +6,20 @@ RSpec.describe GlobalSearch::ProductSearcher do
 
   subject(:results) { searcher.results }
 
-  let(:facility) { nil }
+  let(:facility) { Facility.find_by(url_name: "facility") }
   let(:searcher) { described_class.new(user, facility, query) }
   let(:user) { create(:user) }
   let(:item) { Item.find_by!(url_name: "lsc-andor") }
+
+  # Both MySql and Oracle do not update their full-text indexes until a COMMIT. RSpec
+  # runs each example inside of a transaction, so if we query the full text index,
+  # we'll get no results. These objects are created outside of the transaction, so
+  # we must be very careful that they are cleaned up afterwards.
   before(:context) do
-    facility = create(:facility)
-    item = build(:item, url_name: "lsc-andor", name: "LSC Andor Spinning Disk", facility: facility)
+    # Do not use factories to ensure we don't end up with any zombie associations
+    facility = Facility.new(name: "name", abbreviation: "EF", url_name: "facility", is_active: true, short_description: "abc", journal_mask: "C01")
+    facility.save(validate: false)
+    item = Item.new(url_name: "lsc-andor", name: "LSC Andor Spinning Disk", facility: facility)
     item.save(validate: false)
   end
 
@@ -56,7 +63,11 @@ RSpec.describe GlobalSearch::ProductSearcher do
     end
 
     context "when it matches multiple words, but not exactly" do
-      let!(:item2) { create(:setup_item, name: "LSC Nikon A1RSi") }
+      before(:context) do
+        facility = Facility.find_by(url_name: "facility")
+        Item.new(name: "LSC Nikon A1RSi", facility: facility, url_name: "lsc_nikon").save(validate: false)
+      end
+      let(:item2) { Item.find_by(url_name: "lsc_nikon") }
       let(:query) { "lsc spinning disk" }
 
       it "matches both products with the one matching more words first" do
