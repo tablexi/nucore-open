92ae47a4391a26af2dfd55436158fc6615c610f3
Jason Hanggi <jason@tablexi.com>
2020-02-12 18:48:45 -0600

    Add oracle support for full text searching

--- app/models/full_text_search/model.rb
+++ app/models/full_text_search/model.rb
@@ -7,12 +7,17 @@ module FullTextSearch
     mattr_accessor :full_text_searcher do
       if Nucore::Database.mysql?
         FullTextSearch::MysqlSearcher
+      elsif Nucore::Database.oracle?
+        FullTextSearch::OracleSearcher
       else
         FullTextSearch::LikeSearcher
       end
     end
 
     included do
+      if Nucore::Database.oracle?
+        has_context_index
+      end
       scope :full_text, ->(fields, query) { FullTextSearch::Model.full_text_searcher.new(self).search(fields, query) }
     end
 
