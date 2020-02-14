75ed0e40bf95ddee958047ab7a187af08f2ce962
Jason Hanggi <jason@tablexi.com>
2020-02-14 17:49:38 -0600

    move features -> system

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,8 +23,13 @@ RSpec.configure do |config|
 
   config.use_transactional_fixtures = true
 
-  require "selenium-webdriver"
-  Capybara.javascript_driver = :selenium_chrome_headless
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+  end
 
   Capybara.server = :webrick
   require "capybara/email/rspec"
@@ -139,8 +144,8 @@ RSpec.configure do |config|
     I18n.t("facilities_downcase")
   end
 
-  config.include Warden::Test::Helpers, type: :feature
-  config.after type: :feature do
+  config.include Warden::Test::Helpers, type: :system
+  config.after type: :system do
     Warden.test_reset!
   end
 
