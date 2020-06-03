c8ed7581dc625d6de636f4668697db0e09645d8a
Fernando Cardoce <fcardoce@pernix-solutions.com>
2020-06-03 09:07:12 -0600

    [#152869] Log relay tool use (#2303)
    Release Notes
    Log the following event: Who/when the Relay Tool is used
    
    Additional Context
    Add Rollbar report to ActiveSupport::Notifications

--- app/controllers/relays_activations_controller.rb
+++ app/controllers/relays_activations_controller.rb
@@ -7,16 +7,23 @@ class RelaysActivationsController < ApplicationController
   authorize_resource :facility
 
   def create
+    LogEvent.log(current_facility, :activate, current_user)
     relays.each(&:activate)
     redirect_to facility_instruments_path(current_facility), flash: { notice: text("turned_on") }
-  rescue NetBooter::Error
+  rescue NetBooter::Error => e
+    ActiveSupport::Notifications.instrument(
+        "background_error",
+        exception: e
+    )
     redirect_to facility_instruments_path(current_facility), flash: { alert: text("connection_error") }
   end
 
   def destroy
+    LogEvent.log(current_facility, :deactivate, current_user)
     relays.each(&:deactivate)
     redirect_to facility_instruments_path(current_facility), flash: { notice: text("turned_off") }
-  rescue NetBooter::Error
+  rescue NetBooter::Error => e
+    ActiveSupport::Notifications.instrument("background_error", exception: e)
     redirect_to facility_instruments_path(current_facility), flash: { alert: text("connection_error") }
   end
 
