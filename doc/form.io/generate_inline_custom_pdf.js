2bab8463a76a61df86548aa6f22dcf94bd475946
Jason Hanggi <jason@tablexi.com>
2020-06-03 12:58:45 -0500

    Update Form.io documentation
    Content from NU

--- doc/form.io/generate_inline_custom_pdf.js
+++ doc/form.io/generate_inline_custom_pdf.js
@@ -1,27 +1,25 @@
 // Generate In-Line Custom PDF within Current Form
 // Replace [token_id]
-var pdfFileName = data.orderedForUsernname && data.nucoreOrderNumber ? data.orderedForUsernname + '-' + data.nucoreOrderNumber : 'document';
+var pdfFileName = data.orderedForUsernname && data.nucoreOrderNumber ? 'Form Name' + data.orderedForUsernname + '_' + data.nucoreOrderNumber : 'document';
 var fileToken = '[token_id]'; // PDF token
 
-instance.label = 'Print Tag'; // change label
+instance.label = 'Download PDF'; // change label
 instance.redraw();
 // Create instance using same pdf as nested form
-var pdf = new Formio([form_id]);
+var pdf = new Formio('[form_id]');
 
 // change submission state to save it as draft
 submission.state = 'draft';
-pdf.loadForm().then(function(res) {
-  // create pdf url download
-  var parentProjectId = res.project
-  var downloadUrl = 'https://files.form.io/pdf/' +
+pdf.loadForm().then(function(res){
+    // create pdf url download
+      var parentProjectId = res.project
+        var downloadUrl = 'https://files.form.io/pdf/' +
     parentProjectId +
     '/file/pdf/download?format=pdf';
-  var pdfSubmission = {
-    data: {}
-  };
-  res.components.forEach(function(component) {
+  var pdfSubmission = {data: {}};
+  res.components.forEach(function(component){
     console.log(component.key, data[component.key]);
-    if (component.input && data[component.key]) {
+    if(component.input && data[component.key]) {
       pdfSubmission.data[component.key] = data[component.key];
     }
   });
@@ -30,30 +28,30 @@ pdf.loadForm().then(function(res) {
     form: res,
     submission: pdfSubmission
   });
-  // getting file
-  fetch(downloadUrl, {
-      method: 'POST',
-      body: requestJson,
-      headers: {
-        'Content-Type': 'application/json',
-        'x-file-token': fileToken
-      }
-    })
+      // getting file
+   fetch(downloadUrl, {
+    method: 'POST',
+    body: requestJson,
+    headers: {
+      'Content-Type': 'application/json',
+      'x-file-token': fileToken
+    }
+  })
     .then(function(res) {
-      // creating blob
+        // creating blob
       return res.blob();
     })
     .then(function(blob) {
-      // creating file
-      var reader = new FileReader();
+        // creating file
+    var reader = new FileReader();
 
       reader.onload = function() {
-        //creating link
-        var downloadLink = document.createElement('a');
-        downloadLink.href = reader.result;
-        downloadLink.download = pdfFileName + '.pdf';
-        //simulating click to download
-        downloadLink.click();
+          //creating link
+      var downloadLink = document.createElement('a');
+      downloadLink.href = reader.result;
+      downloadLink.download = pdfFileName + '.pdf';
+      //simulating click to download
+      downloadLink.click();
         instance.label = instance.originalComponent.label;
         instance.redraw();
       };
@@ -61,7 +59,7 @@ pdf.loadForm().then(function(res) {
       reader.readAsDataURL(blob);
     })
     .catch(function(err) {
-      //  errors
+        //  errors
       alert('Error while generating PDF');
       instance.label = instance.originalComponent.label;
       instance.redraw();
