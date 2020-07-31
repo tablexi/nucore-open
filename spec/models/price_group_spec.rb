tablexi/nucore-open

commit 7d26f6c268a98a2aef0fe602c0dac07a2adb4fc8

#[152869] Log when a user gets added or removed from a price group (#2302)
Release Notes
Log the following event: Who/when someone added a user to a price group

app/controllers/concerns/price_group_members_controller.rb
@@ -28,6 +28,7 @@ module PriceGroupMembersController
     raise NUCore::PermissionDenied unless @price_group.can_manage_price_group_members?
 
     if price_group_member.save
+      LogEvent.log(price_group_member, :create, current_user)
       set_flash(:notice, :create, create_flash_arguments)
     else
       set_flash(:error, :create, create_flash_arguments)
@@ -39,6 +40,7 @@ module PriceGroupMembersController
     raise NUCore::PermissionDenied unless @price_group.can_manage_price_group_members?
 
     if destroy_price_group_member!
+      LogEvent.log(price_group_member, :delete, current_user)
       set_flash(:notice, :destroy)
     else
       set_flash(:error, :destroy)

app/models/price_group_member.rb
@@ -4,6 +4,12 @@ class PriceGroupMember < ApplicationRecord
 
   belongs_to :price_group
 
+  acts_as_paranoid
+
   validates_presence_of :price_group_id
+
+  def to_log_s
+    "#{user} / #{price_group}"
+  end
 
 end

config/locales/views/admin/en.log_events.yml
@@ -27,6 +27,9 @@ en:
           problem_queue: Order added to problem queue %{cause}
           price_change: Order's price was manually changed
           resolve_from_problem_queue: Problem order resolved
+        price_group_member:
+          create: User added to price group
+          delete: User removed from price group
         facility:
           activate: Turned on all relays
           deactivate: Turned off all relays

spec/models/price_group_spec.rb
@@ -62,6 +62,14 @@ RSpec.describe PriceGroup do
       expect(PriceGroup.with_deleted.find(@price_group.id)).to be_present
     end
 
+    it "should be able to delete a price group with price group members" do
+      user = create(:user)
+      user_price_group_member = create(:user_price_group_member, price_group: @price_group, user: user)
+      @price_group.destroy
+      expect { PriceGroup.find(@price_group.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      expect(PriceGroup.with_deleted.find(@price_group.id)).to be_present
+    end
+
     context "with price policy" do
       before :each do
         @facility_account = FactoryBot.create(:facility_account, facility: @facility)

app/services/log_event_searcher.rb
@@ -13,6 +13,7 @@ class LogEventSearcher
                     "order_detail.notify", "order_detail.review",
                     "order_detail.problem_queue", "order_detail.price_change",
                     "order_detail.resolve_from_problem_queue",
+                    "price_group_member.create", "price_group_member.delete",
                     "facility.activate", "facility.deactivate",
                     "price_group.create", "price_group.delete",
                     ].freeze

db/migrate/20200519230633_add_soft_delete_to_price_group_member.rb
@@ -1,0 +1,8 @@
+class AddSoftDeleteToPriceGroupMember < ActiveRecord::Migration[5.2]
+  def change
+    change_table :price_group_members do |t|
+      t.timestamps null: true # these didn't exist before, so old ones should be allowed to stay null
+      t.datetime :deleted_at, index: true
+    end
+  end
+end

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_155649) do
+ActiveRecord::Schema.define(version: 2020_05_19_230633) do
 
   create_table "account_facility_joins", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "facility_id", null: false
@@ -413,6 +413,9 @@ ActiveRecord::Schema.define(version: 2020_05_14_155649) do
     t.integer "price_group_id", null: false
     t.integer "user_id"
     t.integer "account_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "deleted_at"
     t.index ["account_id"], name: "index_price_group_members_on_account_id"
     t.index ["price_group_id"], name: "fk_rails_0425013e5b"
     t.index ["user_id"], name: "index_price_group_members_on_user_id"

spec/presenters/statement_presenter_spec.rb
@@ -53,7 +53,7 @@ RSpec.describe StatementPresenter do
     end
 
     context "when the statement's creator does not exist" do
-      before { creator.destroy }
+      before { statement.created_by = nil }
 
       it "returns 'Unknown'" do
         expect(subject.sent_by).to eq("Unknown")

commit a57d2d9f19cf28572b3e9b580e492d6e3355dc47

[#153876] Fix error when viewing logs for account price groups (#2360)
# Release Notes

I think this has actually been broken since https://github.com/tablexi/nucore-open/pull/2302.

Run `rake cleanup:log_events:metadata` after merging

app/controllers/concerns/price_group_members_controller.rb
@@ -28,7 +28,7 @@ module PriceGroupMembersController
     raise NUCore::PermissionDenied unless @price_group.can_manage_price_group_members?
 
     if price_group_member.save
-      LogEvent.log(price_group_member, :create, current_user)
+      LogEvent.log(price_group_member, :create, current_user, metadata: { member_type: price_group_member.member_type })
       set_flash(:notice, :create, create_flash_arguments)
     else
       set_flash(:error, :create, create_flash_arguments)
@@ -40,7 +40,7 @@ module PriceGroupMembersController
     raise NUCore::PermissionDenied unless @price_group.can_manage_price_group_members?
 
     if destroy_price_group_member!
-      LogEvent.log(price_group_member, :delete, current_user)
+      LogEvent.log(price_group_member, :delete, current_user, metadata: { member_type: price_group_member.member_type })
       set_flash(:notice, :destroy)
     else
       set_flash(:error, :destroy)

app/models/account_price_group_member.rb
@@ -7,4 +7,8 @@ class AccountPriceGroupMember < PriceGroupMember
   validates_presence_of :account_id
   validates_uniqueness_of :account_id, scope: [:price_group_id]
 
+  def to_log_s
+    "#{account} / #{price_group_with_deleted}"
+  end
+
 end

app/models/price_group_member.rb
@@ -10,8 +10,8 @@ class PriceGroupMember < ApplicationRecord
 
   validates_presence_of :price_group_id
 
-  def to_log_s
-    "#{user} / #{price_group_with_deleted}"
+  def member_type
+    type.sub("PriceGroupMember", "")
   end
 
 end

app/models/user_price_group_member.rb
@@ -7,4 +7,8 @@ class UserPriceGroupMember < PriceGroupMember
   validates_presence_of :user_id
   validates_uniqueness_of :user_id, scope: [:price_group_id]
 
+  def to_log_s
+    "#{user} / #{price_group_with_deleted}"
+  end
+
 end

config/locales/views/admin/en.log_events.yml
@@ -31,8 +31,8 @@ en:
           create: User added to access list
           delete: User removed from access list
         price_group_member:
-          create: User added to price group
-          delete: User removed from price group
+          create: "%{member_type} added to price group"
+          delete: "%{member_type} removed from price group"
         facility:
           activate: Turned on all relays
           deactivate: Turned off all relays

lib/tasks/cleanup.rake
@@ -31,4 +31,15 @@ namespace :cleanup do
       puts "Price group with id: #{pg.id}, name: #{cc_name} has been destroyed."
     end
   end
+
+  namespace :log_events do
+    desc "update metadata for PriceGroupMembers"
+    task metadata: :environment do
+      events = LogEvent.where(loggable_type: "PriceGroupMember").where(metadata: nil)
+      puts "Updating #{events.size} LogEvents..."
+      events.find_each do |event|
+        event.update(metadata: { member_type: event.loggable.member_type })
+      end
+    end
+  end
 end

spec/models/price_group_spec.rb
@@ -43,6 +43,20 @@ RSpec.describe PriceGroup do
 
   end
 
+  describe "to_log_s" do
+    it "should be loggable with account price groups" do
+      account = create(:setup_account)
+      account_price_group_member = create(:account_price_group_member, price_group: @price_group, account: account)
+      expect(account_price_group_member.to_log_s).to include(account.to_s)
+    end
+
+    it "should be loggable with user price groups" do
+      user = create(:user)
+      account_price_group_member = create(:user_price_group_member, price_group: @price_group, user: user)
+      expect(account_price_group_member.to_log_s).to include(user.to_s)
+    end
+  end
+
   describe "can_delete?" do
     it "should not be deletable if global" do
       @global_price_group = FactoryBot.build(:price_group, facility: nil)
@@ -65,6 +79,14 @@ RSpec.describe PriceGroup do
     it "should be able to delete a price group with price group members" do
       user = create(:user)
       user_price_group_member = create(:user_price_group_member, price_group: @price_group, user: user)
+      @price_group.destroy
+      expect { PriceGroup.find(@price_group.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      expect(PriceGroup.with_deleted.find(@price_group.id)).to be_present
+    end
+
+    it "should be able to delete a price group with price group accounts" do
+      account = create(:setup_account)
+      account_price_group_member = create(:account_price_group_member, price_group: @price_group, account: account)
       @price_group.destroy
       expect { PriceGroup.find(@price_group.id) }.to raise_error(ActiveRecord::RecordNotFound)
       expect(PriceGroup.with_deleted.find(@price_group.id)).to be_present