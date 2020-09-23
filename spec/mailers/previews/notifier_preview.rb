eca90ab6e8ccb5fe28b7a183cf5bdced9ba99c1c
Gilad Shanan <giladshanan@gmail.com>
2020-09-23 13:56:20 -0500

    Use i18n and GlobalID to clean up mailer logic

--- spec/mailers/previews/notifier_preview.rb
+++ spec/mailers/previews/notifier_preview.rb
@@ -32,7 +32,7 @@ class NotifierPreview < ActionMailer::Preview
 
   def order_detail_status_changed
     order_detail = Nucore::Database.random(OrderDetail.complete)
-    Notifier.order_detail_status_changed(order_detail.id)
+    Notifier.order_detail_status_changed(order_detail)
   end
 
 end
