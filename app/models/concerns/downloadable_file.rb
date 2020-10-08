fc7d5622d8962b221b9dc2cec6808866402e2179
Gilad Shanan <giladshanan@gmail.com>
2020-10-08 09:58:00 -0500

    Force download from S3

--- app/models/concerns/downloadable_file.rb
+++ app/models/concerns/downloadable_file.rb
@@ -5,7 +5,10 @@ module DownloadableFile
   extend ActiveSupport::Concern
 
   included do
-    has_attached_file :file, PaperclipSettings.config.merge(validate_media_type: false)
+    has_attached_file :file, PaperclipSettings.config.merge(
+      validate_media_type: false,
+      s3_url_options: { response_content_disposition: "attachment" } # this is ignored when using local storage
+    )
 
     # TODO: Limit attachment types for safe uploads
     do_not_validate_attachment_file_type :file
