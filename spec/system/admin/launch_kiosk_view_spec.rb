3ebd8fddf4f708ef168a689ce9b2f9d0152c30da
Gilad Shanan <giladshanan@gmail.com>
2021-02-02 16:52:04 -0600

    Update specs

--- spec/system/admin/launch_kiosk_view_spec.rb
+++ spec/system/admin/launch_kiosk_view_spec.rb
@@ -3,18 +3,19 @@
 require "rails_helper"
 
 RSpec.describe "Launching Kiosk View", feature_setting: { kiosk_view: true } do
-  let(:facility) { create(:setup_facility) }
+  let(:facility) { create(:setup_facility, kiosk_enabled: true) }
   let(:director) { create(:user, :facility_director, facility: facility) }
+
+  before { login_as director }
 
   context "with active reservations" do
     let(:instrument) { create(:setup_instrument, facility: facility, control_mechanism: "timer") }
-    let!(:reservation) { create(:purchased_reservation, :running, product: instrument) }
+
+    before { create(:purchased_reservation, :running, product: instrument) }
 
     it "can launch the Kiosk View" do
-      login_as director
       visit timeline_facility_reservations_path(facility)
       click_link "Launch Kiosk View"
-
       expect(page.current_path).to eq facility_kiosk_reservations_path(facility)
       # user should be logged out
       expect(page).to have_content("Login")
@@ -23,7 +24,15 @@ RSpec.describe "Launching Kiosk View", feature_setting: { kiosk_view: true } do
 
   context "with no active reservations" do
     it "cannot launch the Kiosk View" do
-      login_as director
+      visit timeline_facility_reservations_path(facility)
+      expect(page).not_to have_content("Launch Kiosk View")
+    end
+  end
+
+  context "with Kiosk view disabled" do
+    let(:facility) { create(:setup_facility, kiosk_enabled: false ) }
+
+    it "cannot launch the Kiosk View" do
       visit timeline_facility_reservations_path(facility)
       expect(page).not_to have_content("Launch Kiosk View")
     end
@@ -31,7 +40,6 @@ RSpec.describe "Launching Kiosk View", feature_setting: { kiosk_view: true } do
 
   context "with the feature flag turned off", feature_setting: { kiosk_view: false } do
     it "cannot launch the Kiosk View" do
-      login_as director
       visit timeline_facility_reservations_path(facility)
       expect(page).not_to have_content("Launch Kiosk View")
     end
