a3cec79ee0cdb4bd7b5b1ca8026012912163359d
Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
2021-02-08 10:28:00 -0600

    [#155874] Add feature flag for using the kiosk without authenticating (#2509)
    # Release Notes
    
    Adds a feature flag that allows users to take actions from the kiosk view without authenticating.
    The intent is to allow gathering some user feedback while exploring further options for enabling the kiosk view feature with SSO.

--- app/services/users/auth_checker.rb
+++ app/services/users/auth_checker.rb
@@ -18,18 +18,23 @@ module Users
 
     def authenticated?
       return false if @user.nil?
+      return true if bypass_kiosk_auth?
 
       if @user.authenticated_locally?
         @user.valid_password?(@password)
       elsif ldap_enabled?
         @user.valid_ldap_authentication?(@password)
-      elsif Settings.saml.present?
-        # TODO
       end
     end
 
     def ldap_enabled?
       SettingsHelper.feature_on?(:uses_ldap_authentication) && LdapAuthentication.configured?
+    end
+
+    # TODO - Decide if we need to authenticate against employee ID, a reservation passcode, or IdP/LDAP API.
+    # Starting with the easiest implementation while gathering feedback from potential users.
+    def bypass_kiosk_auth?
+      SettingsHelper.feature_on?(:bypass_kiosk_auth)
     end
 
   end
