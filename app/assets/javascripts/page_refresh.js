7ebc510a580e59db2531daf2fd0d3c161738c636
Gilad Shanan <giladshanan@gmail.com>
2021-02-22 17:12:55 -0600

    Add hourly refresh for My Reservations page

---
+++ app/assets/javascripts/page_refresh.js
@@ -0,0 +1,20 @@
+/***
+ * Triggers a full refreseh of the page at a specified internval.
+ *
+ * Usage:
+   = content_for :head_content do
+     = javascript_include_tag "page_refresh"
+
+   .js--pageRefresh{ data: { refresh_interval: 1.hour } }
+***/
+
+document.addEventListener("DOMContentLoaded", function() {
+  const page = document.getElementsByClassName("js--pageRefresh")[0];
+  const refreshInterval = page.dataset["refreshInterval"] || 60; // in seconds
+
+  function refresh() {
+    window.location.reload()
+  }
+
+  window.setInterval(refresh, refreshInterval * 1000);
+});
