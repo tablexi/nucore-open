99ca4bde4b71ea7505a51f464aa01498ec753e5f
Gilad Shanan <giladshanan@gmail.com>
2021-03-11 17:28:42 -0600

    Manage auto_cancel process via secrets.yml

--- lib/daemons/auto_cancel.rb
+++ lib/daemons/auto_cancel.rb
@@ -3,8 +3,11 @@
 require File.expand_path("base", File.dirname(__FILE__))
 
 Daemons::Base.new("auto_cancel").start do
-  canceler = AutoCanceler.new
-  canceler.cancel_reservations
+  # Only run on 1 instance.
+  if Rails.application.secrets.run_auto_cancel
+    canceler = AutoCanceler.new
+    canceler.cancel_reservations
+  end
 
   sleep 1.minute.to_i
 end
