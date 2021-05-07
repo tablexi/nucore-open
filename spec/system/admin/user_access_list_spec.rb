048d401a6d227085bbf78a3215b66d5660e1ec23
Annie Wang <wangminghao@gmail.com>
2021-05-07 12:30:22 -0400

    [#144829] Added specs for user and system/admin/user_access_list

--- spec/system/admin/user_access_list_spec.rb
+++ spec/system/admin/user_access_list_spec.rb
@@ -17,6 +17,8 @@ RSpec.describe "User access list" do
       login_as admin
       visit facility_user_path(facility, user)
       click_on "Access List"
+      expect(page).to have_content("Date added")
+      expect(page).to have_content(product_user.approved_at.strftime("%m/%d/%Y"))
       uncheck product1.name
       check product2.name
       click_button "Update Access List"
