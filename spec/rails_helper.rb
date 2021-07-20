5432ead095c7549e5cbe4286b4098e0095d0f21f
Gilad Shanan <giladshanan@gmail.com>
2021-07-20 18:08:03 -0500

    Allow ignoring JS errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,14 +33,17 @@ RSpec.configure do |config|
 
   # Gives more verbose output for JS errors, fails any spec with SEVERE errors
   # Based on https://medium.com/@coorasse/catch-javascript-errors-in-your-system-tests-89c2fe6773b1
-  config.after(:each, type: :system, js: true) do
-    js_errors = page.driver.browser.manage.logs.get(:browser)
-    js_errors.each do |error|
-      if error.level == "SEVERE" || error.level == "WARNING"
-        STDERR.puts "JS error detected (#{error.level}): #{error.message}"
+  config.after(:each, type: :system, js: true) do |example|
+    # Some forms using remote: true return a 406 that is expected
+    unless example.metadata[:ignore_js_errors]
+      js_errors = page.driver.browser.manage.logs.get(:browser)
+      js_errors.each do |error|
+        if error.level == "SEVERE" || error.level == "WARNING"
+          STDERR.puts "JS error detected (#{error.level}): #{error.message}"
+        end
       end
+      expect(js_errors.map(&:level)).not_to include "SEVERE"
     end
-    expect(js_errors.map(&:level)).not_to include "SEVERE"
   end
 
   Capybara.server = :webrick
