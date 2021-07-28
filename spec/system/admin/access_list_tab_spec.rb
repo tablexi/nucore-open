7c211eca6f5862c638cd29eebbc36777f3c3fce6
Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
2021-07-27 20:39:19 -0500

    [#142305] Fix Access List tab (#2633)
    # Release Notes
    
    Use polymorphic routes for CSV export link.  
    
    Addresses errors like:
    ```
     ActionView::Template::Error: No route matches {:action=>"index", :controller=>"product_users", :facility_id=>"dartlab", :format=>:csv, :service_id=>"NonSterileSorterSetUp"}, missing required keys: [:instrument_id]
    ```
    ... which are caused when a user tries to access the "Access List" tab for a non-instrument product.

---
+++ spec/system/admin/access_list_tab_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Access List Tab for various product types", :js do
+  let(:facility) { create(:setup_facility) }
+  let(:director) { create(:user, :facility_director, facility: facility) }
+  let!(:user) { create(:user, username: "ddavidson") }
+
+  before(:each) do
+    create(:product_user, product: product, user: user)
+    login_as director
+    visit polymorphic_path([:manage, facility, product])
+    click_link "Access List"
+  end
+
+  context "with an instrument" do
+    let(:product) { create(:setup_instrument, requires_approval: true, facility: facility) }
+
+    it "renders the page" do
+      expect(page.current_path).to eq polymorphic_path([facility, product, :users])
+    end
+  end
+
+  context "with an item" do
+    let(:product) { create(:setup_item, requires_approval: true, facility: facility) }
+
+    it "renders the page" do
+      expect(page.current_path).to eq polymorphic_path([facility, product, :users])
+    end
+  end
+
+  context "with a service" do
+    let(:product) { create(:setup_service, requires_approval: true, facility: facility) }
+
+    it "renders the page" do
+      expect(page.current_path).to eq polymorphic_path([facility, product, :users])
+    end
+  end
+
+  context "with a timed service" do
+    let(:product) { create(:setup_timed_service, requires_approval: true, facility: facility) }
+
+    it "renders the page" do
+      expect(page.current_path).to eq polymorphic_path([facility, product, :users])
+    end
+  end
+end
