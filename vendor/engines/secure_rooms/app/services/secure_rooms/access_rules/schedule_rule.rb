5ab140cca095ab8271e31ee90354101fbf4aee1d
Gilad Shanan <giladshanan@gmail.com>
2021-08-19 21:35:22 -0500

    Extract holiday access to its own AccessRule

--- vendor/engines/secure_rooms/app/services/secure_rooms/access_rules/schedule_rule.rb
+++ vendor/engines/secure_rooms/app/services/secure_rooms/access_rules/schedule_rule.rb
@@ -6,11 +6,11 @@ module SecureRooms
 
     class ScheduleRule < BaseRule
 
+      # TODO: Consider refactoring to make this logic more explicit.
       def evaluate
         deny!(:no_schedule) if secure_room.schedule_rules.none?
         deny!(:outside_schedule) unless secure_room.schedule_rules.cover?(Time.current)
         deny!(:not_in_group) unless secure_room.available_schedule_rules(user).cover?(Time.current)
-        deny!(:holiday_access_restricted) unless card_reader.egress? || Holiday.allow_access?(user, secure_room, Time.current)
       end
 
     end
