747f87e0440b79460974139e0cc326a53a152a09
Gilad Shanan <giladshanan@gmail.com>
2021-10-29 14:38:22 -0500

    Normalize attributes on assignment

--- vendor/engines/secure_rooms/app/models/secure_rooms/user_extension.rb
+++ vendor/engines/secure_rooms/app/models/secure_rooms/user_extension.rb
@@ -7,9 +7,6 @@ module SecureRooms
     extend ActiveSupport::Concern
 
     included do
-      NULL_ATTRS = %w(card_number i_class_number)
-
-      before_save :nil_if_blank
 
       validates :card_number, uniqueness: { allow_blank: true, case_sensitive: false }
       validates :i_class_number, uniqueness: { allow_blank: true, case_sensitive: false }
@@ -18,9 +15,15 @@ module SecureRooms
         find_by(card_number: card_number) || find_by(card_number: card_number.split("-").first)
       end
 
-      def nil_if_blank
-        NULL_ATTRS.each { |attr| self[attr] = nil if self[attr].blank? }
+      # MySQL uniqueness constraints don't apply to NULL values
+      def card_number=(value)
+        super value.presence
       end
+
+      def i_class_number=(value)
+        super value.presence
+      end
+
     end
 
   end
