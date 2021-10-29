714bbc64b926091fad66c70a0fd536fee3617f40
Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
2021-10-29 18:20:55 -0400

    [#158849] Fix card number and iclass indexes for mysql (#2790)
    Oracle doesn't consider an empty string when enforcing uniqueness, but MySQL does
    https://decipherinfosys.wordpress.com/2007/11/30/multiple-null-values-in-a-unique-index-in-sql-serverdb2-luw/
    
    This prevents saving empty strings to the database, since MySQL enforces uniqueness to non-NULL values.

--- vendor/engines/secure_rooms/app/models/secure_rooms/user_extension.rb
+++ vendor/engines/secure_rooms/app/models/secure_rooms/user_extension.rb
@@ -7,12 +7,23 @@ module SecureRooms
     extend ActiveSupport::Concern
 
     included do
+
       validates :card_number, uniqueness: { allow_blank: true, case_sensitive: false }
       validates :i_class_number, uniqueness: { allow_blank: true, case_sensitive: false }
 
       def self.for_card_number(card_number)
         find_by(card_number: card_number) || find_by(card_number: card_number.split("-").first)
       end
+
+      # MySQL uniqueness constraints don't apply to NULL values
+      def card_number=(value)
+        super value.presence
+      end
+
+      def i_class_number=(value)
+        super value.presence
+      end
+
     end
 
   end
