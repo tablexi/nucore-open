485f927d0f04b525a0a87a3afd58166758172ac8
Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
2022-04-27 14:23:32 -0500

    Tech Task - Set a specific node version in CircleCI config (#3000)
    Various clean up

--- db/migrate/20190409205259_rename_billing_administrator_role.rb
+++ db/migrate/20190409205259_rename_billing_administrator_role.rb
@@ -2,14 +2,18 @@
 
 class RenameBillingAdministratorRole < ActiveRecord::Migration[5.0]
   def up
-  	UserRole.where(role: "Billing Administrator").each do |user|
+    # unscoped to allow migrations to run despite acts_as_paranoid
+    # https://github.com/rubysherpas/paranoia/issues/226
+  	UserRole.unscoped.where(role: "Billing Administrator").each do |user|
   		user.role = "Global Billing Administrator"
   		user.save!
   	end
   end
 
   def down
-  	UserRole.where(role: "Global Billing Administrator").each do |user|
+    # unscoped to allow migrations to run despite acts_as_paranoid
+    # https://github.com/rubysherpas/paranoia/issues/226
+  	UserRole.unscoped.where(role: "Global Billing Administrator").each do |user|
   		user.role = "Billing Administrator"
   		user.save!
   	end
