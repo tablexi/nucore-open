a96289750b0a87111e51ab44cc043982cbc7c8b4
Gilad Shanan <giladshanan@gmail.com>
2022-05-25 16:42:44 -0500

    Add in axe-core-rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rspec/rails"
 require "shoulda/matchers"
+require "axe-rspec"
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
