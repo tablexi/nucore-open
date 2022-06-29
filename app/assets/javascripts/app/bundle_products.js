632c1ef7bdfc59048c658ddd216e7cca5a48b319
Joseph Simmons <josephdsimmons@gmail.com>
2022-06-29 17:57:58 -0500

    Adds dymanic hint logic to new bundle product view

--- app/assets/javascripts/app/bundle_products.js
+++ app/assets/javascripts/app/bundle_products.js
@@ -2,29 +2,33 @@
 // quantity field based on the type of product selected.
 $(function() {
   $productSelect = $(".js--bundleProducts__productSelect");
+  const hints = document.querySelectorAll('.hint');
+
+  hideElements(hints);
 
   if ($productSelect.length) {
-    var timeQuantityTypes = $productSelect.data("timed-quantity-types").split(", ")
-    var $quantityField = $(".js--bundleProducts__quantityField");
-    var $timedQuantityField = $(".js--bundleProducts__timedQuantityField");
 
     $productSelect.bind("change", function(evt) {
-      var selectedType = $(evt.target).find(":selected").closest("optgroup").attr("label");
-      var isTimeSelected = timeQuantityTypes.includes(selectedType);
+      hideElements(hints);
 
-      // Show/hide and Enable/Disable the regular quantity vs time quantit fields
-      $quantityField.toggle(!isTimeSelected).find("input").prop("disabled", isTimeSelected);
-      $timedQuantityField.toggle(isTimeSelected).find("input").prop("disabled", !isTimeSelected);
+      let selectedType = $(evt.target).find(":selected").closest("optgroup").attr("label");
+
+      if (selectedType) {
+        let className = ".js--" + capitalizedToCamelCase(selectedType);
+        document.querySelector(className).hidden = false;
+      }
+
     }).trigger("change");
 
-    // Keep the two fields synchronized. In the time input field, there is a visible
-    // field, which is the formatted (H:MM) version and the hidden is a raw quantity.
-    $quantityField.on("change", function(evt) {
-      $timedQuantityField.find("input[type=hidden]").val($(evt.target).val()).trigger("change");
-    });
+  }
 
-    $timedQuantityField.on("change", "input[type=hidden]", function(evt) {
-      $quantityField.find("input").val($(evt.target).val())
+  function capitalizedToCamelCase(str) {
+    return (str[0].toLowerCase() + str.slice(1)).replaceAll(" ", "");
+  }
+
+  function hideElements(elements) {
+    elements.forEach(function(element) {
+      element.hidden = true;
     });
   }
 });
