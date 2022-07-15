2baec988331dfe4007d17cb34145574d2d3fa821
Joseph Simmons <josephdsimmons@gmail.com>
2022-07-15 17:39:42 -0500

    Removes tabs

--- app/assets/javascripts/app/orders_edit_date.js
+++ app/assets/javascripts/app/orders_edit_date.js
@@ -8,8 +8,8 @@ window.addEventListener("DOMContentLoaded", () => {
 
     // In the _edit_date partial, show the "Fulfilled at" field only if 
     // the "Order Status" is "Complete"
-	  function toggleFulfilledAt() {
-    	const fulfilledAt = document.querySelector(".backdate_fulfilled_at");
+    function toggleFulfilledAt() {
+      const fulfilledAt = document.querySelector(".backdate_fulfilled_at");
       let selectedValue = orderStatusSelect.value;
 		  let displayStyle;
 
