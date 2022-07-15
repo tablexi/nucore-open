e3eaadf4d936be2ffb390e8b037989fb6d11bb65
Joseph Simmons <josephdsimmons@gmail.com>
2022-07-15 16:47:59 -0500

    Removes tabs

--- app/assets/javascripts/app/orders_edit_date.js
+++ app/assets/javascripts/app/orders_edit_date.js
@@ -1,26 +1,26 @@
 window.addEventListener("DOMContentLoaded", function() {
-	const orderStatusSelect = document.querySelector("#order_status_id");
-
-	// In the _edit_date partial, show the "Fulfilled at" field only if 
-	// the "Order Status" is "Complete"
-	if (orderStatusSelect) {
-		const fulfilledAt = document.querySelector(".backdate_fulfilled_at");
-		let displayStyle = "none";
-
-		fulfilledAt.style.display = displayStyle;
-
-		orderStatusSelect.addEventListener("change", function() {
-			const selectedValue = orderStatusSelect.value;
-
-			// "Complete" is the 4th item in the dropdown
-			if (selectedValue === "4") {
-				displayStyle = "";
-			}
-			else {
-				displayStyle = "none";
-			}
-
-			fulfilledAt.style.display = displayStyle;
-		});
-	}
+  const orderStatusSelect = document.querySelector("#order_status_id");
+  
+  // In the _edit_date partial, show the "Fulfilled at" field only if 
+  // the "Order Status" is "Complete"
+  if (orderStatusSelect) {
+    const fulfilledAt = document.querySelector(".backdate_fulfilled_at");
+    let displayStyle = "none";
+  
+    fulfilledAt.style.display = displayStyle;
+  
+    orderStatusSelect.addEventListener("change", function() {
+      const selectedValue = orderStatusSelect.value;
+  
+      // "Complete" is the 4th item in the dropdown
+      if (selectedValue === "4") {
+        displayStyle = "";
+      }
+      else {
+        displayStyle = "none";
+      }
+  
+      fulfilledAt.style.display = displayStyle;
+    });
+  }
 });
