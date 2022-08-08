0edb7871f7e0da7646ce4d55de26d9e5583bfffa
Joseph Simmons <josephdsimmons@gmail.com>
2022-08-08 12:28:43 -0500

    Refactors orders_edit_date.js

--- app/assets/javascripts/app/orders_edit_date.js
+++ app/assets/javascripts/app/orders_edit_date.js
@@ -1,33 +1,39 @@
+/** 
+ * Show and require the "Fulfilled at" field only if the "Order Status" is 
+ * "Complete", this is currently done in the _edit_date partial.
+ * 
+ * TODO: The SetFulfilledOnComplete class does something similar to this,
+ *       so it may be a good idea to generalize this function to replace
+ *       that class.
+**/
+function toggleFulfilledAtSection(orderStatusSelectElem) {
+  const fulfilledAtSection = document.querySelector(".backdate_fulfilled_at");
+  const fulfilledAtElement = document.querySelector(".js--fulfilled_at");
+  let selectedIndex = orderStatusSelectElem.selectedIndex;
+  let selectedText = orderStatusSelectElem.options[selectedIndex].text; 
+  let displayStyle;
+  let required;
+
+  if (selectedText === "Complete") {
+    displayStyle = "";
+    required = true;
+  }
+  else {
+    displayStyle = "none";
+    required = false;
+    fulfilledAtElement.value = null;
+  }
+
+  fulfilledAtSection.style.display = displayStyle;
+  fulfilledAtElement.required = required;
+}
+
+
 window.addEventListener("DOMContentLoaded", () => {
-  const orderStatusSelect = document.querySelector("#order_status_id");
+  const orderStatusSelect = document.querySelector(".js--order_status");
 
   if (orderStatusSelect) {
-    toggleFulfilledAt();
-  
-    orderStatusSelect.addEventListener("change", () => toggleFulfilledAt());
-
-    // In the _edit_date partial, show the "Fulfilled at" field only if 
-    // the "Order Status" is "Complete"
-    function toggleFulfilledAt() {
-      const fulfilledAtSection = document.querySelector(".backdate_fulfilled_at");
-      const fulfilledAtElement = document.querySelector("#fulfilled_at");
-      let selectedIndex = orderStatusSelect.selectedIndex;
-      let selectedText = orderStatusSelect.options[selectedIndex].text; 
-      let displayStyle;
-      let required;
-
-      if (selectedText === "Complete") {
-        displayStyle = "";
-        required = true;
-      }
-      else {
-        displayStyle = "none";
-        required = false;
-        fulfilledAtElement.value = null;
-      }
-  
-      fulfilledAtSection.style.display = displayStyle;
-      fulfilledAtElement.required = required;
-    }
+    toggleFulfilledAtSection(orderStatusSelect);
+    orderStatusSelect.addEventListener("change", () => toggleFulfilledAtSection(orderStatusSelect));
   }
 });
