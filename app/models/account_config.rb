73f8e9cdb5675b03b0db2c8a95ae4cad61d5497a
Joseph Simmons <josephdsimmons@gmail.com>
2022-10-12 10:23:27 -0500

    Updates `account_types_for_facility`to use sets consistently

--- app/models/account_config.rb
+++ app/models/account_config.rb
@@ -71,7 +71,7 @@ class AccountConfig
   # and the NullObject always returns `true` for cross_facility?.
   def account_types_for_facility(facility, action)
     types = account_types
-    types = types.select { |type| type.constantize.global? } if facility.try(:cross_facility?)
+    types = types.select { |type| type.constantize.global? }.to_set if facility.try(:cross_facility?)
     types -= creation_disabled_types if action == :create
     types
   end
