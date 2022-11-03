82c013b1138579bf58219078c1779cef8d971b4a
Joseph Simmons <josephdsimmons@gmail.com>
2022-11-03 17:54:41 -0500

    Adds reply-to adddress to ResultsFileNotifierMailer

--- app/mailers/results_file_notifier_mailer.rb
+++ app/mailers/results_file_notifier_mailer.rb
@@ -4,7 +4,8 @@ class ResultsFileNotifierMailer < ApplicationMailer
 
   def file_uploaded(file)
     @order_detail = file.order_detail
-    mail(to: @order_detail.user.email, subject: text("results_file_notifier_mailer.file_uploaded.subject"))
+    reply_to = SettingsHelper.setting("email.results_file_notifier.reply_to") || SettingsHelper.setting("email.from")
+    mail(reply_to: reply_to, to: @order_detail.user.email, subject: text("results_file_notifier_mailer.file_uploaded.subject"))
   end
 
 end
