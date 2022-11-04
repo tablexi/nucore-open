e945d77f78f228e481e98e7836f462ab5c468d19
Joseph Simmons <josephdsimmons@gmail.com>
2022-11-04 12:15:08 -0500

    Updates reply-to address with correct fallbacks
    The `ResultsFileNotifierMailer#file_uploaded` reply-to address will be
    the product address, if it exists. If not, it'll be the facility
    address, if it exists. If not, it'll be the default from address

--- app/mailers/results_file_notifier_mailer.rb
+++ app/mailers/results_file_notifier_mailer.rb
@@ -4,7 +4,10 @@ class ResultsFileNotifierMailer < ApplicationMailer
 
   def file_uploaded(file)
     @order_detail = file.order_detail
-    reply_to = SettingsHelper.setting("email.results_file_notifier.reply_to") || SettingsHelper.setting("email.from")
+    product = @order_detail.product
+    product_email = product.email
+    reply_to = product_email || SettingsHelper.setting("email.from")
+
     mail(reply_to: reply_to, to: @order_detail.user.email, subject: text("results_file_notifier_mailer.file_uploaded.subject"))
   end
 
