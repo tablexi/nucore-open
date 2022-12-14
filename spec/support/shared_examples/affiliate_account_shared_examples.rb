df37053705ebf1f5ebd3b29e9c6014e268db3bfb
Joseph Simmons <josephdsimmons@gmail.com>
2022-12-14 13:56:07 -0600

    Checks for PurchaseOrder in spec
    Only purchase order accounts' validation should be affected by the
    `po_require_affiliate_account` feature flag

--- spec/support/shared_examples/affiliate_account_shared_examples.rb
+++ spec/support/shared_examples/affiliate_account_shared_examples.rb
@@ -28,7 +28,7 @@ RSpec.shared_examples_for "AffiliateAccount" do
   end
 
   context "with the :po_require_affiliate_account flag set to false", feature_setting: { po_require_affiliate_account: false } do
-    it { is_expected.not_to validate_presence_of(:affiliate_id) }
+    it { is_expected.not_to validate_presence_of(:affiliate_id) if account.is_a? PurchaseOrderAccount }
 
     it "requires affiliate_other if affiliate is 'Other'" do
       account.affiliate = Affiliate.OTHER
