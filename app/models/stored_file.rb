d07c1271b1145cbcf0ba04309d36e7699e18fa32
Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
2023-01-19 10:21:18 -0600

    Update app/models/stored_file.rb

--- app/models/stored_file.rb
+++ app/models/stored_file.rb
@@ -15,7 +15,7 @@ class StoredFile < ApplicationRecord
   if SettingsHelper.feature_on?(:active_storage)
     validates :file, size: { between: 1.kilobyte..10.megabytes }, if: ->(o) { o.file_type == "user_info" }
   else
-    validates_with AttachmentSizeValidator, attributes: :file, less_than: 10.megabytes,                             if: ->(o) { o.file_type == "user_info" }
+    validates_with AttachmentSizeValidator, attributes: :file, less_than: 10.megabytes, if: ->(o) { o.file_type == "user_info" }
   end
 
   delegate :user, to: :order_detail
