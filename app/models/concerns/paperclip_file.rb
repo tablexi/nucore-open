203e8c1b33a34bfd7d2ff9b24e126d2d427b793f
Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
2023-02-23 08:45:42 -0600

    Tech Task - Move paperclip validations patch into PaperclipFile module (#3382)
    Move paperclip validations patch into PaperclipFile module

--- app/models/concerns/paperclip_file.rb
+++ app/models/concerns/paperclip_file.rb
@@ -12,6 +12,8 @@ module PaperclipFile
 
     # TODO: Limit attachment types for safe uploads
     do_not_validate_attachment_file_type :file
+
+    after_validation :clean_up_paperclip_errors
   end
 
   def download_url
@@ -36,6 +38,15 @@ module PaperclipFile
 
   def file=(attachable)
     super
+  end
+
+  private
+
+  # This is because paperclip duplicates error messages
+  # See: https://github.com/thoughtbot/paperclip/pull/1554 and
+  # https://github.com/thoughtbot/paperclip/commit/2aeb491fa79df886a39c35911603fad053a201c0
+  def clean_up_paperclip_errors
+    errors.delete(:file) if errors[:file] == errors[:file_file_size]
   end
 
 end
