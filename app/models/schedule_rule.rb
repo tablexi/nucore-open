78121de51b32dbfa98d872fed005e46f18f967fa
Joseph Simmons <josephdsimmons@gmail.com>
2023-03-15 12:12:22 -0500

    Removes `ignored_columns` from `ScheduleRule`
    This is needed for the `schedule_rule:add_price_group_discounts` rake
    task to work

--- app/models/schedule_rule.rb
+++ app/models/schedule_rule.rb
@@ -4,9 +4,6 @@ class ScheduleRule < ApplicationRecord
 
   belongs_to :product
 
-  # TODO - remove this column
-  self.ignored_columns = [:discount_percent]
-
   # oracle has a maximum table name length of 30, so we have to abbreviate it down
   has_and_belongs_to_many :product_access_groups, join_table: "product_access_schedule_rules"
   has_many :price_group_discounts, dependent: :destroy
