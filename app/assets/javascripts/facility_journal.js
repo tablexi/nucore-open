4ec1d8f3a191aac252d3c801c5049bdab8e62949
Joseph Simmons <josephdsimmons@gmail.com>
2023-05-16 17:47:41 -0500

    Addresses PR feedback

--- app/assets/javascripts/facility_journal.js
+++ app/assets/javascripts/facility_journal.js
@@ -1,21 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-  $(function(){
-    const dateData = document.querySelector("#js--journal-date-data");
-    const today = dateData.dataset["today"];
-    const earliestJournalDate = dateData.dataset["earliestJournalDate"];
-
-    $("#journal_date").val(today).datepicker({
-      "minDate": earliestJournalDate,
-      "maxDate": today
-    });
-  });
-
-  $(function(){
-    $("#journals_create_form").submit(function(e) {
-      $(e.target).find(":submit").attr("disabled", "true");
-    });
-  });
-
   const table = document.querySelector("table.js--transactions-table");
   const submitDiv = document.querySelector(".submit");
   let earliestFulfilledAtDate;
@@ -24,17 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
   submitDiv.addEventListener("click", handleModals);
   
   function setEarliestFulfilledAtDate(event) {
-    const dates = [];
+    const fulfilledAtDates = [];
     const checked = document.querySelectorAll("table.js--transactions-table tr td input[type='checkbox']:checked");
   
     checked.forEach(checkedBox => {
       const row = checkedBox.parentElement.parentElement;
-      const date= new Date(row.querySelector(".js--date-field").innerHTML);
-      dates.push(date);
+      const date = new Date(row.querySelector(".js--date-field").innerHTML);
+      fulfilledAtDates.push(date);
     });
 
-    dates.sort((a, b) => a.getTime() - b.getTime());
-    earliestFulfilledAtDate = dates[0];
+    fulfilledAtDates.sort((a, b) => a.getTime() - b.getTime());
+    earliestFulfilledAtDate = fulfilledAtDates[0];
   }
   
   function handleModals(event) {
