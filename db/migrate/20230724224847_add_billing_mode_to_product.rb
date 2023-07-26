38444bcebf3c44837fd2f31cb4a4a4b7d3481f74
Joseph Simmons <joseph@tablexi.com>
2023-07-26 11:20:06 -0500

    Apply suggestions from code review
    Co-authored-by: Gilad Shanan <30355046+giladshanan@users.noreply.github.com>

--- db/migrate/20230724224847_add_billing_mode_to_product.rb
+++ db/migrate/20230724224847_add_billing_mode_to_product.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddBillingModeToProduct < ActiveRecord::Migration[6.1]
   def change
-    add_column :products, :billing_mode, :string
+    add_column :products, :billing_mode, :string, null: false, default: "Default"
   end
 end
