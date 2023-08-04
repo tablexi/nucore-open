7635759c444f623efd540ed8d34e11032e82cd88
Joseph Simmons <josephdsimmons@gmail.com>
2023-08-04 12:50:02 -0500

    Adds spec for invalid account

--- spec/system/admin/billing_mode_workflow_spec.rb
+++ spec/system/admin/billing_mode_workflow_spec.rb
@@ -3,38 +3,62 @@
 require "rails_helper"
 
 RSpec.describe "Billing mode workflows" do
-  let(:facility) { create(:setup_facility) }
+  let(:facility) { create(:setup_facility, accepts_po:) }
+  let(:accepts_po) { true }
   let(:billing_mode) { "Default" }
-  let!(:account) { create(:setup_account, :with_account_owner, facility: order_detail.facility, owner: order_detail.user) }
-  let(:instrument) { create(:setup_instrument, facility:, billing_mode:) }
   let(:item) { create(:setup_item, facility:, billing_mode:) }
-  let(:now) { Time.now }
-  let(:reservation) { create(:purchased_reservation, product: instrument, reserve_start_at: 1.hour.ago, reserve_end_at: now, actual_start_at: 1.hour.ago, actual_end_at: now) }
-  # let(:order_detail) { reservation.order_detail }
   let(:order) { create(:setup_order, product: item) }
-  let(:order_detail) { order.order_details.first }
+  let(:nufs_account) { order.account }
+  let(:account) { nufs_account }
   let(:administrator) { create(:user, :administrator) }
   let(:director) { create(:user, :facility_director, facility:) }
   let(:logged_in_user) { director }
 
-  before do
-    order._validate_order!
-    order.purchase!
-    login_as logged_in_user
-    visit manage_facility_order_order_detail_path(facility, order, order_detail)
+  let(:po_account) do
+    po = create(:purchase_order_account, account_users: nufs_account.account_users)
+    create(:account_price_group_member, account: po, price_group: item.price_groups.last)
+    po
   end
 
-  context "'Default' billing mode"
+  let(:order_detail) do
+    od = order.order_details.first
+    od.account_id = order.account_id
+    od.save
+    od
+  end
 
-  context "'Skip Review' billing mdoe" do
+  before do
+    order.account = account
+    order_detail.account_id = order.account_id # needed for cart validation
+    login_as logged_in_user
+  end
+
+  describe "'Default' billing mode"
+
+  describe "'Skip Review' billing mdoe" do
     let(:billing_mode) { "Skip Review" }
 
-    it "is reconciled when complete" do
-      select "Complete", from: "Order Status"
-      click_button "Save"
-      expect(order_detail.reload.reconciled?).to be true
+    context "valid account" do
+      it "is reconciled when complete" do
+        order._validate_order!
+        order.purchase!
+        visit manage_facility_order_order_detail_path(facility, order, order_detail)
+
+        select "Complete", from: "Order Status"
+        click_button "Save"
+        expect(order_detail.reload.reconciled?).to be true
+      end
+    end
+
+    context "invalid account" do
+      let(:accepts_po) { false }
+      let(:account) { po_account }
+
+      it "should not have a valid cart" do
+        expect(order.cart_valid?).to be false
+      end
     end
   end
 
-  context "'Nonbillable' billing mode"
+  describe "'Nonbillable' billing mode"
 end
