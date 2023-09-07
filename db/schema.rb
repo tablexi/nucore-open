cdfe816902c03d547eeb7bc5f737ec1574a8536f
Lautaro Bertuzzi <lautaro.bertuzzi@txidigital.com>
2023-09-07 12:18:21 -0300

    [WIP]: Rails update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_24_224847) do
+ActiveRecord::Schema[6.1].define(version: 2023_07_24_224847) do
 
   create_table "account_facility_joins", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.integer "facility_id", null: false
