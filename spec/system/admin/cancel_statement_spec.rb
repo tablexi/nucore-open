d9558efa6e67f46605489eb3faebbc441bf781ca
Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
2023-09-21 09:43:29 -0500

    [#161969] Fix My Payment Sources page (#3677)
    Account owners and business admins will get an error when trying to access the Statements tab of the My Payment Sources - Details page - for an account that has cancelable statements.
    
    Bug introduced in https://github.com/tablexi/nucore-open/pull/3663
    
    Rollbar:
    https://app.rollbar.com/a/tablexi/fix/item/nucore-dartmouth/364/occurrence/365890715445#detail

--- spec/system/admin/cancel_statement_spec.rb
+++ spec/system/admin/cancel_statement_spec.rb
@@ -5,46 +5,76 @@ require "rails_helper"
 RSpec.describe "canceling statements" do
   let(:facility) { create(:setup_facility) }
   let(:item) { create(:setup_item, facility:) }
-  let!(:order) { create(:complete_order, product: item, quantity: 3, account: create(:purchase_order_account, :with_account_owner, facility:)) }
-  let(:order_details) { order.order_details }
-  let!(:statement) { create(:statement, facility:) }
+  let(:statement) { create(:statement, account: po_account, facility:) }
   let(:director) { create(:user, :facility_director, facility:) }
-
-  before do
-    order_details.each do |od|
-      od.statement = statement
-      od.save
-    end
-
-    login_as director
-    visit facility_statements_path(facility)
+  let(:po_account) { create(:purchase_order_account, :with_account_owner, facility:) }
+  let(:account_owner) { po_account.owner_user }
+  let!(:order) do
+    create(:complete_order,
+      product: item,
+      quantity: 3,
+      account: po_account
+    )
   end
+  let(:order_detail) { order.order_details.first }
 
-  context "when an order detail is NOT reconciled" do
-    it "cancels a statement" do
-      click_on "Cancel"
-      expect(page).to have_content "#{I18n.t('Statement')} has been canceled"
-      expect(page).to have_content "Canceled"
-    end
+  before(:each) { order_detail.update(statement: statement) }
 
-    it "does not allow download or emailing of statement" do
-      click_on "Cancel"
-      expect(page).to_not have_content "Download"
-      expect(page).to_not have_content "Resend"
-      expect(page).to_not have_link "Cancel"
-    end
-  end
-
-  context "when an order detail is reconciled" do
+  context "as an admin" do
     before do
-      order_details.first.to_reconciled!
+      login_as director
       visit facility_statements_path(facility)
     end
 
-    it "does not allow statement to be canceled" do
+    context "when an order detail is NOT reconciled" do
+      it "cancels a statement" do
+        click_on "Cancel"
+        expect(page).to have_content "#{I18n.t('Statement')} has been canceled"
+        expect(page).to have_content "Canceled"
+      end
+
+      it "does not allow download or emailing of statement" do
+        click_on "Cancel"
+        expect(page).to_not have_content "Download"
+        expect(page).to_not have_content "Resend"
+        expect(page).to_not have_link "Cancel"
+      end
+    end
+
+    context "when an order detail is reconciled" do
+      before do
+        order_detail.to_reconciled!
+        visit facility_statements_path(facility)
+      end
+
+      it "does not allow statement to be canceled" do
+        expect(page).to have_content "Download"
+        expect(page).to have_content "Resend" if SettingsHelper.feature_on?(:send_statement_emails)
+        expect(page).to_not have_link "Cancel"
+      end
+    end
+
+    context "when statement is canceled" do
+      let(:statement) { create(:statement, canceled_at: 2.days.ago, facility:) }
+
+      it "does not allow statement to be canceled, downloaded, or resent" do
+        expect(page).not_to have_content "Download"
+        expect(page).not_to have_content "Resend" if SettingsHelper.feature_on?(:send_statement_emails)
+        expect(page).not_to have_link "Cancel"
+      end
+    end
+  end
+
+  context "as an account owner" do
+    before do
+      login_as account_owner
+      visit account_statements_path(po_account)
+    end
+
+    it "does not allow statement to be canceled or resent" do
       expect(page).to have_content "Download"
-      expect(page).to have_content "Resend" if SettingsHelper.feature_on?(:send_statement_emails)
-      expect(page).to_not have_link "Cancel"
+      expect(page).not_to have_content "Resend" if SettingsHelper.feature_on?(:send_statement_emails)
+      expect(page).not_to have_link "Cancel"
     end
   end
 end
