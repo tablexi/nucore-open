e44220f6c04e85cd6acf64ec785cf875cd932015
Joseph Simmons <josephdsimmons@gmail.com>
2023-09-26 10:55:22 -0500

    Uncommneted `hash_digest_class` & `remove_deprecated_time_with_zone_name`

--- config/initializers/new_framework_defaults_7_0.rb
+++ config/initializers/new_framework_defaults_7_0.rb
@@ -28,11 +28,11 @@ Rails.application.config.active_support.key_generator_hash_digest_class = OpenSS
 # Change the digest class for ActiveSupport::Digest.
 # Changing this default means that for example Etags change and
 # various cache keys leading to cache invalidation.
-# Rails.application.config.active_support.hash_digest_class = OpenSSL::Digest::SHA256
+Rails.application.config.active_support.hash_digest_class = OpenSSL::Digest::SHA256
 
 # Don't override ActiveSupport::TimeWithZone.name and use the default Ruby
 # implementation.
-# Rails.application.config.active_support.remove_deprecated_time_with_zone_name = true
+Rails.application.config.active_support.remove_deprecated_time_with_zone_name = true
 
 # Calls `Rails.application.executor.wrap` around test cases.
 # This makes test cases behave closer to an actual request or job.
