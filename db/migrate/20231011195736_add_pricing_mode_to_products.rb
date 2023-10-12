fa8828c4b9de90138670fbfe25a386bb00d09c9a
Leticia Errandonea <leticia@wyeworks.com>
2023-10-12 15:41:37 -0300

    Add frozen_string_literal

--- db/migrate/20231011195736_add_pricing_mode_to_products.rb
+++ db/migrate/20231011195736_add_pricing_mode_to_products.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddPricingModeToProducts < ActiveRecord::Migration[7.0]
   def change
     add_column :products, :pricing_mode, :string, null: false, default: "Schedule Rule"
