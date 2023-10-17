8e87d16d539f8d57ee652e9f041245376c8d3c1b
Leticia Errandonea <leticia@wyeworks.com>
2023-10-17 14:29:50 -0300

    Add new view for Duration rates update/create

---
+++ app/controllers/instrument_duration_rates_controller.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class InstrumentDurationRatesController < ApplicationController
+
+  admin_tab :all
+  before_action :authenticate_user!
+  before_action :check_acting_as
+  before_action :init_instrument_duration_rates
+  before_action :manage
+
+  layout "two_column"
+
+  MAX_DURATION_RATES = 4
+
+  def edit
+  end
+
+  def update
+    params[:duration_rates_attributes].reject! do |key, duration_rate|
+      duration_rate['min_duration'].blank? && duration_rate['rate'].blank?
+    end
+
+    @product.duration_rates = @product.duration_rates.reject { |dr| dr.rate.blank? && dr.min_duration.blank? }
+
+    @product.update! instrument_duration_rate_params
+  end
+
+  private
+
+  def instrument_duration_rate_params
+    params.permit(duration_rates_attributes: [:id, :min_duration, :rate])
+  end
+
+  def manage
+    authorize! :view_details, @product
+    @active_tab = "admin_products"
+  end
+
+  def init_instrument_duration_rates
+    @product = Product.find_by!(url_name: params[:id])
+
+    @product_duration_rates = @product.duration_rates
+
+    (MAX_DURATION_RATES - @product_duration_rates.length).times do
+      @product_duration_rates.build
+    end
+  end
+end
