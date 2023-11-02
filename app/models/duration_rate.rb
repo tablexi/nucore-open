17c9f7ebbc85ffc9d27df833a3470517079ae9fe
Leticia Errandonea <leticia@wyeworks.com>
2023-11-02 14:13:10 -0300

    Update TODO comment

--- app/models/duration_rate.rb
+++ app/models/duration_rate.rb
@@ -9,8 +9,14 @@ class DurationRate < ApplicationRecord
   validates :rate, numericality: { greater_than_or_equal_to: 0, allow_blank: true }
   validates :subsidy, numericality: { greater_than: 0, allow_blank: true }
 
-  # TODO: Validate both rate and subsidy are lesser than or equal to base rate
-  # Question: Are all base rate price groups global? Is that the way of knowing whether a price group is the Base one?
+  # TODO: Validate:
+  # - Rate is <= base rate (if Price group is the “base” group or external).
+  # - Subsidy is <= base rate (if Price group is internal but not the “base”).
+  # For this it looks like we'll need to link Rate Start to Price Policy, instead of Instrument.
+  # In this context we only have a Price Group (which can have N Price policies) and an Instrument (which can have N Price policies).
+  # We need a Price Policy as that's where we store the usage_rate.
+  # Also, makes sense that Steps for an Instrument can change in time but we may want to keep the historic data.
+
 
   private
   def rate_or_subsidy
