7517d0f8e950e8d91b45df93e5f0ac34bcd42b9b
Leticia Errandonea <leticia@wyeworks.com>
2023-11-08 18:32:13 -0300

    Update DB

---
+++ db/migrate/20231108210613_change_associations_duration_rates.rb
@@ -0,0 +1,17 @@
+class ChangeAssociationsDurationRates < ActiveRecord::Migration[7.0]
+  def up
+    change_table :duration_rates do |t|
+      t.references :price_policy
+    end
+
+    remove_column :duration_rates, :price_group_id
+  end
+
+  def down
+    change_table :duration_rates do |t|
+      t.references :price_group
+    end
+
+    remove_column :duration_rates, :price_policy_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_02_000551) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_08_210613) do
   create_table "account_facility_joins", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.integer "facility_id", null: false
     t.integer "account_id", null: false
@@ -140,9 +140,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_02_000551) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "subsidy", precision: 16, scale: 8
-    t.bigint "price_group_id"
     t.bigint "rate_start_id"
-    t.index ["price_group_id"], name: "index_duration_rates_on_price_group_id"
+    t.bigint "price_policy_id"
+    t.index ["price_policy_id"], name: "index_duration_rates_on_price_policy_id"
     t.index ["rate_start_id"], name: "index_duration_rates_on_rate_start_id"
   end
 
