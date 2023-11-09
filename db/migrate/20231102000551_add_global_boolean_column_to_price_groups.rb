7c699a2ce2e116a40f0ed80f25517e66ad54cfc7
Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
2023-11-09 10:17:53 -0600

    Tech task: Properly handle oracle in db migration (#3793)
    # Release Notes
    
    Pulling this up into the open source repo.
    
    Oracle boolean values are 1/0:
    https://stackoverflow.com/questions/19435160/how-to-update-a-boolean-field-in-oracle-table

--- db/migrate/20231102000551_add_global_boolean_column_to_price_groups.rb
+++ db/migrate/20231102000551_add_global_boolean_column_to_price_groups.rb
@@ -5,11 +5,19 @@ class AddGlobalBooleanColumnToPriceGroups < ActiveRecord::Migration[7.0]
   def up
     change_table :price_groups do |t|
       add_column :price_groups, :global, :boolean, null: false, default: false
-      execute <<-SQL
-        UPDATE price_groups SET price_groups.global = true
-        WHERE price_groups.deleted_at IS NULL
-        AND price_groups.facility_id IS NULL
-      SQL
+      if Nucore::Database.oracle?
+        execute <<-SQL
+          UPDATE price_groups SET price_groups.global = 1
+          WHERE price_groups.deleted_at IS NULL
+          AND price_groups.facility_id IS NULL
+        SQL
+      else
+        execute <<-SQL
+          UPDATE price_groups SET price_groups.global = true
+          WHERE price_groups.deleted_at IS NULL
+          AND price_groups.facility_id IS NULL
+        SQL
+      end
     end
   end
 
