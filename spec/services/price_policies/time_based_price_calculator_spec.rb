6ca8b1c2993aaf60c74125ffe611af14da2207eb
Leticia Errandonea <leticia@wyeworks.com>
2023-11-16 12:19:31 -0300

    Fix spec

--- spec/services/price_policies/time_based_price_calculator_spec.rb
+++ spec/services/price_policies/time_based_price_calculator_spec.rb
@@ -281,15 +281,19 @@ RSpec.describe PricePolicies::TimeBasedPriceCalculator do
                 let(:end_at) { start_at + 5.hour + 15.minute }
 
                 it "uses usage rate, usage subsidy and duration rates" do
-                  create(:duration_rate, price_policy: price_policy, min_duration_hours: 1, subsidy: 45)
+                  create(:duration_rate, price_policy: price_policy, min_duration_hours: 1, rate: nil, subsidy: 45)
                   create(:duration_rate, price_policy: price_policy, min_duration_hours: 3, rate: nil, subsidy: 60)
-                  create(:duration_rate, price_policy: price_policy, min_duration_hours: 5, subsidy: 90)
+                  create(:duration_rate, price_policy: price_policy, min_duration_hours: 5, rate: nil, subsidy: 90)
 
-                  # 1 hours       @ $120  - $30   = $90   Usage rate - Usage subsidy
-                  # 2 hours       @ $120  - $45   = $150  Duration rate - Minimum duration: 1 hour
-                  # 2 hours       @ $120  - $60   = $120  Duration rate - Minimum duration: 3 hours
-                  # 0.25 hours    @ $120  - $90   = $7.5  Duration rate - Minimum duration: 5 hours
-                  is_expected.to eq(cost: 630, subsidy: 367.5)
+                  # Cost
+                  # 5.25 hours    @ $120   = $630   Usage rate
+
+                  # Subsidy
+                  # 1 hours       @ $30   = $30     Usage subsidy
+                  # 2 hours       @ $45   = $90     Duration rate - Minimum duration: 1 hour
+                  # 2 hours       @ $60   = $120    Duration rate - Minimum duration: 3 hours
+                  # 0.25 hours    @ $90   = $22.5   Duration rate - Minimum duration: 5 hours
+                  is_expected.to eq(cost: 630, subsidy: 262.5)
                 end
               end
 
