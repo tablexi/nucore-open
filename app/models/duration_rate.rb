ea7afb7ba16397c85337dcfd502a1ed4240688a6
Gilad Shanan <giladshanan@gmail.com>
2023-11-15 20:41:44 -0600

    Clean up duration rate model

--- app/models/duration_rate.rb
+++ app/models/duration_rate.rb
@@ -2,14 +2,14 @@
 
 class DurationRate < ApplicationRecord
 
+  SIXTY_MIN = 60.0
+
   belongs_to :price_policy, required: true
 
   validate :rate_or_subsidy
   validates :rate, numericality: { greater_than_or_equal_to: 0, allow_blank: true }
   validates :subsidy, numericality: { greater_than: 0, allow_blank: true }
-  validates :min_duration_hours, presence: true
-  validates :min_duration_hours, numericality: { greater_than: 0, allow_blank: true }
-  validates :min_duration_hours, uniqueness: { scope: :price_policy_id }
+  validates :min_duration_hours, presence: true, numericality: { greater_than: 0, allow_blank: true }, uniqueness: { scope: :price_policy_id }
   validate :rate_lesser_than_or_equal_to_base_rate
   validate :subsidy_lesser_than_or_equal_to_base_rate
 
@@ -27,7 +27,7 @@ class DurationRate < ApplicationRecord
     return unless price_group.external? || price_group.master_internal?
     return unless price_policy.usage_rate && rate
 
-    if rate / 60.0 > price_policy.usage_rate
+    if rate / SIXTY_MIN > price_policy.usage_rate
       errors.add(:base, "Rate must be lesser than or equal to Base rate")
     end
   end
@@ -36,7 +36,7 @@ class DurationRate < ApplicationRecord
     return if price_group.external? || price_group.master_internal?
     return unless price_policy.usage_rate && subsidy
 
-    if subsidy / 60.0 > price_policy.usage_rate
+    if subsidy / SIXTY_MIN > price_policy.usage_rate
       errors.add(:base, "Subsidy must be lesser than or equal to Base rate")
     end
   end
@@ -44,4 +44,5 @@ class DurationRate < ApplicationRecord
   def price_group
     price_policy.price_group
   end
+
 end
