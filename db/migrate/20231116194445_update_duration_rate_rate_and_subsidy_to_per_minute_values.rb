6e459b7a885d4eba8219af035282b5c074d0b566
Leticia Errandonea <leticia@wyeworks.com>
2023-11-16 16:51:45 -0300

    Add migration to update Duration Rate rate and subsidy to be per minute values

---
+++ db/migrate/20231116194445_update_duration_rate_rate_and_subsidy_to_per_minute_values.rb
@@ -0,0 +1,7 @@
+class UpdateDurationRateRateAndSubsidyToPerMinuteValues < ActiveRecord::Migration[7.0]
+  class DurationRate < ApplicationRecord; end
+
+  def change
+    DurationRate.update_all("rate = rate / 60.0, subsidy = subsidy / 60.0")
+  end
+end
