7bda5f9321a05cad8d0e30bf72ebe165c36443e0
Gilad Shanan <giladshanan@gmail.com>
2023-11-20 16:41:31 -0600

    Handle expired accounts in order imports

--- app/models/order.rb
+++ app/models/order.rb
@@ -18,6 +18,8 @@ class Order < ApplicationRecord
 
   delegate :order_notification_recipient, to: :facility
 
+  attr_reader :import_fulfillment_date
+
   def self.created_by_user(user)
     where(created_by: user.id)
   end
@@ -62,7 +64,8 @@ class Order < ApplicationRecord
 
   # In older versions of AASM, a guard condition failing would not raise an error.
   # This is to maintain the previous API of simply returning false
-  def validate_order!
+  def validate_order!(import_fulfillment_date: nil)
+    @import_fulfillment_date = import_fulfillment_date
     _validate_order!
   rescue AASM::InvalidTransition => e
     false
@@ -81,7 +84,12 @@ class Order < ApplicationRecord
   end
 
   def cart_valid?
-    has_details? && has_valid_payment? && order_details.all? { |od| od.being_purchased_by_admin = @being_purchased_by_admin; od.valid_for_purchase? }
+    has_details? &&
+      has_valid_payment? &&
+      order_details.all? do |od|
+        od.being_purchased_by_admin = @being_purchased_by_admin
+        od.valid_for_purchase?(@import_fulfillment_date || Time.zone.now)
+      end
   end
 
   def has_valid_payment?
@@ -89,7 +97,17 @@ class Order < ApplicationRecord
       order_details.all? { |od| od.account_id == account_id } && # order detail accounts match order account
       facility.can_pay_with_account?(account) &&               # payment is accepted by facility
       account.can_be_used_by?(user) &&                         # user can pay with account
-      account.active?                                          # account is active/valid
+      account_is_active?
+  end
+
+  def account_is_active?
+    if @import_fulfillment_date.present?
+      # account is active/valid at time of fulfillment
+      order_details.all? { |od| f = od.fulfilled_at || @import_fulfillment_date; f < account.expires_at }
+    else
+      # account is active/valid
+      account.active?
+    end
   end
 
   def has_details?
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -531,7 +531,7 @@ class OrderDetail < ApplicationRecord
     journal_id.blank? && statement_id.blank? && !canceled?
   end
 
-  def validate_for_purchase
+  def validate_for_purchase(fulfilled_time = Time.zone.now)
     # can purchase product
     return "The product may not be purchased" unless product.available_for_purchase?
 
@@ -539,7 +539,7 @@ class OrderDetail < ApplicationRecord
     return "You must select a payment method" if account.nil?
 
     # payment method is not expired
-    return "The account is expired and cannot be used" if account.expires_at < Time.zone.now || account.suspended_at
+    return "The account is expired and cannot be used" if account.expires_at < fulfilled_time || account.suspended_at
 
     # TODO: if chart string, is chart string + account valid
     return I18n.t("not_open", model: account.type_string, scope: "activerecord.errors.models.account") if account.respond_to?(:account_open?) && !account.account_open?(product.account)
@@ -560,8 +560,8 @@ class OrderDetail < ApplicationRecord
     "No assigned price groups allow purchase of this product"
   end
 
-  def valid_for_purchase?
-    validate_for_purchase.nil? ? true : false
+  def valid_for_purchase?(fulfilled_time = Time.zone.now)
+    validate_for_purchase(fulfilled_time).nil? ? true : false
   end
 
   def validate_reservation
