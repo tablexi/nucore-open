c366f30205a609507fe7d5d1ce4157ddd5771ffb
Joseph Simmons <joseph@tablexi.com>
2023-11-27 14:02:30 -0600

    Update app/models/scishield_training.rb
    Co-authored-by: Gilad Shanan <30355046+giladshanan@users.noreply.github.com>

--- app/models/scishield_training.rb
+++ app/models/scishield_training.rb
@@ -3,7 +3,7 @@
 # This model is used as a cache for Scishield trainings, the
 # research_safety_adapters:scishield:synchronize_training rake task can be used
 # to synchronize it with the Scishield API, this rake task should be setup as a
-# cron job for any school using Scieshield. ResearchSafetyAdapters::ScishieldApiAdapter
+# cron job for any school using SciShield. ResearchSafetyAdapters::ScishieldApiAdapter
 # checks this model before falling back to check with the Scishield API, allowing
 # safety training checks to work even when the Scishield API is down
 class ScishieldTraining < ApplicationRecord
