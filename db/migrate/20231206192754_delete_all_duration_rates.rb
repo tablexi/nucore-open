de9d39ccaed7d14839463fe5a3db29bdd0cb3472
Leticia Errandonea <leticia@wyeworks.com>
2023-12-12 15:54:13 -0300

    Delete migration

--- db/migrate/20231206192754_delete_all_duration_rates.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class DeleteAllDurationRates < ActiveRecord::Migration[7.0]
-  class DurationRate < ApplicationRecord; end
-
-  def change
-    DurationRate.delete_all
-  end
-end
