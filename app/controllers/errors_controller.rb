f0b96ce22808aa96d516342b11322a070d7f8413
Gilad Shanan <giladshanan@gmail.com>
2023-12-18 22:20:22 -0600

    Refactor 403 handling for consistency

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -19,15 +19,27 @@ class ErrorsController < ApplicationController
   end
 
   def forbidden
-    if request.env["action_dispatch.exception"].instance_of? NUCore::NotPermittedWhileActingAs
-      render "acting_error", status: 403, formats: formats_with_html_fallback
-    elsif current_user
-      render "403", status: 403, formats: formats_with_html_fallback
+    @error_message = if acting_error?
+      "This function is unavailable while you are acting as another user."
+    else
+      "Sorry, you don't have permission to access this page."
+    end
+
+    if current_user || acting_error?
+      respond_to do |format|
+        format.html { render status: :forbidden }
+      end
     else
       # if current_user is nil, the user should be redirected to login
       store_location_for(:user, request.fullpath)
       redirect_to new_user_session_path
     end
+  rescue ActionController::UnknownFormat
+    head :forbidden
+  end
+
+  def acting_error?
+    request.env["action_dispatch.exception"].instance_of?(NUCore::NotPermittedWhileActingAs)
   end
 
 end
