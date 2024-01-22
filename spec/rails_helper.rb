5749771526884eb998676b447a81ba0e76d6d4a2
Joseph Simmons <josephdsimmons@gmail.com>
2024-01-22 11:05:10 -0600

    Adds `--disable-dev-shm-usage` argument

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,6 +56,7 @@ RSpec.configure do |config|
         options.add_argument("--no-sandbox")
         options.add_argument("--headless=new")
         options.add_argument("--disable-gpu")
+        options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1366,768")
         options.add_argument("--remote-debugging-pipe")
         Capybara::Selenium::Driver.new(app,
