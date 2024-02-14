521f01837315e17b7138c740c14240e1e234f8b8
Gilad Shanan <giladshanan@gmail.com>
2024-02-13 21:36:11 -0600

    Use cattr_accessor instead of class methods

--- vendor/engines/projects/app/models/concerns/projects/order_row_importer_extension.rb
+++ vendor/engines/projects/app/models/concerns/projects/order_row_importer_extension.rb
@@ -2,18 +2,6 @@ module Projects
 
   module OrderRowImporterExtension
 
-    def self.included(base)
-      base.extend(ClassMethods)
-    end
-
-    module ClassMethods
-
-      def custom_headers
-        [:project_id]
-      end
-
-    end
-
     def custom_attributes
       { project_id: field(:project_id) }
     end
--- vendor/engines/projects/lib/projects/engine.rb
+++ vendor/engines/projects/lib/projects/engine.rb
@@ -16,6 +16,7 @@ module Projects
       OrdersController.send :include, Projects::OrdersControllerExtension
       Reservation.send :include, Projects::ReservationExtension
       OrderRowImporter.send :include, Projects::OrderRowImporterExtension
+      OrderRowImporter.order_import_headers.concat [:project_id]
 
       ViewHook.add_hook "reservations.account_field",
                         "after_account",
