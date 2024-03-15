dfe979ae4272789191194f5597d4cd6423efe40c
Joseph Simmons <joseph@tablexi.com>
2024-03-15 11:45:32 -0500

    [#160449] Walk up reservation flow (#4052)
    * Creates quick actions controller
    
    * Adds functining links to view
    
    * Adjusts UI to be more like the mockups
    
    * Adds quick_action methods to SchedulingSupport module
    
    * Updates new page to only show form if there's an available reservation
    
    * Fills out QuickActionsController#create to create a reservation
    
    * Deletes reservation_interval method
    
    * Update app/models/concerns/products/scheduling_support.rb
    
    Co-authored-by: Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
    
    * Renames controller and methods to quick_reservation
    
    * Updates the quick reservaton logic
    
    It will now get the next available reservations, if none are available
    right now
    
    * Fixes issue of orders for the future not be purchased
    
    * Deletes redundent scopes
    
    * Adds 2 system specs for quick reservations
    
    * Reorganizes spec
    
    * Adds specs for an upcoming and abandoned reservation
    
    * Adds quick link to instrument page
    
    * Starts refactoring QuickReservationsController#create
    
    The goal is to use ReservationCreator and OrderPurchaser, and to have
    form data submitted instead of `create` using a Reservation made by a
    quick_reservation method
    
    * Fixed specs
    
    Time being frozen caused an issue with reservaton validation, so made
    the quick reservation data start 1 minute in the future. This is not
    ideal.
    
    * Sets reservation start date/time data in #create action
    
    * Adds styling for new page
    
    * Styles index page
    
    * Adds spec for existing reservation outside of walkup intervals
    
    * Styles the case when there's a current, blocking reservation
    
    * Updates whitespace
    
    * Adds comments to controller actions
    
    * Fixes failing spec
    
    * Adds specs on intervals
    
    * Deletes `quick_reservation_reservations`
    
    * Fixes tabs
    
    * Adds translations
    
    * Adds feature flag
    
    * Removes debugging in spec
    
    * Adds missing paragraph copy
    
    * Update config/settings.yml
    
    Co-authored-by: Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
    
    * Update app/controllers/quick_reservations_controller.rb
    
    Co-authored-by: Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
    
    * Formats method
    
    * Removes feature flag in routes
    
    * Adds translations
    
    * Replaces index with show page
    
    * Adds error messages, updates footer, makes radio required
    
    * Adds redirect on error
    
    * Adds accessibility spec
    
    * Removes comment in spec
    
    * Replaces end link with FacilityReservationsHelper method
    
    * Clean up
    
    * Update app/controllers/quick_reservations_controller.rb
    
    Co-authored-by: Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
    
    * Addresses PR feedback
    
    * Replaces color value with variable
    
    * Replaces incorrect reference to @startable
    
    * Disables reservation start when reservation isn't startable
    
    ---------
    
    Co-authored-by: Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
    Co-authored-by: Gilad Shanan <giladshanan@gmail.com>

---
+++ app/controllers/quick_reservations_controller.rb
@@ -0,0 +1,138 @@
+# frozen_string_literal: true
+
+class QuickReservationsController < ApplicationController
+  layout "quick_reservation"
+
+  before_action :authenticate_user!
+  load_resource :facility, find_by: :url_name
+  load_resource :instrument, through: :facility, find_by: :url_name
+  before_action :get_startable_reservation, only: [:start, :new]
+  before_action :prepare_reservation_data, only: [:new, :create]
+
+  # GET /facilities/:facility_id/instruments/:instrument_id/quick_reservations/:id
+  def show
+    @reservation = current_user.reservations.find params[:id]
+    raise ActiveRecord::RecordNotFound unless @reservation
+    redirect_to new_facility_instrument_quick_reservation_path(@facility, @instrument) if @reservation.actual_end_at
+  end
+
+  # GET /facilities/:facility_id/instruments/:instrument_id/quick_reservations/new
+  def new
+    redirect_to facility_instrument_quick_reservation_path(@facility, @instrument, @reservation) if @reservation
+
+    interval = @instrument.quick_reservation_intervals.first
+    @walkup_available = @instrument.walkup_available?(interval:)
+  end
+
+  # POST /facilities/:facility_id/instruments/:instrument_id/quick_reservations
+  def create
+    # Create reservation data here to ensure a valid start time.
+    # If a user sits on the #new page for a little bit of time, the start time shown
+    # on that page will no longer be valid - for example,
+    # if that start time is in the past when the user submits the form.
+    params[:reservation][:reserve_start_date] = @reservation_data[:reserve_start_at].to_s
+    params[:reservation][:reserve_start_hour] = @reservation_data[:reserve_start_at].hour
+    params[:reservation][:reserve_start_min] = @reservation_data[:reserve_start_at].min
+    params[:reservation][:reserve_start_meridian] = @reservation_data[:reserve_start_at].strftime("%p")
+
+    build_order
+    creator = ReservationCreator.new(@order, @order_detail, params)
+
+    if creator.save(current_user)
+      @order.transaction do
+        order_purchaser.purchase!
+      end
+
+      if order_purchaser.success?
+        if creator.reservation.can_switch_instrument_on? && creator.reservation.start_reservation!
+          flash[:notice] = "Reservation started"
+        else
+          # failed to start
+          flash[:error] = creator.reservation.errors.full_messages.join("<br>").html_safe
+        end
+      else
+        # failed to purchase order
+        flash[:error] = order_purchaser.errors.join("<br>").html_safe
+      end
+    else
+      # failed to save reservation
+      flash[:error] = creator.error
+      render :new
+    end
+
+    redirect_to facility_instrument_quick_reservation_path(
+      @facility,
+      @instrument,
+      creator.reservation
+    )
+  end
+
+  # POST /facilities/:facility_id/instruments/:instrument_id/quick_reservations/start
+  def start
+    if @reservation&.startable_now? && @reservation&.move_to_earliest && @reservation&.start_reservation!
+      flash[:notice] = "Reservation started"
+      redirect_to facility_instrument_quick_reservation_path(@facility, @instrument, @reservation)
+    else
+      # failed to start
+      flash[:error] = @startable.errors.full_messages.join("<br>").html_safe
+
+      redirect_to facility_instrument_quick_reservation_path(
+        @facility,
+        @instrument,
+        @startable.reservation
+      )
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.permit(:reservation, :order_account)
+  end
+
+  def prepare_reservation_data
+    @possible_reservation_data = @instrument.quick_reservation_data
+
+    # if no reservations are available right now, find reservations at the next
+    # available time
+    if @possible_reservation_data.empty?
+      duration = @instrument.quick_reservation_intervals.first.minutes
+      next_available_reservation = @instrument.next_available_reservation(duration:)
+      after = next_available_reservation.reserve_start_at
+      @possible_reservation_data = @instrument.quick_reservation_data(after:)
+    end
+
+    @reservation_data = @possible_reservation_data.first
+  end
+
+  def get_startable_reservation
+    reservations = current_user.reservations.where(product_id: @instrument.id)
+    startable = reservations.find(&:startable_now?)
+    ongoing = reservations.ongoing.first
+
+    @reservation = ongoing || startable
+  end
+
+  def build_order
+    @order = Order.new(
+      user: current_user,
+      facility: current_facility,
+      created_by: current_user.id,
+    )
+    @order_detail = @order.order_details.build(
+      product: @instrument,
+      quantity: 1,
+      created_by: current_user.id,
+    )
+  end
+
+  def order_purchaser
+    @order_purchaser ||= OrderPurchaser.new(
+      acting_as: false,
+      order: @order,
+      order_in_past: false,
+      params:,
+      user: current_user,
+    )
+  end
+end
