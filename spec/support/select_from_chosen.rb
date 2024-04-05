a1d8cb2ba1ddbabcec613be16c27ec1255430dd1
Gilad Shanan <giladshanan@gmail.com>
2024-04-04 23:10:41 -0500

    Revert helper method changes

--- spec/support/select_from_chosen.rb
+++ spec/support/select_from_chosen.rb
@@ -5,11 +5,8 @@ module SelectFromChosen
   # Allow running system specs with JS enabled and still access 'chosen' select inputs
   def select_from_chosen(item_text, options)
     field = find_field(options[:from], visible: false)
-    option_value = page.evaluate_script("$(\"##{field[:id]} option:contains('#{item_text}')\").val()")
-    page.execute_script("value = ['#{option_value}']\; if ($('##{field[:id]}').val()) {$.merge(value, $('##{field[:id]}').val())}")
-    option_value = page.evaluate_script("value")
-    page.execute_script("$('##{field[:id]}').val(#{option_value})")
-    page.execute_script("$('##{field[:id]}').trigger('chosen:updated')")
+    find("##{field[:id]}_chosen").click
+    find("##{field[:id]}_chosen ul.chosen-results li", text: item_text).click
   end
 
 end
