a074e74cf75d95941d06631b1ee1bbfd0641f93d
Gilad Shanan <giladshanan@gmail.com>
2024-04-05 10:21:02 -0500

    Revert "DEBUG: only run failing spec"
    This reverts commit e8b3cc8879ce515b5f7037522a73c8f9b789f4c3.

--- spec/system/admin/adding_to_an_order_spec.rb
+++ spec/system/admin/adding_to_an_order_spec.rb
@@ -216,7 +216,6 @@ RSpec.describe "Adding to an existing order" do
 
         visit facility_order_path(facility, order)
         wait_for_ajax
-        puts page.html
         select_from_chosen facility2.name, from: "add_to_order_form[facility_id]"
         select_from_chosen instrument.name, from: "add_to_order_form[product_id]"
         fill_in "add_to_order_form[quantity]", with: "1"
