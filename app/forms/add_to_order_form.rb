65b617be3240945099a4a7ba24d07f31197c7426
Leticia Errandonea <leticia@wyeworks.com>
2024-04-17 18:24:45 -0300

    Fix bug and add spe

--- app/forms/add_to_order_form.rb
+++ app/forms/add_to_order_form.rb
@@ -31,6 +31,7 @@ class AddToOrderForm
 
     if @original_order.facility.id == @facility_id
       add_to_order!
+      return true
     end
 
     return true unless SettingsHelper.feature_on?(:cross_core_projects)
