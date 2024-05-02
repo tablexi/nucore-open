5381f69555be6a0647b0d6cfd5d546a9e56cd20b
Leticia Errandonea <leticia@wyeworks.com>
2024-05-02 12:29:03 -0300

    Add comment explaining why scroll_to may be used

--- spec/support/select_from_chosen.rb
+++ spec/support/select_from_chosen.rb
@@ -3,6 +3,7 @@
 module SelectFromChosen
 
   # Allow running system specs with JS enabled and still access 'chosen' select inputs
+  # Add options[:scroll_to] to solve error `element click intercepted: Element is not clickable at point (,). Other element would receive the click`
   def select_from_chosen(item_text, options)
     page.scroll_to(options[:scroll_to]) if options[:scroll_to]
 
