14fe8f220db19233d97c87dad4f5036330eba35d
Leticia Errandonea <leticia@wyeworks.com>
2024-05-17 12:07:01 -0300

    Update options and query

--- app/services/transaction_search/cross_core_searcher.rb
+++ app/services/transaction_search/cross_core_searcher.rb
@@ -4,7 +4,7 @@ module TransactionSearch
 
   class CrossCoreSearcher < BaseSearcher
     def options
-      ["both", "yes", "no"]
+      ["all", "cross_core"]
     end
 
     def search(params)
@@ -12,18 +12,15 @@ module TransactionSearch
 
       params = params.first
 
-      # Based on params, filter by cross_core_project_id present in order_detail.order
-      if params == "no"
-        order_details.joins(:order).where(orders: { cross_core_project_id: nil })
-      elsif params == "yes"
-        order_details.joins(:order).where(orders: { original_cross_core_order: false }).where.not(orders: { cross_core_project_id: nil })
+      if params == "cross_core"
+        order_details.joins(:order).where.not(orders: { cross_core_project_id: nil })
       else
         order_details
       end
     end
 
     def label_method
-      :capitalize
+      :humanize
     end
 
     def label
