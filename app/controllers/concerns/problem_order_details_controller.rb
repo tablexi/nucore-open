585880640a205320c6fd9becc8d83f76f3c888c5
Leticia Errandonea <leticia@wyeworks.com>
2024-05-17 12:23:58 -0300

    Add filter to Problem Orders tab

--- app/controllers/concerns/problem_order_details_controller.rb
+++ app/controllers/concerns/problem_order_details_controller.rb
@@ -15,10 +15,18 @@ module ProblemOrderDetailsController
     order_details = problem_order_details.joins(:order)
 
     @search_form = TransactionSearch::SearchForm.new(params[:search], defaults: { date_range_field: "ordered_at", allowed_date_fields: ["ordered_at"] })
-    @search = TransactionSearch::Searcher.new(TransactionSearch::ProductSearcher,
-                                              TransactionSearch::AccountSearcher,
-                                              TransactionSearch::OrderedForSearcher,
-                                              TransactionSearch::DateRangeSearcher).search(order_details, @search_form)
+    searchers = [
+      TransactionSearch::ProductSearcher,
+      TransactionSearch::AccountSearcher,
+      TransactionSearch::OrderedForSearcher,
+      TransactionSearch::DateRangeSearcher,
+    ]
+
+    if SettingsHelper.feature_on?(:cross_core_order_view)
+      searchers << TransactionSearch::CrossCoreSearcher
+    end
+
+    @search = TransactionSearch::Searcher.new(*searchers).search(order_details, @search_form)
     @order_details = @search.order_details.preload(:order_status, :assigned_user)
 
     respond_to do |format|
