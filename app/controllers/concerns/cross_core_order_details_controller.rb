121a5fff6351d7de3ec4a6e35d5842a0106f9533
Leticia Errandonea <leticia@wyeworks.com>
2024-05-22 17:40:58 -0300

    WIP

--- app/controllers/concerns/cross_core_order_details_controller.rb
+++ app/controllers/concerns/cross_core_order_details_controller.rb
@@ -16,7 +16,7 @@ module CrossCoreOrderDetailsController
     ]
 
     @search = TransactionSearch::Searcher.new(*searchers).search(order_details, @search_form)
-    @order_details = @search.order_details.preload(:order_status, :assigned_user)
+    @order_details = @search.order_details.includes(:order_status).reorder(sort_clause)
 
     respond_to do |format|
       format.html { @order_details = @order_details.paginate(page: params[:page]) }
@@ -28,6 +28,17 @@ module CrossCoreOrderDetailsController
   def cross_core_order_details
     project_ids = current_facility.order_details.joins(:order).pluck(:cross_core_project_id).compact.uniq
 
-    OrderDetail.joins(:order).where(orders: { cross_core_project_id: project_ids })
+    OrderDetail.joins(:order).joins(order: :facility).where(orders: { cross_core_project_id: project_ids }).where.not(facility_id: current_facility.id)
   end
+
+  # This method is overriding the one in NewInprocessController, which causes problems, as the hash is not the same for both controllers
+  def sort_lookup_hash
+    {
+      "facility" => "facilities.name",
+      "order_number" => ["order_details.order_id", "order_details.id"],
+      "ordered_at" => "order_details.ordered_at",
+      "status" => "order_statuses.name",
+    }
+  end
+
 end
