f22a7c607076d58c391894d4a206475e6f0f0395
Gilad Shanan <giladshanan@gmail.com>
2024-06-10 10:27:43 -0500

    Make scope more readable

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -129,7 +129,10 @@ class OrderDetail < ApplicationRecord
   scope :batch_updatable, -> { where(dispute_at: nil, state: %w(new inprocess)) }
   scope :new_or_inprocess, -> { purchased.where(state: %w(new inprocess)) }
   scope :non_canceled, -> { where.not(state: "canceled") }
-  scope :cross_core, -> { joins(order: :cross_core_project).where.not(orders: { cross_core_project_id: nil }) }
+  scope :cross_core, lambda {
+    joins(order: :cross_core_project)
+    .where.not(orders: { cross_core_project_id: nil })
+  }
 
   def self.for_facility(facility)
     for_facility_id(facility.id)
