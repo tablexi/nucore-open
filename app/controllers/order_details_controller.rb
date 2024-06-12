32a5d534873e35a7650bc60a85c32cb574f53ca1
Leticia Errandonea <leticia@wyeworks.com>
2024-06-11 23:45:52 -0300

    Move redirect path logic to its own method

--- app/controllers/order_details_controller.rb
+++ app/controllers/order_details_controller.rb
@@ -46,14 +46,7 @@ class OrderDetailsController < ApplicationController
         end
       end
 
-      redirect_to_order_id = params[:redirect_to_order_id]
-
-      if redirect_to_order_id.present?
-        order = Order.find(redirect_to_order_id)
-        redirect_to facility_order_path(order.facility, order)
-      else
-        redirect_to(reservations_path)
-      end
+      redirect_to redirect_to_path
     else
       raise ActiveRecord::RecordNotFound
     end
@@ -126,6 +119,17 @@ class OrderDetailsController < ApplicationController
 
   def order_detail_params
     params.require(:order_detail).permit(:account_id)
+  end
+
+  def redirect_to_path
+    redirect_to_order_id = params[:redirect_to_order_id]
+
+    if redirect_to_order_id.present?
+      order = Order.find(redirect_to_order_id)
+      facility_order_path(order.facility, order)
+    else
+      reservations_path
+    end
   end
 
 end
