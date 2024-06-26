3f4f70b51bd3f438a89c948ad368c54add865859
Leticia Errandonea <leticia@wyeworks.com>
2024-06-26 18:33:35 -0300

    Use instance var

--- app/controllers/facility_orders_controller.rb
+++ app/controllers/facility_orders_controller.rb
@@ -132,13 +132,13 @@ class FacilityOrdersController < ApplicationController
   def load_cross_core_order_details
     return unless SettingsHelper.feature_on?(:cross_core_projects)
 
-    project = @order.cross_core_project
+    @cross_core_project = @order.cross_core_project
 
     @cross_core_order_details_by_facility = {}
     @cross_core_orders_by_facility = {}
 
-    if project.present?
-      project_orders = project.orders.where.not(facility_id: @order.facility_id)
+    if @cross_core_project.present?
+      project_orders = @cross_core_project.orders.where.not(facility_id: @order.facility_id)
 
       project_orders.each do |order|
         order_facility = order.facility.to_s
@@ -146,8 +146,6 @@ class FacilityOrdersController < ApplicationController
         @cross_core_order_details_by_facility[order_facility] = order.order_details.ordered_by_parents
         @cross_core_orders_by_facility[order_facility] = order
       end
-
-      @cross_core_project = project
     end
   end
 
