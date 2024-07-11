3b633e3ddd8363eef580face1ceba211f7cefa96
Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
2024-07-11 13:09:44 -0500

    [#158615] Add grand total and subtotal to All Transactions page (#4410)
    # Release Notes
    
    Adds subtotal to the paginated "All Transactions"  page.
    For filtered result sets with less than 1,000 transactions, also shows the Grand Total.
    
    Otherwise shows an informational tooltip.
    <img width="620" alt="Screenshot 2024-07-09 at 1 56 27 PM" src="https://github.com/tablexi/nucore-open/assets/30355046/f1ba8cfd-ec92-47f2-b6ee-a1842727a50d">
    
    <img width="678" alt="Screenshot 2024-07-09 at 1 56 24 PM" src="https://github.com/tablexi/nucore-open/assets/30355046/418b20ce-951c-42cf-b691-d803dbb39bd9">
    
    # Additional Context
    
    # Accessibility
    - [x] Did you scan for accessibility issues?
    - [x] Did you check our accessibility goal checklist?
    - [x] Did you add accessibility [specs](https://github.com/dequelabs/axe-core-gems/blob/develop/packages/axe-core-rspec/README.md)?

--- spec/system/admin/all_transactions_search_spec.rb
+++ spec/system/admin/all_transactions_search_spec.rb
@@ -27,7 +27,8 @@ RSpec.describe "All Transactions Search", :js do
 
     select_from_chosen accounts.second.account_list_item, from: "Payment Sources"
     click_button "Filter"
-
+    
+    expect(page).to have_content("Total")
     expect(page).not_to have_link(order_detail.id.to_s)
     expect(page).to have_link(orders.second.order_details.first.id.to_s)
 
@@ -35,5 +36,13 @@ RSpec.describe "All Transactions Search", :js do
     expect(page).not_to have_link(originating_order_facility1.id)
     expect(page).to have_link(cross_core_orders[2].id)
     expect(page).to have_css(".fa-users", count: 1) # cross_core_orders[2] is a cross-core order that didn't originate in the current facility
+  end
+
+  it "is accessible", :js do
+    login_as director
+    visit facility_transactions_path(facility)
+    
+    # Skip these two violations because the chosen JS library is hard to make accessible
+    expect(page).to be_axe_clean.skipping("label", "select-name")
   end
 end
