3c0894d804c906d163f1d5e2a3c28a3e28c7bfa6
Leticia Errandonea <leticia@wyeworks.com>
2024-07-11 21:45:27 -0300

    Remove projects ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -55,6 +55,10 @@ module ApplicationHelper
     end
   end
 
+  def project_id_assignment_options(projects)
+    blank_project_options + options_from_collection_for_select(projects, :id, :name)
+  end
+
   private
 
   def show_currency(order_detail, method)
@@ -76,6 +80,15 @@ module ApplicationHelper
 
   def responsive?
     !admin_tab?
+  end
+
+  def blank_project_options
+    options_for_select(
+      [
+        [I18n.t("projects.shared.select_project.placeholder"), nil],
+        [I18n.t("projects.shared.select_project.unassign"), "unassign"],
+      ],
+    )
   end
 
 end
