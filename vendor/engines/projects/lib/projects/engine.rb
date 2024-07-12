813e615fe90d46439d66e9aa029dd240dc80ac71
Leticia Errandonea <leticia@wyeworks.com>
2024-07-11 22:28:14 -0300

    Remove code related to moved/deleted files

--- vendor/engines/projects/lib/projects/engine.rb
+++ vendor/engines/projects/lib/projects/engine.rb
@@ -6,15 +6,7 @@ module Projects
 
     config.to_prepare do
       ::AbilityExtensionManager.extensions << "Projects::AbilityExtension"
-      Facility.send :include, Projects::FacilityExtension
-      GlobalSearchController.send :include, Projects::GlobalSearchControllerExtension
       NavTab::LinkCollection.send :include, Projects::LinkCollectionExtension
-      ::OrderDetails::ParamUpdater.send :include, Projects::OrderDetails::ParamUpdaterExtension
-      Order.send :include, Projects::OrderExtension
-      OrderDetail.send :include, Projects::OrderDetailExtension
-      OrderDetailBatchUpdater.send :include, Projects::OrderDetailBatchUpdaterExtension
-      OrdersController.send :include, Projects::OrdersControllerExtension
-      Reservation.send :include, Projects::ReservationExtension
 
       ViewHook.add_hook "reservations.account_field",
                         "after_account",
@@ -44,26 +36,6 @@ module Projects
       ViewHook.add_hook "shared.order_detail_action_form",
                         "batch_update_above_product_column",
                         "projects/shared/select_facility_project"
-
-      ::Reports::GeneralReportsController.reports[:project] = Projects::ReportsExtension.general_report
-      ::Reports::InstrumentReportsController.reports[:project] = Projects::ReportsExtension.instrument_report
-    end
-
-    config.after_initialize do
-      OrderRowImporter.send :prepend, Projects::OrderRowImporterExtension
-      OrderRowImporter.order_import_headers.insert(-2, :project_name)
-    end
-
-    initializer :append_migrations do |app|
-      config.paths["db/migrate"].expanded.each do |expanded_path|
-        app.config.paths["db/migrate"] << expanded_path
-      end
-    end
-
-    initializer "model_core.factories", after: "factory_bot.set_factory_paths" do
-      if defined?(FactoryBot)
-        FactoryBot.definition_file_paths << File.expand_path("../../../spec/factories", __FILE__)
-      end
     end
 
   end
