9b6df1b145303656c3e52f3b080896675cf686af
Gilad Shanan <giladshanan@gmail.com>
2024-07-16 15:52:32 -0500

    Move view templates into the app

--- vendor/engines/projects/lib/projects/engine.rb
+++ vendor/engines/projects/lib/projects/engine.rb
@@ -7,34 +7,10 @@ module Projects
     config.to_prepare do
       ::AbilityExtensionManager.extensions << "Projects::AbilityExtension"
 
-      ViewHook.add_hook "reservations.account_field",
-                        "after_account",
-                        "projects/shared/select_project"
-
-      ViewHook.add_hook "order_management.order_details.form",
-                        "after_order_status",
-                        "projects/shared/select_project"
-
-      ViewHook.add_hook "order_management.order_details.print_details",
-                        "after_account_print",
-                        "projects/shared/project"
-
-      ViewHook.add_hook "orders.form",
-                        "acting_as",
-                        "projects/shared/select_project"
-
       TransactionSearch.register(Projects::ProjectSearcher)
       TransactionSearch.register(Projects::CrossCoreFacilitySearcher, default: false)
 
-      ViewHook.add_hook "shared.transactions.search",
-                        "end_of_first_column",
-                        "projects/shared/transactions/search"
-
       ::Reports::ExportRaw.transformers << "Projects::ExportRawTransformer"
-
-      ViewHook.add_hook "shared.order_detail_action_form",
-                        "batch_update_above_product_column",
-                        "projects/shared/select_facility_project"
     end
 
   end
