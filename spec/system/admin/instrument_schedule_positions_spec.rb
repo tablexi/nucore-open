725481cfbdc8da74a23cb05cdd70dc913b49dc1c
Leticia Errandonea <leticia@wyeworks.com>
2024-10-01 12:09:36 -0300

    Try to fix the Instrument schedule positions spec

--- spec/system/admin/instrument_schedule_positions_spec.rb
+++ spec/system/admin/instrument_schedule_positions_spec.rb
@@ -47,8 +47,12 @@ RSpec.describe "Instrument Schedule Display Order" do
       find("[title='Move Up']").click
       click_button "Update Ordering"
 
-      wait_for_ajax
+      # Sometimes the first click doesn't work, so try again
+      expected_flash_message = "The Instruments have been reordered"
+      click_button "Move" unless page.has_content?(expected_flash_message)
 
+      # This expectation has been failing from time to time even though the arrays look the same in the error message.
+      # If this doesn't work, we should take a look at the matcher.
       expect(["Second", "Third", "First", "AAA New", "CCC New", "ZZZ New"]).to appear_in_order
 
       # check the new display order
