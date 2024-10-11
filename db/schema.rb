35bc5f41c16ec642d724ee11acf0880124b94aa6
Leticia Errandonea <leticia@wyeworks.com>
2024-10-11 18:09:37 -0300

    Add new daily_booking field and feature flag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_16_172924) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_08_223047) do
   create_table "account_facility_joins", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.integer "facility_id", null: false
     t.integer "account_id", null: false
@@ -658,6 +658,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_16_172924) do
     t.string "billing_mode", default: "Default", null: false
     t.string "pricing_mode", default: "Schedule Rule", null: false
     t.boolean "cross_core_ordering_available", default: false, null: false
+    t.boolean "daily_booking", default: false, null: false
     t.index ["dashboard_token"], name: "index_products_on_dashboard_token"
     t.index ["facility_account_id"], name: "fk_facility_accounts"
     t.index ["facility_id"], name: "fk_rails_0c9fa1afbe"
