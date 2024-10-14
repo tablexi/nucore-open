d2b13cfc74d60646862bb6c18c963ee7c4e81a33
Joaquin <joaquin.correa@wyeworks.com>
2024-10-14 09:49:01 -0300

    change compress request back to true

--- vendor/engines/saml_authentication/spec/controllers/saml_authentication/sessions_controller_spec.rb
+++ vendor/engines/saml_authentication/spec/controllers/saml_authentication/sessions_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe SamlAuthentication::SessionsController, type: :controller do
     end
 
     def self.encode(saml)
-      encoded = new.send(:encode_raw_saml, saml, OpenStruct.new(compress_request: false))
+      encoded = new.send(:encode_raw_saml, saml, OpenStruct.new(compress_request: true))
       # encode_raw_saml url-encodes the encoded saml so it's correctly sent over http.
       # Since this is a controller spec we don't have the automatic url-decoding step on params.
       CGI.unescape(encoded)
