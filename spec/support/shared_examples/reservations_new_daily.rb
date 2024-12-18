2a49027731c9c4f990d603cedf824c3adf025f14
Joaquin Correa <joaquin.correa@wyeworks.com>
2024-12-18 09:20:25 -0300

    Create reservation with fixed start time

--- spec/support/shared_examples/reservations_new_daily.rb
+++ spec/support/shared_examples/reservations_new_daily.rb
@@ -18,6 +18,8 @@
 #
 RSpec.shared_examples "new daily reservation" do |before_submit: nil, after_submit: nil|
   it "creates a daily reservation", :js do
+    expect(instrument.daily_booking?).to be true
+
     visit reservation_path
 
     # Form and open hour table to be axe clean
@@ -25,6 +27,8 @@ RSpec.shared_examples "new daily reservation" do |before_submit: nil, after_subm
     expect(page).to be_axe_clean.within(".open-hours")
 
     expect(page).to have_content("Create Reservation")
+    expect(page).to have_content("Reserve Start")
+    expect(page).to have_content("Reserve End")
     expect(page).to have_content("Duration Days")
 
     # Start time availability table is present
@@ -50,6 +54,45 @@ RSpec.shared_examples "new daily reservation" do |before_submit: nil, after_subm
     find_field("reservation[reserve_end_hour]", disabled: true).tap do |field|
       expect(field.value).to eq(start_hour)
     end
+
+    instance_eval(&before_submit) if before_submit.present?
+
+    click_button("Create")
+
+    expect(page).to have_content(
+      if defined?(success_message)
+        success_message
+      else
+        "Reservation created successfully"
+      end
+    )
+
+    instance_eval(&after_submit) if after_submit.present?
+  end
+end
+
+# Examples that create a reservation with a daily booking
+# instrument with fixed start time.
+#
+# See "new daily reservation" shared examples to
+# see requirements
+#
+RSpec.shared_examples "new daily reservation with fixed start time" do |before_submit: nil, after_submit: nil|
+  it "creates a reservation" do
+    expect(instrument.daily_booking?).to be true
+    expect(instrument.fixed_start_time?).to be true
+
+    visit reservation_path
+
+    expect(page).to have_content("Reserve Start")
+    expect(page).to have_content("Reserve End")
+    expect(page).to have_content("Duration Days")
+
+    expect(page).to_not have_field("reservation[reserve_start_hour]")
+    expect(page).to_not have_field("reservation[reserve_start_min]")
+    expect(page).to_not have_field("reservation[reserve_start_meridian]")
+
+    fill_in("Reserve Start", with: I18n.l(1.day.from_now.to_date, format: :usa))
 
     instance_eval(&before_submit) if before_submit.present?
 
