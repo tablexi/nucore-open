51fdb9e2fef58ad4c5648e6bc265ba951bc2be04
Joaquin Correa <joaquin.correa@wyeworks.com>
2024-12-20 11:20:36 -0300

    Render monthly events with offsets

--- app/assets/javascripts/app/calendar.js
+++ app/assets/javascripts/app/calendar.js
@@ -37,16 +37,21 @@ window.FullCalendarConfig = class FullCalendarConfig {
   }
 
   baseOptions() {
+    var self = this;
+
     return {
       editable: false,
       defaultView: "agendaWeek",
       allDaySlot: false,
+      nextDayThreshold: '00:00:00',
       events: events_path,
-      loading: (isLoading, view) => {
+      loading: (isLoading, _view) => {
         return this.toggleOverlay(isLoading);
       },
-
-      eventAfterRender: this.buildTooltip,
+      eventAfterRender: function(event, element, view) {
+        self.buildTooltip(event, element, view);
+        self.adjustEvent(event, element, view);
+      },
       eventAfterAllRender: view => {
         this.$element.trigger("calendar:rendered");
         return this.toggleNextPrev(view);
@@ -140,5 +145,56 @@ window.FullCalendarConfig = class FullCalendarConfig {
 
   linkToEditOrder(event) {
     if ((event.orderId != null) && (typeof orders_path_base !== 'undefined' && orders_path_base !== null)) { return `<a href='${orders_path_base}/${event.orderId}'>Edit</a>`; }
+  }
+
+  /*
+   * Render monthly view events with margins
+   * depending on the start and end offset
+   * from midnight.
+   */
+  adjustEvent(event, element, view) {
+    // Don't apply changes unless monthly view
+    if (view.name != 'month') { return; }
+    // exclude allDay and background events
+    if (event['rendering'] == 'background' || event['allDay']) { return; }
+
+    let seg = $(element).data('fc-seg');
+    // if there's no info about the drawn event segment
+    // there's nothing to do
+    if (!seg) { return; }
+
+    // Don't add margins if event last less than a day
+    if (event.end.diff(event.start, 'days', true) < 1.0) { return; }
+
+    let startOfDay = event.start.clone();
+    startOfDay.startOf('day');
+    let endOfDay = event.end.clone();
+    endOfDay.endOf('day');
+
+    let startOffsetDiff = event.start.diff(startOfDay, 'minutes');
+    let startOffset = startOffsetDiff;
+    if (!seg.isStart) {
+      // Event started some row above
+      startOffset = 0;
+    }
+    let endOffset = endOfDay.diff(event.end, 'minutes');
+    // If startOffsetDiff is zero then reservation starts
+    // at begging of thay and it does not have end offset
+    if (!seg.isEnd || startOffsetDiff === 0) {
+      // Event ends some row below
+      endOffset = 0;
+    }
+
+    let marginLeft = startOffset / 14.40; // 1440 minutes per day in percentage
+    let marginRight = endOffset / 14.40;
+
+    let eventSegmentSlots = seg.rightCol - seg.leftCol + 1;
+
+    // A maximum of 7 days (calendar slots) in each row
+    marginLeft /= eventSegmentSlots;
+    marginRight /= eventSegmentSlots;
+
+    $(element).css('margin-left', marginLeft + "%");
+    $(element).css('margin-right', marginRight + "%");
   }
 };
